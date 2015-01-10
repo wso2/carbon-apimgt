21f51b2c4a9be251f8cfaa57c287f6cf8de570a7
prabathabey <prabathabey@git.com>
2015-01-10 11:46:11 +0530

    Fixing build failures

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/throttling/ApplicationThrottleController.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/throttling/ApplicationThrottleController.java
@@ -3,17 +3,13 @@ package org.wso2.carbon.apimgt.gateway.handlers.throttling;
 import org.apache.axiom.om.*;
 import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 import org.apache.axiom.om.impl.llom.OMDocumentImpl;
-import org.apache.axis2.context.ConfigurationContext;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.neethi.PolicyEngine;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.SynapseException;
-import org.apache.synapse.config.Entry;
 import org.apache.synapse.util.SynapseBinaryDataSource;
 import org.wso2.carbon.base.MultitenantConstants;
-import org.wso2.carbon.context.CarbonContext;
-import org.wso2.carbon.mediation.registry.RegistryExtension;
 import org.wso2.carbon.mediation.registry.RegistryServiceHolder;
 import org.wso2.carbon.registry.core.Collection;
 import org.wso2.carbon.registry.core.Registry;
@@ -41,43 +37,46 @@ public class ApplicationThrottleController {
     private static final Log log = LogFactory.getLog(ApplicationThrottleController.class);
 
     private static final Object lock = new Object();
-    
-    public static ThrottleContext getApplicationThrottleContext(MessageContext synCtx, ThrottleDataHolder dataHolder,
-                                                                String applicationId){
-        synchronized (lock) {
-            Object throttleContext = dataHolder.getThrottleContext(applicationId);
-            if(throttleContext == null){
-                return createThrottleContext(synCtx, dataHolder, applicationId);
-            }
-            return (ThrottleContext)throttleContext;
-        }
-    }
 
-    private static ThrottleContext createThrottleContext(MessageContext synCtx, ThrottleDataHolder dataHolder, String applicationId){
+    //TODO: Fix me with the latest throttling changes
+//    public static ThrottleContext getApplicationThrottleContext(MessageContext synCtx, ThrottleDataHolder dataHolder,
+//                                                                String applicationId){
+//        synchronized (lock) {
+//            Object throttleContext = dataHolder.getThrottleContext(applicationId);
+//            if(throttleContext == null){
+//                return createThrottleContext(synCtx, dataHolder, applicationId);
+//            }
+//            return (ThrottleContext)throttleContext;
+//        }
+//    }
 
-        //Entry entry = synCtx.getConfiguration().getEntryDefinition(APPLICATION_THROTTLE_POLICY_KEY);
-        //if (entry == null) {
-        //    handleException("Cannot find throttling policy using key: " + APPLICATION_THROTTLE_POLICY_KEY);
-        //    return null;
-        //}
 
-        //Object entryValue = synCtx.getEntry(APPLICATION_THROTTLE_POLICY_KEY);
-        Object entryValue = lookup(APPLICATION_THROTTLE_POLICY_KEY);
-        if (entryValue == null || !(entryValue instanceof OMElement)) {
-            handleException("Unable to load throttling policy using key: " + APPLICATION_THROTTLE_POLICY_KEY);
-            return null;
-        }
-
-        try {
-            Throttle throttle = ThrottleFactory.createMediatorThrottle(PolicyEngine.getPolicy((OMElement) entryValue));
-            ThrottleContext context = throttle.getThrottleContext(ThrottleConstants.ROLE_BASED_THROTTLE_KEY);
-            dataHolder.addThrottleContext(applicationId, context);
-            return context;
-        } catch (ThrottleException e) {
-            handleException("Error processing the throttling policy", e);
-        }
-        return null;
-    }
+    //TODO: Fix me with the latest throttling changes
+//    private static ThrottleContext createThrottleContext(MessageContext synCtx, ThrottleDataHolder dataHolder, String applicationId){
+//
+//        //Entry entry = synCtx.getConfiguration().getEntryDefinition(APPLICATION_THROTTLE_POLICY_KEY);
+//        //if (entry == null) {
+//        //    handleException("Cannot find throttling policy using key: " + APPLICATION_THROTTLE_POLICY_KEY);
+//        //    return null;
+//        //}
+//
+//        //Object entryValue = synCtx.getEntry(APPLICATION_THROTTLE_POLICY_KEY);
+//        Object entryValue = lookup(APPLICATION_THROTTLE_POLICY_KEY);
+//        if (entryValue == null || !(entryValue instanceof OMElement)) {
+//            handleException("Unable to load throttling policy using key: " + APPLICATION_THROTTLE_POLICY_KEY);
+//            return null;
+//        }
+//
+//        try {
+//            Throttle throttle = ThrottleFactory.createMediatorThrottle(PolicyEngine.getPolicy((OMElement) entryValue));
+//            ThrottleContext context = throttle.getThrottleContext(ThrottleConstants.ROLE_BASED_THROTTLE_KEY);
+//            dataHolder.addThrottleContext(applicationId, context);
+//            return context;
+//        } catch (ThrottleException e) {
+//            handleException("Error processing the throttling policy", e);
+//        }
+//        return null;
+//    }
 
     private static void handleException(String msg) {
         log.error(msg);
