72129bd09a8cba36ccc07b4e4eac39f7209da92d
rswijesena <roshan86web@gmail.com>
2015-02-25 18:03:54 +0530

    validity period dynamically

--- components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/OpenKeyManagerGrantHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/OpenKeyManagerGrantHandler.java
@@ -1,37 +1,41 @@
-/*
-*  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
 
 package org.wso2.carbon.apimgt.keymgt.handlers;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
+import org.wso2.carbon.identity.oauth2.model.*;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
 import org.wso2.carbon.identity.oauth2.token.handlers.grant.ClientCredentialsGrantHandler;
 
 public class OpenKeyManagerGrantHandler extends ClientCredentialsGrantHandler {
 
     private static Log log = LogFactory.getLog(OpenKeyManagerGrantHandler.class);
+    private static final String OPENKM_GRANT_PARAM = "validity_period";
 
     @Override
     public boolean authorizeAccessDelegation(OAuthTokenReqMessageContext tokReqMsgCtx){
 
-        tokReqMsgCtx.setValidityPeriod(5000000);
+        RequestParameter[] parameters =  tokReqMsgCtx.getOauth2AccessTokenReqDTO().getRequestParameters();
+
+        Long validityPeriod = null;
+
+        // find out validity period
+        for(RequestParameter parameter : parameters){
+            if(OPENKM_GRANT_PARAM.equals(parameter.getKey())){
+                if(parameter.getValue() != null && parameter.getValue().length > 0){
+                    validityPeriod = Long.valueOf(parameter.getValue()[0]);
+                }
+            }
+        }
+
+        if(validityPeriod != null) {
+            //set validity time
+            tokReqMsgCtx.setValidityPeriod(validityPeriod);
+        }else{
+            //set default validity time
+            tokReqMsgCtx.setValidityPeriod(360000);
+        }
 
         return true;
     }
