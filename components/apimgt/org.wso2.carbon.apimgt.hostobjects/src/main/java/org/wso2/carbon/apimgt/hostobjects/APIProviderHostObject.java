c697ddc636a077a9af847274b7f50312b58bbdeb
binalip91 <binalip91@gmail.com>
2015-03-12 12:25:57 +0530

    edit APIProviderHostObject,remove unnecessary imports

--- components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/APIProviderHostObject.java
+++ components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/APIProviderHostObject.java
@@ -81,7 +81,6 @@ import javax.net.ssl.SSLSession;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 import java.net.*;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
