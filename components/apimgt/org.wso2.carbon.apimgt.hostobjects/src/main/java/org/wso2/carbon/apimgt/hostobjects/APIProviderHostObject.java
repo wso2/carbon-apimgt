5f41ef27a4763d7fd7af11ca0092de4518758c01
Nisala <nisalaniroshana@gmail.com>
2015-04-01 20:55:37 +0530

    GraphsAPIM

--- components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/APIProviderHostObject.java
+++ components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/APIProviderHostObject.java
@@ -3504,6 +3504,125 @@ public class APIProviderHostObject extends ScriptableObject {
         return myn;
     }
 
+    public static NativeArray jsFunction_getUserAgentSummaryForALLAPIs(Context cx,
+                                                                       Scriptable thisObj,
+                                                                       Object[] args,
+                                                                       Function funObj)
+            throws APIManagementException {
+        List<APIRequestsByUserAgentsDTO> list = null;
+        try {
+            APIUsageStatisticsClient client = new APIUsageStatisticsClient(((APIProviderHostObject) thisObj).getUsername());
+            list = client.getUserAgentSummaryForALLAPIs();
+        } catch (APIMgtUsageQueryServiceClientException e) {
+            log.error("Error while invoking APIUsageStatisticsClient for ProviderAPIVersionLastAccess", e);
+        }
+        NativeArray myn = new NativeArray(0);
+        Iterator it = null;
+        if (list != null) {
+            it = list.iterator();
+        }
+        int i = 0;
+        if (it != null) {
+            while (it.hasNext()) {
+                NativeObject row = new NativeObject();
+                Object usageObject = it.next();
+                APIRequestsByUserAgentsDTO usage = (APIRequestsByUserAgentsDTO) usageObject;
+                row.put("user_agent", row, userAgentParser(usage.getUserAgent()));
+                row.put("request_count", row, usage.getCount());
+                myn.put(i, myn, row);
+                i++;
+            }
+        }
+        return myn;
+    }
+
+    public static NativeArray jsFunction_getAPIRequestsPerHour(Context cx,
+                                                               Scriptable thisObj,
+                                                               Object[] args,
+                                                               Function funObj)
+            throws APIManagementException {
+
+        List<APIRequestsByHourDTO> list = null ;
+        if (args == null ||  args.length==0) {
+            handleException("Invalid number of parameters.");
+        }
+        NativeArray myn = new NativeArray(0);
+        if (!HostObjectUtils.isUsageDataSourceSpecified()) {
+            //return myn;
+        }
+        String fromDate = (String) args[0];
+        String toDate = (String) args[1];
+        String apiName = (String)args[2];
+        try {
+            APIUsageStatisticsClient client = new APIUsageStatisticsClient(((APIProviderHostObject) thisObj).getUsername());
+            list = client.getAPIRequestsByHour(fromDate, toDate,apiName);
+        } catch (APIMgtUsageQueryServiceClientException e) {
+            log.error("Error while invoking APIUsageStatisticsClient for ProviderAPIVersionLastAccess", e);
+        }
+        Iterator it = null;
+        if (list != null) {
+            it = list.iterator();
+        }
+        int i = 0;
+        if (it != null) {
+            while (it.hasNext()) {
+                NativeObject row = new NativeObject();
+                Object usageObject = it.next();
+                APIRequestsByHourDTO usage = (APIRequestsByHourDTO) usageObject;
+                row.put("apiName", row, usage.getApi());
+                row.put("DateTierCount", row, usage.getDate().concat("|").concat(usage.getTier()).concat("|").concat(usage.getRequestCount()));
+                row.put("Date", row, usage.getDate());
+                row.put("request_count", row, usage.getRequestCount());
+                row.put("tier", row, usage.getTier());
+                myn.put(i, myn, row);
+                i++;
+            }
+        }
+        return myn;
+
+    }
+
+    public static NativeArray jsFunction_getAPIsFromAPIRequestsPerHourTable(Context cx,
+                                                                            Scriptable thisObj,
+                                                                            Object[] args,
+                                                                            Function funObj)
+            throws APIManagementException {
+
+        List<String> list = null ;
+        if (args == null ||  args.length==0) {
+            handleException("Invalid number of parameters.");
+        }
+        NativeArray myn = new NativeArray(0);
+        if (!HostObjectUtils.isUsageDataSourceSpecified()) {
+            //return myn;
+        }
+        String fromDate = (String) args[0];
+        String toDate = (String) args[1];
+        try {
+            APIUsageStatisticsClient client = new APIUsageStatisticsClient(((APIProviderHostObject) thisObj).getUsername());
+            list = client.getAPIsFromAPIRequestsPerHourTable(fromDate, toDate);
+        } catch (APIMgtUsageQueryServiceClientException e) {
+            log.error("Error while invoking APIUsageStatisticsClient for ProviderAPIVersionLastAccess", e);
+        }
+        Iterator it = null;
+        if (list != null) {
+            it = list.iterator();
+        }
+        int i = 0;
+        if (it != null) {
+            while (it.hasNext()) {
+                NativeObject row = new NativeObject();
+                Object usageObject = it.next();
+                String usage = (String) usageObject;
+                row.put("apiName", row, usage);
+                myn.put(i, myn, row);
+                i++;
+            }
+        }
+        return myn;
+
+    }
+
     public static NativeArray jsFunction_getProviderAPIServiceTime(Context cx, Scriptable thisObj,
                                                                    Object[] args, Function funObj)
             throws APIManagementException {
