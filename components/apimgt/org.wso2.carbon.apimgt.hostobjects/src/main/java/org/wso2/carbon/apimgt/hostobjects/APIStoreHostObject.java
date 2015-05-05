9f2c38fc80000344974879256e04e963a7f27a21
kasun32 <kasun32@gmail.com>
2015-05-05 18:22:15 +0530

    APIMANAGER-3513 fixed

--- components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/APIStoreHostObject.java
+++ components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/APIStoreHostObject.java
@@ -2753,7 +2753,7 @@ public class APIStoreHostObject extends ScriptableObject {
 	                    appName.equals(application.getName())) {
 
                         //get Number of subscriptions for the given application by the subscriber.
-                        subscriptionCount = apiConsumer.getSubscriptionCount(subscriber,application.getName());
+                        subscriptionCount = apiConsumer.getSubscriptionCount(subscriber,application.getName(),groupingId);
 	                    //get subscribed APIs set as per the starting and ending indexes for application.
 	                	Set<SubscribedAPI> subscribedAPIs;
                         subscribedAPIs = apiConsumer.getPaginatedSubscribedAPIs(subscriber, application.getName(),startSubIndex,endSubIndex, groupingId);
@@ -3131,7 +3131,7 @@ public class APIStoreHostObject extends ScriptableObject {
             if (applications != null) {
                 int i = 0;
                 for (Application application : applications) {
-                    int subscriptionCount = apiConsumer.getSubscriptionCount(subscriber,application.getName());
+                    int subscriptionCount = apiConsumer.getSubscriptionCount(subscriber,application.getName(),groupId);
                     NativeObject row = new NativeObject();
                     row.put("name", row, application.getName());
                     row.put("tier", row, application.getTier());
