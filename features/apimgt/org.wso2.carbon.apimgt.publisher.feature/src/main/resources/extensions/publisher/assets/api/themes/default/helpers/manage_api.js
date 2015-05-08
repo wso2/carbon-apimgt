6e637ac8c560cd048bd3fb276b78cf33456b978e
thusithakalugamage <thusitha32@yahoo.com>
2015-05-08 12:09:22 +0530

    fixed UX defects and rework on mark up to APIMs add API wizard

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/extensions/publisher/assets/api/themes/default/helpers/manage_api.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/extensions/publisher/assets/api/themes/default/helpers/manage_api.js
@@ -23,6 +23,6 @@ var resources = function(page,meta){
             'bootstrap-multiselect.js','manage_api_design.js','jsonpath-0.8.0.js','handlebars.js', 'manage_api_inline.js', 'typeaheadjs.js', 'bootstrap3-typeahead.min.js'],
         code:['manage_api_meta.hbs'],
         css:['api_designer.css', 'localstyles.css', 'bootstrap/bootstrap-editable.css',
-             'bootstrap/bootstrap-tagsinput.css','api_designer.css','bootstrap/bootsrap.css', 'bootstrap-multiselect.css']
+             'bootstrap/bootstrap-tagsinput.css','api_designer.css', 'bootstrap-multiselect.css']
     }
 };
