dc18324d093d31ea07495b960a30cb31cc14a413
tharindua <ghtd.dharmarathna@gmail.com>
2015-05-20 11:02:41 +0530

    fixed https://wso2.org/jira/browse/APIMANAGER-3612

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -991,15 +991,7 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
         APITemplateBuilderImpl vtb = new APITemplateBuilderImpl(api);
 
         if(!api.getStatus().equals(APIStatus.PROTOTYPED)) {
-            Map<String, String> corsProperties = new HashMap<String, String>();
-            corsProperties.put("inline", api.getImplementation());
-            if (api.getAllowedHeaders() != null && api.getAllowedHeaders() != "") {
-                corsProperties.put("allowHeaders", api.getAllowedHeaders());
-            }
-            if (api.getAllowedOrigins() != null && api.getAllowedOrigins() != "") {
-                corsProperties.put("allowedOrigins", api.getAllowedOrigins());
-            }
-            vtb.addHandler("org.wso2.carbon.apimgt.gateway.handlers.security.CORSRequestHandler", corsProperties);
+
             vtb.addHandler("org.wso2.carbon.apimgt.gateway.handlers.security.APIAuthenticationHandler", Collections.EMPTY_MAP);
 
             Map<String, String> properties = new HashMap<String, String>();
@@ -1022,6 +1014,15 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
                 vtb.addHandler("org.wso2.carbon.apimgt.gateway.handlers.ext.APIManagerExtensionHandler", Collections.EMPTY_MAP);
             }
         }
+        Map<String, String> corsProperties = new HashMap<String, String>();
+        corsProperties.put("inline", api.getImplementation());
+        if (api.getAllowedHeaders() != null && api.getAllowedHeaders() != "") {
+            corsProperties.put("allowHeaders", api.getAllowedHeaders());
+        }
+        if (api.getAllowedOrigins() != null && api.getAllowedOrigins() != "") {
+            corsProperties.put("allowedOrigins", api.getAllowedOrigins());
+        }
+        vtb.addHandler("org.wso2.carbon.apimgt.gateway.handlers.security.CORSRequestHandler", corsProperties);
         return vtb;
     }
 
