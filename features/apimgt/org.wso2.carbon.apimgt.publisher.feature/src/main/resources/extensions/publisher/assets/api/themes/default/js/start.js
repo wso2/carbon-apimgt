96a6095684cf54e1cb2382895ce2e65fe336d7a9
Udara Rathnayake <udarakr@gmail.com>
2015-05-25 13:47:24 +0530

    updating start.hbs and adding helper

---
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/extensions/publisher/assets/api/themes/default/js/start.js
@@ -0,0 +1,89 @@
+$( document ).ready(function() {
+
+    $("#startFromExistingAPI").click(function(){
+        $('#startFromExistingAPI-form').ajaxSubmit({
+            success:function(responseText, statusText, xhr, $form){
+                if (!responseText.error) {
+                    window.location = jagg.site.context + "/design"
+                }else {
+                    if (responseText.message == "timeout") {
+                        if (ssoEnabled) {
+                             var currentLoc = window.location.pathname;
+                             if (currentLoc.indexOf(".jag") >= 0) {
+                                 location.href = "index.jag";
+                             } else {
+                                 location.href = 'site/pages/index.jag';
+                             }
+                        } else {
+                             jagg.showLogin();
+                        }
+                    } else {
+                        jagg.message({content:responseText.message,type:"error"});
+                    }
+                }                
+            }, dataType: 'json'
+        });
+    });
+
+    $("#startFromExistingSOAPEndpoint").click(function(){
+        $('#startFromExistingSOAPEndpoint-form').ajaxSubmit({
+            success:function(responseText, statusText, xhr, $form){
+                if (!responseText.error) {
+                    window.location = jagg.site.context + "/design"
+                }else {
+                    if (responseText.message == "timeout") {
+                        if (ssoEnabled) {
+                             var currentLoc = window.location.pathname;
+                             if (currentLoc.indexOf(".jag") >= 0) {
+                                 location.href = "index.jag";
+                             } else {
+                                 location.href = 'site/pages/index.jag';
+                             }
+                        } else {
+                             jagg.showLogin();
+                        }
+                    } else {
+                        jagg.message({content:responseText.message,type:"error"});
+                    }
+                }                
+            }, dataType: 'json'
+        });
+    });
+    
+
+    $('.create-api').click(function(){
+        $('.create-options').each(function(){
+            $(this).removeClass('selected');
+        });
+
+        $(this).closest('.create-options').addClass('selected');
+        $('#designNewAPI').hide();
+    });
+
+    $('#create-new-api').click(function(){
+        $('#designNewAPI').show();
+    });
+
+
+    $('.create-options input[type=radio]').click(function(){
+       $(this).prop('checked', true);
+
+       $('.create-options input[type=radio]').each(function(){
+           if(!$(this).is(':checked')){
+              $($(this).attr('data-target')).slideUp();
+           }
+           else {
+               $($(this).attr('data-target')).slideToggle();
+           }
+       });
+    });
+
+    $('.toggleRadios input[type=radio]').click(function(){
+        console.log("Toggle radio selected!");
+        $('.toggleContainers .controls').hide();
+        $('.toggleRadios input[type=radio]').prop('checked', false);
+        $('#' + $(this).val()).closest('div').fadeIn();
+        $(this).prop('checked', true);
+    });
+
+});
