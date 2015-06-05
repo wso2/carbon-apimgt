10a240d23a40cec7e2219df194aee68e08de37fc
Udara Rathnayake <udarakr@gmail.com>
2015-06-05 11:49:49 +0530

    adding implement page latest updates

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/extensions/publisher/assets/api/themes/default/helpers/implement_api.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/extensions/publisher/assets/api/themes/default/helpers/implement_api.js
@@ -23,8 +23,8 @@ var resources = function(page,meta){
 		'implement_api.js', 'underscore.js', 'jsonform.js', 'custom-validation.js',
 		'url-validation.js','bootstrap-multiselect.js', 'bootstrap-editable.js', 'jsonpath-0.8.0.js',
 		'endpoint-ui.js', 'handlebars.js', 'implement_api_design.js', 'implement_api_inline.js',
-		'jquery-ace.min.js', 'src-noconflict/ace.js', 'src-noconflict/mode-javascript.js', 'typeaheadjs.js'],
-		css:['bootstrap-multiselect.css','api_designer.css'],
+		'jquery-ace.min.js', 'src-noconflict/ace.js', 'src-noconflict/mode-javascript.js', 'typeaheadjs.js', 'bootstrap-dialog.min.js'],
+		css:['bootstrap-multiselect.css','api_designer.css', 'bootstrap/bootstrap-editable.css', 'bootstrap-dialog.min.css'],
         code:['manage_api_meta.hbs']
     };
 };
