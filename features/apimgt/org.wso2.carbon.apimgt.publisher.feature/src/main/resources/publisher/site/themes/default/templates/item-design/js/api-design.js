70e77c2fec0cb0f170a1ef6f1dd425289f740c32
Joseph Fonseka <hevayo@gmail.com>
2015-06-12 14:35:08 +0530

    validate api name after importing a swagger

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/default/templates/item-design/js/api-design.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/default/templates/item-design/js/api-design.js
@@ -476,6 +476,9 @@ APIDesigner.prototype.load_api_document = function(api_document){
     this.render_scopes();
     $("#version").val(api_document.info.version);
     $("#name").val(api_document.info.title);
+    if(api_document.info.title && api_document.info.title != ""){
+        $("#name").valid();
+    }    
     if(api_document.info.description){
     	$("#description").val(api_document.info.description);
     }
