a5046ba8aecd12a2178aa4fe546c34f226a694ea
tharindua <ghtd.dharmarathna@gmail.com>
2015-07-09 20:39:49 +0530

    reviewed changes given by nuwand

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/default/templates/item-implement/js/api-implementation.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/default/templates/item-implement/js/api-implementation.js
@@ -205,37 +205,37 @@ $('#go_to_manage').click(function(e){
 });
 
 function showGatewayFailure(message) {
-                                   if(message.split("||")[1]=="warning"){
-                                     var environmentsFailed=JSON.parse(message.split("||")[0]);
-                                     var failedToPublishEnvironments=environmentsFailed.PUBLISHED;
-                                     var failedToUnpublishedEnvironments=environmentsFailed.UNPUBLISHED;
-                                     var divPublish="",divUnpublished="";
-                                     for(i= 0; i< failedToPublishEnvironments.split(",").length;i++){
-                                        var splitPublished = (failedToPublishEnvironments.split(",")[i]).split(":");
-                                        divPublish+=splitPublished[0]+"<br>"+splitPublished[1]+"<br>";
-                                         }
-                                     for(i= 0; i< failedToUnpublishedEnvironments.split(",").length;i++){
-                                        var splitUnPublished = (failedToUnpublishedEnvironments.split(",")[i]).split(":");
+    if (message.split("||")[1] == "warning") {
+        var environmentsFailed = JSON.parse(message.split("||")[0]);
+        var failedToPublishEnvironments = environmentsFailed.PUBLISHED;
+        var failedToUnpublishedEnvironments = environmentsFailed.UNPUBLISHED;
+        var divPublish = "", divUnpublished = "";
+        for (i = 0; i < failedToPublishEnvironments.split(",").length; i++) {
+            var splitPublished = (failedToPublishEnvironments.split(",")[i]).split(":");
+            divPublish += splitPublished[0] + "<br>" + splitPublished[1] + "<br>";
+        }
+        for (i = 0; i < failedToUnpublishedEnvironments.split(",").length; i++) {
+            var splitUnPublished = (failedToUnpublishedEnvironments.split(",")[i]).split(":");
 
-                                        divUnpublished+=splitUnPublished[0]+"<br>"+splitUnPublished[1]+"<br>";
-                                         }
-                                       $( "#modal-published-content" ).empty();
-                                       $( "#modal-unpublished-content" ).empty();
-                                       $( "#modal-published-content" ).append(divPublish);
-                                       $( "#modal-unpublished-content" ).append(divUnpublished);
-                                       if(failedToPublishEnvironments==""){
-                                       $("#modal-published-header").hide();
-                                       $("#modal-published-content").hide();
-                                        }
-                                       if(failedToUnpublishedEnvironments==""){
-                                            $("#modal-unpublished-header").hide();
-                                            $("#modal-unpublished-content").hide();
-                                          }
-                                         $("#retryType").val("manage");
-                                         $("#environmentsRetry-modal").modal('show');
-                                   }
-                                   else{
+            divUnpublished += splitUnPublished[0] + "<br>" + splitUnPublished[1] + "<br>";
+        }
+        $("#modal-published-content").empty();
+        $("#modal-unpublished-content").empty();
+        $("#modal-published-content").append(divPublish);
+        $("#modal-unpublished-content").append(divUnpublished);
+        if (failedToPublishEnvironments == "") {
+            $("#modal-published-header").hide();
+            $("#modal-published-content").hide();
+        }
+        if (failedToUnpublishedEnvironments == "") {
+            $("#modal-unpublished-header").hide();
+            $("#modal-unpublished-content").hide();
+        }
+        $("#retryType").val("manage");
+        $("#environmentsRetry-modal").modal('show');
+    }
+    else {
 
-                                   jagg.message({content:responseText.message,type:"error"});
-                                       }    
+        jagg.message({content: responseText.message, type: "error"});
+    }
 }
