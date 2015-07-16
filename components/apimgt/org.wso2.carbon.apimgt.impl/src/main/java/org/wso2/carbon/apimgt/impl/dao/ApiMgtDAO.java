ed3a35fea6e943c68489ddffec68d8171d979c97
Lakshman <Lakshman@Lakshmans-MacBook-Pro.local>
2015-07-16 17:03:19 +0530

    db2 native support

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -2818,7 +2818,7 @@ public class ApiMgtDAO {
                         " IAT.TOKEN_STATE = 'REVOKED')" +
                         " ORDER BY IAT.TIME_CREATED DESC";
 
-        String sql = null, oracleSQL = null, mySQLSQL = null, msSQL = null,postgreSQL = null;
+        String sql = null, oracleSQL = null, mySQLSQL = null, msSQL = null,postgreSQL = null, db2SQL = null;
 
         //Construct database specific sql statements.
 //        oracleSQL = "SELECT ICA.CONSUMER_KEY AS CONSUMER_KEY," +
@@ -2878,6 +2878,8 @@ public class ApiMgtDAO {
 
         mySQLSQL = "SELECT" + statement + " LIMIT 1";
 
+        db2SQL = "SELECT" + statement + " FETCH FIRST 1 ROWS ONLY";
+
         msSQL = "SELECT TOP 1" + statement;
 
         postgreSQL = "SELECT * FROM (SELECT" + statement + ") AS TOKEN LIMIT 1";
@@ -2889,11 +2891,11 @@ public class ApiMgtDAO {
             connection = APIMgtDBUtil.getConnection();
 
             if (connection.getMetaData().getDriverName().contains("MySQL")
-                    || connection.getMetaData().getDriverName().contains("H2")
-                    || connection.getMetaData().getDatabaseProductName().contains("DB2")) {
+                    || connection.getMetaData().getDriverName().contains("H2")) {
                 sql = mySQLSQL;
-            }
-            else if(connection.getMetaData().getDriverName().contains("MS SQL")){
+            } else if(connection.getMetaData().getDatabaseProductName().contains("DB2")){
+                sql = db2SQL;
+            } else if(connection.getMetaData().getDriverName().contains("MS SQL")){
                 sql = msSQL;
             } else if (connection.getMetaData().getDriverName().contains("Microsoft")) {
                 sql = msSQL;
@@ -2961,7 +2963,7 @@ public class ApiMgtDAO {
                         " IAT.TOKEN_STATE = 'REVOKED')" +
                         " ORDER BY IAT.TIME_CREATED DESC";
 
-        String sql = null, oracleSQL = null, mySQLSQL = null, msSQL = null,postgreSQL = null;
+        String sql = null, oracleSQL = null, mySQLSQL = null, msSQL = null,postgreSQL = null, db2SQL = null;
 
         //Construct database specific sql statements.
         oracleSQL =  "SELECT ICA.CONSUMER_KEY AS CONSUMER_KEY," +
@@ -2988,6 +2990,8 @@ public class ApiMgtDAO {
 
         mySQLSQL = "SELECT" + statement + " LIMIT 1";
 
+        db2SQL = "SELECT" + statement + " FETCH FIRST 1 ROWS ONLY";
+
         msSQL = "SELECT TOP 1" + statement;
 
         postgreSQL = "SELECT * FROM (SELECT" + statement + ") AS TOKEN LIMIT 1";
@@ -2999,11 +3003,11 @@ public class ApiMgtDAO {
             connection = APIMgtDBUtil.getConnection();
 
             if (connection.getMetaData().getDriverName().contains("MySQL")
-                    || connection.getMetaData().getDriverName().contains("H2")
-                    || connection.getMetaData().getDatabaseProductName().contains("DB2")) {
+                    || connection.getMetaData().getDriverName().contains("H2")) {
                 sql = mySQLSQL;
-            }
-            else if(connection.getMetaData().getDriverName().contains("MS SQL")){
+            } else if(connection.getMetaData().getDatabaseProductName().contains("DB2")){
+                sql = db2SQL;
+            } else if(connection.getMetaData().getDriverName().contains("MS SQL")){
                 sql = msSQL;
             } else if (connection.getMetaData().getDriverName().contains("Microsoft")) {
                 sql = msSQL;
