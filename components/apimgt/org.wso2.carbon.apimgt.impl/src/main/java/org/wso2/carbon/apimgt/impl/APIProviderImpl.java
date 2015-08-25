e2aa9eb8ce15af4b31d3ae02f94c3b73821dd557
lalaji <lalaji@wso2.com>
2015-08-25 13:51:20 +0530

    Fixed REGISTRY-2695

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -1272,12 +1272,14 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
         }
         APIUtil.setResourcePermissions(apiId.getProviderName(),
                                        artifact.getAttribute(APIConstants.API_OVERVIEW_VISIBILITY), rolesSet, artifactPath);
-        //Here we have to set permission specifically to image icon we added
-        String iconPath = artifact.getAttribute(APIConstants.API_OVERVIEW_THUMBNAIL_URL);
-        if (iconPath != null) {
-            iconPath=iconPath.substring(iconPath.lastIndexOf("/apimgt"));
-            APIUtil.copyResourcePermissions(apiId.getProviderName(),thumbUrl,iconPath);
-        }
+
+      /* Commented below image permission setting code block as we are now using ES specific asset image saving*/
+      //Here we have to set permission specifically to image icon we added
+      //  String iconPath = artifact.getAttribute(APIConstants.API_OVERVIEW_THUMBNAIL_URL);
+      //  if (iconPath != null) {
+      //      iconPath=iconPath.substring(iconPath.lastIndexOf("/apimgt"));
+      //      APIUtil.copyResourcePermissions(apiId.getProviderName(),thumbUrl,iconPath);
+      //  }
         // Retain the tags
         org.wso2.carbon.registry.core.Tag[] tags = registry.getTags(apiSourcePath);
         if (tags != null) {
