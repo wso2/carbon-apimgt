2ba4f1c2a14545a0ede35961b014ad83a5b2384a
thilinicooray <thilinicooray.ucsc@gmail.com>
2015-09-02 09:29:45 +0530

    component/sso-hostobject- merged 1.9.1 with 1.10.x

--- components/sso-hostobject/org.wso2.carbon.hostobjects.sso/src/main/java/org/wso2/carbon/hostobjects/sso/internal/SSOConstants.java
+++ components/sso-hostobject/org.wso2.carbon.hostobjects.sso/src/main/java/org/wso2/carbon/hostobjects/sso/internal/SSOConstants.java
@@ -37,7 +37,8 @@ public class SSOConstants {
     public static final String IS_AUTHENTICATED = "authenticated";
     public static final String USERNAME = "username";
 
-    public static final String SAML2_NAME_ID_POLICY = "urn:oasis:names:tc:SAML:2.0:nameid-format:entity";
+    public static final String NAME_ID_POLICY = "nameIdPolicy";
+    public static final String NAME_ID_POLICY_DEFAULT = "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified";
     public static final String LOGOUT_USER = "urn:oasis:names:tc:SAML:2.0:logout:user";
 
     public SSOConstants() {
