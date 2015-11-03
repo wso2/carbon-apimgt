1469e61999a7b60e43803698fa777be26569b4f8
sanjeewa malalgoda <sanjeewa@wso2.com>
2015-11-03 19:58:13 +0530

    adding new auth handler changes

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/utils/RestApiUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/utils/RestApiUtil.java
@@ -21,29 +21,31 @@ package org.wso2.carbon.apimgt.rest.api.util.utils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.apimgt.api.APIConsumer;
+import org.wso2.carbon.apimgt.api.APIDefinition;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.APIProvider;
+import org.wso2.carbon.apimgt.api.model.*;
+import org.wso2.carbon.apimgt.impl.AMDefaultKeyManagerImpl;
 import org.wso2.carbon.apimgt.impl.APIManagerFactory;
+import org.wso2.carbon.apimgt.impl.definitions.APIDefinitionFromSwagger20;
+import org.wso2.carbon.apimgt.impl.factory.KeyManagerHolder;
 import org.wso2.carbon.apimgt.rest.api.util.dto.ErrorDTO;
 import org.wso2.carbon.apimgt.rest.api.util.dto.ErrorListItemDTO;
 import org.wso2.carbon.context.CarbonContext;
 
 import javax.validation.ConstraintViolation;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class RestApiUtil {
 
     private static final Log log = LogFactory.getLog(RestApiUtil.class);
 
-    public static APIProvider getProvider () throws APIManagementException {
+    public static APIProvider getProvider() throws APIManagementException {
         String loggedInUser = CarbonContext.getThreadLocalCarbonContext().getUsername();
         return APIManagerFactory.getInstance().getAPIProvider(loggedInUser);
     }
-    
-    public static <T> ErrorDTO getConstraintViolationErrorDTO(Set<ConstraintViolation<T>> violations){
+
+    public static <T> ErrorDTO getConstraintViolationErrorDTO(Set<ConstraintViolation<T>> violations) {
         ErrorDTO errorDTO = new ErrorDTO();
         errorDTO.setMessage("Constraint Violation");
         List<ErrorListItemDTO> errorListItemDTOs = new ArrayList<>();
@@ -56,7 +58,7 @@ public class RestApiUtil {
         return errorDTO;
     }
 
-    public static boolean isUUID (String apiId) {
+    public static boolean isUUID(String apiId) {
         try {
             UUID.fromString(apiId);
             return true;
@@ -85,6 +87,80 @@ public class RestApiUtil {
 
     public static String getLoggedInUserTenantDomain() {
         return CarbonContext.getThreadLocalCarbonContext().getTenantDomain();
+    }
+
+    /**
+     * Following 3 methods are temporary added to rest API Util
+     * Ideally they should move to DCR, RR and Introspection API implementation
+     *
+     * @param api
+     * @param swagger
+     * @return
+     */
+    public static boolean registerResource(API api, String swagger) {
+
+        APIDefinition definitionFromSwagger20 = new APIDefinitionFromSwagger20();
+        Set<URITemplate> uriTemplates = null;
+        try {
+            uriTemplates = definitionFromSwagger20.getURITemplates(api, swagger);
+        } catch (APIManagementException e) {
+            log.error("Error while parsing swagger content to get URI Templates" + e.getMessage());
+        }
+        api.setUriTemplates(uriTemplates);
+        KeyManager keyManager = KeyManagerHolder.getKeyManagerInstance();
+        Map registeredResource = null;
+        try {
+            registeredResource = keyManager.getResourceByApiId(api.getId().toString());
+        } catch (APIManagementException e) {
+            log.error("Error while getting registered resources for API: " + api.getId().toString() + e.getMessage());
+        }
+        //Add new resource if not exist
+        if (registeredResource == null) {
+            boolean isNewResourceRegistered = false;
+            try {
+                isNewResourceRegistered = keyManager.registerNewResource(api, null);
+            } catch (APIManagementException e) {
+                log.error("Error while registering new resource for API: " + api.getId().toString() + e.getMessage());
+            }
+            if (!isNewResourceRegistered) {
+                log.error("New resource not registered for API: " + api.getId());
+            }
+        }
+        //update existing resource
+        else {
+            String resourceId = (String) registeredResource.get("resourceId");
+            try {
+                keyManager.updateRegisteredResource(api, registeredResource);
+            } catch (APIManagementException e) {
+                log.error("Error while updating resource");
+            }
+        }
+        return true;
+    }
+
+    public static OAuthApplicationInfo registerOAuthApplication(OAuthAppRequest appRequest) {
+        //Create Oauth Application - Dynamic client registration service
+        AMDefaultKeyManagerImpl impl = new AMDefaultKeyManagerImpl();
+        OAuthApplicationInfo returnedAPP = null;
+        try {
+            returnedAPP = impl.createApplication(appRequest);
+        } catch (APIManagementException e) {
+            log.error("Cannot create OAuth application from provided information, for APP name: " +
+                    appRequest.getOAuthApplicationInfo().getClientName());
+        }
+        return returnedAPP;
+    }
+
+    public static OAuthApplicationInfo retrieveOAuthApplication(String consumerKey) {
+        //Create Oauth Application - Dynamic client registration service
+        AMDefaultKeyManagerImpl impl = new AMDefaultKeyManagerImpl();
+        OAuthApplicationInfo returnedAPP = null;
+        try {
+            returnedAPP = impl.retrieveApplication(consumerKey);
+        } catch (APIManagementException e) {
+            log.error("Error while retrieving OAuth application information for Consumer Key: " + consumerKey);
+        }
+        return returnedAPP;
     }
 
 }
