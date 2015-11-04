06736146eecf0e42b8b2e987537bf1d40a475d65
sanjeewa malalgoda <sanjeewa@wso2.com>
2015-11-04 18:38:19 +0530

    refactor dcr and add it to component pom

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.dcr/src/main/java/org/wso2/carbon/apimgt/rest/api/dcr/web/dto/RegistrationProfile.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.dcr/src/main/java/org/wso2/carbon/apimgt/rest/api/dcr/web/dto/RegistrationProfile.java
@@ -15,7 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.wso2.carbon.apimgt.rest.api.dcr.web.impl;
+package org.wso2.carbon.apimgt.rest.api.dcr.web.dto;
 
 /**
  *
