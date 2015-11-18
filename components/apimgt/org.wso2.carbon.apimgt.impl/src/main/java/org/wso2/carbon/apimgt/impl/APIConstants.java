8b2237f1c9c2b796182a4c94a1175791dbe235ef
Chamin Dias <chamin892@gmail.com>
2015-11-18 22:29:46 +0530

    Include review suggestion - Using constants.

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
@@ -210,7 +210,8 @@ public final class APIConstants {
     public static final String API_OVERVIEW_VISIBLE_ROLES ="overview_visibleRoles";
     public static final String API_OVERVIEW_VISIBLE_TENANTS ="overview_visibleTenants";
     public static final String API_OVERVIEW_ENVIRONMENTS = "overview_environments";
-    public static final String API_TAGS = "tags";
+    public static final String API_OVERVIEW_TAG = "tags";
+    public static final String API_TAG = "Tag";
     public static final String API_STATUS = "STATUS";
     public static final String API_URI_PATTERN ="URITemplate_urlPattern";
     public static final String API_URI_HTTP_METHOD ="URITemplate_httpVerb";
