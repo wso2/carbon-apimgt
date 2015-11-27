ab1b7716adfa6252bd9f3b4551cc013401336809
Malintha Amarasinghe <malintha.prasan@gmail.com>
2015-11-27 09:54:55 +0530

    Adding codegen changes to restapi store

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/gen/java/org/wso2/carbon/apimgt/rest/api/store/ApisApiService.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/gen/java/org/wso2/carbon/apimgt/rest/api/store/ApisApiService.java
@@ -12,7 +12,7 @@ import org.wso2.carbon.apimgt.rest.api.store.dto.DocumentDTO;
 import java.util.List;
 
 import java.io.InputStream;
-
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 
 import javax.ws.rs.core.Response;
 
