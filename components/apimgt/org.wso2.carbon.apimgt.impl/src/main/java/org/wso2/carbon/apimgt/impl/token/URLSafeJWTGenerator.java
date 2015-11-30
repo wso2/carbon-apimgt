88b3b4c2de44b245a02ac9f9412c6766f3d25f29
tharikaGitHub <tharika.m92@gmail.com>
2015-11-30 11:45:13 +0530

    Changes to URLSafeJWTGenerator.java

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/token/URLSafeJWTGenerator.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/token/URLSafeJWTGenerator.java
@@ -18,90 +18,17 @@
 
 package org.wso2.carbon.apimgt.impl.token;
 
-import com.nimbusds.jose.util.Base64URL;
-import org.apache.axiom.util.base64.Base64Utils;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.apimgt.api.APIManagementException;
-import org.wso2.carbon.apimgt.impl.dto.APIKeyValidationInfoDTO;
-import org.wso2.carbon.apimgt.impl.utils.APIUtil;
-import org.wso2.carbon.user.core.util.UserCoreUtil;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.Calendar;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-public class URLSafeJWTGenerator extends AbstractJWTGenerator {
+public class URLSafeJWTGenerator extends JWTGenerator {
 
     private static final Log log = LogFactory.getLog(URLSafeJWTGenerator.class);
 
-    public Map<String, String> populateStandardClaims(APIKeyValidationInfoDTO keyValidationInfoDTO, String apiContext,
-            String version) throws APIManagementException {
-
-        //generating expiring timestamp
-        long currentTime = Calendar.getInstance().getTimeInMillis();
-        long expireIn = currentTime + 1000 * 60 * getTTL();
-
-        //String jwtBody = "";
-        String dialect;
-        ClaimsRetriever claimsRetriever = getClaimsRetriever();
-        if (claimsRetriever != null) {
-            //jwtBody = JWT_INITIAL_BODY.replaceAll("\\[0\\]", claimsRetriever.getDialectURI(endUserName));
-            dialect = claimsRetriever.getDialectURI(keyValidationInfoDTO.getEndUserName());
-        } else {
-            //jwtBody = JWT_INITIAL_BODY.replaceAll("\\[0\\]", dialectURI);
-            dialect = getDialectURI();
-        }
-
-        String subscriber = keyValidationInfoDTO.getSubscriber();
-        String applicationName = keyValidationInfoDTO.getApplicationName();
-        String applicationId = keyValidationInfoDTO.getApplicationId();
-        String tier = keyValidationInfoDTO.getTier();
-        String endUserName = keyValidationInfoDTO.getEndUserName();
-        String keyType = keyValidationInfoDTO.getType();
-        String userType = keyValidationInfoDTO.getUserType();
-        String applicationTier = keyValidationInfoDTO.getApplicationTier();
-        String enduserTenantId = String.valueOf(APIUtil.getTenantId(endUserName));
-
-        Map<String, String> claims = new LinkedHashMap<String, String>(20);
-
-        claims.put("iss", API_GATEWAY_ID);
-        claims.put("exp", String.valueOf(expireIn));
-        claims.put(dialect + "/subscriber", subscriber);
-        claims.put(dialect + "/applicationid", applicationId);
-        claims.put(dialect + "/applicationname", applicationName);
-        claims.put(dialect + "/applicationtier", applicationTier);
-        claims.put(dialect + "/apicontext", apiContext);
-        claims.put(dialect + "/version", version);
-        claims.put(dialect + "/tier", tier);
-        claims.put(dialect + "/keytype", keyType);
-        claims.put(dialect + "/usertype", userType);
-        claims.put(dialect + "/enduser",
-                UserCoreUtil.removeDomainFromName(APIUtil.getUserNameWithTenantSuffix(endUserName)));
-        claims.put(dialect + "/enduserTenantId", enduserTenantId);
-
-        return claims;
-    }
-
-    public Map<String, String> populateCustomClaims(APIKeyValidationInfoDTO keyValidationInfoDTO, String apiContext,
-            String version, String accessToken) throws APIManagementException {
-
-        ClaimsRetriever claimsRetriever = getClaimsRetriever();
-        if (claimsRetriever != null) {
-            String tenantAwareUserName = keyValidationInfoDTO.getEndUserName();
-            try {
-                return claimsRetriever.getClaims(tenantAwareUserName);
-
-            } catch (APIManagementException e) {
-                log.error("Error while retrieving claims ", e);
-            }
-        }
-
-        return null;
-    }
 
     public String encode(String stringToBeEncoded) throws APIManagementException {
 
