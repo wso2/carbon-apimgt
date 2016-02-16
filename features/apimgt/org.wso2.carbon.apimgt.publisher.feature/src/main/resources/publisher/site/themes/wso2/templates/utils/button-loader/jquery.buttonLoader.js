f55747f2501215e8e56eb879b12998795315d15c
Lakmali <lakmali8781@gmail.com>
2016-02-16 14:17:06 +0530

    Adding new wso2 bootstrap theme

---
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/utils/button-loader/jquery.buttonLoader.js
@@ -0,0 +1,22 @@
+/*A jQuery plugin which add loading indicators into buttons*/
+
+(function ($) {
+
+    $.fn.buttonLoader = function (action) {
+        var self = $(this);
+        if (action == 'start') {
+            if ($(self).attr("disabled") == "disabled") {
+                return false;
+            }
+            $('.has-spinner').attr("disabled", true);
+            $(self).attr('data-btn-text', $(self).text());
+            $(self).html('<span class="spinner"><i class="fa fa-spinner fa-pulse"></i></span>Loading');
+            $(self).addClass('active');
+        }
+        if (action == 'stop') {
+            $(self).html($(self).attr('data-btn-text'));
+            $(self).removeClass('active');
+            $('.has-spinner').attr("disabled", false);
+        }
+    }
+})(jQuery);
