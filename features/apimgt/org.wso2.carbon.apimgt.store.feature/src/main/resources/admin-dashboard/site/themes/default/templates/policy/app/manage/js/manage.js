2ccbeace178d724adce777f21d1f412ec76a2c0b
UpekkaA <upekka@wso2.com>
2016-03-10 16:54:47 +0530

    added the new UI components to Admin Dashboard for defining throttling policies

---
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin-dashboard/site/themes/default/templates/policy/app/manage/js/manage.js
@@ -0,0 +1,21 @@
+var deleteAppPolicy = function (policyObject) {
+    jagg.message({
+        content:'Tier deletion might affect current subscriptions. Are you sure you want to delete this tier? ',
+        title:'Confirm Deletion',
+        type:'confirm',
+        anotherDialog:true,
+        okCallback:function(){
+           /* jagg.post("/site/blocks/policy/app/manage/ajax/app-policy-manage.jag", {
+                action:"deleteTier",
+                tier:tierObject
+                }, function (result) {
+                    if (result.error == false) {
+                        window.location.reload(true);
+                    } else {
+                        jagg.message({content:result.message,type:"error"});
+                    }
+                },
+            "json");*/
+        }
+    });
+};
