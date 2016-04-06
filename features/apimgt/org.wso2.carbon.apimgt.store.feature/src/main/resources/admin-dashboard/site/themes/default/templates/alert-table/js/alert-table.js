881326bbcdc9a193ee584742b4206c580b1838d4
fazlan-nazeem <fazlann@wso2.com>
2016-04-06 10:33:14 +0530

    adding new changes

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin-dashboard/site/themes/default/templates/alert-table/js/alert-table.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin-dashboard/site/themes/default/templates/alert-table/js/alert-table.js
@@ -1,16 +1,18 @@
-$( document ).ready(function() {      
+$( document ).ready(function() {  
 
-    //entriesPerPage default value
+    changeSelectedOption();
+    
+    //default values
     var count = 10;
     var searchEntry = null;
     var startIndex = 0;
+    var currentPage = 1;
     
     loadAlertTable(); 
     $('#alertSelected').change(function(){
         loadAlertTable(); 
     });
 
- 
     $('#entriesPerPage').change(function(){
         count = $('#entriesPerPage option:selected').text();
         loadAlertTable(); 
@@ -27,19 +29,31 @@ $( document ).ready(function() {
             loadAlertTable();
         }
     });
-
     
-    if (($.cookie("selectedTab") != null)) {
-        $.cookie("selectedTab", null);
+    function changeSelectedOption(){
+        var tableName = getParameterByName('tableName');
+        if(tableName != null){
+            if(tableName == "RequestPatternChanged"){
+                $('#alertSelected').val(1).change();
+            } else if(tableName == "UnusualIPAccessAlert"){
+                 $('#alertSelected').val(2).change();
+            } else if(tableName == "abnormalRequestsPerMin"){
+                 $('#alertSelected').val(3).change();
+            } else if(tableName == "abnormalResponseTime"){
+                 $('#alertSelected').val(4).change();
+            } else if(tableName == "AbnormalTierUsage"){
+                 $('#alertSelected').val(5).change();
+            } else if(tableName == "AbnormalRefreshAlert"){
+                 $('#alertSelected').val(6).change();
+            } else if(tableName == "FrequentTierHittingAlert"){
+                 $('#alertSelected').val(7).change();
+            } else if(tableName == "abnormalBackendTime"){
+                 $('#alertSelected').val(8).change();
+            } else if(tableName == "healthAvailabilityPerMin"){
+                 $('#alertSelected').val(9).change();
+            }      
+        }   
     }
-
-    $('.pagination a').click(function(ex){
-      ex.preventDefault();
-      var navigatedPage = parseInt(getParameterByName('page', this));
-      startIndex = ((navigatedPage-1)*count) + 1;
-      loadAlertTable();
-    });
-    
 
     function getParameterByName(name, url) {
        if (!url) url = window.location.href;
@@ -51,15 +65,41 @@ $( document ).ready(function() {
        return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
 
-    //jagg.post("/site/blocks/alert-table/ajax/alert-table.jag",{action:"getCountFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_REQUESTPATTERNCHANGEDSTREAM",searchQuery:searchEntry}, function(json){},"json");
+
+    function generatePagination(rowCount,count){
+
+        numberOfPages = Math.ceil(rowCount / count );     
+          $.ajax({
+            url: "site/blocks/alert-table/ajax/alert-table-pagination.jag",
+            data : { currentPage : currentPage, numberOfPages : numberOfPages }, 
+            success: function(result){
+            $('#pagination-div').html(result);
+            $('.pagination a').click(function(ex){
+              ex.preventDefault();
+              var navigatedPage = parseInt(getParameterByName('page', this));
+              startIndex = ((navigatedPage-1)*count) + 1;
+              currentPage = navigatedPage;
+              loadAlertTable();
+            });
+          }});        
+    }
+
+    function getRowCountFromTable(tableName,searchQuery){
+        jagg.post("/site/blocks/alert-table/ajax/alert-table.jag",{action:"getCountFromAlertTable", tableName:tableName,searchQuery:searchQuery}, function(json){
+            var tempResult = json;
+            var rowCount = tempResult["values"];
+            generatePagination(rowCount,count);
+        },"json");
+    }
   	
     function loadAlertTable(){
-    	
+
        	var selectedOption = $("#alertSelected option:selected" ).val();
         
         if(selectedOption == 1){
-        	//result = loadData("ORG_WSO2_ANALYTICS_APIM_REQUESTPATTERNCHANGEDSTREAM"); 
-            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_REQUESTPATTERNCHANGEDSTREAM", entriesPerPage: count , searchQuery:searchEntry, startIndex:startIndex}, function(json){ 
+
+            var tableName = "ORG_WSO2_ANALYTICS_APIM_REQUESTPATTERNCHANGEDSTREAM";
+            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:tableName, entriesPerPage: count , searchQuery:searchEntry, startIndex:startIndex}, function(json){ 
                 var tempResult = json;
                 var rowCount = tempResult["values"].length;
                 
@@ -72,9 +112,12 @@ $( document ).ready(function() {
                 }                           
             }
             ,"json");
+            getRowCountFromTable(tableName,searchEntry);
                   
         } else if(selectedOption == 2){
-            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_ALERT_UNUSUALIPACCESS", entriesPerPage: count, startIndex:startIndex}, function(json){ 
+
+            var tableName = "ORG_WSO2_ANALYTICS_APIM_ALERT_UNUSUALIPACCESS";
+            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:tableName, entriesPerPage: count, startIndex:startIndex}, function(json){ 
                 var tempResult = json;
                 var rowCount = tempResult["values"].length;
                 
@@ -87,8 +130,12 @@ $( document ).ready(function() {
                 }
             }
             ,"json");
+             getRowCountFromTable(tableName,searchEntry);
+
         } else if(selectedOption == 3){
-             jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_ABNORMALREQUESTSPERMINALERTSTREAM", entriesPerPage: count,startIndex:startIndex}, function(json){ 
+
+            var tableName = "ORG_WSO2_ANALYTICS_APIM_ABNORMALREQUESTSPERMINALERTSTREAM";
+            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_ABNORMALREQUESTSPERMINALERTSTREAM", entriesPerPage: count,startIndex:startIndex}, function(json){ 
                 var tempResult = json;
                 var rowCount = tempResult["values"].length;
                 
@@ -101,8 +148,12 @@ $( document ).ready(function() {
                 }                           
             }
             ,"json");
+            getRowCountFromTable(tableName,searchEntry);
+
         } else if(selectedOption == 4){
-            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_ABNORMALRESPONSETIMEALERTSTREAM", entriesPerPage: count, startIndex:startIndex}, function(json){ 
+
+            var tableName = "ORG_WSO2_ANALYTICS_APIM_ABNORMALRESPONSETIMEALERTSTREAM";
+            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:tableName, entriesPerPage: count, startIndex:startIndex}, function(json){ 
                 var tempResult = json;
                 var rowCount = tempResult["values"].length;
 
@@ -115,11 +166,29 @@ $( document ).ready(function() {
                 }                           
             }
             ,"json");
+            getRowCountFromTable(tableName,searchEntry);
+
         } else if(selectedOption == 5){
+
+            var tableName = "ORG_WSO2_ANALYTICS_APIM_ALERT_ABNORMALTIERUSAGEALERT";
+            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:tableName, entriesPerPage: count, startIndex:startIndex}, function(json){ 
+                var tempResult = json;
+                var rowCount = tempResult["values"].length;
                 $('#alertsHistory').empty();
-                $('#alertsHistory').append('<thead id="alertsHistoryHeader"> <tr><th class="appSpecialCell">Userid</th><th class="appSpecialCell">Message</th><th class="userSpecialCell">API</th><th class="userSpecialCell">Alert Timestamp</th></tr>  </thead> ');
+                $('#alertsHistory').append('<thead id="alertsHistoryHeader"> <tr><th class="appSpecialCell">Userid</th><th class="appSpecialCell">Message</th><th class="userSpecialCell">API Version</th><th class="userSpecialCell">Alert Timestamp</th></tr>  </thead> ');
+
+                 for(i=0;i<rowCount;i++){
+                    var result = tempResult["values"][i]["values"];
+                    $('#alertsHistory').append('<tr><td>'+ result["userId"]+'</td><td>'+result["msg"]+'</td><td>'+ result["api_version"]+'</td><td>'+ new Date(result["alertTimestamp"])+'</td></tr>');
+                }                           
+            }
+            ,"json");
+            getRowCountFromTable(tableName,searchEntry);
+
         } else if(selectedOption == 6){
-            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_ALERT_ABNORMALTOKENREFRESH", entriesPerPage: count, startIndex:startIndex}, function(json){ 
+            
+            var tableName = "ORG_WSO2_ANALYTICS_APIM_ALERT_ABNORMALTOKENREFRESH";
+            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:tableName, entriesPerPage: count, startIndex:startIndex}, function(json){ 
                 var tempResult = json;
                 var rowCount = tempResult["values"].length;
                
@@ -132,22 +201,30 @@ $( document ).ready(function() {
                 }                           
             }
             ,"json");
+            getRowCountFromTable(tableName,searchEntry);
+
         } else if(selectedOption == 7){
-             jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_ALERT_ABNORMALTOKENREFRESH", entriesPerPage: count, startIndex:startIndex}, function(json){ 
+
+            var tableName = "ORG_WSO2_ANALYTICS_APIM_TIERLIMITHITTINGALERT";
+            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:tableName, entriesPerPage: count, startIndex:startIndex}, function(json){ 
                 var tempResult = json;
                 var rowCount = tempResult["values"].length;
             
                 $('#alertsHistory').empty();
-                $('#alertsHistory').append('<thead id="alertsHistoryHeader"> <tr><th class="appSpecialCell">Userid</th><th class="appSpecialCell">Message</th><th class="userSpecialCell">API</th><th class="userSpecialCell">Application ID</th><th class="userSpecialCell">Application Name</th><th class="userSpecialCell">Alert Timestamp</th></tr>  </thead> ');
+                $('#alertsHistory').append('<thead id="alertsHistoryHeader"> <tr><th class="appSpecialCell">API Publisher</th><th class="appSpecialCell">Message</th><th class="userSpecialCell">API</th><th class="userSpecialCell">Application ID</th><th class="userSpecialCell">Application Name</th><th class="userSpecialCell">Alert Timestamp</th></tr>  </thead> ');
 
                 for(i=0;i<rowCount;i++){
                     var result = tempResult["values"][i]["values"];
-                    $('#alertsHistory').append('<tr><td>'+ result["userId"]+'</td><td>'+result["message"]+'</td><td>'+ result["api"]+'</td><td>'+ result["applicationId"]+'</td><td>'+ result["applicationName"]+'</td><td>'+ new Date(result["alertTimestamp"])+'</td></tr>');
+                    $('#alertsHistory').append('<tr><td>'+ result["apiPublisher"]+'</td><td>'+result["msg"]+'</td><td>'+ result["api"]+'</td><td>'+ result["applicationId"]+'</td><td>'+ result["applicationName"]+'</td><td>'+ new Date(result["alertTimestamp"])+'</td></tr>');
                 }                           
             }
             ,"json");
+            getRowCountFromTable(tableName,searchEntry);
+
         } else if(selectedOption == 8){
-             jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_ABNORMALBACKENDTIMEALERTSTREAM", entriesPerPage: count, startIndex:startIndex}, function(json){ 
+
+            var tableName = "ORG_WSO2_ANALYTICS_APIM_ABNORMALBACKENDTIMEALERTSTREAM";
+            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:tableName, entriesPerPage: count, startIndex:startIndex}, function(json){ 
                 var tempResult = json;
                 var rowCount = tempResult["values"].length;
                 
@@ -160,9 +237,12 @@ $( document ).ready(function() {
                 }                           
             }
             ,"json");
-        } else if(selectedOption == 9){
+            getRowCountFromTable(tableName,searchEntry);
 
-            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:"ORG_WSO2_ANALYTICS_APIM_APIHEALTHMONITORSTREAM", entriesPerPage: count, startIndex:startIndex}, function(json){ 
+        } else if(selectedOption == 9){
+            
+            var tableName = "ORG_WSO2_ANALYTICS_APIM_APIHEALTHMONITORSTREAM";
+            jagg.post("/site/blocks/alert-table/ajax/alert-table.jag", { action: "getDataFromAlertTable", tableName:tableName, entriesPerPage: count, startIndex:startIndex}, function(json){ 
                 var tempResult = json;
                 var rowCount = tempResult["values"].length;
             
@@ -175,21 +255,11 @@ $( document ).ready(function() {
                 }                           
             }
             ,"json");
+            getRowCountFromTable(tableName,searchEntry);
+
         }else {
             $('#alertsHistory').empty();
         }
-
-
-       // var heightOfTable = $('#table_div').css('height');
-
-       // console.log(heightOfTable);
-
-   }
-
-  
-   
-
- 
-	        
+   }        
 });
 
