21a738014a0b009ba3a8f8cce8209efedc912493
sanjeewa-malalgoda <sanjeewa190@gmail.com>
2016-04-06 13:13:04 +0530

    fixing conflict

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/throttling/ThrottleDataHolder.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/throttling/ThrottleDataHolder.java
@@ -46,205 +46,190 @@ import java.util.concurrent.ConcurrentHashMap;
  * When throttle data holder initialize it should read complete throttle decision table from global policy engine
  * via web service calls. In addition to that it should subscribe to topic and listen throttle updates.
  */
-<<<<<<< HEAD
+
 public class ThrottleDataHolder implements Runnable {
-    private static final Log log = LogFactory.getLog(JMSMessageListener.class);
-=======
-public class ThrottleDataHolder implements Runnable{
-    private static final Log log = LogFactory.getLog(ThrottleDataHolder.class);
->>>>>>> upstream/throttle_feature
-    private DataPublisher dataPublisher = null;
-    private String streamID;
-    private Map<String, String> throttleDataMap = new ConcurrentHashMap();
 
-    public Map<String, String> getThrottleDataMap() {
-        return throttleDataMap;
-    }
+        private static final Log log = LogFactory.getLog(ThrottleDataHolder.class);
+        private DataPublisher dataPublisher = null;
+        private String streamID;
 
-    public void setThrottleDataMap(Map<String, String> throttleDataMap) {
-        this.throttleDataMap = throttleDataMap;
-    }
-
-    /**
-     * This method will start throtle data holder.This need to perform following tasks
-     * 01. Call throttle data web service and get throttled events.
-     * 02. Subscribe to JMS events and keep on listner for new events come to topic
-     * 03. Initialize data publisher.
-     */
-    public void start() {
-        //First do web service call and update map.
-        //Then init JMS listner to listen que and update it.
-        //Following method will initialize JMS listnet and listen all updates and keep throttle data map up to date
-        //loadThrottleDecisionsFromWebService();
-        subscribeForJmsEvents();
-        initDataPublisher();
-    }
-
-<<<<<<< HEAD
-
-    /**
-     * This method will check given key in throttle data Map. Throttle data map need to be update from topic
-     * subscriber with all latest updates from global policy engine. This method will perfoem only local map
-     * lookup and return results.
-     *
-     * @param key String unique key of throttle event.
-     * @return Return true if event throttled(means key is available in throttle data map).
-     * false if key is not there in throttle map(that means its not throttled).
-     */
-=======
-    public Map<String, String> getThrottleDataMap() {
-        return throttleDataMap;
-    }
-
-    public void setThrottleDataMap(Map<String, String> throttleDataMap) {
-        this.throttleDataMap = throttleDataMap;
-    }
-
-    Map<String, String> throttleDataMap = new ConcurrentHashMap();
-
->>>>>>> upstream/throttle_feature
-    public boolean isThrottled(String key) {
-        if (null != this.throttleDataMap.get(key)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-    /**
-     * This method will used to subscribe JMS and update throttle data map.
-     * Then this will listen to topic updates and check all update and updare throttle data map accordingly.
-     */
-    public void subscribeForJmsEvents() {
-        for (int i = 1; i < 10000; i++) {
-            String str = "test" + i;
-            this.throttleDataMap.put(str, "throttled");
-        }
-        Properties properties = new Properties();
-        try {
-            ClassLoader classLoader = getClass().getClassLoader();
-            properties.load(classLoader.getResourceAsStream("mb.properties"));
-            Hashtable<String, String> parameters = new Hashtable<String, String>();
-            for (final String name : properties.stringPropertyNames()) {
-                parameters.put(name, properties.getProperty(name));
-            }
-
-            String destination = "throttleData";
-            JMSConnectionFactory jmsConnectionFactory = new JMSConnectionFactory(parameters, "Siddhi-JMS-Consumer");
-            Map<String, String> messageConfig = new HashMap<String, String>();
-            messageConfig.put(JMSConstants.PARAM_DESTINATION, destination);
-            int minThreadPoolSize = 4;
-            int maxThreadPoolSize = 4;
-            int keepAliveTimeInMillis = 1000;
-            int jobQueueSize = 1000;
-            JMSTaskManager jmsTaskManager = JMSTaskManagerFactory.createTaskManagerForService(jmsConnectionFactory,
-                    "Siddhi-JMS-Consumer", new NativeWorkerPool(minThreadPoolSize, maxThreadPoolSize,
-                            keepAliveTimeInMillis, jobQueueSize, "JMS Threads",
-                            "JMSThreads" + UUID.randomUUID().toString()), messageConfig);
-            jmsTaskManager.setJmsMessageListener(new JMSMessageListener(this));
-
-            JMSListener jmsListener = new JMSListener("Siddhi-JMS-Consumer" + "#" + destination,
-                    jmsTaskManager);
-            jmsListener.startListener();
-            log.info("Starting jms topic consumer thread...");
-
-        } catch (IOException e) {
-            log.error("Cannot read properties file from resources. " + e.getMessage(), e);
-        }
-    }
-
-
-    /**
-     * This method will used to push unthrottled data events to global policy engine.
-     *
-     * @param throttleRequest is objects map which contains certain information retrieved from incoming message
-     *                        need to add as much as data to this map and send events.
-     */
-    public void sendToGlobalThrottler(Object[] throttleRequest) {
-        org.wso2.carbon.databridge.commons.Event event = new org.wso2.carbon.databridge.commons.Event(streamID,
-                System.currentTimeMillis(), null, null, throttleRequest);
-        dataPublisher.tryPublish(event);
-    }
-
-
-    /**
-     * This method will initialize data publisher and this data publisher will be used to push events to central policy
-     * server.
-     */
-    private void initDataPublisher() {
-        try {
-            dataPublisher = new DataPublisher("Binary", "tcp://localhost:9611", "ssl://localhost:9711", "admin", "admin");
-            streamID = DataBridgeCommonsUtils.generateStreamId("org.wso2.throttle.request.stream", "1.0.0");
-        } catch (DataEndpointAgentConfigurationException e) {
-            log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
-                    e.getMessage(), e);
-        } catch (DataEndpointException e) {
-            log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
-                    e.getMessage(), e);
-        } catch (DataEndpointConfigurationException e) {
-            log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
-                    e.getMessage(), e);
-        } catch (DataEndpointAuthenticationException e) {
-            log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
-                    e.getMessage(), e);
-        } catch (TransportException e) {
-            log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
-                    e.getMessage(), e);
+        public Map<String, String> getThrottleDataMap() {
+            return throttleDataMap;
         }
 
-    }
-
-    /**
-     * This method will retrieve throttled events from service deployed in global policy server.
-     *
-     * @return String object array which contains throttled keys.
-     */
-    private String[] retrieveThrottlingData() {
-
-        try {
-            String url = "http://localhost:9763/throttle/data/v1/throttleAsString";
-
-            HttpGet method = new HttpGet(url);
-            HttpClient httpClient = HttpClientBuilder.create().build();
-            HttpResponse httpResponse = httpClient.execute(method);
-
-            String responseString = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
-            if (responseString != null && !responseString.isEmpty()) {
-                return responseString.split(",");
-            }
-
-        } catch (IOException e) {
-            log.error("Exception when retrieving throttling data from remote endpoint ", e);
+        public void setThrottleDataMap(Map<String, String> throttleDataMap) {
+            this.throttleDataMap = throttleDataMap;
         }
 
-        return null;
+        /**
+         * This method will start throtle data holder.This need to perform following tasks
+         * 01. Call throttle data web service and get throttled events.
+         * 02. Subscribe to JMS events and keep on listner for new events come to topic
+         * 03. Initialize data publisher.
+         */
+        public void start() {
+            //First do web service call and update map.
+            //Then init JMS listner to listen que and update it.
+            //Following method will initialize JMS listnet and listen all updates and keep throttle data map up to date
+            //loadThrottleDecisionsFromWebService();
+            subscribeForJmsEvents();
+            initDataPublisher();
+        }
 
-    }
 
+        Map<String, String> throttleDataMap = new ConcurrentHashMap();
 
-    /**
-     * This method will call throttle data web service deployed in central plocy engine at server loading time.
-     * Then it will update local throttle data map with the results obtained. This need to be fine tuned as large
-     * number of results can slow down web service call. However this need to be controlled from server side and
-     * this client will add all recieved events to local map. Even if we missed few events from this call it will
-     * eventually update as missed events go to global policy engine and decision will be anyway pushed to topic and
-     * all subscriber will notify it
-     */
-    public void loadThrottleDecisionsFromWebService() {
-        String[] throttleKeyArray = retrieveThrottlingData();
-        if (throttleKeyArray != null && throttleKeyArray.length > 0) {
-            for (String throttleKey : throttleKeyArray) {
-                this.getThrottleDataMap().put(throttleKey, "throttled");
+        /**
+         * This method will check given key in throttle data Map. Throttle data map need to be update from topic
+         * subscriber with all latest updates from global policy engine. This method will perfoem only local map
+         * lookup and return results.
+         *
+         * @param key String unique key of throttle event.
+         * @return Return true if event throttled(means key is available in throttle data map).
+         * false if key is not there in throttle map(that means its not throttled).
+         */
+        public boolean isThrottled(String key) {
+            if (null != this.throttleDataMap.get(key)) {
+                return true;
+            } else {
+                return false;
             }
         }
-    }
 
-    public void run() {
-        start();
-    }
 
-    public void startThrottler() {
-        new Thread(this).start();
+        /**
+         * This method will used to subscribe JMS and update throttle data map.
+         * Then this will listen to topic updates and check all update and updare throttle data map accordingly.
+         */
+        public void subscribeForJmsEvents() {
+            for (int i = 1; i < 10000; i++) {
+                String str = "test" + i;
+                this.throttleDataMap.put(str, "throttled");
+            }
+            Properties properties = new Properties();
+            try {
+                ClassLoader classLoader = getClass().getClassLoader();
+                properties.load(classLoader.getResourceAsStream("mb.properties"));
+                Hashtable<String, String> parameters = new Hashtable<String, String>();
+                for (final String name : properties.stringPropertyNames()) {
+                    parameters.put(name, properties.getProperty(name));
+                }
+
+                String destination = "throttleData";
+                JMSConnectionFactory jmsConnectionFactory = new JMSConnectionFactory(parameters, "Siddhi-JMS-Consumer");
+                Map<String, String> messageConfig = new HashMap<String, String>();
+                messageConfig.put(JMSConstants.PARAM_DESTINATION, destination);
+                int minThreadPoolSize = 4;
+                int maxThreadPoolSize = 4;
+                int keepAliveTimeInMillis = 1000;
+                int jobQueueSize = 1000;
+                JMSTaskManager jmsTaskManager = JMSTaskManagerFactory.createTaskManagerForService(jmsConnectionFactory,
+                        "Siddhi-JMS-Consumer", new NativeWorkerPool(minThreadPoolSize, maxThreadPoolSize,
+                                keepAliveTimeInMillis, jobQueueSize, "JMS Threads",
+                                "JMSThreads" + UUID.randomUUID().toString()), messageConfig);
+                jmsTaskManager.setJmsMessageListener(new JMSMessageListener(this));
+
+                JMSListener jmsListener = new JMSListener("Siddhi-JMS-Consumer" + "#" + destination,
+                        jmsTaskManager);
+                jmsListener.startListener();
+                log.info("Starting jms topic consumer thread...");
+
+            } catch (IOException e) {
+                log.error("Cannot read properties file from resources. " + e.getMessage(), e);
+            }
+        }
+
+
+        /**
+         * This method will used to push unthrottled data events to global policy engine.
+         *
+         * @param throttleRequest is objects map which contains certain information retrieved from incoming message
+         *                        need to add as much as data to this map and send events.
+         */
+        public void sendToGlobalThrottler(Object[] throttleRequest) {
+            org.wso2.carbon.databridge.commons.Event event = new org.wso2.carbon.databridge.commons.Event(streamID,
+                    System.currentTimeMillis(), null, null, throttleRequest);
+            dataPublisher.tryPublish(event);
+        }
+
+
+        /**
+         * This method will initialize data publisher and this data publisher will be used to push events to central policy
+         * server.
+         */
+        private void initDataPublisher() {
+            try {
+                dataPublisher = new DataPublisher("Binary", "tcp://localhost:9611", "ssl://localhost:9711", "admin", "admin");
+                streamID = DataBridgeCommonsUtils.generateStreamId("org.wso2.throttle.request.stream", "1.0.0");
+            } catch (DataEndpointAgentConfigurationException e) {
+                log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
+                        e.getMessage(), e);
+            } catch (DataEndpointException e) {
+                log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
+                        e.getMessage(), e);
+            } catch (DataEndpointConfigurationException e) {
+                log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
+                        e.getMessage(), e);
+            } catch (DataEndpointAuthenticationException e) {
+                log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
+                        e.getMessage(), e);
+            } catch (TransportException e) {
+                log.error("Error in initializing binary data-publisher to send requests to global throttling engine " +
+                        e.getMessage(), e);
+            }
+
+        }
+
+        /**
+         * This method will retrieve throttled events from service deployed in global policy server.
+         *
+         * @return String object array which contains throttled keys.
+         */
+        private String[] retrieveThrottlingData() {
+
+            try {
+                String url = "http://localhost:9763/throttle/data/v1/throttleAsString";
+
+                HttpGet method = new HttpGet(url);
+                HttpClient httpClient = HttpClientBuilder.create().build();
+                HttpResponse httpResponse = httpClient.execute(method);
+
+                String responseString = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+                if (responseString != null && !responseString.isEmpty()) {
+                    return responseString.split(",");
+                }
+
+            } catch (IOException e) {
+                log.error("Exception when retrieving throttling data from remote endpoint ", e);
+            }
+
+            return null;
+
+        }
+
+
+        /**
+         * This method will call throttle data web service deployed in central plocy engine at server loading time.
+         * Then it will update local throttle data map with the results obtained. This need to be fine tuned as large
+         * number of results can slow down web service call. However this need to be controlled from server side and
+         * this client will add all recieved events to local map. Even if we missed few events from this call it will
+         * eventually update as missed events go to global policy engine and decision will be anyway pushed to topic and
+         * all subscriber will notify it
+         */
+        public void loadThrottleDecisionsFromWebService() {
+            String[] throttleKeyArray = retrieveThrottlingData();
+            if (throttleKeyArray != null && throttleKeyArray.length > 0) {
+                for (String throttleKey : throttleKeyArray) {
+                    this.getThrottleDataMap().put(throttleKey, "throttled");
+                }
+            }
+        }
+
+        public void run() {
+            start();
+        }
+
+        public void startThrottler() {
+            new Thread(this).start();
+        }
     }
-}
