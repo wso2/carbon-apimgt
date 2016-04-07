bc2dd2f4bf452c14ec22abd68700acf3412a7a28
dinusha92 <dinudiss@gmail.com>
2016-04-07 21:18:34 +0530

    adding api policy delete method

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin-dashboard/site/themes/wso2/templates/policy-list/js/policy-list.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin-dashboard/site/themes/wso2/templates/policy-list/js/policy-list.js
@@ -1,5 +1,21 @@
-var updatePermissions = function () {
+var deleteAPIPolicy = function (policyObject) {
+    jagg.message({
+        content:'Policy deletion might affect current subscriptions. Are you sure you want to delete this policy? ',
+        title:'Confirm Deletion',
+        type:'confirm',
+        anotherDialog:true,
+        okCallback:function(){
+           jagg.post("/site/blocks/policy/api/manage/ajax/api-policy-manage.jag", {
+                action:"deleteAPIPolicy",
+                policy:policyObject
+                }, function (result) {
+                    if (result.error == false) {
+                        window.location.reload(true);
+                    } else {
+                        jagg.message({content:result.message,type:"error"});
+                    }
+                },
+            "json");
+        }
+    });
 };
-
-
-
