5ae73744b7758a5793dcd1a171cefec21a049a54
Dhannuka Priyanath Ranasinghe <dhanuka.priyanath@gmail.com>
2016-04-08 19:54:35 +0530

    getAllURLTemplates

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -3696,7 +3696,7 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
                 if (!(policy instanceof GlobalPolicy)) {    //exclude global level policies deploying to GlobalCEP
                     manager.deployPolicyToGlobalCEP(flowString);
                 }
-                manager.deployPolicyToGatewayManager(flowString);
+                //manager.deployPolicyToGatewayManager(flowString);
             }
             apiMgtDAO.setPolicyDeploymentStatus(policyLevel, policy.getPolicyName(), policy.getTenantId(), true);
         } catch (APIManagementException e) {
