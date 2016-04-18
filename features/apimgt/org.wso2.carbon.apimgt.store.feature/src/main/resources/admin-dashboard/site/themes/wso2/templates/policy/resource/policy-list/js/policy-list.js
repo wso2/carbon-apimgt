5adb909408669aae0f78a8b8cffb95bb5c51b523
harsha89 <harsz89@gmail.com>
2016-04-19 00:13:52 +0530

    adding missed files

---
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin-dashboard/site/themes/wso2/templates/policy/resource/policy-list/js/policy-list.js
@@ -0,0 +1,21 @@
+var deleteAPIPolicy = function (policyObject) {
+    jagg.message({
+        content:'Policy deletion might affect current subscriptions. Are you sure you want to delete this policy? ',
+        title:'Confirm Deletion',
+        type:'confirm',
+        anotherDialog:true,
+        okCallback:function(){
+           jagg.post("/site/blocks/policy/policy/resource/policy-list/ajax/api-policy-manage.jag", {
+                action:"deleteAPIPolicy",
+                policy:policyObject
+                }, function (result) {
+                    if (result.error == false) {
+                        window.location.reload(true);
+                    } else {
+                        jagg.message({content:result.message,type:"error"});
+                    }
+                },
+            "json");
+        }
+    });
+};
