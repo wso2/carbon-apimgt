7f3008c1ad33a99d73f8740ef39d1b2fab2c59c9
rswijesena <roshan86web@gmail.com>
2016-04-19 09:42:05 +0530

    removing tokenfeild and used bootstrap

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/manage-alerts/js/manage-alerts.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/manage-alerts/js/manage-alerts.js
@@ -26,37 +26,21 @@ var saveAlertTypes = function (alertTypesIDs,emailList,checkedValues) {
 
 $( document ).ready(function() {
 
-    $('#tokenfield')
+    $('#tokenfield').on('beforeItemAdd', function(event) {
+        /* Validate url */
 
-        .on('tokenfield:createtoken', function (e) {
-            var data = e.attrs.value.split('|')
-            e.attrs.value = data[1] || data[0]
-            e.attrs.label = data[1] ? data[0] + ' (' + data[1] + ')' : data[0];
+        var re = /\S+@\S+\.\S+/
+        var valid = re.test(event.item)
 
-        })
-
-        .on('tokenfield:createdtoken', function (e) {
-            // Über-simplistic e-mail validation
-            var re = /\S+@\S+\.\S+/
-            var valid = re.test(e.attrs.value)
-            if (!valid) {
-                $(e.relatedTarget).addClass('invalid');
-            }
-        })
-
-        .on('tokenfield:edittoken', function (e) {
-            if (e.attrs.label !== e.attrs.value) {
-                var label = e.attrs.label.split(' (')
-                e.attrs.value = label[0] + '|' + e.attrs.value
-            }
-        })
-
-        .on('tokenfield:removedtoken', function (e) {
-            //alert('Token removed! Token value was: ' + e.attrs.value)
-        })
-
-        .tokenfield()
-
+        if (!valid)
+        {
+            event.cancel = true;
+        }
+        else
+        {
+            event.cancel = false;
+        }
+    });
 
     $( "#saveBtn" ).click(function() {
 
