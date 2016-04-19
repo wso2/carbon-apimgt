9c302c71adbcc82ad49c6507e2d48829169709c9
dinusha92 <dinudiss@gmail.com>
2016-04-19 20:41:20 +0530

    Making contectAware false if quota type is null

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -828,7 +828,7 @@ public class ApiMgtDAO {
 
                 //check "API_POLICY" or "TIER_ID" or "APPLICATION_TIER" related policy is content aware
                 //TODO isContentAware
-                boolean isContentAware = isAnyPolicyContentAware(conn, rs.getString("API_PROVIDER"), null, rs.getString("APPLICATION_TIER"), rs.getString("TIER_ID"));
+                boolean isContentAware = false;  //isAnyPolicyContentAware(conn, rs.getString("API_PROVIDER"), null, rs.getString("APPLICATION_TIER"), rs.getString("TIER_ID"));
 
 
                 infoDTO.setContentAware(isContentAware);
