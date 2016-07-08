73d78e0881d998f45fc4293398bf42c9439c2fca
Chamin Dias <chamin892@gmail.com>
2016-07-08 20:28:47 +0530

    Adding the localization fixes for Store.

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin/site/themes/wso2/templates/utils/custom-validation/custom-validation.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin/site/themes/wso2/templates/utils/custom-validation/custom-validation.js
@@ -12,29 +12,29 @@ $(document).ready(function() {
                           }
                       });
         return this.optional(element) || contextExist != "true";
-    }, 'Duplicate context value.');
+    }, i18n.t('Duplicate context value.'));
 
     $.validator.addMethod('selected', function(value, element) {
         return value!="";
-    },'Select a value for the tier.');
+    },i18n.t('Select a value for the tier.'));
 
     $.validator.addMethod('validRegistryName', function(value, element) {
         var illegalChars = /([~!@#;%^*+={}\|\\<>\"\'\/,])/;
         return !illegalChars.test(value);
-    }, 'Input contains one or more illegal characters  (~ ! @ #  ; % ^ * + = { } | &lt; &gt;, \' / " \\ ) .');
+    }, i18n.t('Input contains one or more illegal characters ') + '(~ ! @ #  ; % ^ * + = { } | &lt; &gt;, \' / " \\ ) .');
 
     $.validator.addMethod('noSpace', function(value, element) {
         return !/\s/g.test(value);
-    },'Input contains white spaces.');
+    },i18n.t('Input contains white spaces.'));
 
     $.validator.addMethod('notEmpty', function(value, element) {
         return value !== null && jQuery.trim(value).length > 0;
-    },'Input cannot be empty.');
+    },i18n.t('Input cannot be empty.'));
 
     $.validator.addMethod('validInput', function(value, element) {
         var illegalChars = /([<>\"\'])/;
         return !illegalChars.test(value);
-    }, 'Input contains one or more illegal characters  (& &lt; &gt; \'  " ');
+    }, i18n.t('Input contains one or more illegal characters ') + '(& &lt; &gt; \'  " ');
 
     $.validator.addMethod('validateRoles', function(value, element) {
         var valid = false;
@@ -46,18 +46,18 @@ $(document).ready(function() {
                           }
                       });
         return this.optional(element) || valid == true;
-    }, 'Invalid role name[s]');
+    }, i18n.t('Invalid role name[s]'));
 
     $.validator.addMethod('validateEndpoints', function (value, element){
         var productionEP = $('#endpoint').val();
         var sandboxEP = $('#sandbox').val();
 
         return productionEP != "" || sandboxEP != "";
-    }, 'A Production or Sandbox URL must be provided.');
+    }, i18n.t('A Production or Sandbox URL must be provided.'));
 
     $.validator.addMethod("positiveInteger", function(value, element) {
             return this.optional( element ) || /^[1-9]\d*$/.test( value );
-    }, 'Inavlid value');
+    }, i18n.t('Invalid value'));
 
 
 });
