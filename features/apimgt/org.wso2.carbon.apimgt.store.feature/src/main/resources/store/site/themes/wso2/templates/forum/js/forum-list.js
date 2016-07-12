47edf20d14939d656e8eaee5fe5998d5a06862f9
thusithakalugamage <thusitha32@yahoo.com>
2016-07-12 13:23:25 +0530

    API Forum topic page layout and general UX improvements

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/templates/forum/js/forum-list.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/templates/forum/js/forum-list.js
@@ -323,8 +323,10 @@ $(document).ready(function () {
 
         var description = $('#forum_topic_description').html().trim();
         var topicDescriptionEditor = $("#forum_topic_description_edit_editor");
+        $(topicDescriptionEditor)
         $(topicDescriptionEditor).summernote({
-            height: 300
+            height: 300,
+            width:"99.9%"
         });
 
         $(topicDescriptionEditor).code(description);
@@ -439,7 +441,7 @@ $(document).ready(function () {
         var date = new Date();
         var time = date.getTime();
 
-        var replyInfo = getDate(date) + " <br/>" + getTime(time) + " <br/> " + i18n.t('info.replyAdded');
+        var replyInfo = '<div class="comment-extra">Posted By <strong>You</strong> on <span class="dateFull">' +getDate(date)+ ', ' + getTime(time) + '</span></div>';
         $('#forum_replies_list').show();
         $('#forum_reply_content_temp').html(replyContent);
         $('#forum_reply_added_block').show();
@@ -484,7 +486,7 @@ $(document).ready(function () {
 	jagg.sessionAwareJS({redirect:'/site/pages/index.jag'});
         var currentLocation = window.location.pathname;
         var topicId = currentLocation.split('/').pop();
-        var reply = $(this).parent().next().html();
+        var reply = $(this).parent().next().children().html();
         var id = $(this).data('id');
 
         // Hide reply content.
