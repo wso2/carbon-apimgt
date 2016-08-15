ecf90ffe793fdfb38e62a126e4a8bad25633b207
thilinicooray <thilinicooray.ucsc@gmail.com>
2016-08-15 11:50:59 +0530

    Initial commit for composite-api with composite API create page UI

---
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/libs/swagger-editor/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Polyfills
+require('es6-promise').polyfill();
+require('es6-shim');
+
+// App CSS files
+require('font-awesome-webpack');
+require('styles/main.less');
+
+// App JavaScript files
+require('scripts/app.js');
