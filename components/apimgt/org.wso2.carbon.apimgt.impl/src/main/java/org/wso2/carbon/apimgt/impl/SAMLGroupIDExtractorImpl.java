88d204f9892f5b5fc9b8794d903b53dcb23f68ec
harsha89 <harsz89@gmail.com>
2016-09-01 00:29:43 +0530

    adding logs and licence

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/SAMLGroupIDExtractorImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/SAMLGroupIDExtractorImpl.java
@@ -1,3 +1,20 @@
+/*
+*  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 package org.wso2.carbon.apimgt.impl;
 
 import org.apache.commons.logging.Log;
@@ -32,6 +49,9 @@ public class SAMLGroupIDExtractorImpl implements LoginPostExecutor {
     private static final Log log = LogFactory.getLog(SAMLGroupIDExtractorImpl.class);
 
     public String getGroupingIdentifiers(String loginResponse) {
+        if (log.isDebugEnabled()) {
+            log.debug("Login response " + loginResponse);
+        }
         ByteArrayInputStream samlResponseStream = null;
         DocumentBuilder docBuilder;
         String username = "";
@@ -65,6 +85,9 @@ public class SAMLGroupIDExtractorImpl implements LoginPostExecutor {
             UserStoreManager manager = realm.getUserStoreManager();
             organization =
                     manager.getUserClaimValue(MultitenantUtils.getTenantAwareUsername(username), claim, null);
+            if (log.isDebugEnabled()) {
+                log.debug("User organization " + organization);
+            }
             if (organization != null) {
                 organization = tenantDomain + "/" + organization.trim();
             }
