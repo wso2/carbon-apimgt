f9b035e02630503ee634b21a25aa0c82760f147b
Chamila Adhikarinayake <cka.adhi@gmail.com>
2016-09-30 13:59:48 +0530

    add error handling for the rest api

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/WorkflowsApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/WorkflowsApiServiceImpl.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.wso2.carbon.apimgt.rest.api.publisher.impl;
 
 import org.wso2.carbon.apimgt.api.APIManagementException;
@@ -9,6 +27,7 @@ import org.wso2.carbon.apimgt.impl.workflow.WorkflowException;
 import org.wso2.carbon.apimgt.rest.api.publisher.*;
 import org.wso2.carbon.apimgt.rest.api.publisher.dto.*;
 import org.wso2.carbon.apimgt.rest.api.publisher.dto.WorkflowDTO.*;
+import org.wso2.carbon.apimgt.rest.api.util.RestApiConstants;
 import org.wso2.carbon.apimgt.rest.api.util.utils.RestApiUtil;
 import org.wso2.carbon.apimgt.rest.api.publisher.dto.ErrorDTO;
 import org.wso2.carbon.apimgt.rest.api.publisher.dto.WorkflowDTO;
@@ -25,41 +44,44 @@ import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import javax.ws.rs.core.Response;
 
 public class WorkflowsApiServiceImpl extends WorkflowsApiService {
-    
+
     private static final Log log = LogFactory.getLog(WorkflowsApiService.class);
+
     @Override
-    public Response workflowsUpdateWorkflowStatusPost(String workflowReferenceId,WorkflowDTO body){
-        
-        
+    public Response workflowsUpdateWorkflowStatusPost(String workflowReferenceId, WorkflowDTO body) {
+
         ApiMgtDAO apiMgtDAO = ApiMgtDAO.getInstance();
 
         boolean isTenantFlowStarted = false;
-        
-        
 
         try {
             if (workflowReferenceId != null) {
-                org.wso2.carbon.apimgt.impl.dto.WorkflowDTO workflowDTO = apiMgtDAO.retrieveWorkflow(workflowReferenceId);
-                String tenantDomain = workflowDTO.getTenantDomain();
-                if (tenantDomain != null && !org.wso2.carbon.utils.multitenancy.MultitenantConstants.SUPER_TENANT_DOMAIN_NAME.equals(tenantDomain)) {
-                    isTenantFlowStarted = true;
-                    PrivilegedCarbonContext.startTenantFlow();
-                    PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(tenantDomain, true);
-                }
-
+                org.wso2.carbon.apimgt.impl.dto.WorkflowDTO workflowDTO = apiMgtDAO
+                        .retrieveWorkflow(workflowReferenceId);
                 if (workflowDTO == null) {
-           ////////////////////////////////////////////////////////////
-                }
+                    RestApiUtil.handleResourceNotFoundError(RestApiConstants.RESOURCE_WORKFLOW, workflowReferenceId,
+                            log);
+                } else {
+                    String tenantDomain = workflowDTO.getTenantDomain();
+                    if (tenantDomain != null
+                            && !org.wso2.carbon.utils.multitenancy.MultitenantConstants.SUPER_TENANT_DOMAIN_NAME
+                                    .equals(tenantDomain)) {
+                        isTenantFlowStarted = true;
+                        PrivilegedCarbonContext.startTenantFlow();
+                        PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(tenantDomain, true);
+                    }
 
-                workflowDTO.setWorkflowDescription(body.getDescription());
-                workflowDTO.setStatus(WorkflowStatus.valueOf(body.getStatus().toString()));
-                workflowDTO.setAttributes(body.getAttributes());
-                String workflowType = workflowDTO.getWorkflowType();
-                WorkflowExecutor workflowExecutor = WorkflowExecutorFactory.getInstance()
-                        .getWorkflowExecutor(workflowType);
+                    workflowDTO.setWorkflowDescription(body.getDescription());
+                    workflowDTO.setStatus(WorkflowStatus.valueOf(body.getStatus().toString()));
+                    workflowDTO.setAttributes(body.getAttributes());
+                    String workflowType = workflowDTO.getWorkflowType();
+                    WorkflowExecutor workflowExecutor = WorkflowExecutorFactory.getInstance()
+                            .getWorkflowExecutor(workflowType);
 
-                workflowExecutor.complete(workflowDTO);
-                return Response.ok().entity(body).build(); /////////check
+                    workflowExecutor.complete(workflowDTO);
+                    return Response.ok().entity(body).build();
+                    
+                }                
             }
         } catch (APIManagementException e) {
             String msg = "Error while resuming workflow " + workflowReferenceId;
@@ -72,6 +94,6 @@ public class WorkflowsApiServiceImpl extends WorkflowsApiService {
                 PrivilegedCarbonContext.endTenantFlow();
             }
         }
-       return null;
+        return null;
     }
 }
