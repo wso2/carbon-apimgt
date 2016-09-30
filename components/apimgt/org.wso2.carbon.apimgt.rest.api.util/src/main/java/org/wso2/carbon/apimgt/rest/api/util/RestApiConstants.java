b4bc59fc85040a09706de41acfb2c3b3d95de8f4
Chamila Adhikarinayake <cka.adhi@gmail.com>
2016-09-30 14:03:43 +0530

    add scope for workflow rest api

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/RestApiConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/RestApiConstants.java
@@ -47,6 +47,7 @@ public final class RestApiConstants {
     public static final String RESOURCE_TIER = "tier";
     public static final String RESOURCE_TIER_UPDATE_PERMISSION = RESOURCE_TIER + "/update-permission";
     public static final String RESOURCE_TAG = "tag";
+    public static final String RESOURCE_WORKFLOW = "workflow";
 
     public static final String API_ID_DELIMITER = "-";
     public static final String QUERY_PARAM = "{query}";
