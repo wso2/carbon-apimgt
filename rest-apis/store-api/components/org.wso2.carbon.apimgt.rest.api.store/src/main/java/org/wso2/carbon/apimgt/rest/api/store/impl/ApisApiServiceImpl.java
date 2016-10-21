49232f8cde40d3e6f60235ece6d811321c6c8d04
sajith <sajhak@gmail.com>
2016-10-21 11:08:11 +0530

    Added constant class to api store. Updates to APiServiceImpl. Added API Mapping util class. Added dependencies

--- rest-apis/store-api/components/org.wso2.carbon.apimgt.rest.api.store/src/main/java/org/wso2/carbon/apimgt/rest/api/store/impl/ApisApiServiceImpl.java
+++ rest-apis/store-api/components/org.wso2.carbon.apimgt.rest.api.store/src/main/java/org/wso2/carbon/apimgt/rest/api/store/impl/ApisApiServiceImpl.java
@@ -1,23 +1,41 @@
 package org.wso2.carbon.apimgt.rest.api.store.impl;
 
+import org.wso2.carbon.apimgt.api.APIConsumer;
+import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.api.model.API;
 import org.wso2.carbon.apimgt.rest.api.store.*;
+import org.wso2.carbon.apimgt.rest.api.store.constants.APIConstants;
 import org.wso2.carbon.apimgt.rest.api.store.dto.*;
 
 
 import org.wso2.carbon.apimgt.rest.api.store.dto.ErrorDTO;
+import org.wso2.carbon.apimgt.rest.api.store.utils.mappings.APIMappingUtil;
+import org.wso2.carbon.apimgt.rest.api.util.RestApiConstants;
+import org.wso2.carbon.apimgt.rest.api.util.utils.RestApiUtil;
 import org.wso2.carbon.apimgt.rest.api.store.dto.DocumentDTO;
 import org.wso2.carbon.apimgt.rest.api.store.dto.DocumentListDTO;
 import org.wso2.carbon.apimgt.rest.api.store.dto.APIDTO;
 import org.wso2.carbon.apimgt.rest.api.store.dto.APIListDTO;
 
 import java.util.List;
-
+import java.util.Map;
+import java.util.Set;
 import java.io.InputStream;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 
 import javax.ws.rs.core.Response;
 
+/**
+ *  This is the service implementation class for Store API related operations*
+ */
 public class ApisApiServiceImpl extends ApisApiService {
+    
+    private static final Log log = LogFactory.getLog(ApplicationsApiServiceImpl.class);
+
     @Override
     public Response apisApiIdDocumentsDocumentIdContentGet(String apiId,String documentId,String xWSO2Tenant,String accept,String ifNoneMatch,String ifModifiedSince){
         // do some magic!
@@ -43,9 +61,99 @@ public class ApisApiServiceImpl extends ApisApiService {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
+    
+    /** 
+    * Retrieves APIs qualifying under given search condition 
+    *
+    * @param limit maximum number of APIs returns
+    * @param offset starting index
+    * @param xWSO2Tenant requested tenant domain for cross tenant invocations
+    * @param query search condition
+    * @param accept Accept header value
+    * @param ifNoneMatch If-None-Match header value
+    * @return matched APIs for the given search condition
+    */
     @Override
     public Response apisGet(Integer limit,Integer offset,String xWSO2Tenant,String query,String accept,String ifNoneMatch){
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+       
+        Map<String, Object> apisMap;
+        int size = 0;
+        //pre-processing
+        //setting default limit and offset values if they are not set
+        limit = limit != null ? limit : RestApiConstants.PAGINATION_LIMIT_DEFAULT;
+        offset = offset != null ? offset : RestApiConstants.PAGINATION_OFFSET_DEFAULT;
+        query = query == null ? "" : query;
+        String requestedTenantDomain = RestApiUtil.getRequestedTenantDomain(xWSO2Tenant);
+        APIListDTO apiListDTO = new APIListDTO();
+        try {
+            String username = RestApiUtil.getLoggedInUsername();
+            APIConsumer apiConsumer = RestApiUtil.getConsumer(username);
+
+            // TODO - check tenant availability
+            /*if (!RestApiUtil.isTenantAvailable(requestedTenantDomain)) {
+                RestApiUtil.handleBadRequest("Provided tenant domain '" + xWSO2Tenant + "' is invalid", log);
+            }*/
+
+            //if query parameter is not specified, This will search by name
+            // FIXME- move constants to a different component.
+            String searchType = APIConstants.API_NAME;
+            String searchContent = "*";
+            if (!StringUtils.isBlank(query)) {
+                String[] querySplit = query.split(":");
+                if (querySplit.length == 2 && StringUtils.isNotBlank(querySplit[0]) && StringUtils
+                        .isNotBlank(querySplit[1])) {
+                    searchType = querySplit[0];
+                    searchContent = querySplit[1];
+                } else if (querySplit.length == 1) {
+                    searchContent = query;
+                } else {
+                    RestApiUtil.handleBadRequest("Provided query parameter '" + query + "' is invalid", log);
+                }
+            }
+
+            if (searchType.equalsIgnoreCase(APIConstants.API_STATUS) && 
+                    searchContent.equalsIgnoreCase(APIConstants.PROTOTYPED)) {
+                apisMap = apiConsumer.getAllPaginatedAPIsByStatus(requestedTenantDomain, offset, limit,
+                        APIConstants.PROTOTYPED, false);
+            } else {
+                apisMap = apiConsumer
+                        .searchPaginatedAPIs(searchContent, searchType, requestedTenantDomain, offset, limit, true);
+            }
+
+            Object apisResult = apisMap.get(APIConstants.API_DATA_APIS);
+            //APIConstants.API_DATA_LENGTH is returned by executing searchPaginatedAPIs()
+            if (apisMap.containsKey(APIConstants.API_DATA_LENGTH)) {
+                size = (int) apisMap.get(APIConstants.API_DATA_LENGTH);
+            //APIConstants.API_DATA_TOT_LENGTH is returned by executing getAllPaginatedAPIsByStatus()
+            } else if (apisMap.containsKey(APIConstants.API_DATA_TOT_LENGTH)) {
+                size = (int) apisMap.get(APIConstants.API_DATA_TOT_LENGTH);
+            } else {
+                log.warn("Size could not be determined from apis GET result for query " + query);
+            }
+
+            if (apisResult != null) {
+                Set<API> apiSet = (Set)apisResult;
+                apiListDTO = APIMappingUtil.fromAPISetToDTO(apiSet);
+                APIMappingUtil.setPaginationParams(apiListDTO, query, offset, limit, size);
+            }
+
+            return Response.ok().entity(apiListDTO).build();
+        } catch (APIManagementException e) {
+            if (RestApiUtil.rootCauseMessageMatches(e, "start index seems to be greater than the limit count")) {
+                //this is not an error of the user as he does not know the total number of apis available. Thus sends 
+                //  an empty response
+                apiListDTO.setCount(0);
+                apiListDTO.setNext("");
+                apiListDTO.setPrevious("");
+                return Response.ok().entity(apiListDTO).build();
+            } else {
+                String errorMessage = "Error while retrieving APIs";
+                RestApiUtil.handleInternalServerError(errorMessage, e, log);
+            }
+        } catch (UserStoreException e) {
+            String errorMessage = "Error while checking availability of tenant " + requestedTenantDomain;
+            RestApiUtil.handleInternalServerError(errorMessage, e, log);
+        }
+        return null;
     }
 }
---
+++ rest-apis/store-api/components/org.wso2.carbon.apimgt.rest.api.store/src/main/java/org/wso2/carbon/apimgt/rest/api/store/utils/mappings/APIMappingUtil.java
@@ -0,0 +1,229 @@
+/*
+ *
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * /
+ */
+
+package org.wso2.carbon.apimgt.rest.api.store.utils.mappings;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.wso2.carbon.apimgt.api.model.API;
+import org.wso2.carbon.apimgt.api.model.APIIdentifier;
+import org.wso2.carbon.apimgt.rest.api.store.dto.APIInfoDTO;
+import org.wso2.carbon.apimgt.rest.api.store.dto.APIListDTO;
+import org.wso2.carbon.apimgt.rest.api.util.RestApiConstants;
+import org.wso2.carbon.apimgt.rest.api.util.utils.RestApiUtil;
+
+public class APIMappingUtil {
+
+   /* public static APIIdentifier getAPIIdentifierFromApiId(String apiId){
+        //if apiId contains -AT-, that need to be replaced before splitting
+        apiId = APIUtil.replaceEmailDomainBack(apiId);
+        String[] apiIdDetails = apiId.split(RestApiConstants.API_ID_DELIMITER);
+
+        if (apiIdDetails.length < 3) {
+            throw RestApiUtil.buildBadRequestException("Provided API identifier '" + apiId + "' is invalid");
+        }
+
+        // apiId format: provider-apiName-version
+        String providerName = apiIdDetails[0];
+        String apiName = apiIdDetails[1];
+        String version = apiIdDetails[2];
+        String providerNameEmailReplaced = APIUtil.replaceEmailDomain(providerName);
+        return new APIIdentifier(providerNameEmailReplaced, apiName, version);
+    }*/
+
+   /* public static APIIdentifier getAPIIdentifierFromApiIdOrUUID(String apiId, String requestedTenantDomain)
+            throws APIManagementException {
+        APIIdentifier apiIdentifier;
+        APIConsumer apiConsumer = RestApiUtil.getLoggedInUserConsumer();
+        if (RestApiUtil.isUUID(apiId)) {
+            apiIdentifier = apiConsumer.getLightweightAPIByUUID(apiId, requestedTenantDomain).getId();
+        } else {
+            apiIdentifier = apiConsumer.getLightweightAPI(getAPIIdentifierFromApiId(apiId)).getId();
+        }
+        return  apiIdentifier;
+    }*/
+
+   /* public static APIDTO fromAPItoDTO(API model) throws APIManagementException {
+
+        APIConsumer apiConsumer = RestApiUtil.getLoggedInUserConsumer();
+
+        APIDTO dto = new APIDTO();
+        dto.setName(model.getId().getApiName());
+        dto.setVersion(model.getId().getVersion());
+        String providerName = model.getId().getProviderName();
+        dto.setProvider(APIUtil.replaceEmailDomainBack(providerName));
+        dto.setId(model.getUUID());
+        dto.setContext(model.getContext());
+        dto.setDescription(model.getDescription());
+        dto.setIsDefaultVersion(model.isDefaultVersion());
+        dto.setStatus(model.getStatus().getStatus());
+
+        //Get Swagger definition which has URL templates, scopes and resource details
+        String apiSwaggerDefinition;
+
+        apiSwaggerDefinition = apiConsumer.getSwagger20Definition(model.getId());
+        apiSwaggerDefinition = RestAPIStoreUtils.removeXMediationScriptsFromSwagger(apiSwaggerDefinition);
+        dto.setApiDefinition(apiSwaggerDefinition);
+
+        Set<String> apiTags = model.getTags();
+        List<String> tagsToReturn = new ArrayList<>();
+        tagsToReturn.addAll(apiTags);
+        dto.setTags(tagsToReturn);
+
+        Set<org.wso2.carbon.apimgt.api.model.Tier> apiTiers = model.getAvailableTiers();
+        List<String> tiersToReturn = new ArrayList<>();
+        for (org.wso2.carbon.apimgt.api.model.Tier tier : apiTiers) {
+            tiersToReturn.add(tier.getName());
+        }
+        dto.setTiers(tiersToReturn);
+
+        dto.setTransport(Arrays.asList(model.getTransports().split(",")));
+
+        dto.setEndpointURLs(extractEnpointURLs(model));
+
+        APIBusinessInformationDTO apiBusinessInformationDTO = new APIBusinessInformationDTO();
+        apiBusinessInformationDTO.setBusinessOwner(model.getBusinessOwner());
+        apiBusinessInformationDTO.setBusinessOwnerEmail(model.getBusinessOwnerEmail());
+        apiBusinessInformationDTO.setTechnicalOwner(model.getTechnicalOwner());
+        apiBusinessInformationDTO.setTechnicalOwnerEmail(model.getTechnicalOwnerEmail());
+        dto.setBusinessInformation(apiBusinessInformationDTO);
+        dto.setThumbnailUrl(model.getThumbnailUrl());
+        dto.setWsdlUri(model.getWsdlUrl());
+        return dto;
+    }*/
+
+    /** Sets pagination urls for a APIListDTO object given pagination parameters and url parameters
+     * 
+     * @param apiListDTO APIListDTO object to which pagination urls need to be set 
+     * @param query query parameter
+     * @param offset starting index
+     * @param limit max number of returned objects
+     * @param size max offset
+     */
+    public static void setPaginationParams(APIListDTO apiListDTO, String query, int offset, int limit, int size) {
+        Map<String, Integer> paginatedParams = RestApiUtil.getPaginationParams(offset, limit, size);
+
+        String paginatedPrevious = "";
+        String paginatedNext = "";
+
+        if (paginatedParams.get(RestApiConstants.PAGINATION_PREVIOUS_OFFSET) != null) {
+            paginatedPrevious = RestApiUtil
+                    .getAPIPaginatedURL(paginatedParams.get(RestApiConstants.PAGINATION_PREVIOUS_OFFSET),
+                            paginatedParams.get(RestApiConstants.PAGINATION_PREVIOUS_LIMIT), query);
+        }
+
+        if (paginatedParams.get(RestApiConstants.PAGINATION_NEXT_OFFSET) != null) {
+            paginatedNext = RestApiUtil
+                    .getAPIPaginatedURL(paginatedParams.get(RestApiConstants.PAGINATION_NEXT_OFFSET),
+                            paginatedParams.get(RestApiConstants.PAGINATION_NEXT_LIMIT), query);
+        }
+
+        apiListDTO.setNext(paginatedNext);
+        apiListDTO.setPrevious(paginatedPrevious);
+    }
+
+    /** Converts an API Set object into corresponding REST API DTO
+     * 
+     * @param apiSet Set of API objects
+     * @return APIListDTO object 
+     */
+    public static APIListDTO fromAPISetToDTO(Set<API> apiSet) {
+        APIListDTO apiListDTO = new APIListDTO();
+        List<APIInfoDTO> apiInfoDTOs = apiListDTO.getList();
+        if (apiInfoDTOs == null) {
+            apiInfoDTOs = new ArrayList<>();
+            apiListDTO.setList(apiInfoDTOs);
+        }
+        for (API api : apiSet) {
+            apiInfoDTOs.add(fromAPIToInfoDTO(api));
+        }
+        apiListDTO.setCount(apiSet.size());
+
+        return apiListDTO;
+    }
+
+    /**
+     * Creates a minimal DTO representation of an API object
+     *
+     * @param api API object
+     * @return a minimal representation DTO
+     */
+    public static APIInfoDTO fromAPIToInfoDTO(API api) {
+        APIInfoDTO apiInfoDTO = new APIInfoDTO();
+        apiInfoDTO.setDescription(api.getDescription());
+        apiInfoDTO.setContext(api.getContext());
+        apiInfoDTO.setId(api.getUUID());
+        APIIdentifier apiId = api.getId();
+        apiInfoDTO.setName(apiId.getApiName());
+        apiInfoDTO.setVersion(apiId.getVersion());
+        apiInfoDTO.setProvider(apiId.getProviderName());
+        apiInfoDTO.setStatus(api.getStatus().toString());
+        String providerName = api.getId().getProviderName();
+        // TODO -- temp comment
+        //apiInfoDTO.setProvider(APIUtil.replaceEmailDomainBack(providerName));
+        return apiInfoDTO;
+    }
+
+
+   /* private static List<APIEndpointURLsDTO> extractEnpointURLs(API api) {
+        List<APIEndpointURLsDTO> apiEndpointsList = new ArrayList<>();
+
+        APIManagerConfiguration config = ServiceReferenceHolder.getInstance().getAPIManagerConfigurationService()
+                .getAPIManagerConfiguration();
+        Map<String, Environment> environments = config.getApiGatewayEnvironments();
+
+        Set<String> environmentsPublishedByAPI = new HashSet<String>(api.getEnvironments());
+        environmentsPublishedByAPI.remove("none");
+
+        Set<String> apiTransports = new HashSet<>(Arrays.asList(api.getTransports().split(",")));
+
+        for (String environmentName : environmentsPublishedByAPI) {
+            Environment environment = environments.get(environmentName);
+            if (environment != null) {
+                APIEnvironmentURLsDTO environmentURLsDTO = new APIEnvironmentURLsDTO();
+                String[] gwEndpoints = environment.getApiGatewayEndpoint().split(",");
+
+                for (String gwEndpoint : gwEndpoints) {
+                    StringBuilder endpointBuilder = new StringBuilder(gwEndpoint);
+                    endpointBuilder.append('/');
+                    endpointBuilder.append(api.getContext());
+
+                    if (gwEndpoint.contains("http:") && apiTransports.contains("http")) {
+                        environmentURLsDTO.setHttp(endpointBuilder.toString());
+                    }
+                    else if (gwEndpoint.contains("https:") && apiTransports.contains("https")) {
+                        environmentURLsDTO.setHttps(endpointBuilder.toString());
+                    }
+                }
+
+                APIEndpointURLsDTO apiEndpointURLsDTO = new APIEndpointURLsDTO();
+                apiEndpointURLsDTO.setEnvironmentURLs(environmentURLsDTO);
+
+                apiEndpointURLsDTO.setEnvironmentName(environment.getName());
+                apiEndpointURLsDTO.setEnvironmentType(environment.getType());
+
+                apiEndpointsList.add(apiEndpointURLsDTO);
+            }
+        }
+
+        return apiEndpointsList;
+    }*/
+}
