6cbd2548c7cd40af648be613f32415b261446994
fazlan-nazeem <fazlann@wso2.com>
2016-11-01 15:47:03 +0530

    removing db and sample app impl

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/main/java/org/wso2/carbon/apimgt/rest/api/store/impl/ApplicationsApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/main/java/org/wso2/carbon/apimgt/rest/api/store/impl/ApplicationsApiServiceImpl.java
@@ -1,29 +1,50 @@
 package org.wso2.carbon.apimgt.rest.api.store.impl;
 
-import org.wso2.carbon.apimgt.rest.api.store.*;
-import org.wso2.carbon.apimgt.rest.api.store.dto.*;
-
-
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.apimgt.core.api.APIStore;
+import org.wso2.carbon.apimgt.core.exception.APIManagementException;
+import org.wso2.carbon.apimgt.core.impl.APIManagerFactory;
+import org.wso2.carbon.apimgt.core.models.Application;
+import org.wso2.carbon.apimgt.rest.api.store.ApiResponseMessage;
+import org.wso2.carbon.apimgt.rest.api.store.ApplicationsApiService;
 import org.wso2.carbon.apimgt.rest.api.store.NotFoundException;
+import org.wso2.carbon.apimgt.rest.api.store.dto.ApplicationDTO;
+import org.wso2.carbon.apimgt.rest.api.store.dto.ApplicationKeyGenerateRequestDTO;
+import org.wso2.carbon.apimgt.rest.api.store.utils.RestAPIStoreUtils;
+import org.wso2.carbon.apimgt.rest.api.store.utils.RestApiConstants;
+import org.wso2.carbon.apimgt.rest.api.store.utils.RestApiUtil;
 
-import java.io.InputStream;
-
-import org.wso2.msf4j.formparam.FormDataParam;
-import org.wso2.msf4j.formparam.FileInfo;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 
 @javax.annotation.Generated(value = "class org.wso2.maven.plugins.JavaMSF4JServerCodegen", date = "2016-11-01T13:48:55.078+05:30")
 public class ApplicationsApiServiceImpl extends ApplicationsApiService {
+
+    private static final Logger log = LoggerFactory.getLogger(ApplicationsApiServiceImpl.class);
+
     @Override
-    public Response applicationsApplicationIdDelete(String applicationId
-, String ifMatch
-, String ifUnmodifiedSince
- ) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    public Response applicationsApplicationIdDelete(String applicationId, String ifMatch, String ifUnmodifiedSince)
+            throws NotFoundException {
+        String username = RestApiUtil.getLoggedInUsername();
+        try {
+            APIStore apiConsumer = APIManagerFactory.getInstance().getAPIConsumer(username);
+            Application application = apiConsumer.getApplicationByUUID(applicationId);
+            if (application != null) {
+                if (RestAPIStoreUtils.isUserAccessAllowedForApplication(application)) {
+                    apiConsumer.removeApplication(application);
+                    return Response.ok().build();
+                } else {
+                    RestApiUtil.handleAuthorizationFailure(RestApiConstants.RESOURCE_APPLICATION, applicationId, log);
+                }
+            } else {
+                RestApiUtil.handleResourceNotFoundError(RestApiConstants.RESOURCE_APPLICATION, applicationId, log);
+            }
+        } catch (APIManagementException e) {
+            RestApiUtil.handleInternalServerError("Error while deleting application " + applicationId, e, log);
+        }
+        return null;
     }
+
     @Override
     public Response applicationsApplicationIdGet(String applicationId
 , String accept
