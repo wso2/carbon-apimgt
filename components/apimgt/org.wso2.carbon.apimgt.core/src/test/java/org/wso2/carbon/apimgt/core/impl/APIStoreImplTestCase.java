8f22845572f0d1f873da354d3cebf2ccef83015a
fazlan-nazeem <fazlann@wso2.com>
2016-11-10 11:43:44 +0530

    testGetApplicationById Test case

--- components/apimgt/org.wso2.carbon.apimgt.core/src/test/java/org/wso2/carbon/apimgt/core/impl/APIStoreImplTestCase.java
+++ components/apimgt/org.wso2.carbon.apimgt.core/src/test/java/org/wso2/carbon/apimgt/core/impl/APIStoreImplTestCase.java
@@ -20,12 +20,6 @@
 
 package org.wso2.carbon.apimgt.core.impl;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.atLeastOnce;
-
 import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -43,6 +37,9 @@ import org.wso2.carbon.apimgt.core.exception.APIManagementException;
 import org.wso2.carbon.apimgt.core.models.API;
 import org.wso2.carbon.apimgt.core.models.APISummary;
 import org.wso2.carbon.apimgt.core.models.APISummaryResults;
+import org.wso2.carbon.apimgt.core.models.Application;
+
+import static org.mockito.Mockito.*;
 
 /**
  * Test class for APIStore
@@ -102,6 +99,20 @@ public class APIStoreImplTestCase {
             InputStream docContent = apiStore.getDocumentationContent("");
         } catch (APIManagementException e) {
             Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testGetApplicationById () {
+        try {
+            Application applicationFromDAO = new Application("username", null);
+            when(applicationDAO.getApplicationById(1)).thenReturn(applicationFromDAO);
+
+            Application application = apiStore.getApplicationById(1);
+            Assert.assertNotNull(application);
+            verify(applicationDAO, times(1)).getApplicationById(1);
+        } catch (APIManagementException | SQLException e) {
+            Assert.assertTrue(false);
         }
     }
     
