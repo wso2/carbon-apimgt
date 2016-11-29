b42cc109f1d7ab83f71efa6a4aff032da21aeb80
rswijesena <roshan86web@gmail.com>
2016-11-29 13:19:49 +0530

    Exception handling initial commits

--- components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/exception/ExceptionCodes.java
+++ components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/exception/ExceptionCodes.java
@@ -24,23 +24,38 @@ package org.wso2.carbon.apimgt.core.exception;
  * This enum class holds error codes that we need to pass to upper level. For example, to the UI.
  * You have to define your custom error codes here.
  */
-public enum ExceptionCodes {
+public enum ExceptionCodes implements ErrorHandler {
 
-    API_ALREADY_EXISTS(900300, "The API already exists.");
+    API_ALREADY_EXISTS(900300, "The API already exists.", 400, " This is error description"),
+    APIMGT_DAO_EXCEPTION(900301, "Something wrong in DAO layer.", 500, " This is error description"),
+    APIMGT_LIFECYCLE_EXCEPTION(900302, "Life cycle exception occurred", 500, " This is error description");
 
     private final long errorCode;
     private final String errorMessage;
+    private final int httpStatusCode;
+    private final String errorDescription;
 
-    ExceptionCodes(long errorCode, String msg) {
+    ExceptionCodes(long errorCode, String msg, int httpErrorCode, String errorDescription) {
         this.errorCode = errorCode;
         this.errorMessage = msg;
+        this.httpStatusCode = httpErrorCode;
+        this.errorDescription = errorDescription;
     }
 
     public long getErrorCode() {
         return this.errorCode;
     }
 
-    public String getMsg() {
+    public String getErrorMessage() {
         return this.errorMessage;
     }
+
+    public int getHttpStatusCode() {
+        return this.httpStatusCode;
+    }
+
+    public String getErrorDescription() {
+        return this.errorDescription;
+    }
+
 }
