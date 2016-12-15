5fd5026e8201d4cd2413bd4ed576ed0a2c79bda5
lalaji <lalaji@wso2.com>
2016-12-15 12:40:08 +0530

    Fixed exception handling

--- components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/api/KeyManager.java
+++ components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/api/KeyManager.java
@@ -25,7 +25,7 @@ import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.wso2.carbon.apimgt.core.exception.KeyManagerException;
+import org.wso2.carbon.apimgt.core.exception.KeyManagementException;
 
 import org.wso2.carbon.apimgt.core.models.API;
 import org.wso2.carbon.apimgt.core.models.AccessTokenInfo;
@@ -59,7 +59,7 @@ public interface KeyManager {
      * @param oauthAppRequest - this object contains values of oAuth app properties.
      * @return OAuthApplicationInfo object with oAuthApplication properties.
      */
-    OAuthApplicationInfo createApplication(OAuthAppRequest oauthAppRequest) throws KeyManagerException;
+    OAuthApplicationInfo createApplication(OAuthAppRequest oauthAppRequest) throws KeyManagementException;
 
 
     /**
@@ -68,14 +68,14 @@ public interface KeyManager {
      * @param appInfoDTO accept an appinfoDTO object
      * @return OAuthApplicationInfo this object will  contain all the properties of updated oAuth application
      */
-    OAuthApplicationInfo updateApplication(OAuthAppRequest appInfoDTO) throws KeyManagerException;
+    OAuthApplicationInfo updateApplication(OAuthAppRequest appInfoDTO) throws KeyManagementException;
 
     /**
      * Delete auth application
      *
      * @param consumerKey - will take consumer key as parameter
      */
-    void deleteApplication(String consumerKey) throws KeyManagerException;
+    void deleteApplication(String consumerKey) throws KeyManagementException;
 
     /**
      * Populate auth application.this will fetch data from oAuth server and will save properties to a java object
@@ -83,7 +83,7 @@ public interface KeyManager {
      * @param consumerKey will take consumer key as parameter
      * @return json string
      */
-    OAuthApplicationInfo retrieveApplication(String consumerKey) throws KeyManagerException;
+    OAuthApplicationInfo retrieveApplication(String consumerKey) throws KeyManagementException;
 
     /**
      * Store calls this method to get a new Application Access Token. This will be called when getting the token for
@@ -91,9 +91,9 @@ public interface KeyManager {
      *
      * @param tokenRequest AccessTokenRequest which encapsulates parameters sent from UI.
      * @return Details of the Generated Token. AccessToken and Validity period are a must.
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
-    AccessTokenInfo getNewApplicationAccessToken(AccessTokenRequest tokenRequest) throws KeyManagerException;
+    AccessTokenInfo getNewApplicationAccessToken(AccessTokenRequest tokenRequest) throws KeyManagementException;
 
     /**
      * Get details about an access token. As a part of the response, consumer key against which token was obtained
@@ -101,30 +101,30 @@ public interface KeyManager {
      *
      * @return {@code AccessTokenInfo}
      */
-    AccessTokenInfo getTokenMetaData(String accessToken) throws KeyManagerException;
+    AccessTokenInfo getTokenMetaData(String accessToken) throws KeyManagementException;
 
     /**
      * Key manager implementation should be read from hardcoded json file
      *
      * @return {@code KeyManagerConfiguration}
      */
-    KeyManagerConfiguration getKeyManagerConfiguration() throws KeyManagerException;
+    KeyManagerConfiguration getKeyManagerConfiguration() throws KeyManagementException;
 
     /**
      * @param jsonInput this jsonInput will contain set of oAuth application properties.
      * @return OAuthApplicationInfo object will return after parsed jsonInput
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
-    OAuthApplicationInfo buildFromJSON(String jsonInput) throws KeyManagerException;
+    OAuthApplicationInfo buildFromJSON(String jsonInput) throws KeyManagementException;
 
     /**
      * @param authApplicationInfo
      * @param jsonInput           this jsonInput will contain set of oAuth application properties.
      * @return OAuthApplicationInfo object will return after parsed jsonInput
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
     default OAuthApplicationInfo buildFromJSON(OAuthApplicationInfo authApplicationInfo, String jsonInput) throws
-            KeyManagerException {
+            KeyManagementException {
         //initiate json parser.
         JSONParser parser = new JSONParser();
         JSONObject jsonObject;
@@ -145,7 +145,7 @@ public interface KeyManager {
                 return authApplicationInfo;
             }
         } catch (ParseException e) {
-            throw new KeyManagerException("Error occurred while parsing JSON String", e);
+            throw new KeyManagementException("Error occurred while parsing JSON String", e);
         }
         return null;
     }
@@ -158,11 +158,11 @@ public interface KeyManager {
      * @param jsonInput    Input as a JSON. This is the same JSON passed from Store UI.
      * @param tokenRequest Object encapsulating parameters sent from UI.
      * @return If input AccessTokenRequest is null, a new object will be returned,
-     *         else the additional parameters will be added to the input object passed.
-     * @throws KeyManagerException
+     * else the additional parameters will be added to the input object passed.
+     * @throws KeyManagementException
      */
     default AccessTokenRequest buildAccessTokenRequestFromJSON(String jsonInput, AccessTokenRequest tokenRequest)
-            throws KeyManagerException {
+            throws KeyManagementException {
         if (jsonInput == null || jsonInput.isEmpty()) {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("JsonInput is null or Empty.");
@@ -202,7 +202,7 @@ public interface KeyManager {
                 return tokenRequest;
             }
         } catch (ParseException e) {
-            throw new KeyManagerException("Error occurred while parsing JSON String", e);
+            throw new KeyManagementException("Error occurred while parsing JSON String", e);
         }
         return null;
 
@@ -214,9 +214,9 @@ public interface KeyManager {
      *
      * @param appInfoRequest
      * @return OAuthApplicationInfo with oAuth application properties.
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
-    OAuthApplicationInfo mapOAuthApplication(OAuthAppRequest appInfoRequest) throws KeyManagerException;
+    OAuthApplicationInfo mapOAuthApplication(OAuthAppRequest appInfoRequest) throws KeyManagementException;
 
     /**
      * This method will create an AccessTokenRequest using OAuthApplicationInfo object. If tokenRequest is null,
@@ -228,7 +228,7 @@ public interface KeyManager {
      */
     default AccessTokenRequest buildAccessTokenRequestFromOAuthApp(OAuthApplicationInfo oAuthApplication,
                                                                    AccessTokenRequest tokenRequest)
-            throws KeyManagerException {
+            throws KeyManagementException {
         if (oAuthApplication == null) {
             return tokenRequest;
         }
@@ -237,7 +237,7 @@ public interface KeyManager {
         }
 
         if (oAuthApplication.getClientId() == null || oAuthApplication.getClientSecret() == null) {
-            throw new KeyManagerException("Consumer key or Consumer Secret missing.");
+            throw new KeyManagementException("Consumer key or Consumer Secret missing.");
         }
         tokenRequest.setClientId(oAuthApplication.getClientId());
         tokenRequest.setClientSecret(oAuthApplication.getClientSecret());
@@ -258,7 +258,7 @@ public interface KeyManager {
         return tokenRequest;
     }
 
-    void loadConfiguration(KeyManagerConfiguration configuration) throws KeyManagerException;
+    void loadConfiguration(KeyManagerConfiguration configuration) throws KeyManagementException;
 
     /**
      * This Method will talk to APIResource registration end point  of  authorization server and creates a new resource
@@ -266,19 +266,19 @@ public interface KeyManager {
      * @param api                this is a API object which contains all details about a API.
      * @param resourceAttributes this param will contains additional details if required.
      * @return true if sucessfully registered. false if there is a error while registering a new resource.
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
 
-    boolean registerNewResource(API api, Map resourceAttributes) throws KeyManagerException;
+    boolean registerNewResource(API api, Map resourceAttributes) throws KeyManagementException;
 
     /**
      * This method will be used to retrieve registered resource by given API ID.
      *
      * @param apiId APIM api id.
      * @return It will return a Map with registered resource details.
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
-    Map getResourceByApiId(String apiId) throws KeyManagerException;
+    Map getResourceByApiId(String apiId) throws KeyManagementException;
 
     /**
      * This method is responsible for update given APIResource  by its resourceId.
@@ -286,34 +286,34 @@ public interface KeyManager {
      * @param api                this is a API object which contains all details about a API.
      * @param resourceAttributes this param will contains additional details if required.
      * @return TRUE|FALSE. if it is successfully updated it will return TRUE or else FALSE.
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
-    boolean updateRegisteredResource(API api, Map resourceAttributes) throws KeyManagerException;
+    boolean updateRegisteredResource(API api, Map resourceAttributes) throws KeyManagementException;
 
     /**
      * This method will accept API id  as a parameter  and will delete the registered resource.
      *
      * @param apiID API id.
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
-    void deleteRegisteredResourceByAPIId(String apiID) throws KeyManagerException;
+    void deleteRegisteredResourceByAPIId(String apiID) throws KeyManagementException;
 
     /**
      * This method will be used to delete mapping records of oAuth applications.
      *
      * @param consumerKey
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
-    void deleteMappedApplication(String consumerKey) throws KeyManagerException;
+    void deleteMappedApplication(String consumerKey) throws KeyManagementException;
 
     /**
      * When provided the ConsumerKey, this method will provide all the Active tokens issued against that Key.
      *
      * @param consumerKey ConsumerKey of the OAuthClient
      * @return {@link java.util.Set} having active access tokens.
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
-    Set<String> getActiveTokensByConsumerKey(String consumerKey) throws KeyManagerException;
+    Set<String> getActiveTokensByConsumerKey(String consumerKey) throws KeyManagementException;
 
 
     /**
@@ -321,9 +321,9 @@ public interface KeyManager {
      *
      * @param consumerKey
      * @return {@link org.wso2.carbon.apimgt.core.models.AccessTokenInfo} populating all the details of the AccessToken.
-     * @throws KeyManagerException
+     * @throws KeyManagementException
      */
-    AccessTokenInfo getAccessTokenByConsumerKey(String consumerKey) throws KeyManagerException;
+    AccessTokenInfo getAccessTokenByConsumerKey(String consumerKey) throws KeyManagementException;
 
 }
 
