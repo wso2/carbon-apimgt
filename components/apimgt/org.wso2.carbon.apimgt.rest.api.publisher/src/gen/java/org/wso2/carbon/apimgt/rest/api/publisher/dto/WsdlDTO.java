f534e212cf57d8484a01ee335c4046764cad97ce
Malintha Amarasinghe <malintha.prasan@gmail.com>
2017-01-13 00:25:17 +0530

    Adding wsdl api fixes

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/dto/WsdlDTO.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/dto/WsdlDTO.java
@@ -14,26 +14,11 @@ import javax.validation.constraints.NotNull;
 public class WsdlDTO  {
   
   
-  
-  private String id = null;
-  
   @NotNull
   private String name = null;
   
-  @NotNull
-  private String config = null;
-
   
-  /**
-   **/
-  @ApiModelProperty(value = "")
-  @JsonProperty("id")
-  public String getId() {
-    return id;
-  }
-  public void setId(String id) {
-    this.id = id;
-  }
+  private String wsdlDefinition = null;
 
   
   /**
@@ -50,13 +35,13 @@ public class WsdlDTO  {
   
   /**
    **/
-  @ApiModelProperty(required = true, value = "")
-  @JsonProperty("config")
-  public String getConfig() {
-    return config;
+  @ApiModelProperty(value = "")
+  @JsonProperty("wsdlDefinition")
+  public String getWsdlDefinition() {
+    return wsdlDefinition;
   }
-  public void setConfig(String config) {
-    this.config = config;
+  public void setWsdlDefinition(String wsdlDefinition) {
+    this.wsdlDefinition = wsdlDefinition;
   }
 
   
@@ -66,9 +51,8 @@ public class WsdlDTO  {
     StringBuilder sb = new StringBuilder();
     sb.append("class WsdlDTO {\n");
     
-    sb.append("  id: ").append(id).append("\n");
     sb.append("  name: ").append(name).append("\n");
-    sb.append("  config: ").append(config).append("\n");
+    sb.append("  wsdlDefinition: ").append(wsdlDefinition).append("\n");
     sb.append("}\n");
     return sb.toString();
   }
