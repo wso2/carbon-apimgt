36130f197beedd7b4a1fec7df6b82c5f5cddf849
Abimaran Kugathasan <abimaran@wso2.com>
2017-01-19 15:46:38 +0530

    Added admin REST API released initial file files

---
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/gen/java/org/wso2/carbon/apimgt/rest/api/admin/NotFoundException.java
@@ -0,0 +1,10 @@
+package org.wso2.carbon.apimgt.rest.api.admin;
+
+@javax.annotation.Generated(value = "org.wso2.maven.plugins.JavaMSF4JServerCodegen", date = "2017-01-19T14:57:15.952+05:30")
+public class NotFoundException extends ApiException {
+    private int code;
+    public NotFoundException (int code, String msg) {
+        super(code, msg);
+        this.code = code;
+    }
+}
