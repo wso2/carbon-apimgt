24906fcc67569d8362e21ed25c95cd993a3f9fc7
Abimaran Kugathasan <abimaran@wso2.com>
2017-01-30 13:56:24 +0530

    Code refactored

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/PoliciesApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/PoliciesApiServiceImpl.java
@@ -17,60 +17,43 @@ import javax.ws.rs.core.SecurityContext;
 @javax.annotation.Generated(value = "class org.wso2.maven.plugins.JavaMSF4JServerCodegen", date = "2017-01-03T20:31:12.997+05:30")
 public class PoliciesApiServiceImpl extends PoliciesApiService {
     @Override
-    public Response policiesTierLevelGet(String tierLevel
-, Integer limit
-, Integer offset
-, String accept
-, String ifNoneMatch
- ) throws NotFoundException {
+    public Response policiesTierLevelGet(String tierLevel, Integer limit, Integer offset, String accept,
+                                         String ifNoneMatch) throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
+
     @Override
-    public Response policiesTierLevelPost(TierDTO body
-, String tierLevel
-, String contentType
- ) throws NotFoundException {
+    public Response policiesTierLevelPost(TierDTO body, String tierLevel, String contentType) throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
+
     @Override
-    public Response policiesTierLevelTierNameDelete(String tierName
-, String tierLevel
-, String ifMatch
-, String ifUnmodifiedSince
- ) throws NotFoundException {
+    public Response policiesTierLevelTierNameDelete(String tierName, String tierLevel, String ifMatch,
+                                                    String ifUnmodifiedSince) throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
+
     @Override
-    public Response policiesTierLevelTierNameGet(String tierName
-, String tierLevel
-, String accept
-, String ifNoneMatch
-, String ifModifiedSince
- ) throws NotFoundException {
+    public Response policiesTierLevelTierNameGet(String tierName, String tierLevel, String accept, String ifNoneMatch,
+                                                 String ifModifiedSince) throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
+
     @Override
-    public Response policiesTierLevelTierNamePut(String tierName
-, TierDTO body
-, String tierLevel
-, String contentType
-, String ifMatch
-, String ifUnmodifiedSince
- ) throws NotFoundException {
+    public Response policiesTierLevelTierNamePut(String tierName, TierDTO body, String tierLevel, String contentType,
+                                                 String ifMatch, String ifUnmodifiedSince) throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
+
     @Override
-    public Response policiesUpdatePermissionPost(String tierName
-, String tierLevel
-, String ifMatch
-, String ifUnmodifiedSince
-, TierPermissionDTO permissions
- ) throws NotFoundException {
+    public Response policiesUpdatePermissionPost(String tierName, String tierLevel, String ifMatch,
+                                                 String ifUnmodifiedSince, TierPermissionDTO permissions)
+            throws NotFoundException {
         // do some magic!
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
