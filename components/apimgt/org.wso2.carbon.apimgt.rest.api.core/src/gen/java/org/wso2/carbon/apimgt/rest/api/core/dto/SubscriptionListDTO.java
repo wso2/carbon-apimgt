d8e4583aa3c44ab44bdaed15d8d0c96bfc539bd6
ishara.cooray@gmail.com <ishara.cooray@gmail.com>
2017-01-31 14:25:25 +0530

    latest changes - context loading

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.core/src/gen/java/org/wso2/carbon/apimgt/rest/api/core/dto/SubscriptionListDTO.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.core/src/gen/java/org/wso2/carbon/apimgt/rest/api/core/dto/SubscriptionListDTO.java
@@ -12,9 +12,8 @@ import org.wso2.carbon.apimgt.rest.api.core.dto.SubscriptionDTO;
 /**
  * SubscriptionListDTO
  */
-@javax.annotation.Generated(value = "class org.wso2.maven.plugins.JavaMSF4JServerCodegen", date = "2017-01-06T17:54:33.855+05:30")
+@javax.annotation.Generated(value = "class org.wso2.maven.plugins.JavaMSF4JServerCodegen", date = "2017-01-16T15:55:32.437+05:30")
 public class SubscriptionListDTO   {
-  @JsonProperty("list")
   private List<SubscriptionDTO> list = new ArrayList<SubscriptionDTO>();
 
   public SubscriptionListDTO list(List<SubscriptionDTO> list) {
