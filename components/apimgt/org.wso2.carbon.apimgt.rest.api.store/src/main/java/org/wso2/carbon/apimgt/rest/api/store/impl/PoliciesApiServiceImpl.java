bffab09246ea4dea069b1c6cfc77ea57c1b3eb44
anuruddhal <anuruddhal@wso2.com>
2017-02-13 16:48:54 +0530

    Add PoliciesAPI implementation

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/main/java/org/wso2/carbon/apimgt/rest/api/store/impl/PoliciesApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/main/java/org/wso2/carbon/apimgt/rest/api/store/impl/PoliciesApiServiceImpl.java
@@ -1,24 +1,65 @@
 package org.wso2.carbon.apimgt.rest.api.store.impl;
 
-import org.wso2.carbon.apimgt.rest.api.store.ApiResponseMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.apimgt.core.api.APIStore;
+import org.wso2.carbon.apimgt.core.exception.APIManagementException;
+import org.wso2.carbon.apimgt.core.models.policy.Policy;
+import org.wso2.carbon.apimgt.core.util.APIMgtConstants;
+import org.wso2.carbon.apimgt.rest.api.common.dto.ErrorDTO;
+import org.wso2.carbon.apimgt.rest.api.common.util.RestApiUtil;
 import org.wso2.carbon.apimgt.rest.api.store.NotFoundException;
 import org.wso2.carbon.apimgt.rest.api.store.PoliciesApiService;
+import org.wso2.carbon.apimgt.rest.api.store.dto.TierDTO;
+import org.wso2.carbon.apimgt.rest.api.store.dto.TierListDTO;
+import org.wso2.carbon.apimgt.rest.api.store.mappings.TierMappingUtil;
 
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.List;
 
 @javax.annotation.Generated(value = "org.wso2.maven.plugins.JavaMSF4JServerCodegen", date = "2017-02-09T12:36:56.084+05:30")
 public class PoliciesApiServiceImpl extends PoliciesApiService {
+
+    private static final Logger log = LoggerFactory.getLogger(PoliciesApiServiceImpl.class);
+
     @Override
     public Response policiesTierLevelGet(String tierLevel, Integer limit, Integer offset, String accept,
                                          String ifNoneMatch, String minorVersion) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        TierListDTO tierListDTO = null;
+        String username = RestApiUtil.getLoggedInUsername();
+        try {
+            APIStore apiStore = RestApiUtil.getConsumer(username);
+            List<Policy> tierList = apiStore.getPolicies(tierLevel);
+            tierListDTO = TierMappingUtil.fromTierListToDTO(tierList, tierLevel, limit, offset);
+        } catch (APIManagementException e) {
+            String errorMessage = "Error while retrieving tiers";
+            HashMap<String, String> paramList = new HashMap<String, String>();
+            paramList.put(APIMgtConstants.ExceptionsConstants.TIER_LEVEL, tierLevel);
+            ErrorDTO errorDTO = RestApiUtil.getErrorDTO(e.getErrorHandler(), paramList);
+            log.error(errorMessage, e);
+            return Response.status(e.getErrorHandler().getHttpStatusCode()).entity(errorDTO).build();
+        }
+        return Response.ok().entity(tierListDTO).build();
     }
 
     @Override
     public Response policiesTierLevelTierNameGet(String tierName, String tierLevel, String accept, String ifNoneMatch,
                                                  String ifModifiedSince, String minorVersion) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        TierDTO tierDTO = null;
+        String username = RestApiUtil.getLoggedInUsername();
+        try {
+            APIStore apiStore = RestApiUtil.getConsumer(username);
+            Policy tier = apiStore.getPolicy(tierLevel, tierName);
+            tierDTO = TierMappingUtil.fromTierToDTO(tier, tierLevel);
+        } catch (APIManagementException e) {
+            String errorMessage = "Error while retrieving tier";
+            HashMap<String, String> paramList = new HashMap<String, String>();
+            paramList.put(APIMgtConstants.ExceptionsConstants.TIER_LEVEL, tierLevel);
+            ErrorDTO errorDTO = RestApiUtil.getErrorDTO(e.getErrorHandler(), paramList);
+            log.error(errorMessage, e);
+            return Response.status(e.getErrorHandler().getHttpStatusCode()).entity(errorDTO).build();
+        }
+        return Response.ok().entity(tierDTO).build();
     }
 }
