e9dd2c2e181ae7255331fce33db0f2a563936c38
harsha89 <harsz89@gmail.com>
2017-02-21 18:37:27 +0530

    fixing add edit update applications

--- components/web/apps/org.wso2.carbon.apimgt.store/src/main/public/js/applications/{id}/edit.js
+++ components/web/apps/org.wso2.carbon.apimgt.store/src/main/public/js/applications/{id}/edit.js
@@ -3,6 +3,7 @@ function tierChanged(element) {
     var selectedDesc = $("#tierDescriptions").val().split(",")[index];
     $("#tierHelpStr em").text(selectedDesc);
 }
+var applicationClient;
 $(function () {
 
     $(".navigation ul li.active").removeClass('active');
@@ -21,44 +22,48 @@ $(function () {
             ({"tierLevel": "application"},
                 function (jsonData) {
                     tierList = jsonData.obj.list;
+                    applicationClient = new SwaggerClient({
+                        url: swaggerURL + "applications",
+                        success: function (swaggerData) {
+
+                            var applicationId = $("#applicationId").val();
+                            setAuthHeader(applicationClient);
+                            applicationClient["Application (individual)"].get_applications_applicationId
+                            ({"applicationId": applicationId},
+                                function (jsonData) {
+
+                                    var application = jsonData.obj;
+                                    var context = {};
+                                    for (var i=0; i < tierList.length; i++) {
+                                        if(tierList[i].name == application.name) {
+                                            tierList[i].selected = true;
+                                        }
+                                    }
+                                    console.log(tierList);
+                                    context.appTiers = tierList;
+                                    context.application = application;
+
+                                    $.get('/store/public/components/root/base/templates/applications/editAppContentTemplate.hbs', function (templateData) {
+                                        var template = Handlebars.compile(templateData);
+                                        // Inject data into template
+                                        var theCompiledHtml = template(context);
+
+                                        // Add the compiled html to the page
+                                        $('#editAppContent').html(theCompiledHtml)
+                                    }, 'html');
+
+                                },
+                                function (error) {
+                                    console.log('failed with the following: ' + error.statusText);
+                                    if(error.status==401){
+                                        redirectToLogin(contextPath);
+                                    }
+                                });
+                        }
+                    });
                 },
                 function (error) {
                     console.log('failed with the following: ' + error.statusText);
-                });
-        }
-    });
-
-
-    var applicationClient = new SwaggerClient({
-        url: swaggerURL + "applications",
-        success: function (swaggerData) {
-
-            var applicationId = $("#applicationId").val();
-            setAuthHeader(applicationClient);
-            applicationClient["Application (individual)"].get_applications_applicationId
-            ({"applicationId": applicationId},
-                function (jsonData) {
-
-                    var application = jsonData.obj;
-                    var context = {};
-                    context.appTiers = tierList;
-                    context.application = application;
-
-                    $.get('/store/public/components/root/base/templates/applications/editAppContentTemplate.hbs', function (templateData) {
-                        var template = Handlebars.compile(templateData);
-                        // Inject data into template
-                        var theCompiledHtml = template(context);
-
-                        // Add the compiled html to the page
-                        $('#editAppContent').html(theCompiledHtml)
-                    }, 'html');
-
-                },
-                function (error) {
-                    console.log('failed with the following: ' + error.statusText);
-                    if(error.status==401){
-                        redirectToLogin(contextPath);
-                    }
                 });
         }
     });
@@ -75,9 +80,11 @@ $(function () {
         application.name = $("#application-name").val();
         application.throttlingTier = $("#appTier").val();
         application.description = $("#description").val();
+        application.lifeCycleStatus = $("#status").val();
 
-        applicationClient.default.applicationsApplicationIdPut({
-                "applicationId":applicationId,
+        applicationClient["Application (individual)"].put_applications_applicationId
+            ({
+                "applicationId": applicationId,
                 "body": application,
                 "Content-Type": "application/json"
             },
