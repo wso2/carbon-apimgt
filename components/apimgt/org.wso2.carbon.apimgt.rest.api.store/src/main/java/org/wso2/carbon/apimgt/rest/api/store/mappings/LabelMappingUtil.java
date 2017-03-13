a333e3f7feee8da9460459a8533cf4b61da3da71
Pubudu Gunatilaka <pubudu538@gmail.com>
2017-03-13 12:39:56 +0530

    Adding mapping utils for labels in store

---
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/main/java/org/wso2/carbon/apimgt/rest/api/store/mappings/LabelMappingUtil.java
@@ -0,0 +1,72 @@
+/*
+* Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.wso2.carbon.apimgt.rest.api.store.mappings;
+
+import org.wso2.carbon.apimgt.core.models.Label;
+import org.wso2.carbon.apimgt.rest.api.store.dto.LabelDTO;
+import org.wso2.carbon.apimgt.rest.api.store.dto.LabelInfoListDTO;
+import org.wso2.carbon.apimgt.rest.api.store.dto.LabelListDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LabelMappingUtil {
+
+    /** Converts a LabelInfoListDTO to a List<String> of label names
+     *
+     * @param labelInfoListDTO LabelInfoList which contains label names
+     * @return List<String> List of label names
+     */
+    public static List<String> fromLabelInfoListDTO(LabelInfoListDTO labelInfoListDTO) {
+        List<String> labelNames = new ArrayList<>();
+
+        for (int i = 0; i < labelInfoListDTO.getLabels().size(); i++) {
+            labelNames.add(labelInfoListDTO.getLabels().get(i));
+        }
+
+        return labelNames;
+    }
+
+    /**
+     * Convert {@link List<Label>} to {@link LabelListDTO}
+     *
+     * @param labels List of Labels
+     * @return LabelListDTO
+     */
+    public static LabelListDTO toLabelListDTO(List<Label> labels) {
+        LabelListDTO labelListDTO = new LabelListDTO();
+        labelListDTO.setCount(labels.size());
+        labelListDTO.setList(toLabelDTO(labels));
+        return labelListDTO;
+    }
+
+    /**
+     * Converts {@link Label} List to an {@link LabelListDTO} List.
+     *
+     * @param labels List of Labels
+     * @return List of LabelDTOs
+     */
+    private static List<LabelDTO> toLabelDTO(List<Label> labels) {
+        List<LabelDTO> labelDTOs = new ArrayList<>();
+        for (Label label : labels) {
+            LabelDTO labelDTO = new LabelDTO();
+            labelDTO.setName(label.getName());
+            labelDTO.setAccessUrl(label.getAccessUrl());
+            labelDTOs.add(labelDTO);
+        }
+        return labelDTOs;
+    }
+}
