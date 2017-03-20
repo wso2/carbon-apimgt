e63e85982cf8c2a68afa99adad26091ab2df8056
Abimaran Kugathasan <abimaran@wso2.com>
2017-03-20 14:48:44 +0530

    Added action buttons to edit/delete policies

--- components/web/apps/org.wso2.carbon.apimgt.admin.app/src/main/public/js/throttling/advanced-throttling/index.js
+++ components/web/apps/org.wso2.carbon.apimgt.admin.app/src/main/public/js/throttling/advanced-throttling/index.js
@@ -42,9 +42,48 @@ $(function () {
             },
             columns: [
                 {'data': 'name'},
-                {'data': 'description'}
+                {'data': 'description'},
+                {'data': 'name'}
+            ],
+            columnDefs: [
+                {
+                    targets: ["policy-listing-action"], //class name will be matched on the TH for the column
+                    searchable: false,
+                    sortable: false,
+                    render: _renderActionButtons // Method to render the action buttons per row
+                }
             ]
         })
+    }
+
+    function _renderActionButtons(data, type, row) {
+        if (type === "display") {
+
+            var editIcon1 = $("<i>").addClass("fw fw-ring fw-stack-2x");
+            var editIcon2 = $("<i>").addClass("fw fw-edit fw-stack-1x");
+            var editSpanIcon = $("<span>").addClass("fw-stack").append(editIcon1).append(editIcon2);
+            var editSpanText = $("<span>").addClass("hidden-xs").text("Edit");
+            var edit_button = $('<a>', {
+                id: data.id,
+                href: contextPath + '/applications/' + data + '/edit',
+                title: 'Edit'
+            })
+                    .addClass("btn  btn-sm padding-reduce-on-grid-view")
+                    .append(editSpanIcon)
+                    .append(editSpanText);
+
+            var deleteIcon1 = $("<i>").addClass("fw fw-ring fw-stack-2x");
+            var deleteIcon2 = $("<i>").addClass("fw fw-delete fw-stack-1x");
+            var deleteSpanIcon = $("<span>").addClass("fw-stack").append(deleteIcon1).append(deleteIcon2);
+            var deleteSpanText = $("<span>").addClass("hidden-xs").text("delete");
+            var delete_button = $('<a>', {id: data, href: '#', 'data-id': data, title: 'delete'})
+                    .addClass("btn btn-sm padding-reduce-on-grid-view deleteApp")
+                    .append(deleteSpanIcon)
+                    .append(deleteSpanText);
+            return $('<div></div>').append(edit_button).append(delete_button).html();
+        } else {
+            return data;
+        }
     }
 
 })
