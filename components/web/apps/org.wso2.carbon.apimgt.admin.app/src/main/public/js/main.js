61bd09d71c86b86849060e4912cc2cbd9b412896
Abimaran Kugathasan <abimaran@wso2.com>
2017-03-27 10:22:31 +0530

    Removed unused JS functions.

--- components/web/apps/org.wso2.carbon.apimgt.admin.app/src/main/public/js/main.js
+++ components/web/apps/org.wso2.carbon.apimgt.admin.app/src/main/public/js/main.js
@@ -11,34 +11,7 @@ $(document).ready(function(){
     }).click(function(e) {
         e.preventDefault();
     });
-
-
 });
-
-var requestMetaData = function(data={}) {
-    var access_key_header = "Bearer " + getCookie("WSO2_AM_TOKEN_1"); //TODO: tmkb Depend on result from
-    // promise
-    var request_meta = {
-        clientAuthorizations: {
-            api_key: new SwaggerClient.ApiKeyAuthorization("Authorization", access_key_header, "header")
-        },
-        responseContentType: data['Content-Type'] || "application/json"
-    };
-    return request_meta;
-};
-
-var getCookie = function(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
-};
-
-var setAuthHeader = function(swaggerClient) {
-    var bearerToken = "Bearer " + getCookie("WSO2_AM_TOKEN_1");
-    swaggerClient.clientAuthorizations.add("apiKey", new SwaggerClient.ApiKeyAuthorization("Authorization", bearerToken, "header"));
-    swaggerClient.setHost(location.host);
-
-};
 
 var redirectToLogin = function (contextPath) {
     var message = "The session has expired" + ".<br/> You will be redirect to the login page ...";
