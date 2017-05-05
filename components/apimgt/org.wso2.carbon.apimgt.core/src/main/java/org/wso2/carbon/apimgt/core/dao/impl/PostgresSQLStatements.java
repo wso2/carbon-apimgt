2d3249e507c30784a19c0082646498b383e5985f
chamin dias <chamin892@gmail.com>
2017-05-06 00:38:04 +0530

    Add vendor specific Postgres queries for searching APIs in store

--- components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/dao/impl/PostgresSQLStatements.java
+++ components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/dao/impl/PostgresSQLStatements.java
@@ -20,6 +20,9 @@ package org.wso2.carbon.apimgt.core.dao.impl;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.wso2.carbon.apimgt.core.exception.APIMgtDAOException;
+import org.wso2.carbon.apimgt.core.models.API;
+import org.wso2.carbon.apimgt.core.models.APIStatus;
+import org.wso2.carbon.apimgt.core.util.APIMgtConstants;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -38,6 +41,10 @@ public class PostgresSQLStatements implements ApiDAOVendorSpecificStatements {
             "SELECT API.UUID, API.PROVIDER, API.NAME, API.CONTEXT, API.VERSION, API.DESCRIPTION,"
                     + "API.CURRENT_LC_STATUS, API.LIFECYCLE_INSTANCE_ID, API.LC_WORKFLOW_STATUS "
                     + "FROM AM_API API LEFT JOIN AM_API_GROUP_PERMISSION PERMISSION ON UUID = API_ID";
+
+    private static final String API_SUMMARY_SELECT_STORE = "SELECT UUID, PROVIDER, NAME, CONTEXT, " +
+            "VERSION, DESCRIPTION, CURRENT_LC_STATUS, LIFECYCLE_INSTANCE_ID, LC_WORKFLOW_STATUS " +
+            "FROM AM_API ";
 
     /**
      * Creates full text search query specific to database.
@@ -136,12 +143,121 @@ public class PostgresSQLStatements implements ApiDAOVendorSpecificStatements {
         }
     }
 
+    /**
+     * @see ApiDAOVendorSpecificStatements#attributeSearchStore(Connection connection, List,
+     * Map, int, int)
+     */
     @Override
     @SuppressFBWarnings({"SQL_PREPARED_STATEMENT_GENERATED_FROM_NONCONSTANT_STRING",
             "OBL_UNSATISFIED_OBLIGATION_EXCEPTION_EDGE"})
     public PreparedStatement attributeSearchStore(Connection connection, List<String> roles,
                                                   Map<String, String> attributeMap, int offset,
                                                   int limit) throws APIMgtDAOException {
-        return null;
+        StringBuilder roleListBuilder = new StringBuilder();
+        roleListBuilder.append("?");
+        for (int i = 0; i < roles.size() - 1; i++) {
+            roleListBuilder.append(",?");
+        }
+        StringBuilder searchQuery = new StringBuilder();
+        Iterator<Map.Entry<String, String>> entries = attributeMap.entrySet().iterator();
+
+        while (entries.hasNext()) {
+            Map.Entry<String, String> entry = entries.next();
+            searchQuery.append("LOWER(");
+            if (APIMgtConstants.TAG_SEARCH_TYPE_PREFIX.equalsIgnoreCase(entry.getKey())) {
+                searchQuery.append(APIMgtConstants.TAG_NAME_COLUMN);
+            } else if (APIMgtConstants.SUBCONTEXT_SEARCH_TYPE_PREFIX.
+                    equalsIgnoreCase(entry.getKey())) {
+                searchQuery.append(APIMgtConstants.URL_PATTERN_COLUMN);
+            } else {
+                searchQuery.append(entry.getKey());
+            }
+            searchQuery.append(") LIKE ?");
+            if (entries.hasNext()) {
+                searchQuery.append(" AND ");
+            }
+        }
+
+        String query = null;
+        if (attributeMap.containsKey(APIMgtConstants.TAG_SEARCH_TYPE_PREFIX)) {
+            //for tag search, need to check AM_API_TAG_MAPPING and AM_TAGS tables
+            query = API_SUMMARY_SELECT_STORE + " WHERE CURRENT_LC_STATUS  IN ('" +
+                    APIStatus.PUBLISHED.getStatus() + "','" +
+                    APIStatus.PROTOTYPED.getStatus() + "') AND " +
+                    "VISIBILITY = '" + API.Visibility.PUBLIC + "' AND " +
+                    "UUID IN (SELECT API_ID FROM AM_API_TAG_MAPPING WHERE TAG_ID IN " +
+                    "(SELECT TAG_ID FROM AM_TAGS WHERE " + searchQuery.toString() + ")) " +
+                    "UNION " +
+                    API_SUMMARY_SELECT_STORE + " WHERE CURRENT_LC_STATUS  IN ('" +
+                    APIStatus.PUBLISHED.getStatus() + "','" +
+                    APIStatus.PROTOTYPED.getStatus() + "') AND " +
+                    "VISIBILITY = '" + API.Visibility.RESTRICTED + "' AND " +
+                    "UUID IN (SELECT API_ID FROM AM_API_VISIBLE_ROLES WHERE ROLE IN (" +
+                    roleListBuilder.toString() + ")) AND " +
+                    "UUID IN (SELECT API_ID FROM AM_API_TAG_MAPPING WHERE TAG_ID IN " +
+                    "(SELECT TAG_ID FROM AM_TAGS WHERE " + searchQuery.toString() + ")) " +
+                    "LIMIT ? OFFSET ?";
+        } else if (attributeMap.containsKey(APIMgtConstants.SUBCONTEXT_SEARCH_TYPE_PREFIX)) {
+            //for subcontext search, need to check AM_API_OPERATION_MAPPING table
+            query = API_SUMMARY_SELECT_STORE + " WHERE CURRENT_LC_STATUS  IN ('" +
+                    APIStatus.PUBLISHED.getStatus() + "','" +
+                    APIStatus.PROTOTYPED.getStatus() + "') AND " +
+                    "VISIBILITY = '" + API.Visibility.PUBLIC + "' AND " +
+                    "UUID IN (SELECT API_ID FROM AM_API_OPERATION_MAPPING WHERE " +
+                    searchQuery.toString() + ") " +
+                    "UNION " +
+                    API_SUMMARY_SELECT_STORE + " WHERE CURRENT_LC_STATUS  IN ('" +
+                    APIStatus.PUBLISHED.getStatus() + "','" +
+                    APIStatus.PROTOTYPED.getStatus() + "') AND " +
+                    "VISIBILITY = '" + API.Visibility.RESTRICTED + "' AND " +
+                    "UUID IN (SELECT API_ID FROM AM_API_VISIBLE_ROLES WHERE ROLE IN (" +
+                    roleListBuilder.toString() + ")) AND " +
+                    "UUID IN (SELECT API_ID FROM AM_API_OPERATION_MAPPING WHERE " +
+                    searchQuery.toString() + ") " +
+                    "LIMIT ? OFFSET ?";
+        } else {
+            //for any other attribute search, need to check AM_API table
+            query = API_SUMMARY_SELECT_STORE + " WHERE CURRENT_LC_STATUS  IN ('" +
+                    APIStatus.PUBLISHED.getStatus() + "','" +
+                    APIStatus.PROTOTYPED.getStatus() + "') AND " +
+                    "VISIBILITY = '" + API.Visibility.PUBLIC + "' AND " +
+                    searchQuery.toString() +
+                    " UNION " +
+                    API_SUMMARY_SELECT_STORE + " WHERE CURRENT_LC_STATUS  IN ('" +
+                    APIStatus.PUBLISHED.getStatus() + "','" +
+                    APIStatus.PROTOTYPED.getStatus() + "') AND " +
+                    "VISIBILITY = '" + API.Visibility.RESTRICTED + "' AND " +
+                    "UUID IN (SELECT API_ID FROM AM_API_VISIBLE_ROLES WHERE ROLE IN (" +
+                    roleListBuilder.toString() + ")) AND " +
+                    searchQuery.toString() +
+                    " LIMIT ? OFFSET ?";
+        }
+        try {
+            int queryIndex = 1;
+            PreparedStatement statement = connection.prepareStatement(query);
+            //include the attribute in the query (for APIs with public visibility)
+            for (Map.Entry<String, String> entry : attributeMap.entrySet()) {
+                statement.setString(queryIndex, '%' + entry.getValue().
+                        toLowerCase(Locale.ENGLISH) + '%');
+                queryIndex++;
+            }
+            //include user roles in the query
+            for (String role : roles) {
+                statement.setString(queryIndex, role);
+                queryIndex++;
+            }
+            //include the attribute in the query (for APIs with restricted visibility)
+            for (Map.Entry<String, String> entry : attributeMap.entrySet()) {
+                statement.setString(queryIndex, '%' + entry.getValue().
+                        toLowerCase(Locale.ENGLISH) + '%');
+                queryIndex++;
+            }
+            //setting 0 as the default offset based on store-api.yaml and Postgress specifications
+            statement.setInt(queryIndex, (offset < 0) ? 0 : offset);
+            statement.setInt(++queryIndex, limit);
+            return statement;
+        } catch (SQLException e) {
+            throw new APIMgtDAOException(e);
+        }
     }
 }
