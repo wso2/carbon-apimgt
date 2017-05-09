5e339c6b148cbc623ef2eeaa89d8ae5633cdfe47
uvindra <uvindra_dj@yahoo.com>
2017-05-09 16:54:29 +0530

    Reorder imports of CompositeApisApi

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/gen/java/org/wso2/carbon/apimgt/rest/api/store/CompositeApisApi.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/gen/java/org/wso2/carbon/apimgt/rest/api/store/CompositeApisApi.java
@@ -1,14 +1,20 @@
 package org.wso2.carbon.apimgt.rest.api.store;
 
+
 import io.swagger.annotations.ApiParam;
-import org.osgi.service.component.annotations.Component;
+
 import org.wso2.carbon.apimgt.rest.api.store.dto.APIDTO;
 import org.wso2.carbon.apimgt.rest.api.store.dto.APIListDTO;
+import org.wso2.carbon.apimgt.rest.api.store.dto.ErrorDTO;
 import org.wso2.carbon.apimgt.rest.api.store.factories.CompositeApisApiServiceFactory;
+
 import org.wso2.msf4j.Microservice;
 import org.wso2.msf4j.Request;
+import org.wso2.msf4j.formparam.FileInfo;
 import org.wso2.msf4j.formparam.FormDataParam;
+import org.osgi.service.component.annotations.Component;
 
+import java.io.InputStream;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
