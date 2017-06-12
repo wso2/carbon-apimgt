99e7f8e64e566a663448f2b38dcea411fca8cadb
Kasun Thennakoon <tmkasun@gmail.com>
2017-06-12 12:27:31 +0530

    Move user login check logic to separate component for re-userbility.

--- components/web/apps/org.wso2.carbon.apimgt.publisher_originaluufapp/src/main/pages/src/app/components/Apis/Details/index.js
+++ components/web/apps/org.wso2.carbon.apimgt.publisher_originaluufapp/src/main/pages/src/app/components/Apis/Details/index.js
@@ -22,19 +22,18 @@ import {Route, Switch, Redirect} from 'react-router-dom'
 import Overview from './Overview'
 import NavBar from './NavBar'
 import LifeCycle from './LifeCycle'
-import {PageNotFound, APINotFound} from '../../Base/Errors/index'
+import {PageNotFound} from '../../Base/Errors/index'
 import Loading from '../../Base/Loading/Loading'
 
 import Api from '../../../data/api'
+import AuthCheck from "../../Base/Auth/AuthCheck";
 
 export default class Details extends Component {
     constructor(props) {
         super(props);
         this.api_uuid = props.match.params.api_uuid;
         this.state = {
-            api: null,
-            isAuthorize: false,
-            notFound: false
+            response: null,
         }
     }
 
@@ -47,42 +46,34 @@ export default class Details extends Component {
             }
         ).catch(
             error => {
-                this.bindApi(error)
+                if (process.env.NODE_ENV !== "production")
+                    console.log(error);
+                this.bindApi(error);
             }
         );
     }
 
     bindApi(api_response) {
-        let status = api_response.status;
-        let api = api_response.obj;
-        if (status === 200) {
-            this.setState({api: api});
-        } else if (status === 404) {
-            this.setState({notFound: true});
-        }
+        this.setState({response: api_response});
     }
 
     render() {
-        let redirect_url = this.state.api ? "/apis/" + this.props.match.params.api_uuid + "/overview" : "/apis/notFound";
-        if (this.state.api) {
+        let redirect_url = "/apis/" + this.props.match.params.api_uuid + "/overview";
+        if (this.state.response) {
             return (
-                <div>
-                    <NavBar/>
+                <AuthCheck response={this.state.response} {...this.props}>
                     <div className="tab-content">
+                    <NavBar {...this.props}/>
                         <Switch>
                             <Redirect exact from="/apis/:api_uuid"
                                       to={redirect_url}/>
                             <Route path="/apis/:api_uuid/overview"
-                                   render={ props => <Overview api={this.state.api} {...props} /> }/>
-                            <Route path="/apis/:api_uuid/life-cycle" component={LifeCycle}/>
+                                   render={ props => <Overview api={this.state.response.obj} {...props} /> }/>
+                            <Route path="/apis/:api_uuid/lifecycle" component={LifeCycle}/>
                             <Route component={PageNotFound}/>
                         </Switch>
                     </div>
-                </div>
-            );
-        } else if (this.state.notFound) {
-            return (
-                <APINotFound {...this.props}/>
+                </AuthCheck>
             );
         } else {
             return (
