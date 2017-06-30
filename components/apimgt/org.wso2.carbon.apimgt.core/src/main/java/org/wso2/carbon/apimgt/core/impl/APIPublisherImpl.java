6970025f1f3f04359a5303b52a8ed418f196c9a1
tharikaGitHub <tharika.m92@gmail.com>
2017-06-30 10:07:20 +0530

    Added debug logs to APIPublisherImpl

--- components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/impl/APIPublisherImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/impl/APIPublisherImpl.java
@@ -756,6 +756,11 @@ public class APIPublisherImpl extends AbstractAPIManager implements APIPublisher
         }
         List<String> finalAggregatedPermissionList = new ArrayList<>();
         finalAggregatedPermissionList.addAll(permissionArrayForUser);
+        String message = "Aggregate permissions of user " + loggedInUserName + " for the API " + api.getName() + " are "
+                + StringUtils.join(finalAggregatedPermissionList, ", ") + ".";
+        if (log.isDebugEnabled()) {
+            log.debug(message);
+        }
         return finalAggregatedPermissionList;
     }
 
