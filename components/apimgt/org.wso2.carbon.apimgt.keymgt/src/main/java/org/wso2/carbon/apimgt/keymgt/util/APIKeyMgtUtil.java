8bc1daccb0e5239329bca680f52a0513880e399a
bhathiya <bhathiya@wso2.com>
2017-07-11 12:17:52 +0530

    Fix for APIMANAGER-5831

--- components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/util/APIKeyMgtUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/util/APIKeyMgtUtil.java
@@ -20,6 +20,12 @@ package org.wso2.carbon.apimgt.keymgt.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.opensaml.saml2.core.Assertion;
+import org.opensaml.saml2.core.Attribute;
+import org.opensaml.saml2.core.AttributeStatement;
+import org.opensaml.xml.XMLObject;
+import org.opensaml.xml.schema.XSString;
+import org.opensaml.xml.schema.impl.XSAnyImpl;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.model.API;
 import org.wso2.carbon.apimgt.api.model.APIIdentifier;
@@ -27,7 +33,9 @@ import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.impl.dto.APIKeyValidationInfoDTO;
 import org.wso2.carbon.apimgt.impl.utils.APIUtil;
 import org.wso2.carbon.apimgt.keymgt.APIKeyMgtException;
+import org.wso2.carbon.apimgt.keymgt.handlers.ResourceConstants;
 import org.wso2.carbon.apimgt.keymgt.internal.ServiceReferenceHolder;
+import org.wso2.carbon.core.security.AuthenticatorsConfiguration;
 import org.wso2.carbon.governance.api.generic.GenericArtifactManager;
 import org.wso2.carbon.governance.api.generic.dataobjects.GenericArtifact;
 import org.wso2.carbon.identity.core.util.IdentityDatabaseUtil;
@@ -38,19 +46,24 @@ import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.user.api.TenantManager;
 import org.wso2.carbon.user.api.UserStoreException;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import javax.cache.Cache;
 import javax.cache.CacheConfiguration;
 import javax.cache.Caching;
-import java.sql.Connection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class APIKeyMgtUtil {
 
     private static final Log log = LogFactory.getLog(APIKeyMgtUtil.class);
 
     private  static boolean isKeyCacheInistialized = false;
+
+    private static final String AUTHENTICATOR_NAME = ResourceConstants.SAML2_SSO_AUTHENTICATOR_NAME;
 
     public static String getTenantDomainFromTenantId(int tenantId) throws APIKeyMgtException {
         try {
@@ -197,6 +210,113 @@ public class APIKeyMgtUtil {
         } catch (RegistryException e) {
             return null;
         }
+    }
+
+    /**
+     * Get the role list from the SAML2 Assertion
+     *
+     * @param assertion SAML2 assertion
+     * @return Role list from the assertion
+     */
+    public static String[] getRolesFromAssertion(Assertion assertion) {
+        List<String> roles = new ArrayList<String>();
+        String roleClaim = getRoleClaim();
+        List<AttributeStatement> attributeStatementList = assertion.getAttributeStatements();
+
+        if (attributeStatementList != null) {
+            for (AttributeStatement statement : attributeStatementList) {
+                List<Attribute> attributesList = statement.getAttributes();
+                for (Attribute attribute : attributesList) {
+                    String attributeName = attribute.getName();
+                    if (attributeName != null && roleClaim.equals(attributeName)) {
+                        List<XMLObject> attributeValues = attribute.getAttributeValues();
+                        if (attributeValues != null && attributeValues.size() == 1) {
+                            String attributeValueString = getAttributeValue(attributeValues.get(0));
+                            String multiAttributeSeparator = getAttributeSeparator();
+                            String[] attributeValuesArray = attributeValueString.split(multiAttributeSeparator);
+                            if (log.isDebugEnabled()) {
+                                log.debug("Adding attributes for Assertion: " + assertion + " AttributeName : " +
+                                        attributeName + ", AttributeValue : " + Arrays.toString(attributeValuesArray));
+                            }
+                            roles.addAll(Arrays.asList(attributeValuesArray));
+                        } else if (attributeValues != null && attributeValues.size() > 1) {
+                            for (XMLObject attributeValue : attributeValues) {
+                                String attributeValueString = getAttributeValue(attributeValue);
+                                if (log.isDebugEnabled()) {
+                                    log.debug("Adding attributes for Assertion: " + assertion + " AttributeName : " +
+                                            attributeName + ", AttributeValue : " + attributeValue);
+                                }
+                                roles.add(attributeValueString);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("Role list found for assertion: " + assertion + ", roles: " + roles);
+        }
+        return roles.toArray(new String[roles.size()]);
+    }
+
+    private static String getAttributeValue(XMLObject attributeValue) {
+        if (attributeValue == null){
+            return null;
+        } else if (attributeValue instanceof XSString){
+            return getStringAttributeValue((XSString) attributeValue);
+        } else if(attributeValue instanceof XSAnyImpl){
+            return getAnyAttributeValue((XSAnyImpl) attributeValue);
+        } else {
+            return attributeValue.toString();
+        }
+    }
+
+    private static String getStringAttributeValue(XSString attributeValue) {
+        return attributeValue.getValue();
+    }
+
+    private static String getAnyAttributeValue(XSAnyImpl attributeValue) {
+        return attributeValue.getTextContent();
+    }
+
+    /**
+     * Get attribute separator from configuration or from the constants
+     *
+     * @return
+     */
+    private static String getAttributeSeparator() {
+        AuthenticatorsConfiguration authenticatorsConfiguration = AuthenticatorsConfiguration.getInstance();
+        AuthenticatorsConfiguration.AuthenticatorConfig authenticatorConfig = authenticatorsConfiguration
+                .getAuthenticatorConfig(AUTHENTICATOR_NAME);
+
+        if (authenticatorConfig != null) {
+            Map<String, String> configParameters = authenticatorConfig.getParameters();
+            if (configParameters.containsKey(ResourceConstants.ATTRIBUTE_VALUE_SEPARATOR)) {
+                return configParameters.get(ResourceConstants.ATTRIBUTE_VALUE_SEPARATOR);
+            }
+        }
+
+        return ResourceConstants.ATTRIBUTE_VALUE_SEPERATER;
+    }
+
+    /**
+     * Role claim attribute value from configuration file or from constants
+     *
+     * @return
+     */
+    private static String getRoleClaim() {
+        AuthenticatorsConfiguration authenticatorsConfiguration = AuthenticatorsConfiguration.getInstance();
+        AuthenticatorsConfiguration.AuthenticatorConfig authenticatorConfig = authenticatorsConfiguration
+                .getAuthenticatorConfig(AUTHENTICATOR_NAME);
+
+        if (authenticatorConfig != null) {
+            Map<String, String> configParameters = authenticatorConfig.getParameters();
+            if (configParameters.containsKey(ResourceConstants.ROLE_CLAIM_ATTRIBUTE)) {
+                return configParameters.get(ResourceConstants.ROLE_CLAIM_ATTRIBUTE);
+            }
+        }
+
+        return ResourceConstants.ROLE_ATTRIBUTE_NAME;
     }
 
 }
