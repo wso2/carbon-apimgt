4fd034fbc7a0e94d7bcea7a47355ba641fc2ccf6
Nuwan Silva <nu1silva@gmail.com>
2017-07-31 18:21:52 +0530

    Removed partial tests and updated copyrights.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/test/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/TestUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/test/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/TestUtil.java
@@ -1,3 +1,22 @@
+/*
+ *
+ *   Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
 package org.wso2.carbon.apimgt.rest.api.publisher.impl;
 
 import org.wso2.carbon.apimgt.core.models.BusinessInformation;
@@ -76,20 +95,4 @@ public class TestUtil {
         Request request = null;
         return request;
     }
-
-//    id(apidto.getId()).
-//    context(apidto.getContext()).
-//    description(apidto.getDescription()).
-//    lifeCycleStatus(apidto.getLifeCycleStatus()).
-//    endpoint(fromEndpointListToMap(apidto.getEndpoint())).
-//    visibleRoles(new HashSet<>(apidto.getVisibleRoles())).
-//    policies(subscriptionPolicies).
-//    apiPermission(apidto.getPermission()).
-//    tags(new HashSet<>(apidto.getTags())).
-//    labels(new HashSet<>(apidto.getLabels())).
-//    transport(new HashSet<>(apidto.getTransport())).
-//    isResponseCachingEnabled(Boolean.valueOf(apidto.getResponseCaching())).
-//    businessInformation(businessInformation).
-//    uriTemplates(uriTemplateList).
-//    corsConfiguration(corsConfiguration);
 }
