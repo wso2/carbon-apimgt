004cfd765de88fa1b8a2c206b351975e352ec566
Abimaran Kugathasan <abimaran@wso2.com>
2017-08-01 10:59:42 +0530

    Added testcases for throttling policies.

---
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/test/java/org/wso2/carbon/apimgt/rest/api/admin/throttling/mappings/BlockingConditionMappingUtilTest.java
@@ -0,0 +1,66 @@
+/*
+ *
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ *
+ */
+
+package org.wso2.carbon.apimgt.rest.api.admin.throttling.mappings;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.wso2.carbon.apimgt.core.models.BlockConditions;
+import org.wso2.carbon.apimgt.rest.api.admin.dto.BlockingConditionDTO;
+import org.wso2.carbon.apimgt.rest.api.admin.mappings.BlockingConditionMappingUtil;
+
+import java.util.UUID;
+
+import static org.wso2.carbon.apimgt.core.util.APIMgtConstants.ThrottlePolicyConstants.BLOCKING_CONDITIONS_IP;
+
+public class BlockingConditionMappingUtilTest {
+
+    @Test(description = "Convert Blocking Condition to DTO")
+    public void fromBlockingConditionToDTOTest()   throws  Exception   {
+
+        BlockConditions conditions = new BlockConditions();
+        conditions.setUuid(UUID.randomUUID().toString());
+        BlockingConditionDTO dto = BlockingConditionMappingUtil.fromBlockingConditionToDTO(conditions);
+
+        Assert.assertNotNull(dto);
+    }
+
+    @Test(description = "From Blocking Condition DTO to Model")
+    public void fromBlockingConditionDTOToBlockConditionTest() throws Exception {
+        BlockingConditionDTO dto = new BlockingConditionDTO();
+        dto.setConditionId(UUID.randomUUID().toString());
+        dto.setConditionType(BLOCKING_CONDITIONS_IP);
+        dto.setConditionValue("12.32.45.3");
+        dto.setStatus(true);
+
+        BlockConditions conditions = BlockingConditionMappingUtil.fromBlockingConditionDTOToBlockCondition(dto);
+        Assert.assertNotNull(conditions);
+    }
+
+    @Test(description = "From Blocking Condition to DTO")
+    public void fromBlockConditionToIpConditionDTOTest() throws Exception   {
+
+        BlockConditions conditions = new BlockConditions();
+        conditions.setUuid(UUID.randomUUID().toString());
+        BlockingConditionDTO dto = BlockingConditionMappingUtil.fromBlockingConditionToDTO(conditions);
+        Assert.assertNotNull(dto);
+    }
+}
