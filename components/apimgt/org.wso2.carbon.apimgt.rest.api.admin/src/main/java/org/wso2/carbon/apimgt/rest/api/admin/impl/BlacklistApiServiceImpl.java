4582ce1d9a2698a78092490f72ea6b55b67621eb
Abimaran Kugathasan <abimaran@wso2.com>
2017-08-01 10:57:59 +0530

    Removed unused imports, added copyrights, removed redundant code.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/BlacklistApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/BlacklistApiServiceImpl.java
@@ -1,6 +1,26 @@
+/*
+ *
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ *
+ */
+
 package org.wso2.carbon.apimgt.rest.api.admin.impl;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.carbon.apimgt.core.api.APIMgtAdminService;
@@ -12,16 +32,12 @@ import org.wso2.carbon.apimgt.rest.api.admin.dto.*;
 import java.util.List;
 import org.wso2.carbon.apimgt.rest.api.admin.NotFoundException;
 
-import java.io.InputStream;
-
 import org.wso2.carbon.apimgt.rest.api.admin.mappings.BlockingConditionMappingUtil;
 import org.wso2.carbon.apimgt.rest.api.common.dto.ErrorDTO;
 import org.wso2.carbon.apimgt.rest.api.common.util.RestApiUtil;
-import org.wso2.msf4j.formparam.FormDataParam;
-import org.wso2.msf4j.formparam.FileInfo;
 import org.wso2.msf4j.Request;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
+
 
 public class BlacklistApiServiceImpl extends BlacklistApiService {
 
