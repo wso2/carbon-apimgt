52cef6d8eed5965019fcdab87ba629a41777b4c2
harsha89 <harsz89@gmail.com>
2017-08-08 18:02:59 +0530

    adding test cases for import api

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/test/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ImportApiServiceImplTestCase.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/test/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ImportApiServiceImplTestCase.java
@@ -1,5 +1,6 @@
 package org.wso2.carbon.apimgt.rest.api.publisher.impl;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
@@ -7,9 +8,19 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wso2.carbon.apimgt.core.api.APIPublisher;
+import org.wso2.carbon.apimgt.core.impl.APIPublisherImpl;
 import org.wso2.carbon.apimgt.rest.api.publisher.utils.RestAPIPublisherUtil;
 import org.wso2.carbon.messaging.CarbonMessage;
 import org.wso2.msf4j.Request;
+
+import javax.ws.rs.core.Response;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(RestAPIPublisherUtil.class)
@@ -17,6 +28,43 @@ public class ImportApiServiceImplTestCase {
 
     private static final Logger log = LoggerFactory.getLogger(ImportApiServiceImplTestCase.class);
     private static final String USER = "admin";
+
+    @Test
+    public void testImportApisPostError() throws Exception {
+        printTestMethodName();
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("swagger.json").getFile());
+        FileInputStream fis = null;
+        fis = new FileInputStream(file);
+        ImportApiServiceImpl importApiService = new ImportApiServiceImpl();
+        APIPublisher apiPublisher = Mockito.mock(APIPublisherImpl.class);
+        PowerMockito.mockStatic(RestAPIPublisherUtil.class);
+        PowerMockito.when(RestAPIPublisherUtil.getApiPublisher(USER)).
+                thenReturn(apiPublisher);
+        Response response = importApiService.
+                importApisPost(fis, null, null, null, getRequest());
+        assertEquals(response.getStatus(), 500);
+        assertTrue(response.getEntity().toString().contains("Error while importing the given APIs"));
+    }
+
+    @Test
+    public void testImportApisPutError() throws Exception {
+        printTestMethodName();
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("swagger.json").getFile());
+        FileInputStream fis = null;
+        fis = new FileInputStream(file);
+        ImportApiServiceImpl importApiService = new ImportApiServiceImpl();
+        APIPublisher apiPublisher = Mockito.mock(APIPublisherImpl.class);
+        PowerMockito.mockStatic(RestAPIPublisherUtil.class);
+        PowerMockito.when(RestAPIPublisherUtil.getApiPublisher(USER)).
+                thenReturn(apiPublisher);
+        Response response = importApiService.
+                importApisPut(fis, null, null, null, getRequest());
+        assertEquals(response.getStatus(), 500);
+        assertTrue(response.getEntity().toString().contains("Error while importing the given APIs"));
+    }
+
 
     // Sample request to be used by tests
     private Request getRequest() throws Exception {
