a83be4ddc2fc00881cbe9a7e027c7a482fb4d843
harsha89 <harsz89@gmail.com>
2017-08-08 17:56:33 +0530

    adding test cases for export impl

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/test/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ExportApiServiceImplTestCase.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/test/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ExportApiServiceImplTestCase.java
@@ -1,5 +1,6 @@
 package org.wso2.carbon.apimgt.rest.api.publisher.impl;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
@@ -7,9 +8,24 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wso2.carbon.apimgt.core.api.APIPublisher;
+import org.wso2.carbon.apimgt.core.impl.APIPublisherImpl;
+import org.wso2.carbon.apimgt.core.models.API;
+import org.wso2.carbon.apimgt.core.models.APIDetails;
+import org.wso2.carbon.apimgt.rest.api.publisher.common.SampleTestObjectCreator;
+import org.wso2.carbon.apimgt.rest.api.publisher.utils.FileBasedApiImportExportManager;
 import org.wso2.carbon.apimgt.rest.api.publisher.utils.RestAPIPublisherUtil;
 import org.wso2.carbon.messaging.CarbonMessage;
 import org.wso2.msf4j.Request;
+
+import javax.ws.rs.core.Response;
+import java.util.HashSet;
+import java.util.Set;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(RestAPIPublisherUtil.class)
@@ -18,11 +34,24 @@ public class ExportApiServiceImplTestCase {
     private static final Logger log = LoggerFactory.getLogger(ExportApiServiceImplTestCase.class);
     private static final String USER = "admin";
 
+    @Test
+    public void testExportApisGetNotFound() throws Exception {
+        printTestMethodName();
+        ExportApiServiceImpl exportApiService = new ExportApiServiceImpl();
+        APIPublisher apiPublisher = Mockito.mock(APIPublisherImpl.class);
+        PowerMockito.mockStatic(RestAPIPublisherUtil.class);
+        PowerMockito.when(RestAPIPublisherUtil.getApiPublisher(USER)).
+                thenReturn(apiPublisher);
+        Response response = exportApiService.
+                exportApisGet("", null, null, null, getRequest());
+        assertEquals(response.getStatus(), 404);
+    }
+
     // Sample request to be used by tests
     private Request getRequest() throws Exception {
         CarbonMessage carbonMessage = Mockito.mock(CarbonMessage.class);
         Request request = new Request(carbonMessage);
-        PowerMockito.whenNew(Request.class).withArguments(carbonMessage).thenReturn(request);
+        whenNew(Request.class).withArguments(carbonMessage).thenReturn(request);
         return request;
     }
 
