f7e5bfce50063ac79c2af454174ea67073c67c46
harsha89 <harsz89@gmail.com>
2017-08-09 12:22:09 +0530

    tests for store mappins of subscription and tier

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/test/java/org/wso2/carbon/apimgt/rest/api/store/mappings/SubscriptionMappingUtilTestCase.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store/src/test/java/org/wso2/carbon/apimgt/rest/api/store/mappings/SubscriptionMappingUtilTestCase.java
@@ -20,15 +20,44 @@
 package org.wso2.carbon.apimgt.rest.api.store.mappings;
 
 import org.testng.annotations.Test;
-import org.wso2.carbon.apimgt.core.api.WorkflowResponse;
-import org.wso2.carbon.apimgt.core.models.User;
-import org.wso2.carbon.apimgt.core.models.WorkflowStatus;
-import org.wso2.carbon.apimgt.core.workflow.GeneralWorkflowResponse;
-import org.wso2.carbon.apimgt.rest.api.store.dto.UserDTO;
-import org.wso2.carbon.apimgt.rest.api.store.dto.WorkflowResponseDTO;
+import org.wso2.carbon.apimgt.core.models.Subscription;
+import org.wso2.carbon.apimgt.rest.api.store.common.SampleTestObjectCreator;
+import org.wso2.carbon.apimgt.rest.api.store.dto.SubscriptionDTO;
+import org.wso2.carbon.apimgt.rest.api.store.dto.SubscriptionListDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.testng.Assert.assertEquals;
 
 public class SubscriptionMappingUtilTestCase {
 
+    @Test
+    public void testFromSubscriptionListToDTO() {
+        String subUuid1 = UUID.randomUUID().toString();
+        String subUuid2 = UUID.randomUUID().toString();
+        Subscription subscription1 = SampleTestObjectCreator.createSubscription(subUuid1);
+        Subscription subscription2 = SampleTestObjectCreator.createSubscription(subUuid2);
+        List<Subscription> subscriptionList = new ArrayList<>();
+        subscriptionList.add(subscription1);
+        subscriptionList.add(subscription2);
+        SubscriptionListDTO subscriptionListDTO = SubscriptionMappingUtil.
+                                                                    fromSubscriptionListToDTO(subscriptionList, 10, 0);
+        assertEquals(subscriptionListDTO.getCount(), (Integer)subscriptionList.size());
+        assertEquals(subscription1.getId(), subscriptionListDTO.getList().get(0).getSubscriptionId());
+        assertEquals(subscription1.getApi().getName(), subscriptionListDTO.getList().get(0).getApiName());
+        assertEquals(subscription1.getApi().getId(), subscriptionListDTO.getList().get(0).getApiIdentifier());
+        assertEquals(subscription1.getApi().getVersion(), subscriptionListDTO.getList().get(0).getApiVersion());
+        assertEquals(SubscriptionDTO.LifeCycleStatusEnum.valueOf(subscription1.getStatus()
+                .toString()).name(),subscriptionListDTO.getList().get(0).getLifeCycleStatus().name());
+
+        assertEquals(subscriptionListDTO.getCount(), (Integer)subscriptionList.size());
+        assertEquals(subscription2.getId(), subscriptionListDTO.getList().get(1).getSubscriptionId());
+        assertEquals(subscription2.getApi().getName(), subscriptionListDTO.getList().get(1).getApiName());
+        assertEquals(subscription2.getApi().getId(), subscriptionListDTO.getList().get(1).getApiIdentifier());
+        assertEquals(subscription2.getApi().getVersion(), subscriptionListDTO.getList().get(1).getApiVersion());
+        assertEquals(SubscriptionDTO.LifeCycleStatusEnum.valueOf(subscription2.getStatus()
+                .toString()).name(),subscriptionListDTO.getList().get(1).getLifeCycleStatus().name());
+    }
 }
