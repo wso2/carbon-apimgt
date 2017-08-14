7218e2385eaa3167967ca6be300a3f4b6df2878c
Chamila Adhikarinayake <cka.adhi@gmail.com>
2017-08-14 14:05:17 +0530

    dao methods to retrieve workflow related data

--- components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/impl/APIManagerFactory.java
+++ components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/impl/APIManagerFactory.java
@@ -125,7 +125,7 @@ public class APIManagerFactory {
         try {
             return new APIMgtAdminServiceImpl(DAOFactory.getAPISubscriptionDAO(), DAOFactory.getPolicyDAO(),
                     DAOFactory.getApiDAO(), DAOFactory.getLabelDAO(), DAOFactory.getApplicationDAO(), new
-                    APIGatewayPublisherImpl());
+                    APIGatewayPublisherImpl(), DAOFactory.getWorkflowDAO());
         } catch (APIMgtDAOException e) {
             log.error("Couldn't create API Management Admin Service", e);
             throw new APIMgtDAOException("Couldn't create API Management Admin Service",
