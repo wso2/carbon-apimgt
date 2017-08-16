db602e42189311029ee7671f9118ce3b6d4a2baa
tharikaGitHub <tharika.m92@gmail.com>
2017-08-16 17:09:18 +0530

    Added UI functionality to add and update api permissions

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/Permission.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/Permission.js
@@ -54,13 +54,24 @@ class Permission extends Component {
         super(props);
         this.state = {
             api: null,
-            notFound: false
+            notFound: false,
+            roleField: null,
+            readField: false,
+            updateField: false,
+            deleteField: false,
+            manageSubField: false,
+            permissionData: []
         };
         this.api_uuid = this.props.match.params.api_uuid;
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInputs = this.handleInputs.bind(this);
         this.handleSwitch = this.handleSwitch.bind(this);
         this.handleMaxTPS = this.handleMaxTPS.bind(this);
+        this.handleChangeRoleField = this.handleChangeRoleField.bind(this);
+        this.handleChangeReadField = this.handleChangeReadField.bind(this);
+        this.handleChangeUpdateField = this.handleChangeUpdateField.bind(this);
+        this.handleChangeDeleteField = this.handleChangeDeleteField.bind(this);
+        this.handleChangeManageSubField = this.handleChangeManageSubField.bind(this);
     }
 
     componentDidMount() {
@@ -69,6 +80,7 @@ class Permission extends Component {
         promised_api.then(
             response => {
                 this.setState({api: response.obj});
+                this.getExistingPermissions(this);
             }
         ).catch(
             error => {
@@ -95,10 +107,167 @@ class Permission extends Component {
         this.setState({maxTPS: maxTPS});
     }
 
-
     handleSubmit = (e) => {
-        debugger;
+        const api = new Api();
+        let promised_api = api.get(this.api_uuid);
+        promised_api.then(
+            response => {
+                var api_data = JSON.parse(response.data);
+                var permissionString = this.createPermissionJsonString(this);
+                api_data.permission = permissionString;
+                let promised_update = api.update(api_data);
+                promised_update.then(
+                    response => {
+                        message.success("Permissions updated successfully");
+                    }
+                ).catch (
+                    error => {
+                        console.error(error);
+                        message.error("Error occurred while updating permissions!");
+                        this.setState({loading: false});
+                    }
+                );
+            }
+        ).catch(
+            error => {
+                if (process.env.NODE_ENV !== "production") {
+                    console.log(error);
+                }
+                let status = error.status;
+                if (status === 404) {
+                    this.setState({notFound: true});
+                }
+            }
+        );
     }
+
+    handleChangeRoleField(event) {
+        this.setState({roleField: event.target.value});
+    }
+
+    handleChangeReadField(event) {
+        this.setState({readField: event.target.checked});
+    }
+
+    handleChangeUpdateField(event) {
+        this.setState({updateField: event.target.checked});
+    }
+
+    handleChangeDeleteField(event) {
+        this.setState({deleteField: event.target.checked});
+    }
+
+    handleChangeManageSubField(event) {
+        this.setState({manageSubField: event.target.checked});
+    }
+
+    handleAddRole(permissionData) {
+        var groupPermissions = {};
+        groupPermissions['key'] = this.state.roleField.trim();
+        groupPermissions['isRead'] = this.state.readField;
+        groupPermissions['isUpdate'] = this.state.updateField;
+        groupPermissions['isDelete'] = this.state.deleteField;
+        groupPermissions['isManageSubscription'] = this.state.manageSubField;
+        permissionData.push(groupPermissions);
+        this.setState({permissionData: permissionData,
+                        roleField: null,
+                        readField: false,
+                        updateField: false,
+                        deleteField: false,
+                        manageSubField: false});
+    }
+
+    handleRemoveRole(value) {
+        var permissionData = this.state.permissionData.slice();
+        var index = -1;
+        for(var i = 0; i < permissionData.length; i++) {
+            if(permissionData[i]["key"] === value) {
+                index = i;
+            }
+        }
+        permissionData.splice(index, 1);
+        this.setState({permissionData: permissionData});
+    }
+
+    createPermissionJsonString() {
+        var apiPermissionArray = [];
+        var permissionData = this.state.permissionData;
+        Object.keys(permissionData).map(function(key) {
+            var groupPermissions = {};
+            var permissionSubJson = permissionData[key];
+            var roleName = permissionSubJson['key'];
+            var isRead = permissionSubJson['isRead'];;
+            var isUpdate = permissionSubJson['isUpdate'];
+            var isDelete = permissionSubJson['isDelete'];
+            var isManageSubscription = permissionSubJson['isManageSubscription'];
+            var permissionArray = [];
+            if (isRead) {
+                permissionArray.push("READ");
+            }
+            if (isUpdate) {
+                permissionArray.push("UPDATE");
+            }
+            if (isDelete) {
+                permissionArray.push("DELETE");
+            }
+            if (isManageSubscription) {
+                permissionArray.push("MANAGE_SUBSCRIPTION");
+            }
+            groupPermissions['groupId'] = roleName;
+            groupPermissions['permission'] = permissionArray;
+            apiPermissionArray.push(groupPermissions);
+        });
+        var updatedPermissionString = JSON.stringify(apiPermissionArray);
+        if (updatedPermissionString == "[]") {
+            return "";
+        } else {
+            return updatedPermissionString;
+        }
+    }
+
+    getExistingPermissions() {
+        if (!this.state.api) {
+            return <Loading/>
+        }
+        const permissionData = [];
+        var permissionString = this.state.api.permission;
+        if (permissionString != "") {
+            const permissionJson = JSON.parse(permissionString);
+            Object.keys(permissionJson).map(function(key) {
+                /* permissionSubJson contains permissions for a single role*/
+                var permissionSubJson = permissionJson[key];
+                var roleName = permissionSubJson['groupId'];
+                var permissionArray = permissionSubJson['permission'];
+                var isRead = false;
+                var isUpdate = false;
+                var isDelete = false;
+                var isManageSubscription = false;
+
+                var groupPermissions = {};
+
+                if(permissionArray.includes("READ")) {
+                   isRead = true;
+                }
+                if (permissionArray.includes("UPDATE")){
+                   isUpdate = true;
+                }
+                if (permissionArray.includes("DELETE")) {
+                   isDelete = true;
+                }
+                if (permissionArray.includes("MANAGE_SUBSCRIPTION")) {
+                   isManageSubscription = true;
+                }
+                groupPermissions['key'] = roleName;
+                groupPermissions['isRead'] = isRead;
+                groupPermissions['isUpdate'] = isUpdate;
+                groupPermissions['isDelete'] = isDelete;
+                groupPermissions['isManageSubscription'] = isManageSubscription;
+                permissionData.push(groupPermissions);
+            });
+        }
+        this.setState({permissionData: permissionData});
+    }
+
 
     render() {
         const {getFieldDecorator} = this.props.form;
@@ -109,31 +278,38 @@ class Permission extends Component {
 
         const columns = [{
             title: 'Role',
-            dataIndex: 'name',
-            key: 'name',
+            dataIndex: 'key',
+            key: 'key',
             render: text => <a href="#">{text}</a>,
         }, {
             title: 'Read',
-            dataIndex: 'age',
-            key: 'age',
-        }, {
-            title: 'Write',
-            dataIndex: 'address',
-            key: 'address',
+            dataIndex: 'isRead',
+            key: 'isRead',
+            render: isChecked => <Checkbox checked={isChecked} name="read" value="READ"></Checkbox>
         }, {
             title: 'Update',
-            dataIndex: 'update',
-            key: 'update',
-        }
-            , {
-                title: 'Action',
-                key: 'action',
-                render: (text, record) => (
-                    <span>
-                    <a href="#">Delete</a>
+            dataIndex: 'isUpdate',
+            key: 'isUpdate',
+            render: isChecked => <Checkbox checked={isChecked} name="update" value="UPDATE"></Checkbox>
+        }, {
+            title: 'Delete',
+            dataIndex: 'isDelete',
+            key: 'isDelete',
+            render: isChecked => <Checkbox checked={isChecked} name="delete" value="DELETE"></Checkbox>,
+        }, {
+            title: 'Manage Subscription',
+            dataIndex: 'isManageSubscription',
+            key: 'isManageSubscription',
+            render: isChecked => <Checkbox checked={isChecked} name="manage_subscription" value="MANAGE_SUBSCRIPTION"></Checkbox>,
+        }, {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <span>
+                    <a href="#" onClick={this.handleRemoveRole.bind(this, record.key)}>Delete</a>
                 </span>
-                ),
-            }];
+            ),
+        }];
 
         const columnsOfScopeTable = [{
             title: 'Scopes',
@@ -160,35 +336,16 @@ class Permission extends Component {
 
         }];
 
-        const data = [{
-            key: '1',
-            name: 'Engineering',
-            age: 'Yes',
-            address: 'New York No. 1 Lake Park',
-            update: 'update'
-        }, {
-            key: '2',
-            name: 'Sales',
-            age: 42,
-            address: 'New York No. 1 Lake Park',
-            update: 'update'
-        }, {
-            key: '3',
-            name: 'QA',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            update: 'update'
-        }];
-
+        const permissionData = this.state.permissionData;
 
         const api = this.state.api;
+
         if (this.state.notFound) {
             return <ResourceNotFound/>
         }
         if (!this.state.api) {
             return <Loading/>
         }
-
 
         return (
 
@@ -220,7 +377,7 @@ class Permission extends Component {
                                 </Row>
 
                                 <Row style={{marginBottom: "10px"}} type="flex" justify="left">
-                                    <Col span={8}>Roels</Col>
+                                    <Col span={8}>Roles</Col>
                                     <Col span={16}>
                                         <Input name="roles" placeholder="Sales-group,Engineering"/>
                                     </Col>
@@ -232,17 +389,24 @@ class Permission extends Component {
                                     <Col span={8}>API Permission</Col>
                                     <Col span={16}>
                                         <Row>
-                                            <Col span={7} style={{margin: "10px"}}>
-                                                <Input name="role" placeholder="role"/>
+                                            <Col span={4} style={{margin: "10px"}}>
+                                                <Input name="roleField" placeholder="role" value={this.state.roleField} onChange={this.handleChangeRoleField}/>
                                             </Col>
-                                            <Col span={3} style={{margin: "10px"}}>
-                                                <Checkbox name="read"> Read </Checkbox>
+                                            <Col span={2} style={{margin: "10px"}}>
+                                                <Checkbox name="readField" value="READ" checked={this.state.readField} onChange={this.handleChangeReadField}> Read </Checkbox>
                                             </Col>
-                                            <Col span={3} style={{margin: "10px"}}>
-                                                <Checkbox name="update"> Update </Checkbox>
+                                            <Col span={2} style={{margin: "10px"}}>
+                                                <Checkbox name="updateField" value="UPDATE" checked={this.state.updateField} onChange={this.handleChangeUpdateField}> Update </Checkbox>
                                             </Col>
-                                            <Col span={3} style={{margin: "10px"}}>
-                                                <Checkbox name="delete"> Delete </Checkbox>
+                                            <Col span={2} style={{margin: "10px"}}>
+                                                <Checkbox name="deleteField" value="DELETE" checked={this.state.deleteField} onChange={this.handleChangeDeleteField}> Delete </Checkbox>
+                                            </Col>
+                                            <Col span={5} style={{margin: "10px"}}>
+                                                <Checkbox name="manageSubField" value="MANAGE_SUBSCRIPTION" checked={this.state.manageSubField} onChange={this.handleChangeManageSubField}>
+                                                Manage Subscriptions </Checkbox>
+                                            </Col>
+                                            <Col span={1} style={{margin: "10px"}}>
+                                                <Button name="add" onClick={this.handleAddRole.bind(this, permissionData)}> Add </Button>
                                             </Col>
                                         </Row>
                                     </Col>
@@ -251,7 +415,7 @@ class Permission extends Component {
                                 <Row style={{marginBottom: "10px"}} type="flex" justify="center">
                                     <Col span={8}></Col>
                                     <Col span={16}>
-                                        <Table columns={columns} dataSource={data}/>
+                                        <Table columns={columns} dataSource={permissionData}/>
                                     </Col>
                                 </Row>
                             </Card>
@@ -287,7 +451,7 @@ class Permission extends Component {
                             </Card>
 
                             <Button loading={this.state.creating} type="primary"
-                                    onClick={this.handleSubmit}>Create</Button>
+                                    onClick={this.handleSubmit}>Update</Button>
                         </form>
                     </Col>
                 </Row>
