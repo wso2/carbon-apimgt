2f551fbd20120332e6e53ec877521ed7cfb5db03
harsha89 <harsz89@gmail.com>
2017-08-17 13:38:42 +0530

    removed all store app

---
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/source/src/app/components/Base/Errors/ResourceNotFound.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, {Component} from 'react'
+
+const ResourceNotFound = (props) => {
+    return (
+        <div>
+            <div className="message message-danger">
+                <h4><i className="icon fw fw-error"/>404 Resource Not Found!</h4>
+                <p>
+                    Can't find the resource you are looking for
+                    <span style={{color: 'green'}}> {props.response ? props.response.statusText : ""} </span>
+                </p>
+            </div>
+
+        </div>
+    );
+};
+
+export default ResourceNotFound
