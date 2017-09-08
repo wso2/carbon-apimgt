6a838d4ed3e000001aafbbe23b1ed9121bc92de7
dinusha92 <dinushad92@gmail.com>
2017-09-08 11:54:05 +0530

    adding api when extenstions are not present in definition.

--- components/apimgt/org.wso2.carbon.apimgt.core/src/test/java/org/wso2/carbon/apimgt/core/impl/APIDefinitionFromSwagger20TestCase.java
+++ components/apimgt/org.wso2.carbon.apimgt.core/src/test/java/org/wso2/carbon/apimgt/core/impl/APIDefinitionFromSwagger20TestCase.java
@@ -22,34 +22,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.wso2.carbon.apimgt.core.SampleTestObjectCreator;
 import org.wso2.carbon.apimgt.core.exception.APIManagementException;
+import org.wso2.carbon.apimgt.core.models.API;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class APIDefinitionFromSwagger20TestCase {
 
-    private static final String API_VIEW = "apim:api_view";
-    private static final String API_CREATE = "apim:api_create";
-    private static final String API_UPDATE = "apim:api_update";
-    private static final String API_DELETE = "apim:api_delete";
-    private static final String VIEW = "View API";
-    private static final String CREATE = "Create API";
-    private static final String UPDATE = "Update API";
-    private static final String DELETE = "Delete API";
-
     @Test
-    public void testGetScope() throws IOException, APIManagementException {
-        Map<String, String> defMap = new HashMap<>();
-        defMap.put(API_VIEW, VIEW);
-        defMap.put(API_CREATE, CREATE);
-        defMap.put(API_UPDATE, UPDATE);
-        defMap.put(API_DELETE, DELETE);
-
+    public void testApiResourceParseFromSwagger() throws IOException, APIManagementException {
         APIDefinitionFromSwagger20 apiDefinitionFromSwagger20 = new APIDefinitionFromSwagger20();
         String sampleApi = SampleTestObjectCreator.getSampleApiSwagger();
-        Map<String, String> scopes = apiDefinitionFromSwagger20.getScope(sampleApi);
-        Assert.assertEquals(scopes, defMap);
+        API.APIBuilder apiBuilder = apiDefinitionFromSwagger20
+                .generateApiFromSwaggerResource("testProvider", sampleApi);
+        API api = apiBuilder.build();
+        Assert.assertNotNull(api);
     }
 }
 
