f944998d90071d0ba2159efb5499df86f8eddb55
npamudika <rpnpamudika@gmail.com>
2017-09-21 10:56:05 +0530

    Fix login issue due to localhost

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/source/src/App.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/source/src/App.js
@@ -43,7 +43,7 @@ class Protected extends Component {
         /* TODO: need to fix the header to avoid conflicting with messages ~tmkb*/
         this.handleResponse = this.handleResponse.bind(this);
         /* TODO: Get apim base url from config*/
-        Axios.get("https://localhost:9292/login/login/store").then(this.handleResponse);
+        Axios.get(location.origin + "/login/login/store").then(this.handleResponse);
     }
 
     handleResponse = (response) => {
