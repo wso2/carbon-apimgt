b2a4e99f1a6bf7ebc85f6c21fdf51ca58b367604
RavinduHasithanjana <ravinduceylofana@outlook.com>
2017-09-22 01:02:53 +0530

    fixed issues and made changes in the UI

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/ExportApi.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/ExportApi.java
@@ -14,19 +14,7 @@ import org.wso2.msf4j.formparam.FormDataParam;
 import org.osgi.service.component.annotations.Component;
 
 import java.io.InputStream;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.HEAD;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
@@ -44,6 +32,7 @@ public class ExportApi implements Microservice  {
    private final ExportApiService delegate = ExportApiServiceFactory.getExportApi();
 
     @GET
+    @OPTIONS
     @Path("/apis")
     @Consumes({ "application/json" })
     @Produces({ "application/zip" })
--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/ImportApi.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/ImportApi.java
@@ -15,19 +15,7 @@ import org.wso2.msf4j.formparam.FormDataParam;
 import org.osgi.service.component.annotations.Component;
 
 import java.io.InputStream;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.HEAD;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
@@ -45,6 +33,7 @@ public class ImportApi implements Microservice  {
    private final ImportApiService delegate = ImportApiServiceFactory.getImportApi();
 
     @POST
+    @OPTIONS
     @Path("/apis")
     @Consumes({ "multipart/form-data" })
     @Produces({ "application/json" })
@@ -70,6 +59,7 @@ public class ImportApi implements Microservice  {
         return delegate.importApisPost(fileInputStream, fileDetail,provider, request);
     }
     @PUT
+    @OPTIONS
     @Path("/apis")
     @Consumes({ "multipart/form-data" })
     @Produces({ "application/json" })
