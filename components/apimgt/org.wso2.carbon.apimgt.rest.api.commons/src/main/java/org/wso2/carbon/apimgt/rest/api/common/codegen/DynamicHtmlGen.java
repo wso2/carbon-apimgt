88f63134b163d157a8dc94df44d0cb9550c2750e
Malintha Amarasinghe <malintha.prasan@gmail.com>
2017-09-25 13:17:32 +0530

    Adding a testcase for DynamicHtmlGen

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.commons/src/main/java/org/wso2/carbon/apimgt/rest/api/common/codegen/DynamicHtmlGen.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.commons/src/main/java/org/wso2/carbon/apimgt/rest/api/common/codegen/DynamicHtmlGen.java
@@ -28,7 +28,6 @@ import io.swagger.models.Swagger;
 import io.swagger.models.parameters.Parameter;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
