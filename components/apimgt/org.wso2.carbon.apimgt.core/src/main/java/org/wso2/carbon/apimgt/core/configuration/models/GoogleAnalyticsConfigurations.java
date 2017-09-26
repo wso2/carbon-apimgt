fbbb13f5584e8427928c4df98a857410d990d722
Thilini Shanika <tgtshanika@gmail.com>
2017-09-26 16:49:23 +0530

    Adding Google Analytics tracking configurations to APIMConfiguration

---
+++ components/apimgt/org.wso2.carbon.apimgt.core/src/main/java/org/wso2/carbon/apimgt/core/configuration/models/GoogleAnalyticsConfigurations.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.apimgt.core.configuration.models;
+
+import org.wso2.carbon.kernel.annotations.Configuration;
+import org.wso2.carbon.kernel.annotations.Element;
+
+/**
+ * Class to hold Google Analytics Tracking configurations.
+ */
+@Configuration(description = "Analytics configurations")
+public class GoogleAnalyticsConfigurations {
+
+    @Element(description = "Enable Google Analytics")
+    private boolean enabled = false;
+    @Element(description = "Google Analytics Tracking ID")
+    private String trackingID = "UA-XXXXXXXX-X";
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getTrackingID() {
+        return trackingID;
+    }
+
+    public void setTrackingID(String trackingID) {
+        this.trackingID = trackingID;
+    }
+}
