c268a78aef51aa6c8baf19ee594694595a71972e
RavinduHasithanjana <ravinduceylofana@outlook.com>
2017-10-06 17:19:24 +0530

    Done with the minor alterations made by reviews

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/ImportApi.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/ImportApi.java
@@ -15,7 +15,15 @@ import org.wso2.msf4j.formparam.FormDataParam;
 import org.osgi.service.component.annotations.Component;
 
 import java.io.InputStream;
-import javax.ws.rs.*;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
