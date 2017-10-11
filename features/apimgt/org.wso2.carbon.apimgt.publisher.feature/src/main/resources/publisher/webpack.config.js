3c6ac0d199c2a2ee7c2686dbe0cb6f9312698ad4
chanaka <Chanaka Jayasena>
2017-10-11 16:25:19 +0530

    Adding missing dynamic config config for bablerc

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/webpack.config.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/webpack.config.js
@@ -54,29 +54,7 @@ if (process.env.NODE_ENV === "development") {
     config.devtool = "source-map";
 }
 if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(
-        new webpack.LoaderOptionsPlugin({
-            minimize: true,
-            debug: false
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-                screw_ie8: true,
-                conditionals: true,
-                unused: true,
-                comparisons: true,
-                sequences: true,
-                dead_code: true,
-                evaluate: true,
-                if_return: true,
-                join_vars: true
-            },
-            output: {
-                comments: false
-            },
-        })
-    );
+   
 }
 
 module.exports = config;
