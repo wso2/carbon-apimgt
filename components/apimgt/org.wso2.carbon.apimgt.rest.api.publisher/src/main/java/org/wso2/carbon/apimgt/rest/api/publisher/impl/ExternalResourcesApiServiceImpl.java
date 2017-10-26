274bdcdf6d602064634631ecfb78914cf0613656
SuKSW <subhashinieksw.14@cse.mrt.ac.lk>
2017-10-26 19:29:42 +0530

    comments added and impl class called using Class.forName()

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ExternalResourcesApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ExternalResourcesApiServiceImpl.java
@@ -10,8 +10,8 @@ import org.wso2.carbon.apimgt.core.impl.ServiceDiscoveryConfigBuilder;
 import org.wso2.carbon.apimgt.core.models.Endpoint;
 import org.wso2.carbon.apimgt.rest.api.common.dto.ErrorDTO;
 import org.wso2.carbon.apimgt.rest.api.common.util.RestApiUtil;
-import org.wso2.carbon.apimgt.rest.api.publisher.*;
-import org.wso2.carbon.apimgt.rest.api.publisher.dto.*;
+import org.wso2.carbon.apimgt.rest.api.publisher.ExternalResourcesApiService;
+import org.wso2.carbon.apimgt.rest.api.publisher.dto.EndPointListDTO;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -27,48 +27,73 @@ public class ExternalResourcesApiServiceImpl extends ExternalResourcesApiService
     private static final Logger log = LoggerFactory.getLogger(ExternalResourcesApiServiceImpl.class);
 
     /**
-     * Retrieve all service endpoints via service discovery
+     * Retrieve all service endpoints by iterating through all service discovery impl classes
      *
      * @param ifNoneMatch     If-None-Match header value
      * @param ifModifiedSince If-Modified-Since header
      * @param request         msf4j request object
-     * @return A list of service endpoints available in the cluster
+     * @return A list of service endpoints available in the cluster(s)
      * @throws NotFoundException When the particular resource does not exist in the system
      */
     @Override
     public Response externalResourcesServicesGet(String ifNoneMatch, String ifModifiedSince,
                                                  Request request) throws NotFoundException {
         try{
-            EndPointListDTO endPointListDTO = new EndPointListDTO();
             ServiceDiscoveryConfigurations serviceDiscoveryConfig = ServiceDiscoveryConfigBuilder
                     .getServiceDiscoveryConfiguration();
+
+            // If service discovery not enabled - do not proceed
             if (!serviceDiscoveryConfig.isServiceDiscoveryEnabled()) {
                 String errorMessage = "Service discovery is not enabled";
                 return handleInternalServerError(errorMessage, null);
             }
+
+            EndPointListDTO endPointListDTO = new EndPointListDTO();
+
             List<ServiceDiscoveryImplConfig> implConfigsList = serviceDiscoveryConfig.getImplementationsList();
             for (ServiceDiscoveryImplConfig implConfig : implConfigsList) {
+                //Every implConfig has two elements. The implClass and the implParameters.
+
+                //Get the implClass instance
                 String implClassName = implConfig.getImplClass();
-                Class implClazz = ExternalResourcesApiServiceImpl.class.getClassLoader().loadClass(implClassName);
+                Class implClazz = Class.forName(implClassName);
+
+                //Pass the implParameters to the above instance
                 ServiceDiscoverer serviceDiscoverer = (ServiceDiscoverer) implClazz.newInstance();
                 serviceDiscoverer.init(implConfig.getImplParameters());
 
+                //The .init() method above sets the filtering parameters to the serviceDiscoverer instance, if provided.
+                //Let's check whether those filtering parameters : "namespace" and/or "criteria" are set,
+                //and call ServiceDiscoverer#listServices method accordingly
                 String namespaceFilter = serviceDiscoverer.getNamespaceFilter();
                 HashMap<String, String> criteriaFilter = serviceDiscoverer.getCriteriaFilter();
                 List<Endpoint> discoveredEndpointList;
+
+                //both not set
                 if (namespaceFilter == null && criteriaFilter == null) {
                     discoveredEndpointList = serviceDiscoverer.listServices();
+
+                //both set
                 } else if (namespaceFilter != null && criteriaFilter != null) {
                     discoveredEndpointList = serviceDiscoverer.listServices(namespaceFilter, criteriaFilter);
+
+                //only "namespace" is set
                 } else if (namespaceFilter != null) {
                     discoveredEndpointList = serviceDiscoverer.listServices(namespaceFilter);
+
+                //remaining -> only "criteria" is set
                 } else {
                     discoveredEndpointList = serviceDiscoverer.listServices(criteriaFilter);
                 }
+
+                // Notice, the discoveredEndpointList is the list returned by ServiceDiscoverer#listServices method
+                // Hence before moving on to the next implClass, let's add those Endpoints to endPointListDTO
                 for (Endpoint endpoint : discoveredEndpointList) {
                     endPointListDTO.addListItem(MappingUtil.toEndPointDTO(endpoint));
                 }
             }
+
+            //Count is set after adding endpoints return by all implClasses
             endPointListDTO.setCount(endPointListDTO.getList().size());
             return Response.ok().entity(endPointListDTO).build();
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
