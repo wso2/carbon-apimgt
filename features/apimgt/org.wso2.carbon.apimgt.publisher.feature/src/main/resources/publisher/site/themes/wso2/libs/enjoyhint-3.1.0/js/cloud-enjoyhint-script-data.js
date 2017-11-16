27d8a135632756b79eb5705a9176424d95746388
Shenavi <shenavi21@gmail.com>
2017-11-16 14:29:28 +0530

    APIMANAGER-5992 - enjoyhint improvements for the api publisher

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/libs/enjoyhint-3.1.0/js/cloud-enjoyhint-script-data.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/libs/enjoyhint-3.1.0/js/cloud-enjoyhint-script-data.js
@@ -19,13 +19,13 @@
 var item_listing_with_worldbank_script_data = [
     {
         'click #listing-title': 'Welcome to WSO2 API Manager. In this tutorial, we will lead you through' +
-        ' publishing and invoking an API based on World Banks country statistics data. As you already ' +
-        'have an API named WorldBank, let\'s use a different name and a context when creating the API.' +
-        ' Click the highlighted area to continue the interactive tutorial.',
+                                ' publishing and invoking an API based on World Banks country statistics data. As you already ' +
+                                'have an API named WorldBank, let\'s use a different name and a context when creating the API.' +
+                                ' Click the highlighted area to continue the interactive tutorial.',
         'showSkip' : false
     },
     {
-        'click #top-menu-api-add': 'Click "ADD NEW API" to get started.',
+        'click #top-menu-api-add': 'Click <b>ADD NEW API</b> to get started.',
         'showSkip' : false
     }
 ];
@@ -33,8 +33,8 @@ var item_listing_with_worldbank_script_data = [
 var item_listing_with_apis_script_data = [
     {
         'click #top-menu-api-add': 'Welcome to WSO2 API Manager. In this tutorial, we will lead you through' +
-        ' publishing and invoking an API based on World Bank\'s country statistics data. Click "Add" ' +
-        'to get started',
+                                   ' publishing and invoking an API based on World Bank\'s country statistics data. Click <b>Add</b> ' +
+                                   'to get started',
         'showSkip' : false
 
     }
@@ -43,12 +43,12 @@ var item_listing_with_apis_script_data = [
 var item_listing_script_data = [
     {
         'click #listing': 'Welcome to WSO2 API Manager. In this tutorial, we will lead you through ' +
-        'publishing and invoking an API based on World Banks country statistics data. Click the ' +
-        'highlighted area to continue the interactive tutorial.',
+                          'publishing and invoking an API based on World Banks country statistics data. Click the ' +
+                          'highlighted area to continue the interactive tutorial.',
         'showSkip' : false
     },
     {
-        'click #btn-add-new-api': 'Click New API to get started.',
+        'click #btn-add-new-api': 'Click <b>New API</b> to get started.',
         'showSkip' : false
     }
 ];
@@ -57,7 +57,7 @@ var item_add_script_data = [
     {
         selector: '#create-new-api',
         event: 'click',
-        description: 'Let\'s create an API from scratch. Select the Design new API option.',
+        description: 'Let\'s create an API from scratch. Select the <b>Design a New REST API</b> option.',
         shape: 'circle',
         'radius': 15,
         'bottom':2,
@@ -66,7 +66,7 @@ var item_add_script_data = [
     {
         selector: '#designNewAPI',
         event: 'click',
-        description: 'Start creating a new API',
+        description: '<b>Start creating</b> a new API',
         'showSkip' : false
     }
 
@@ -74,31 +74,31 @@ var item_add_script_data = [
 
 var item_design_script_data = [
     {
-        'key #name': 'Lets type WorldBank as the API display name and press Tab',
+        'key #name': 'Let\'s type <b class="enjoy_hint_emphasize_text">WorldBank</b> as the API display name and press Tab',
         'keyCode': 9,
         'showSkip' : false
     },
     {
-        'key #context': 'The API\'s context is included in its URL path. Type "wb" as the context ' +
-        'and press Tab.',
+        'key #context': 'The API\'s context is included in its URL path. Type <b class="enjoy_hint_emphasize_text">wb</b> as the context ' +
+                        'and press Tab.',
         'keyCode': 9,
         'showSkip' : false
     },
     {
-        'key #version': 'Type 1.0.0 as the API version and press Tab',
+        'key #version': 'Type <b class="enjoy_hint_emphasize_text">1.0.0</b> as the API version and press Tab',
         'keyCode': 9,
         'showSkip' : false
     },
     {
         'key #resource_url_pattern': 'Let\'s create a REST resource called "countries" with the ' +
-        'country\'s code as a parameter. Type "countries/{code}" as the URL Pattern and press Tab.',
+                                     'country\'s code as a parameter. Type <b class="enjoy_hint_emphasize_text">countries/{code}</b> as the URL Pattern and press Tab.',
         'keyCode': 9,
         'showSkip' : false
     },
     {
         selector: '#get',
         event: 'click',
-        description: 'Click the GET option to allow the GET HTTP method for this URL pattern.',
+        description: 'Click the <b>GET</b> option to allow the GET HTTP method for this URL pattern.',
         shape: 'circle',
         'showSkip' : false,
         onBeforeStart:function(){
@@ -106,15 +106,15 @@ var item_design_script_data = [
         }
     },
     {
-        'click #add_resource': 'Click Add to add this pattern to the list of allowed REST resources',
+        'click #add_resource': 'Click <b>Add</b> to add this pattern to the list of allowed REST resources',
         'showSkip' : false,
         onBeforeStart:function(){
             $(window).scrollTop($('#add_resource').offset().top);
         }
     },
     {
-        'click #go_to_implement': 'Next, click Implement to proceed to the implementation phase of' +
-        ' the API creation.',
+        'click #go_to_implement': 'Next, click <b>Implement</b> to proceed to the implementation phase of' +
+                                  ' the API creation.',
         'showSkip' : false,
         onBeforeStart:function(){
             $(window).scrollTop($('#go_to_implement').offset().top);
@@ -124,33 +124,33 @@ var item_design_script_data = [
 
 var item_design_with_worldbank_api_script_data = [
     {
-        'key #name': 'Lets type API display name. API name is unique. As you already have an ' +
-        'API named "WorldBank" let\'s type a different name. Ex: WorldBank2, WorldBankTest etc.. ' +
-        'and press Tab',
+        'key #name': 'Let\'s type API display name. API name is unique. As you already have an ' +
+                     'API named "WorldBank" let\'s type a different name. Ex: <b class="enjoy_hint_emphasize_text">WorldBank2, WorldBankTest</b> etc.. ' +
+                     'and press Tab',
         'keyCode': 9,
         'showSkip' : false
     },
     {
         'key #context': 'Context is what API will have in the URL path. API context also cannot be ' +
-        'duplicate. So let\'s type a different context than "wb". Ex: wb1, wbtest etc... and press Tab.',
+                        'duplicate. So let\'s type a different context than "wb". Ex: <b class="enjoy_hint_emphasize_text">wb1, wbtest</b> etc... and press Tab.',
         'keyCode': 9,
         'showSkip' : false
     },
     {
-        'key #version': 'Type 1.0.0 as the API version and press Tab',
+        'key #version': 'Type <b class="enjoy_hint_emphasize_text">1.0.0</b> as the API version and press Tab',
         'keyCode': 9,
         'showSkip' : false
     },
     {
-        'key #resource_url_pattern': 'Lets define REST resource "countries" that then takes country ' +
-        'code as the parameter. To do this, type "countries/{code}" as the URL Pattern and press Tab.',
+        'key #resource_url_pattern': 'Let\'s define REST resource "countries" that then takes country ' +
+                                     'code as the parameter. To do this, type <b class="enjoy_hint_emphasize_text">countries/{code}</b> as the URL Pattern and press Tab.',
         'keyCode': 9,
         'showSkip' : false
     },
     {
         selector: '#get',
         event: 'click',
-        description: 'Select the GET checkbox to allow GET method for this URL pattern',
+        description: 'Select the <b>GET</b> checkbox to allow GET method for this URL pattern',
         shape: 'circle',
         'showSkip' : false,
         onBeforeStart:function(){
@@ -159,15 +159,15 @@ var item_design_with_worldbank_api_script_data = [
 
     },
     {
-        'click #add_resource': 'Click Add to add this pattern to the list of allowed REST resources',
+        'click #add_resource': 'Click <b>Add</b to add this pattern to the list of allowed REST resources',
         'showSkip' : false,
         onBeforeStart:function(){
             $(window).scrollTop($('#add_resource').offset().top);
         }
     },
     {
-        'click #go_to_implement': 'Now click Implement to proceed to the implementation phase of the' +
-        ' API creation',
+        'click #go_to_implement': 'Now click <b>Implement</b> to proceed to the implementation phase of the' +
+                                  ' API creation',
         'showSkip' : false,
         onBeforeStart:function(){
             $(window).scrollTop($('#go_to_implement').offset().top);
@@ -179,12 +179,12 @@ var item_implement_script_data = [
 
     {
         'click #select-managed-api': 'Let\'s implement the actual API rather than a prototype. Click' +
-        ' the Managed API option to proceed.',
+                                     ' the <b>Managed API</b> option to proceed.',
         'showSkip' : false
     },
     {
         'key #endpoint-input': 'Provide an existing backend service URL for World ' +
-        'Bank data in our example, type: http://api.worldbank.org and press Tab',
+                               'Bank data in our example, type: <b class="enjoy_hint_emphasize_text">http://api.worldbank.org</b> and press Tab',
         'keyCode': 9,
         'showSkip' : false,
         'scrollAnimationSpeed': 1000,
@@ -194,7 +194,7 @@ var item_implement_script_data = [
         }
     },
     {
-        'click #go_to_manage': 'Next, click the "Next: Manage" button to proceed to the last phase of' +
+        'click #go_to_manage': 'Next, click the <b>Next: Manage</b> button to proceed to the last phase of' +
         ' the API creation.',
         'showSkip' : false,
         onBeforeStart:function(){
@@ -207,7 +207,7 @@ var item_implement_script_data = [
 
 var item_info_script_data = [
     {
-        'click #goToStore': 'Click the Go to API Store link to open the API in your default API Store.',
+        'click #goToStore': 'Click the <b>Go to API Store</b> link to open the API in your default API Store.',
         'showSkip' : false
     }
 
@@ -216,11 +216,11 @@ var item_info_script_data = [
 var item_manage_script_data = [
     {
         'click #Gold': 'Let\'s select usage tiers or subscription plans that you offer to subscribers. ' +
-        'Select Gold from the Subscription Tiers.',
+                       'Select <b class="enjoy_hint_emphasize_text">Gold</b> from the Subscription Tiers.',
         'showSkip' : false
     },
     {
-        'click #publish_api': 'Click Save & Publish to publish the API in the API Store.',
+        'click #publish_api': 'Click <b>Save & Publish</b> to publish the API in the API Store.',
         'showSkip' : false,
         'scrollAnimationSpeed': 1000,
         onBeforeStart:function(){
@@ -231,8 +231,8 @@ var item_manage_script_data = [
 
 var item_manage_success_message_script_data = [
     {
-        'click #goToStore-btn': 'Your API is now published. Click "Go to API Store" to see the API ' +
-        'in the API Store and subscribe to it.',
+        'click #goToStore-btn': 'Your API is now published. Click <b>Go to API Store</b> to see the API ' +
+                                'in the API Store and subscribe to it.',
         'showSkip' : false
     }
 ];
