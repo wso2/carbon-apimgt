7b5a9f6e96c93ee30b23467403f63a588bfdd242
Hasunie <hasunie@wso2.com>
2017-11-23 13:38:11 +0530

    enhanced the unit test coverage

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/test/java/org/wso2/carbon/apimgt/gateway/mediators/RegularExpressionProtectorTest.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/test/java/org/wso2/carbon/apimgt/gateway/mediators/RegularExpressionProtectorTest.java
@@ -28,6 +28,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpHeaders;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -47,7 +48,9 @@ public class RegularExpressionProtectorTest {
     private static final Log log = LogFactory.getLog(RegularExpressionProtectorTest.class);
     private RegularExpressionProtector regularExpressionProtector;
     private MessageContext messageContext;
-    org.apache.axis2.context.MessageContext axis2MsgContext;
+    private org.apache.axis2.context.MessageContext axis2MsgContext;
+    private String enabledStatus = "true";
+
 
     @Before
     public void init() {
@@ -66,11 +69,11 @@ public class RegularExpressionProtectorTest {
      */
     @Test
     public void testSqlInjectionInPathParam() {
-        log.info("Running Sql injection query param test case.");
+        log.info("Running the test case to validate the request path param.");
         Mockito.when(messageContext.getProperty(APIMgtGatewayConstants.THREAT_TYPE)).thenReturn
                 (String.valueOf("SQL-Injection"));
         Mockito.when(messageContext.getProperty(APIMgtGatewayConstants.ENABLED_CHECK_PATHPARAM)).thenReturn
-                (String.valueOf("true"));
+                (String.valueOf(enabledStatus));
         Mockito.when(axis2MsgContext.getProperty(APIMgtGatewayConstants.REST_URL_POSTFIX)).thenReturn
                 (String.valueOf("/drop"));
         Mockito.when(((Axis2MessageContext) messageContext).getAxis2MessageContext()).thenReturn(axis2MsgContext);
@@ -83,7 +86,7 @@ public class RegularExpressionProtectorTest {
      */
     @Test
     public void testSqlInjectionInHeaders() {
-        log.info("Running test case for sql injection in request headers.");
+        log.info("Running the test case to validate the request header from sql injection attacks.");
         Map<String, String> transportHeaders = new HashMap<>();
         String acceptHeader = "application/json";
         String acceptLanguage = "en-US,en;q=drop";
@@ -92,7 +95,7 @@ public class RegularExpressionProtectorTest {
         transportHeaders.put(HttpHeaders.ACCEPT_LANGUAGE, acceptLanguage);
         transportHeaders.put(HttpHeaders.USER_AGENT, userAgent);
         Mockito.when(messageContext.getProperty(APIMgtGatewayConstants.ENABLED_CHECK_HEADERS)).thenReturn
-                (String.valueOf("true"));
+                (String.valueOf(enabledStatus));
         Mockito.when(axis2MsgContext.getProperty(APIMgtGatewayConstants.TRANSPORT_HEADERS)).thenReturn
                 (transportHeaders);
         Mockito.when(((Axis2MessageContext) messageContext).getAxis2MessageContext()).thenReturn(axis2MsgContext);
@@ -105,13 +108,37 @@ public class RegularExpressionProtectorTest {
      */
     @Test
     public void testSqlInjectionInBody() throws AxisFault {
+        log.info(" Running the test case to validate the request body from sql injection attacks.");
         SOAPFactory fac = OMAbstractFactory.getSOAP12Factory();
         SOAPEnvelope env = fac.createSOAPEnvelope();
         fac.createSOAPBody(env);
         env.getBody().addChild(fac.createOMElement("test", "Drop database", "testBody"));
+        Mockito.when(messageContext.getProperty(APIMgtGatewayConstants.ENABLED_CHECK_BODY)).thenReturn
+                (String.valueOf(enabledStatus));
         Mockito.when(((Axis2MessageContext) messageContext).getAxis2MessageContext()).thenReturn(axis2MsgContext);
         Mockito.doReturn(env).when(axis2MsgContext).getEnvelope();
         regularExpressionProtector = new RegularExpressionProtector();
         regularExpressionProtector.mediate(messageContext);
+    }
+
+    /**
+     * This is the test case to check the return value of the isContentAware method.
+     */
+    @Test
+    public void testIsContentAware() {
+        log.info("Running the test case to check the return status of the isContentAware method.");
+        SOAPFactory fac = OMAbstractFactory.getSOAP12Factory();
+        SOAPEnvelope env = fac.createSOAPEnvelope();
+        fac.createSOAPBody(env);
+        env.getBody().addChild(fac.createOMElement("test", "Content aware", "MessageBody"));
+        Mockito.when(((Axis2MessageContext) messageContext).getAxis2MessageContext()).thenReturn(axis2MsgContext);
+        Mockito.doReturn(env).when(axis2MsgContext).getEnvelope();
+        Mockito.when(messageContext.getProperty(APIMgtGatewayConstants.ENABLED_CHECK_BODY)).thenReturn
+                (String.valueOf(enabledStatus));
+        regularExpressionProtector = new RegularExpressionProtector();
+        regularExpressionProtector.mediate(messageContext);
+        String enableBuild = String.valueOf(regularExpressionProtector.isContentAware());
+        Assert.assertEquals(enabledStatus, enableBuild);
+        log.info("Successfully completed testIsContentAware test case.");
     }
 }
