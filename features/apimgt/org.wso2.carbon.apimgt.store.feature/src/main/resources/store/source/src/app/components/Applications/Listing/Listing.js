205aa9f088766ecb1195f202c1651406c71d8bb7
Kasun Thennakoon <tmkasun@gmail.com>
2017-12-08 23:47:49 +0530

    Update application listing page to allow application delete functionality

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/source/src/app/components/Applications/Listing/Listing.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/source/src/app/components/Applications/Listing/Listing.js
@@ -16,14 +16,16 @@
  * under the License.
  */
 
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import Button from 'material-ui/Button';
-import API from '../../../data/api.js'
+import API from '../../../data/api'
+import Application from '../../../data/Application'
 
 import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
-import { Delete, Edit, CreateNewFolder, Description  }from 'material-ui-icons';
+import {Delete, Edit, CreateNewFolder, Description}from 'material-ui-icons';
+import { withStyles } from 'material-ui/styles';
 import PropTypes from 'prop-types';
 import Table, {
     TableBody,
@@ -33,14 +35,25 @@ import Table, {
     TableSortLabel,
 } from 'material-ui/Table';
 import Paper from 'material-ui/Paper';
+import IconButton from 'material-ui/IconButton';
+import DeleteIcon from 'material-ui-icons/Delete';
 
 const columnData = [
-    { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
-    { id: 'throttlingTier', numeric: false, disablePadding: false, label: 'Tier' },
-    { id: 'lifeCycleStatus', numeric: false, disablePadding: false, label: 'Workflow Status' },
-    { id: 'subscriptions', numeric: true, disablePadding: false, label: 'Subscriptions' },
-    { id: 'actions', numeric: false, disablePadding: false, label: 'Actions' },
+    {id: 'name', numeric: false, disablePadding: true, label: 'Name'},
+    {id: 'throttlingTier', numeric: false, disablePadding: false, label: 'Tier'},
+    {id: 'lifeCycleStatus', numeric: false, disablePadding: false, label: 'Workflow Status'},
+    {id: 'subscriptions', numeric: true, disablePadding: false, label: 'Subscriptions'},
+    {id: 'actions', numeric: false, disablePadding: false, label: 'Actions'},
 ];
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none',
+    },
+});
 
 class ApplicationTableHead extends Component {
     static propTypes = {
@@ -54,7 +67,7 @@ class ApplicationTableHead extends Component {
     };
 
     render() {
-        const {  order, orderBy } = this.props;
+        const {order, orderBy} = this.props;
         return (
             <TableHead>
                 <TableRow>
@@ -86,7 +99,7 @@ class Listing extends Component {
 
     constructor(props) {
         super(props);
-	
+
     }
 
     componentDidMount() {
@@ -115,9 +128,13 @@ class Listing extends Component {
         order: 'asc',
         orderBy: 'name',
         selected: [],
-        data : []
+        data: []
     };
 
+    handleAppDelete(event){
+        const id = "";
+        Application.delete(id);
+    }
     handleRequestSort = (event, property) => {
         const orderBy = property;
         let order = 'desc';
@@ -127,68 +144,73 @@ class Listing extends Component {
         const data = this.state.data.sort(
             (a, b) => (order === 'desc' ? b[orderBy] > a[orderBy] : a[orderBy] > b[orderBy]),
         );
-        this.setState({ data, order, orderBy });
+        this.setState({data, order, orderBy});
     };
 
     render() {
-        const { data, order, orderBy, selected } = this.state;
-
+        const {data, order, orderBy, selected} = this.state;
+        const {classes} = this.props;
         return (
-
-        <Grid container style={{paddingLeft:"40px"}}>
-            <Grid item xs={12}>
-                <Paper style={{display:"flex"}}>
-                    <Typography type="display2" gutterBottom className="page-title">
-                        Applications
-                    </Typography>
-                    <Link to={"/application/create"}>
-                        <Button aria-owns="simple-menu" aria-haspopup="true" >
-                            <CreateNewFolder /> Add Application
-                         </Button>
-                    </Link>
-                </Paper>
+            <Grid container style={{paddingLeft: "40px"}}>
+                <Grid item xs={12}>
+                    <Paper style={{display: "flex"}}>
+                        <Typography type="display2" gutterBottom className="page-title">
+                            Applications
+                        </Typography>
+                        <Link to={"/application/create"}>
+                            <Button aria-owns="simple-menu" aria-haspopup="true">
+                                <CreateNewFolder /> Add Application
+                            </Button>
+                        </Link>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={6} md={9} lg={9} xl={10}>
+                    <Paper style={{paddingLeft: "40px"}}>
+                        <p>
+                            An application is a logical collection of APIs. Applications allow you to use a single
+                            access
+                            token to invoke a collection of APIs and to subscribe to one API multiple times with
+                            different
+                            SLA levels. The DefaultApplication is pre-created and allows unlimited access by default.
+                        </p>
+                        <Table>
+                            <ApplicationTableHead order={order} orderBy={orderBy}
+                                                  onRequestSort={this.handleRequestSort}/>
+                            <TableBody>
+                                {data.map(n => {
+                                    return (
+                                        <TableRow hover tabIndex="-1" key={n.applicationId}>
+                                            <TableCell disablePadding>
+                                                <Link to={"/applications/" + n.applicationId}>
+                                                    {n.name}
+                                                </Link>
+                                            </TableCell>
+                                            <TableCell disablePadding>
+                                                {n.throttlingTier}
+                                            </TableCell>
+                                            <TableCell disablePadding>
+                                                {n.lifeCycleStatus}
+                                            </TableCell>
+                                            <TableCell disablePadding>
+                                                0
+                                            </TableCell>
+                                            <TableCell disablePadding>
+                                                <IconButton color="accent" className={classes.button}
+                                                            aria-label="Delete">
+                                                    <DeleteIcon />
+                                                </IconButton>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            </TableBody>
+                        </Table>
+                    </Paper>
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={6} md={9} lg={9} xl={10} >
-                <Paper style={{paddingLeft:"40px"}}>
-                    <p>
-                        An application is a logical collection of APIs. Applications allow you to use a single access
-                        token to invoke a collection of APIs and to subscribe to one API multiple times with different
-                        SLA levels. The DefaultApplication is pre-created and allows unlimited access by default.
-                    </p>
-                    <Table>
-                        <ApplicationTableHead order={order} orderBy={orderBy} onRequestSort={this.handleRequestSort}/>
-                        <TableBody>
-                            {data.map(n => {
-                                return (
-                                    <TableRow hover tabIndex="-1" key={n.applicationId}>
-                                        <TableCell disablePadding>
-                                            <Link to={"/applications/" + n.applicationId}>
-                                                {n.name}
-                                            </Link>
-                                        </TableCell>
-                                        <TableCell disablePadding>
-                                            {n.throttlingTier}
-                                        </TableCell>
-                                        <TableCell disablePadding>
-                                            {n.lifeCycleStatus}
-                                        </TableCell>
-                                        <TableCell disablePadding>
-                                            0
-                                        </TableCell>
-                                        <TableCell disablePadding>
-                                            View Edit Delete
-                                        </TableCell>
-                                    </TableRow>
-                                );
-                            })}
-                        </TableBody>
-                    </Table>
-                </Paper>
-            </Grid>
-        </Grid>
         );
     }
 }
 
 
-export default Listing;
+export default withStyles(styles)(Listing);
