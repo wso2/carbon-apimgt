2a6a4719a8ba78c7402b657ae0f0ab31820985d2
ishara.cooray@gmail.com <ishara.cooray@gmail.com>
2017-12-20 12:00:04 +0530

    fix for https://github.com//wso2/product-apim/issues/1735

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/search/api-search/js/api-search.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/search/api-search/js/api-search.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#apiSearch').keydown(function(event) {
         if (event.which == 13) {
             event.preventDefault();
-            apiProviderApp.searchAPIs();
+            $("#searchAPI")[0].click();
         }
     });
 
