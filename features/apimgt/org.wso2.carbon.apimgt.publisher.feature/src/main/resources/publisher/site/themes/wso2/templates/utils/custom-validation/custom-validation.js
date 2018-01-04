b24b5bd050a114d6af4597811d955b489ab7e352
Thilini Shanika <tgtshanika@gmail.com>
2018-01-04 12:16:49 +0530

    Fixing validation issues in API create phase

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/utils/custom-validation/custom-validation.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/utils/custom-validation/custom-validation.js
@@ -47,8 +47,7 @@ $(document).ready(function() {
     $.validator.addMethod('validAPIName', function(value, element) {
         var illegalChars = /([~!@#;:%^*()+={}|\\<>"',&\/$])/;
         return !illegalChars.test(value);
-    }, i18n.t('Name contains one or more illegal characters  (~ ! @ #  ; : % ^ & * + = { } () | &lt; &gt;, \' " \\ $)
-    .'));
+    }, i18n.t('Name contains one or more illegal characters  (~ ! @ #  ; : % ^ & * + = { } () | &lt; &gt;, \' " \\ $) .'));
 
     $.validator.addMethod('validContextTemplate', function(value, element) {
         var illegalChars = /([~!@#;%^&*+=\|\\<>\"\',])/;
