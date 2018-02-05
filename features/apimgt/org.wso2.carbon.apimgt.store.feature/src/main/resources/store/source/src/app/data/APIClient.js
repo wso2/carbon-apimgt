f0678ea4e6f27bf47cda437bc54d13036fcdf0d7
Renuka Fernando <renukapiyumal@gmail.com>
2018-02-05 10:43:47 +0530

    replace swagger-js patch with swagger-js 3.4.8

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/source/src/app/data/APIClient.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/source/src/app/data/APIClient.js
@@ -40,6 +40,7 @@ class APIClient {
             }
         };
 
+        SwaggerClient.http.withCredentials = true;
         let promisedResolve = SwaggerClient.resolve({url: Utils.getSwaggerURL()});
         APIClient.spec = promisedResolve;
         this._client = promisedResolve.then(
@@ -51,11 +52,8 @@ class APIClient {
                         requestInterceptor: this._getRequestInterceptor(),
                         responseInterceptor: this._getResponseInterceptor()
                     });
-                let swaggerClient = new SwaggerClient(argsv);
-                swaggerClient.then(client => {
-                    client.http.withCredentials = true
-                });
-                return swaggerClient;
+                SwaggerClient.http.withCredentials = true;
+                return new SwaggerClient(argsv);
             }
         );
         this._client.catch(AuthManager.unauthorizedErrorHandler);
@@ -95,6 +93,7 @@ class APIClient {
      */
     static getScopeForResource(resourcePath, resourceMethod) {
         if (!APIClient.spec) {
+            SwaggerClient.http.withCredentials = true;
             APIClient.spec = SwaggerClient.resolve({url: Utils.getSwaggerURL()});
         }
         return APIClient.spec.then(
