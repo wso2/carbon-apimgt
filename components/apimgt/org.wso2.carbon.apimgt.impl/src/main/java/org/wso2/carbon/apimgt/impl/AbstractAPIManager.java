b420c3fa83b0312740c11813448efb262146f993
Menaka Jayawardena <menaka@wso2.com>
2018-02-27 17:09:54 +0530

    Add the Capability of Restricting the Unlimited Subscription Tier
    * Feature implementation.
    * UI modifications.
    * Test Cases.

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/AbstractAPIManager.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/AbstractAPIManager.java
@@ -57,6 +57,7 @@ import org.wso2.carbon.apimgt.api.model.policy.Policy;
 import org.wso2.carbon.apimgt.api.model.policy.PolicyConstants;
 import org.wso2.carbon.apimgt.impl.dao.ApiMgtDAO;
 import org.wso2.carbon.apimgt.impl.definitions.APIDefinitionFromOpenAPISpec;
+import org.wso2.carbon.apimgt.impl.dto.ThrottleProperties;
 import org.wso2.carbon.apimgt.impl.internal.ServiceReferenceHolder;
 import org.wso2.carbon.apimgt.impl.utils.APINameComparator;
 import org.wso2.carbon.apimgt.impl.utils.APIUtil;
@@ -1903,6 +1904,26 @@ public abstract class AbstractAPIManager implements APIManager {
         } else if (PolicyConstants.POLICY_LEVEL_GLOBAL.equals(level)) {
             policies = apiMgtDAO.getGlobalPolicies(tenantID);
         }
+
+        //Get the API Manager configurations and check whether the unlimited tier is disabled. If disabled, remove
+        // the tier from the array.
+        APIManagerConfiguration apiManagerConfiguration = ServiceReferenceHolder.getInstance()
+                .getAPIManagerConfigurationService().getAPIManagerConfiguration();
+        ThrottleProperties throttleProperties = apiManagerConfiguration.getThrottleProperties();
+
+        if (!throttleProperties.isEnableUnlimitedTier()) {
+            List<Policy> policiesWithoutUnlimitedTier = new ArrayList<Policy>();
+
+            if (policies != null) {
+                for (Policy policy : policies) {
+                    if (!"unlimited".equalsIgnoreCase(policy.getPolicyName())) {
+                        policiesWithoutUnlimitedTier.add(policy);
+                    }
+                }
+            }
+            policies = policiesWithoutUnlimitedTier.toArray(new Policy[0]);
+        }
+
         return policies;
     }
 
