1ab4036369ebb0ef933f08acd0e0e1266f74bbb1
Tharindu Dharmarathna <tharindua@wso2.com>
2018-02-28 22:16:59 +0530

    fixing https://github.com/wso2/product-apim/issues/2815

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
@@ -6553,11 +6553,13 @@ public final class APIUtil {
                 searchValue = criteria.split(":")[1];
                 if (!APIConstants.DOCUMENTATION_SEARCH_TYPE_PREFIX.equalsIgnoreCase(searchKey) &&
                         !APIConstants.TAG_SEARCH_TYPE_PREFIX.equalsIgnoreCase(searchKey)) {
-                    if (!searchValue.endsWith("*")) {
-                        searchValue = searchValue + "*";
-                    }
-                    if (!searchValue.startsWith("*")) {
-                        searchValue = "*" + searchValue;
+                    if (!(searchValue.endsWith("\"") && searchValue.startsWith("\""))) {
+                        if (!searchValue.endsWith("*")) {
+                            searchValue = searchValue + "*";
+                        }
+                        if (!searchValue.startsWith("*")) {
+                            searchValue = "*" + searchValue;
+                        }
                     }
                 }
 
@@ -6565,11 +6567,13 @@ public final class APIUtil {
                 throw new APIManagementException("Search term is missing. Try again with valid search query.");
             }
         } else {
-            if (!searchValue.endsWith("*")) {
-                searchValue = searchValue + "*";
-            }
-            if (!searchValue.startsWith("*")) {
-                searchValue = "*" + searchValue;
+            if (!(searchValue.endsWith("\"") && searchValue.startsWith("\""))) {
+                if (!searchValue.endsWith("*")) {
+                    searchValue = searchValue + "*";
+                }
+                if (!searchValue.startsWith("*")) {
+                    searchValue = "*" + searchValue;
+                }
             }
         }
         if (APIConstants.API_PROVIDER.equalsIgnoreCase(searchKey)) {
