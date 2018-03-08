16948c75afb09987192e39eccb37964fb9a4daf1
Kasun Thennakoon <tmkasun@gmail.com>
2018-03-08 15:24:16 +0530

    Fix issues thrown from eslint

---
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/App.jsx
@@ -0,0 +1,192 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import qs from 'qs';
+import { getAsyncComponent } from 'async-react-component';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import Log from 'log4javascript';
+import './App.css';
+import Utils from './app/data/Utils';
+import ConfigManager from './app/data/ConfigManager';
+import MaterialDesignCustomTheme from './app/components/Shared/CustomTheme';
+import { PageNotFound } from './app/components/Base/Errors';
+import AuthManager from './app/data/AuthManager';
+
+const themes = [];
+const darkTheme = createMuiTheme({
+    palette: {
+        type: 'dark', // Switching the dark mode on is a single property value change.
+    },
+});
+const lightTheme = createMuiTheme({
+    palette: {
+        type: 'light', // Switching the dark mode on is a single property value change.
+    },
+});
+darkTheme.palette.background.active = 'rgba(27, 94, 32, 1)';
+darkTheme.palette.background.appBar = 'rgba(63, 81, 181, 1)';
+lightTheme.palette.background.active = 'rgba(165, 214, 167, 1)';
+lightTheme.palette.background.appBar = 'rgb(59, 120, 231,1)';
+lightTheme.palette.background.contentFrame = 'rgba(227, 242, 253, 1)';
+lightTheme.palette.text.brand = 'rgba(255,255,255,1)';
+themes.push(darkTheme);
+themes.push(lightTheme);
+themes.push(createMuiTheme(MaterialDesignCustomTheme));
+
+const Apis = () => import(/* webpackChunkName: "apis" */ './app/components/Apis/Apis');
+const Endpoints = () => import(/* webpackChunkName: "endpoints" */ './app/components/Endpoints');
+const ApiCreate = () => import(/* webpackChunkName: "create" */ './app/components/Apis/Create/ApiCreate');
+const Base = () => import(/* webpackChunkName: "base" */ './app/components/Base');
+const BaseLayout = getAsyncComponent(Base);
+const Login = () => import(/* webpackChunkName: "login" */ './app/components/Login/Login');
+const Logout = () => import(/* webpackChunkName: "logout" */ './app/components/Logout');
+
+/**
+ * Render protected application paths
+ */
+class Protected extends Component {
+    /**
+     * Creates an instance of Protected.
+     * @param {any} props @inheritDoc
+     * @memberof Protected
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            themeIndex: 0,
+        };
+        this.environments = [];
+    }
+
+    /**
+     * @inheritDoc
+     * @memberof Protected
+     */
+    componentWillMount() {
+        const storedThemeIndex = localStorage.getItem('themeIndex');
+        if (storedThemeIndex) {
+            this.setState({ themeIndex: parseInt(storedThemeIndex, 10) });
+        }
+    }
+
+    /**
+     * @inheritDoc
+     * @memberof Protected
+     */
+    componentDidMount() {
+        ConfigManager.getConfigs()
+            .environments.then((response) => {
+                this.environments = response.data.environments;
+                this.handleEnvironmentQueryParam();
+            })
+            .catch((error) => {
+                Log.error('Error while receiving environment configurations : ', error);
+            });
+    }
+
+    /**
+     * Change the theme index incrementally
+     */
+    setTheme() {
+        this.state.themeIndex++;
+        localStorage.setItem('themeIndex', this.state.themeIndex);
+    }
+
+    /**
+     * Change the environment with "environment" query parameter
+     * @return {String} environment name in the query param
+     */
+    handleEnvironmentQueryParam() {
+        let queryString = this.props.location.search;
+        queryString = queryString.replace(/^\?/, '');
+        /* With QS version up we can directly use {ignoreQueryPrefix: true} option */
+        const queryParams = qs.parse(queryString);
+        const environmentName = queryParams.environment;
+
+        if (!environmentName || Utils.getCurrentEnvironment() === environmentName) {
+            // no environment query param or the same environment
+            return environmentName;
+        }
+
+        const environmentId = Utils.getEnvironmentID(this.environments, environmentName);
+        if (environmentId === -1) {
+            return environmentName;
+        }
+
+        const environment = this.environments[environmentId];
+        Utils.setEnvironment(environment);
+        return environmentName;
+    }
+
+    /**
+     * @returns {React.Component} @inheritDoc
+     * @memberof Protected
+     */
+    render() {
+        const environmentName = this.handleEnvironmentQueryParam();
+        // Note: AuthManager.getUser() method is a passive check, which simply check the user availability in browser
+        // storage,
+        // Not actively check validity of access token from back-end
+
+        if (AuthManager.getUser(environmentName)) {
+            return (
+                <MuiThemeProvider theme={themes[this.state.themeIndex % 3]}>
+                    <BaseLayout setTheme={() => this.setTheme()}>
+                        <Switch>
+                            <Redirect exact from='/' to='/apis' />
+                            <Route path='/apis' component={getAsyncComponent(Apis)} />
+                            <Route path='/endpoints' component={getAsyncComponent(Endpoints)} />
+                            <Route path='/api/create' component={getAsyncComponent(ApiCreate)} />
+                            <Route component={PageNotFound} />
+                        </Switch>
+                    </BaseLayout>
+                </MuiThemeProvider>
+            );
+        }
+
+        const params = qs.stringify({ referrer: this.props.location.pathname });
+        return <Redirect to={{ pathname: '/login', search: params }} />;
+    }
+}
+
+Protected.propTypes = {
+    location: PropTypes.shape({
+        pathname: PropTypes.string.isRequired,
+        search: PropTypes.string,
+    }).isRequired,
+};
+
+/**
+ * Define base routes for the application
+ * @returns {React.Component} base routes for the application
+ */
+const Publisher = () => {
+    return (
+        <Router basename='/publisher'>
+            <Switch>
+                <Route path='/login' component={getAsyncComponent(Login)} />
+                <Route path='/logout' component={getAsyncComponent(Logout)} /> <Route component={Protected} />
+            </Switch>
+        </Router>
+    );
+};
+
+export default Publisher;
---
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/index.jsx
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, { Component } from 'react';
+import { Link, Redirect, Route, Switch } from 'react-router-dom';
+import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
+import Log from 'log4javascript';
+import PropTypes from 'prop-types';
+
+import Overview from './Overview';
+import LifeCycle from './LifeCycle/LifeCycle';
+import Documents from './Documents/Documents';
+import { PageNotFound } from '../../Base/Errors/index';
+import Resources from './Resources/Resources';
+import PermissionFormWrapper from './Permission';
+import Endpoints from './Endpoints';
+import Subscriptions from './Subscriptions/Subscriptions';
+import Scopes from './Scopes/Scopes';
+import Security from './Security';
+import NavBar from './NavBar';
+import EnvironmentOverview from './EnvironmentOverview/EnvironmentOverview';
+import Utils from '../../../data/Utils';
+import ConfigManager from '../../../data/ConfigManager';
+
+/**
+ * Base component for API specific Details page, This component will be mount for any request coming for /apis/:api_uuid
+ */
+export default class Details extends Component {
+    /**
+     * Creates an instance of Details.
+     * @param {any} props @inheritDoc
+     * @memberof Details
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            resourceNotFountMessage: { multi_environments: false },
+        };
+    }
+
+    /**
+     * @inheritDoc
+     * @memberof Details
+     */
+    componentDidMount() {
+        ConfigManager.getConfigs()
+            .environments.then((response) => {
+                const multiEnvironments = response.data.environments.length > 1;
+                const more = multiEnvironments && (
+                    <Link to='/apis'>
+                        <Button variant='raised' color='secondary'>
+                            Go Home
+                        </Button>
+                    </Link>
+                );
+                this.setState({
+                    resourceNotFountMessage: { more, multiEnvironments },
+                });
+            })
+            .catch((error) => {
+                Log.error('Error while receiving environment configurations : ', error);
+            });
+    }
+
+    /**
+     * Renders Grid container layout with NavBar place static in LHS, Components which coming as children for
+     * Details page
+     * should wrap it's content with <Grid item > element
+     * @returns {Component} Render API Details page
+     */
+    render() {
+        const apiUUID = this.props.match.params.api_uuid;
+        const redirectUrl = `/apis/${apiUUID}/overview`;
+
+        const environmentOverview = Utils.isMultiEnvironmentOverviewEnabled();
+        const { resourceNotFountMessage } = this.state;
+
+        if (resourceNotFountMessage.multi_environments) {
+            resourceNotFountMessage.title = `API is Not Found in the "${
+                Utils.getCurrentEnvironment().label
+            }" Environment`;
+            resourceNotFountMessage.body = `Can't find the API with the id "${apiUUID}"`;
+        }
+
+        return (
+            <Grid container spacing={0}>
+                <Grid item xs={12}>
+                    <NavBar api_uuid={this.props.match.params.api_uuid} />
+                </Grid>
+                <Grid item xs={12}>
+                    <Switch>
+                        <Redirect exact from='/apis/:api_uuid' to={redirectUrl} />
+                        <Route
+                            path='/apis/:api_uuid/overview'
+                            render={props => <Overview resourceNotFountMessage={resourceNotFountMessage} {...props} />}
+                        />
+                        {environmentOverview ? (
+                            <Route
+                                path='/apis/:api_uuid/environment view'
+                                render={props => (
+                                    <EnvironmentOverview resourceNotFountMessage={resourceNotFountMessage} {...props} />
+                                )}
+                            />
+                        ) : null}
+                        <Route
+                            path='/apis/:api_uuid/lifecycle'
+                            render={props => <LifeCycle resourceNotFountMessage={resourceNotFountMessage} {...props} />}
+                        />
+                        <Route
+                            path='/apis/:api_uuid/resources'
+                            render={props => <Resources resourceNotFountMessage={resourceNotFountMessage} {...props} />}
+                        />
+                        <Route
+                            path='/apis/:api_uuid/permission'
+                            render={props => (
+                                <PermissionFormWrapper resourceNotFountMessage={resourceNotFountMessage} {...props} />
+                            )}
+                        />
+                        <Route
+                            path='/apis/:api_uuid/documents'
+                            render={props => <Documents resourceNotFountMessage={resourceNotFountMessage} {...props} />}
+                        />
+                        <Route
+                            path='/apis/:api_uuid/endpoints'
+                            render={props => <Endpoints resourceNotFountMessage={resourceNotFountMessage} {...props} />}
+                        />
+                        <Route
+                            path='/apis/:api_uuid/subscriptions'
+                            render={props => (
+                                <Subscriptions resourceNotFountMessage={resourceNotFountMessage} {...props} />
+                            )}
+                        />
+                        <Route
+                            path='/apis/:api_uuid/security'
+                            render={props => <Security resourceNotFountMessage={resourceNotFountMessage} {...props} />}
+                        />
+                        <Route
+                            path='/apis/:api_uuid/scopes'
+                            render={props => <Scopes resourceNotFountMessage={resourceNotFountMessage} {...props} />}
+                        />
+                        <Route component={PageNotFound} />
+                    </Switch>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+Details.propTypes = {
+    match: PropTypes.shape({
+        params: PropTypes.object,
+    }).isRequired,
+};
