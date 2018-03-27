eee495853cbeb9cb59686fbc1566e405479741b9
Kasun Thennakoon <tmkasun@gmail.com>
2018-03-27 10:11:12 +0530

    Fix invalid prop usage in NavBar MenueItem and InteractiveButton props validation

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Shared/InteractiveButton.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Shared/InteractiveButton.jsx
@@ -17,9 +17,10 @@
  */
 
 import React from 'react';
-import { Button, CircularProgress } from 'material-ui/Progress';
+import { CircularProgress } from 'material-ui/Progress';
 import { withStyles } from 'material-ui/styles';
 import green from 'material-ui/colors/green';
+import { Button } from 'material-ui';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -50,15 +51,37 @@ const styles = theme => ({
  * @returns {React.Component} Button component
  */
 const InteractiveButton = (props) => {
+    // TODO: This prop transformation can be nicely done with ES6  (Rest/Spread Properties) if use stage 2 support ~tmkb
     const {
-        loading, success, children, classes,
+        loading,
+        success,
+        children,
+        classes,
+        color,
+        disableFocusRipple,
+        disableRipple,
+        fullWidth,
+        href,
+        mini,
+        size,
+        variant,
     } = props;
+    const buttonProps = {
+        color,
+        disableFocusRipple,
+        disableRipple,
+        fullWidth,
+        href,
+        mini,
+        size,
+        variant,
+    };
     const buttonClassName = classNames({
         [classes.buttonSuccess]: success,
     });
     return (
         <div className={classes.wrapper}>
-            <Button {...props} className={buttonClassName} disabled={loading}>
+            <Button {...buttonProps} className={buttonClassName} disabled={loading}>
                 {children}
             </Button>
             {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
@@ -69,12 +92,66 @@ const InteractiveButton = (props) => {
 InteractiveButton.defaultProps = {
     loading: false,
     success: false,
+    color: 'default',
+    disabled: false,
+    disableFocusRipple: false,
+    fullWidth: false,
+    mini: false,
+    size: 'medium',
+    type: 'button',
+    variant: 'flat',
+    disableRipple: false,
+    href: '',
 };
 
+/* porpTypes taken from MUI button class */
 InteractiveButton.propTypes = {
     loading: PropTypes.bool,
     success: PropTypes.bool,
     children: PropTypes.node.isRequired,
     classes: PropTypes.shape({}).isRequired,
+    /**
+     * The color of the component. It supports those theme colors that make sense for this component.
+     */
+    color: PropTypes.oneOf(['default', 'inherit', 'primary', 'secondary']),
+    /**
+     * If `true`, the button will be disabled.
+     */
+    disabled: PropTypes.bool,
+    /**
+     * If `true`, the  keyboard focus ripple will be disabled.
+     * `disableRipple` must also be true.
+     */
+    disableFocusRipple: PropTypes.bool,
+    /**
+     * If `true`, the ripple effect will be disabled.
+     */
+    disableRipple: PropTypes.bool,
+    /**
+     * If `true`, the button will take up the full width of its container.
+     */
+    fullWidth: PropTypes.bool,
+    /**
+     * The URL to link to when the button is clicked.
+     * If defined, an `a` element will be used as the root node.
+     */
+    href: PropTypes.string,
+    /**
+     * If `true`, and `variant` is `'fab'`, will use mini floating action button styling.
+     */
+    mini: PropTypes.bool,
+    /**
+     * The size of the button.
+     * `small` is equivalent to the dense button styling.
+     */
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    /**
+     * @ignore
+     */
+    type: PropTypes.string,
+    /**
+     * The type of button.
+     */
+    variant: PropTypes.oneOf(['flat', 'raised', 'fab']),
 };
 export default withStyles(styles)(InteractiveButton);
