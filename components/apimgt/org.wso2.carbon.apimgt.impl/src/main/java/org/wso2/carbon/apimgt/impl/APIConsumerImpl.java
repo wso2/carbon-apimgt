ea8713b963232f7bb1e94eb6c1126edfe85838ac
krish <krishanx92>
2018-04-04 14:32:58 +0530

    Fix store visibility for anonymous user

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConsumerImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConsumerImpl.java
@@ -3828,15 +3828,9 @@ class APIConsumerImpl extends AbstractAPIManager implements APIConsumer {
 
     @Override
     protected String getSearchQuery(String searchQuery) throws APIManagementException {
-        if (userNameWithoutChange == null) {
-            String criteria = APIConstants.STORE_VIEW_ROLES + "= (" + APIConstants.NULL_USER_ROLE_LIST + ")";
-            if (!StringUtils.isEmpty(searchQuery)) {
-                criteria = criteria + "&" + searchQuery;
-            }
-            return criteria;
-        }
-        if (!isAccessControlRestrictionEnabled || APIUtil.hasPermission(userNameWithoutChange, APIConstants.Permissions
-                .APIM_ADMIN)) {
+        if (!isAccessControlRestrictionEnabled || ( userNameWithoutChange != null &&
+                APIUtil.hasPermission(userNameWithoutChange, APIConstants.Permissions
+                .APIM_ADMIN))) {
             return searchQuery;
         }
         String criteria = getUserRoleListQuery();
