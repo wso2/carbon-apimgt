f9ff6381ef47b96b85e4e8f293904d198cb5dc19
bhathiya <bhathiya@wso2.com>
2018-05-22 16:39:45 +0530

    Add missing license headers

--- components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/test/java/org/wso2/carbon/apimgt/hostobjects/HostObjectUtilsTest.java
+++ components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/test/java/org/wso2/carbon/apimgt/hostobjects/HostObjectUtilsTest.java
@@ -1,3 +1,19 @@
+/*
+* Copyright (c), WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.wso2.carbon.apimgt.hostobjects;
 
 import org.apache.axis2.context.ConfigurationContext;
@@ -5,7 +21,6 @@ import org.apache.axis2.engine.AxisConfiguration;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -14,10 +29,8 @@ import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.hostobjects.internal.HostObjectComponent;
 import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.impl.APIManagerConfiguration;
-import org.wso2.carbon.apimgt.impl.utils.APIUtil;
 import org.wso2.carbon.apimgt.keymgt.client.ProviderKeyMgtClient;
 import org.wso2.carbon.apimgt.keymgt.client.SubscriberKeyMgtClient;
-import org.wso2.carbon.authenticator.stub.AuthenticationAdminStub;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.identity.user.registration.stub.dto.UserFieldDTO;
 import org.wso2.carbon.ndatasource.common.DataSourceException;
