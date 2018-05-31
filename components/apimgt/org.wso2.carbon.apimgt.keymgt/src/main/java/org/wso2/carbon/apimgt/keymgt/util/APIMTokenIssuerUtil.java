8de3d12cf91be87fdfb77b3cecf6585bdfb9feca
Prasanna Dangalla <prasanna@Prasannas-MacBook-Pro-2.local>
2018-05-31 17:13:33 +0530

    Added the requested PR comments.

--- components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/util/APIMTokenIssuerUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/util/APIMTokenIssuerUtil.java
@@ -1,20 +1,20 @@
 /*
-*Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*WSO2 Inc. licenses this file to you under the Apache License,
-*Version 2.0 (the "License"); you may not use this file except
-*in compliance with the License.
-*You may obtain a copy of the License at
-*
-*http://www.apache.org/licenses/LICENSE-2.0
-*
-*Unless required by applicable law or agreed to in writing,
-*software distributed under the License is distributed on an
-*"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-*KIND, either express or implied.  See the License for the
-*specific language governing permissions and limitations
-*under the License.
-*/
+ *Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *WSO2 Inc. licenses this file to you under the Apache License,
+ *Version 2.0 (the "License"); you may not use this file except
+ *in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing,
+ *software distributed under the License is distributed on an
+ *"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *KIND, either express or implied.  See the License for the
+ *specific language governing permissions and limitations
+ *under the License.
+ */
 package org.wso2.carbon.apimgt.keymgt.util;
 
 import org.apache.commons.lang.StringUtils;
@@ -37,7 +37,7 @@ import java.util.Map;
 
 public class APIMTokenIssuerUtil {
 
-    public static JwtTokenInfoDTO getJwtTokenInfoDTO(Application application) {
+    public static JwtTokenInfoDTO getJwtTokenInfoDTO(Application application) throws APIManagementException {
 
         String tenantDomain = PrivilegedCarbonContext.getThreadLocalCarbonContext().getTenantDomain();
         String userName = application.getOwner();
@@ -49,12 +49,9 @@ public class APIMTokenIssuerUtil {
 
             tenantedUserName = userName + "@" + tenantDomain;
         }
-        APISubscriptionInfoDTO[] apis = new APISubscriptionInfoDTO[0];
-        try {
-            apis = ApiMgtDAO.getInstance().getSubscribedAPIsOfUserWithSubscriptionInfo(tenantedUserName);
-        } catch (APIManagementException e) {
-            e.printStackTrace();
-        }
+
+        APISubscriptionInfoDTO[] apis = ApiMgtDAO.getInstance().getSubscribedAPIsOfUserWithSubscriptionInfo(tenantedUserName);
+
 
         JwtTokenInfoDTO jwtTokenInfoDTO = new JwtTokenInfoDTO();
         jwtTokenInfoDTO.setSubscriber("sub");
@@ -62,9 +59,7 @@ public class APIMTokenIssuerUtil {
         jwtTokenInfoDTO.setContentAware(true);
 
         List<SubscribedApiDTO> subscribedApiDTOList = new ArrayList<SubscribedApiDTO>();
-        for (APISubscriptionInfoDTO api : apis)
-
-        {
+        for (APISubscriptionInfoDTO api : apis) {
             SubscribedApiDTO subscribedApiDTO = new SubscribedApiDTO();
             subscribedApiDTO.setName(api.getApiName());
             subscribedApiDTO.setContext(api.getContext());
