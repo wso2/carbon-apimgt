2d3b60845bc0273879f659987e0d2c980db6ba13
krish <krishanx92>
2018-06-18 13:43:06 +0530

    Fix label attach issue in IE 11

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/utils/common/common.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/utils/common/common.js
@@ -133,3 +133,16 @@ $('.secondary-nav').on('shown.bs.collapse', function(e){
 }).on('hidden.bs.collapse',function(e){
     $('.secondary-nav-toggle').removeClass('fw-up').addClass('fw-down');
 })
+
+/***********************************************************
+ *  String endWith Implementation
+ ***********************************************************/
+
+if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(search, this_len) {
+ 		if (this_len === undefined || this_len > this.length) {
+ 			this_len = this.length;
+ 		}
+ 		return this.substring(this_len - search.length, this_len) === search;
+ 	};
+}
