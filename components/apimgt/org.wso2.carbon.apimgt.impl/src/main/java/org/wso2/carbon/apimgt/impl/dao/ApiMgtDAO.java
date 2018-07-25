79aa7d0b261adb9fb5c45d14d608078e9f6cc3dc
krish <krishanx92>
2018-07-25 18:11:16 +0530

    Fix formatting

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -5697,7 +5697,7 @@ public class ApiMgtDAO {
         ArrayList<URITemplate> uriTemplates = new ArrayList<URITemplate>();
 
         String apiTenantDomain = MultitenantUtils.getTenantDomainFromRequestURL(apiContext);
-        if(apiTenantDomain != null) {
+        if (apiTenantDomain != null) {
             tenantId = APIUtil.getTenantIdFromTenantDomain(apiTenantDomain);
         } else {
             tenantId = MultitenantConstants.SUPER_TENANT_ID;
