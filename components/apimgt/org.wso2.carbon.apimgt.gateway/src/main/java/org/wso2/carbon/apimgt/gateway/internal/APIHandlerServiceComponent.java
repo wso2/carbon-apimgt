2d5ecd32cda802bf225b39cfd985a5ee675f161d
Praveen Nadarajah <n.praveen.rc@gmail.com>
2018-08-26 22:45:02 +0530

    remove dependency

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/internal/APIHandlerServiceComponent.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/internal/APIHandlerServiceComponent.java
@@ -189,7 +189,6 @@ public class APIHandlerServiceComponent {
         return axis2ClientXml;
     }
     protected void setTracingService(TracingService tracingService) {
-
         ServiceReferenceHolder.getInstance().setTracingService(tracingService);
     }
     protected void unsetTracingService(TracingService tracingService) {
--- components/apimgt/org.wso2.carbon.apimgt.tracing/src/main/java/org/wso2/carbon/apimgt/tracing/internal/TracingServiceComponent.java
+++ components/apimgt/org.wso2.carbon.apimgt.tracing/src/main/java/org/wso2/carbon/apimgt/tracing/internal/TracingServiceComponent.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.carbon.apimgt.tracing.internal;
 
 import org.apache.commons.logging.Log;
@@ -26,7 +44,7 @@ public class TracingServiceComponent {
             bundleContext.registerService(TracingService.class, new TracingServiceImpl(), null);
 
         } catch (Throwable t) {
-            log.error("Error occured", t);
+            log.error("Error occured in tracing component activation", t);
         }
     }
     protected void deactivate(ComponentContext componentContext) {
@@ -34,16 +52,10 @@ public class TracingServiceComponent {
     }
 
     protected void setAPIManagerConfigurationService(APIManagerConfigurationService amcService) {
-        if (log.isDebugEnabled()) {
-            log.debug("API manager configuration service bound to the API handlers");
-        }
         ServiceReferenceHolder.getInstance().setAPIManagerConfigurationService(amcService);
     }
 
     protected void unsetAPIManagerConfigurationService(APIManagerConfigurationService amcService) {
-        if (log.isDebugEnabled()) {
-            log.debug("API manager configuration service unbound from the API handlers");
-        }
         ServiceReferenceHolder.getInstance().setAPIManagerConfigurationService(null);
     }
 }
