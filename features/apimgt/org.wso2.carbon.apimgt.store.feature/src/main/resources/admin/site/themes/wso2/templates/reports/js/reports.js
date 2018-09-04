046915eb950cfa047183e9e66b31609c8cafd32b
Chamila Adhikarinayake <cka.adhi@gmail.com>
2018-09-04 06:08:30 +0530

    Revert "Revert "Update MGW Summary Report Generation UI""

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin/site/themes/wso2/templates/reports/js/reports.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin/site/themes/wso2/templates/reports/js/reports.js
@@ -1,31 +1,21 @@
 $(document).ready(
-   function() {
-       $('#downloadPDFForm').validate({
-           submitHandler: function(form) {
-                var month = $("#datepickerVal").val();
-                window.location.href = '/admin/site/themes/wso2/templates/reports/download.jag?month=' + month;
-           }
-       });
+    function() {
+        var date = new Date();
+        var currentYear = date.getFullYear();
+        var currentMonth = date.getMonth(); // This is the index. getMonth() starts with 0th index
 
-    chart = "";
-    chartData = "";
-    var d = new Date();
-    var currentDay = new Date(d.getFullYear(), d.getMonth(), d.getDate(),d.getHours(),d.getMinutes());
+        var yearSel = $('#year-selector');
+        var monthSel = $('#month-selector');
+        yearSel.val(currentYear);
+        yearSel.attr('max', currentYear);
+        monthSel.prop("selectedIndex", currentMonth);
 
-    $('#date-range').daterangepicker({
-        singleDatePicker: true,
-        timePicker: false,
-        timePickerIncrement: 30,
-        format: 'YYYY-MM-DD HH:mm:ss',
-        startDate: moment().subtract(1, 'month'),
-        endDate: moment().add(1, 'day').format('YYYY-MM-DD HH:mm:ss'),
-        opens: 'right'
-
-    }).on('hide.daterangepicker', function (ev, picker) {
-      $('.table-condensed tbody tr:nth-child(2) td').click();
-      var option = picker.startDate.format('YYYY-MM');
-      $('#datepickerVal').val(option);
-    });
-
-   }
+        $('#downloadPDFForm').validate({
+            submitHandler: function(form) {
+                var monthOfYear = yearSel.val() + '-' + monthSel.val();
+                form.action = '/admin/site/themes/wso2/templates/reports/download.jag?month=' + monthOfYear;
+                form.submit();
+            }
+        });
+    }
 );
