ce21f8e24d36a1af5a331aaa929f07be92831327
Praminda Jayawardana <pramindabj@gmail.com>
2018-09-04 15:00:15 +0530

    store: alerts: Add initial UI for alert config
    Some alerts have configuration values that should be set by
    subscribers. This effort is to move current SP side feature
    to API Store.

---
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/templates/configure-alert/js/configure-alert.js
@@ -0,0 +1,51 @@
+$(function() {
+    var appsElement = $("#appName");
+    var apisElement = $("#apiName");
+    populateAppList();
+
+    /**
+     * Populate Application Name 'select' element with subscribed application list
+    **/
+    function populateAppList() {
+        var params = {action: "getApplications"};
+        
+        $.get("../blocks/application/application-list/ajax/application-list.jag", params, function(data) {
+            if (data && data.error == false) {
+                $.each(data.applications, function() {
+                    appsElement.append($("<option />").val(this.id).text(this.name));
+                });
+
+                appsElement.selectpicker('refresh');
+                populateApiList();
+            }
+        });
+    }
+
+    /**
+     * Populate API Name 'select' element with subscribed list of APIs
+     * provider--apiName--apiVersion format is used to list the APIs. This is to
+     * avoid complexity in the UI and the logic required to show the API list
+    **/
+    function populateApiList() {
+        var appId = $("#appName option:selected").text();
+        var params = {action: "getSubscriptionByApplication", app: appId, groupId: ""};
+        apisElement.empty();
+
+        $.get("../blocks/subscription/subscription-list/ajax/subscription-list.jag", params, function(data) {
+            if (data && data.error == false) {
+                $.each(data.apis, function() {
+                    var apiId = this.apiProvider + '--' + this.apiName + '--' + this.apiVersion;
+                    apisElement.append($("<option />").val(apiId).text(apiId));
+                });
+
+                apisElement.selectpicker('refresh');
+            }
+        });
+    }
+
+    // Register on change event listener for Application Name 'select' element
+    appsElement.on('changed.bs.select', function (e) {
+        populateApiList();
+    });
+
+});
