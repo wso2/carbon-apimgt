5f675133b7681927513ffce54a9c7e943f983b4d
Chanaka Jayasena <chanaka3d@gmail.com>
2018-09-27 16:49:33 +0530

    Update InfoBar.js

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/source/src/app/components/Applications/Details/InfoBar.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/source/src/app/components/Applications/Details/InfoBar.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
@@ -239,9 +257,9 @@ class InfoBar extends React.Component {
           </Link>
           <div className="vertical-divider-70" />
           <div style={{ marginLeft: theme.spacing.unit }}>
-            <Typography variant="display1">sddff</Typography>
+            <Typography variant="display1">{application.name}</Typography>
             <Typography variant="caption" gutterBottom align="left">
-              {application.name} | 21-May 2018
+              N Subscriptions
             </Typography>
           </div>
         </div>
