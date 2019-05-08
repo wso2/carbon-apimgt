fa54ece1b0593bc4755e223dae7cf592ac5afe02
Kasun Thennakoon <tmkasun@gmail.com>
2019-05-08 16:47:57 +0530

    Add delete scope to DCR call scope payload, Change API lifecycle component to work with changed REST API calls

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/data/AuthManager.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/data/AuthManager.js
@@ -121,6 +121,7 @@ class AuthManager {
                 if (data.active) {
                     const currentEnv = Utils.getCurrentEnvironment();
                     user = new User(currentEnv.label, data.username);
+                    user.scopes = data.scope.split(' ');
                     AuthManager.setUser(user, currentEnv.label);
                 } else {
                     console.warn('User with ' + partialToken + ' is not active!');
@@ -153,6 +154,16 @@ class AuthManager {
         User.destroyInMemoryUser(environmentName);
     }
 
+
+    /**
+     *
+     * Get scope for resources
+     * @static
+     * @param {String} resourcePath
+     * @param {String} resourceMethod
+     * @returns Boolean
+     * @memberof AuthManager
+     */
     static hasScopes(resourcePath, resourceMethod) {
         const userscopes = AuthManager.getUser().scopes;
         const validScope = APIClient.getScopeForResource(resourcePath, resourceMethod);
