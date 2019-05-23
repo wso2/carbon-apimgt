2504b74301c0f84bb8409b143db4ebfc6a3bfa8c
tharikaGitHub <tharika.m92@gmail.com>
2019-05-23 12:04:20 +0530

    Fix destination stats with query parameters

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/utils/GatewayUtils.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/utils/GatewayUtils.java
@@ -429,4 +429,17 @@ public class GatewayUtils {
         }
         return hostname;
     }
+
+    /**
+     * This method extracts the endpoint address base path if query parameters are contained in endpoint
+     * @param mc The message context
+     * @return The endpoint address base path
+     */
+    public static String extractAddressBasePath(org.apache.synapse.MessageContext mc) {
+        String endpointAddress = (String) mc.getProperty(APIMgtGatewayConstants.SYNAPSE_ENDPOINT_ADDRESS);
+        if (endpointAddress.contains("?")) {
+            endpointAddress = endpointAddress.substring(0, endpointAddress.indexOf("?"));
+        }
+        return endpointAddress;
+    }
 }
