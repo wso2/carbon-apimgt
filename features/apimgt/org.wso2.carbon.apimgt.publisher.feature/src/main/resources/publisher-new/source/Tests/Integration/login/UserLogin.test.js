37afd46cfc87d3294bb91aefa9364e99575030da
ruwiniwj <ruwi1025@gmail.com>
2019-06-28 16:54:36 +0530

    Add integration test for login redirect

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/Tests/Integration/login/UserLogin.test.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/Tests/Integration/login/UserLogin.test.js
@@ -32,7 +32,6 @@ describe(
         beforeEach(async () => {
             await page._client.send('Network.clearBrowserCookies');
             await page._client.send('Network.clearBrowserCache');
-            await page.goto('https://localhost:9443/publisher-new');
         });
 
         afterAll(async () => {
@@ -40,6 +39,7 @@ describe(
         });
 
         test('should able to login without error', async () => {
+            await page.goto('https://localhost:9443/publisher-new');
             await page.type('input[name="username"]', 'admin');
             await page.type('input[name="password"]', 'admin');
             await Promise.all([page.$eval('#loginForm', form => form.submit()), page.waitForNavigation()]);
@@ -61,6 +61,7 @@ describe(
         });
 
         test('should not able to login with an invalid username', async () => {
+            await page.goto('https://localhost:9443/publisher-new');
             await page.type('input[name="username"]', 'chuckNorris');
             await page.type('input[name="password"]', 'chuckNorris');
 
@@ -72,7 +73,21 @@ describe(
             expect(authFailure).toEqual('true');
         });
 
-        test.todo('should return to original location after login');
+
+        test('should return to original location after login', async () => {
+            const pathName = '/publisher-new/apis/create/rest';
+
+            await page.goto('https://localhost:9443' + pathName);
+            await page.type('input[name="username"]', 'admin');
+            await page.type('input[name="password"]', 'admin');
+            await Promise.all([page.$eval('#loginForm', form => form.submit()), page.waitForNavigation()]);
+            await page.click('input#approveCb[type="radio"]');
+            const consentSelector = await page.$('input#consent_select_all[type="checkbox"]');
+            if (consentSelector) await page.click('input#consent_select_all[type="checkbox"]');
+            await Promise.all([page.click('#approve'), page.waitForNavigation({ waitUntil: 'load' })]);
+            const currentPageURL = await page.url();
+            expect(currentPageURL).toContain(pathName);
+        });
     },
     timeout,
 );
