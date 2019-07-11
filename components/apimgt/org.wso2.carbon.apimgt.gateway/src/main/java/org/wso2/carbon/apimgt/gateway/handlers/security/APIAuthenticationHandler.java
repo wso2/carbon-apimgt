52e60d370e65daf0b9db342fe81997380d25b585
Chamod Samarajeewa <chamod.14@cse.mrt.ac.lk>
2019-07-11 12:44:32 +0530

    Move swagger parsing logic to APIAuthenticationHandler

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/APIAuthenticationHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/APIAuthenticationHandler.java
@@ -16,6 +16,8 @@
 
 package org.wso2.carbon.apimgt.gateway.handlers.security;
 
+import io.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
 import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMFactory;
@@ -30,6 +32,7 @@ import org.apache.synapse.ManagedLifecycle;
 import org.apache.synapse.Mediator;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.SynapseConstants;
+import org.apache.synapse.config.Entry;
 import org.apache.synapse.core.SynapseEnvironment;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
 import org.apache.synapse.rest.AbstractHandler;
@@ -88,6 +91,7 @@ public class APIAuthenticationHandler extends AbstractHandler implements Managed
     private String certificateInformation;
     private String apiUUID;
     private String apiType = String.valueOf(APIConstants.ApiTypes.API); // Default API Type
+    private Swagger swagger;
 
     public String getApiUUID() {
         return apiUUID;
@@ -281,7 +285,7 @@ public class APIAuthenticationHandler extends AbstractHandler implements Managed
         }
         if (isOAuthProtected) {
             authenticator = new OAuthAuthenticator(authorizationHeader, isOAuthBasicAuthMandatory,
-                    removeOAuthHeadersFromOutMessage, apiUUID, apiLevelPolicy);
+                    removeOAuthHeadersFromOutMessage, apiLevelPolicy);
             authenticator.init(synapseEnvironment);
             authenticators.add(authenticator);
         }
@@ -303,6 +307,21 @@ public class APIAuthenticationHandler extends AbstractHandler implements Managed
     @edu.umd.cs.findbugs.annotations.SuppressWarnings(value = "EXS_EXCEPTION_SOFTENING_RETURN_FALSE",
             justification = "Error is sent through payload")
     public boolean handleRequest(MessageContext messageContext) {
+        // Read swagger from local entry
+        if (swagger == null && apiUUID != null) {
+            synchronized (this) {
+                if (swagger == null) {
+                    Entry localEntryObj = (Entry) messageContext.getConfiguration().getLocalRegistry().get(apiUUID);
+                    if (localEntryObj != null) {
+                        SwaggerParser parser = new SwaggerParser();
+                        swagger = parser.parse(localEntryObj.getValue().toString());
+                    }
+                }
+            }
+        }
+        // Add swagger to message context
+        messageContext.setProperty(APIMgtGatewayConstants.API_SWAGGER, swagger);
+
         TracingSpan keySpan = null;
         if (Util.tracingEnabled()) {
             TracingSpan responseLatencySpan =
--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/oauth/OAuthAuthenticator.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/oauth/OAuthAuthenticator.java
@@ -17,14 +17,12 @@
 package org.wso2.carbon.apimgt.gateway.handlers.security.oauth;
 
 import io.swagger.models.Swagger;
-import io.swagger.parser.SwaggerParser;
 import org.apache.axis2.Constants;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpHeaders;
 import org.apache.synapse.MessageContext;
-import org.apache.synapse.config.Entry;
 import org.apache.synapse.core.SynapseEnvironment;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
 import org.apache.synapse.rest.RESTConstants;
@@ -75,19 +73,16 @@ public class OAuthAuthenticator implements Authenticator {
     private String requestOrigin;
     private String remainingAuthHeader;
     private boolean isMandatory;
-    private String apiUUID;
-    private Swagger swagger = null;
     private String apiLevelPolicy;
 
     public OAuthAuthenticator() {
     }
 
     public OAuthAuthenticator(String authorizationHeader, boolean isMandatory, boolean removeOAuthHeader,
-                              String apiUUID, String apiLevelPolicy) {
+                              String apiLevelPolicy) {
         this.securityHeader = authorizationHeader;
         this.removeOAuthHeadersFromOutMessage = removeOAuthHeader;
         this.isMandatory = isMandatory;
-        this.apiUUID = apiUUID;
         this.apiLevelPolicy = apiLevelPolicy;
     }
 
@@ -257,24 +252,20 @@ public class OAuthAuthenticator implements Authenticator {
             //Start JWT token validation
             if (StringUtils.countMatches(apiKey, ".") == 2) { // JWT token contains two dots
 
-                // Read swagger from local entry
-                if (swagger == null && apiUUID != null) {
-                    Entry localEntryObj = (Entry) synCtx.getConfiguration().getLocalRegistry().get(apiUUID);
-                    if (localEntryObj != null) {
-                        SwaggerParser parser = new SwaggerParser();
-                        swagger = parser.parse(localEntryObj.getValue().toString());
-                    }
-                }
-
                 try {
-                    AuthenticationContext authenticationContext = jwtValidator.authenticate(apiKey, synCtx, swagger);
-                    APISecurityUtils.setAuthenticationContext(synCtx, authenticationContext, securityContextHeader);
-
-                    if (log.isDebugEnabled()) {
+                    Swagger swagger = (Swagger) synCtx.getProperty(APIMgtGatewayConstants.API_SWAGGER);
+                    if (swagger == null) {
+                        log.debug("Swagger is missing in the gateway. " +
+                                "Therefore, JWT authentication cannot be performed.");
+                        return new AuthenticationResponse(false, isMandatory, true,
+                                APISecurityConstants.API_AUTH_GENERAL_ERROR,
+                                "JWT authentication cannot be performed.");
+                    } else {
+                        AuthenticationContext authenticationContext = jwtValidator.authenticate(apiKey, synCtx ,swagger);
+                        APISecurityUtils.setAuthenticationContext(synCtx, authenticationContext, securityContextHeader);
                         log.debug("User is authorized using JWT token to access the resource.");
+                        return new AuthenticationResponse(true, isMandatory, false, 0, null);
                     }
-                    return new AuthenticationResponse(true, isMandatory, false,
-                            0, null);
                 } catch (APISecurityException ex) {
                     return new AuthenticationResponse(false, isMandatory, true,
                             ex.getErrorCode(), ex.getMessage());
