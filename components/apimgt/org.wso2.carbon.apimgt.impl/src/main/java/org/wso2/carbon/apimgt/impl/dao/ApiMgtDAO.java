aa95f613f6bbf09ad8142ad1b8046e4da64ad114
tharikaGitHub <tharika.m92@gmail.com>
2019-07-19 11:05:27 +0530

    Fix review comments

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -3584,7 +3584,11 @@ public class ApiMgtDAO {
         return userRating;
     }
 
-
+    /**
+     * @param apiId API Identifier
+     * @param user        User name
+     * @throws APIManagementException if failed to get user API Ratings
+     */
     public JSONObject getUserRatingInfo(APIIdentifier apiId, String user) throws APIManagementException {
         Connection conn = null;
         JSONObject userRating = null;
@@ -3613,9 +3617,10 @@ public class ApiMgtDAO {
     /**
      * @param apiIdentifier API Identifier
      * @param userId        User Id
+     * @param conn          Database connection
      * @throws APIManagementException if failed to get user API Ratings
      */
-    public JSONObject getUserRatingInfo(APIIdentifier apiIdentifier, String userId, Connection conn)
+    private JSONObject getUserRatingInfo(APIIdentifier apiIdentifier, String userId, Connection conn)
             throws APIManagementException, SQLException {
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -3667,6 +3672,11 @@ public class ApiMgtDAO {
         return ratingObj;
     }
 
+     /**
+     * @param apiId           API Identifier
+     * @param ratingId        rating Id
+     * @throws APIManagementException if failed to get API Ratings by ID
+     */
     public JSONObject getApiRatingInfoById(APIIdentifier apiId, String ratingId) throws APIManagementException {
         Connection conn = null;
         JSONObject userRating = null;
@@ -3695,9 +3705,10 @@ public class ApiMgtDAO {
     /**
      * @param apiIdentifier API Identifier
      * @param ratingId        rating Id
+     * @param conn          Database connection
      * @throws APIManagementException if failed to get API Ratings by ID
      */
-    public JSONObject getApiRatingInfoById(APIIdentifier apiIdentifier, String ratingId, Connection conn)
+    private JSONObject getApiRatingInfoById(APIIdentifier apiIdentifier, String ratingId, Connection conn)
             throws APIManagementException, SQLException {
         PreparedStatement ps = null;
         PreparedStatement psSubscriber = null;
@@ -3755,6 +3766,10 @@ public class ApiMgtDAO {
         return ratingObj;
     }
 
+    /**
+     * @param apiId API Identifier
+     * @throws APIManagementException if failed to get API Ratings
+     */
     public JSONArray getAPIRatings(APIIdentifier apiId) throws APIManagementException {
         Connection conn = null;
         JSONArray apiRatings = null;
@@ -3782,9 +3797,10 @@ public class ApiMgtDAO {
 
     /**
      * @param apiIdentifier API Identifier
+     * @param conn          Database connection
      * @throws APIManagementException if failed to get API Ratings
      */
-    public JSONArray getAPIRatings(APIIdentifier apiIdentifier, Connection conn)
+    private JSONArray getAPIRatings(APIIdentifier apiIdentifier, Connection conn)
             throws APIManagementException, SQLException {
         PreparedStatement ps = null;
         PreparedStatement psSubscriber = null;
