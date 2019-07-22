903f0ac6351c676358e7f3c894b1a3b925fb0713
selakap <selakap@wso2.com>
2019-07-22 11:49:42 +0530

    Fix for the supporting the inline implementation method for prototype API creation using REST API

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/utils/mappings/APIMappingUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/utils/mappings/APIMappingUtil.java
@@ -124,6 +124,7 @@ public class APIMappingUtil {
             handleException("Error while converting endpointConfig to json", e);
         }
 
+        model.setImplementation(dto.getEndpointImplementationType().toString());
         model.setWsdlUrl(dto.getWsdlUri());
         model.setType(dto.getType().toString());
 
