73b061256efb4b687a05e88f935ed2982596d139
Chanaka Jayasena <chanaka3d@gmail.com>
2019-07-30 14:22:03 +0530

    Adding new Landing page

---
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store-new/source/src/app/AppRouts.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { withStyles } from '@material-ui/core';
+import Apis from './components/Apis/Apis';
+import Applications from './components/Applications/Applications';
+import Landing from './components/LandingPage/Landing';
+import ApplicationCreate from './components/Shared/AppsAndKeys/ApplicationCreateForm';
+import { PageNotFound, ScopeNotFound } from './components/Base/Errors';
+import ApplicationEdit from './components/Applications/Edit/ApplicationEdit';
+
+function AppRouts(props) {
+    const { isAuthenticated, isUserFound, theme, } = props;
+    return (
+        <Switch>
+            <Redirect exact from='/' to={theme.custom.landingPage.active ? '/home' : '/apis'} />
+            {theme.custom.landingPage.active && <Route path='/home' component={Landing} /> }
+            <Route path='/apis' component={Apis} />
+            {isAuthenticated ? (
+                <React.Fragment>
+                    <Route path='/applications' component={Applications} />
+                    <Route path='/application/create' component={ApplicationCreate} />
+                    <Route path='/application/edit/:application_id' component={ApplicationEdit} />
+                </React.Fragment>
+            ) : (
+                [
+                    isUserFound ? (
+                        <React.Fragment>
+                            <Route path='/applications' component={ScopeNotFound} />
+                            <Route path='/application/create' component={ScopeNotFound} />
+                            <Route path='/application/edit/:application_id' component={ScopeNotFound} />
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <Route path='/applications' component={PageNotFound} />
+                            <Route path='/application/create' component={PageNotFound} />
+                            <Route path='/application/edit/:application_id' component={PageNotFound} />
+                        </React.Fragment>
+                    ),
+                ]
+            )}
+            <Route component={PageNotFound} />
+        </Switch>
+    );
+}
+
+export default withStyles({}, { withTheme: true })(AppRouts);
