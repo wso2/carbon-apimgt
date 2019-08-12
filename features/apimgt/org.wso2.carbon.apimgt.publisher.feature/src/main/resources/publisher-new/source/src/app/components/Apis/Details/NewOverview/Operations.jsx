96a8b22743c77de5757683927301c1d645af4b1e
Hiranya Abeyrathne <hiranyaa@wso2.com>
2019-08-12 17:07:53 +0530

    Fixed issue in withTheme

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/components/Apis/Details/NewOverview/Operations.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/components/Apis/Details/NewOverview/Operations.jsx
@@ -57,7 +57,7 @@ RenderMethodBase.propTypes = {
     classes: PropTypes.shape({}).isRequired,
 };
 
-const RenderMethod = withTheme()(RenderMethodBase);
+const RenderMethod = withTheme(RenderMethodBase);
 
 const styles = {
     root: {
