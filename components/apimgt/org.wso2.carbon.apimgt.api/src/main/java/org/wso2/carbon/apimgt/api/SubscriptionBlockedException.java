2143a146841b963ddd89fc7ba05d5bffe82b6311
kavishkafernando <kavishkafdo95@gmail.com>
2019-08-12 11:47:01 +0530

    Fix rejected subscriptions moving to active state for new version apis

---
+++ components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/SubscriptionBlockedException.java
@@ -0,0 +1,9 @@
+package org.wso2.carbon.apimgt.api;
+
+public class SubscriptionBlockedException extends APIManagementException {
+
+    public SubscriptionBlockedException(String msg) {
+        super(msg);
+    }
+
+}
