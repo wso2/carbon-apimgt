a4ac6faa4e664cbda6811015186be9cf19e83d2e
dushaniw <dushaniw@wso2.com>
2019-08-21 04:51:54 +0530

    Add API import-export REST API and implementation.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/ExportApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/ExportApiServiceImpl.java
@@ -27,8 +27,15 @@ import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.apimgt.api.APIConsumer;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.model.APIKey;
+import org.wso2.carbon.apimgt.api.APIProvider;
+import org.wso2.carbon.apimgt.api.model.API;
+import org.wso2.carbon.apimgt.api.model.APIIdentifier;
 import org.wso2.carbon.apimgt.api.model.Application;
 import org.wso2.carbon.apimgt.api.model.OAuthApplicationInfo;
+import org.wso2.carbon.apimgt.impl.importexport.APIImportExportException;
+import org.wso2.carbon.apimgt.impl.importexport.APIImportExportManager;
+import org.wso2.carbon.apimgt.impl.importexport.ExportFormat;
+import org.wso2.carbon.apimgt.impl.utils.APIUtil;
 import org.wso2.carbon.apimgt.rest.api.admin.ExportApiService;
 import org.wso2.carbon.apimgt.rest.api.admin.utils.FileBasedApplicationImportExportManager;
 import org.wso2.carbon.apimgt.rest.api.util.RestApiConstants;
@@ -48,6 +55,75 @@ public class ExportApiServiceImpl extends ExportApiService {
     private static final String DEFAULT_APPLICATION_EXPORT_DIR = "exported-application";
     private static final String PRODUCTION = "PRODUCTION";
     private static final String SANDBOX = "SANDBOX";
+
+    /**
+     * Exports an API from API Manager for a given API ID. Meta information, API icon, documentation, WSDL
+     * and sequences are exported. This service generates a zipped archive which contains all the above mentioned
+     * resources for a given API.
+     *
+     * @param name           Name of the API that needs to be exported
+     * @param version        Version of the API that needs to be exported
+     * @param providerName   Provider name of the API that needs to be exported
+     * @param format         Format of output documents. Can be YAML or JSON
+     * @param preserveStatus Preserve API status on export
+     * @return Zipped file containing exported API
+     */
+    @Override
+    public Response exportApiGet(String name, String version, String providerName, String format,
+                                 Boolean preserveStatus) {
+
+        ExportFormat exportFormat;
+        API api;
+        APIImportExportManager apiImportExportManager;
+        String userName;
+        APIIdentifier apiIdentifier;
+        APIProvider apiProvider;
+        String apiDomain;
+        String apiRequesterDomain;
+        //If not specified status is preserved by default
+        boolean isStatusPreserved = preserveStatus == null || preserveStatus;
+
+        if (name == null || version == null || providerName == null) {
+            RestApiUtil.handleBadRequest("Invalid API Information ", log);
+        }
+
+        try {
+            //Default export format is YAML
+            exportFormat = StringUtils.isNotEmpty(format) ? ExportFormat.valueOf(format.toUpperCase()) :
+                    ExportFormat.YAML;
+
+            userName = RestApiUtil.getLoggedInUsername();
+            //provider names with @ signs are only accepted
+            apiDomain = MultitenantUtils.getTenantDomain(providerName);
+            apiRequesterDomain = RestApiUtil.getLoggedInUserTenantDomain();
+
+            if (!StringUtils.equals(apiDomain, apiRequesterDomain)) {
+                //not authorized to export requested API
+                RestApiUtil.handleAuthorizationFailure(RestApiConstants.RESOURCE_API +
+                        " name:" + name + " version:" + version + " provider:" + providerName, (String) null, log);
+            }
+
+            apiIdentifier = new APIIdentifier(APIUtil.replaceEmailDomain(providerName), name, version);
+            apiProvider = RestApiUtil.getLoggedInUserProvider();
+            // Checking whether the API exists
+            if (!apiProvider.isAPIAvailable(apiIdentifier)) {
+                String errorMessage = "Error occurred while updating. API: " + name + " version: " + version
+                        + " not found";
+                RestApiUtil.handleResourceNotFoundError(errorMessage, log);
+            }
+
+            api = apiProvider.getAPI(apiIdentifier);
+            apiImportExportManager = new APIImportExportManager(apiProvider, userName);
+            File file = apiImportExportManager.exportAPIArchive(api, isStatusPreserved, exportFormat);
+            return Response.ok(file)
+                    .header(RestApiConstants.HEADER_CONTENT_DISPOSITION, "attachment; filename=\""
+                            + file.getName() + "\"")
+                    .build();
+        } catch (APIManagementException | APIImportExportException e) {
+            RestApiUtil.handleInternalServerError("Error while exporting " + RestApiConstants.RESOURCE_API, e, log);
+        }
+        return null;
+    }
 
     /**
      * Export an existing Application
