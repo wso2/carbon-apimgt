35b51ac3a48f47111e5906f7d1c37929ab2e0c25
Hiranya Abeyrathne <hiranyaa@wso2.com>
2019-08-21 17:25:04 +0530

    Add Product Resource View

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/components/Apis/Details/Resources/ProductResourcesView.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/components/Apis/Details/Resources/ProductResourcesView.jsx
@@ -17,31 +17,21 @@
  */
 
 import React from 'react';
-import {withStyles, withTheme} from '@material-ui/core/styles';
+import { withStyles, withTheme } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import Chip from '@material-ui/core/Chip';
-import {Link} from 'react-router-dom';
-
-import classNames from 'classnames';
-import Paper from '@material-ui/core/Paper';
+import { Progress } from 'AppComponents/Shared';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+import APIProduct from 'AppData/APIProduct';
+import CONSTS from 'AppData/Constants';
 
-import InputLabel from '@material-ui/core/InputLabel';
-import TextField from '@material-ui/core/TextField';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Checkbox from '@material-ui/core/Checkbox';
 import EditIcon from '@material-ui/icons/edit';
-import {FormattedMessage} from "react-intl";
+import { FormattedMessage } from 'react-intl';
 import Resource from './Resource';
 
 /**
@@ -126,54 +116,139 @@ const styles = theme => ({
 /**
  * Component to show api product resources in resources tab
  */
-class ProductResourcesView extends React.PureComponent {
+class ProductResourcesView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            paths: null,
+            swagger: null,
+            scopes: [],
+            notFound: false,
+            showAddResource: false,
+            showPolicy: false,
+            apiPolicies: [],
+            selectedPolicy: props.api.apiThrottlingPolicy,
+            policyLevel: props.api.apiThrottlingPolicy ? 'perAPI' : 'perResource',
+        };
+
+        this.api_uuid = props.api.id;
+        this.childResources = [];
+    }
+
+    componentDidMount() {
+        const apiProduct = new APIProduct();
+        const promised_api = apiProduct.getSwagger(this.api_uuid);
+        promised_api
+            .then((response) => {
+                let tempScopes = [];
+                if (response.obj.security && response.obj.security.length !== 0) {
+                    response.obj.security.map((object, i) => {
+                        if (object.OAuth2Security) {
+                            tempScopes = object.OAuth2Security;
+                        }
+                    });
+                }
+                this.setState({ swagger: response.obj, scopes: tempScopes });
+                if (response.obj.paths !== undefined) {
+                    this.setState({ paths: response.obj.paths });
+                }
+            })
+            .catch((error) => {
+                if (process.env.NODE_ENV !== 'production') console.log(error);
+                const status = error.status;
+                if (status === 404) {
+                    this.setState({ notFound: true });
+                } else if (status === 401) {
+                    this.setState({ isAuthorize: false });
+                }
+            });
+    }
+
     render() {
-        const {classes, api} = this.props;
-        const apiResources = api.apis;
+        const {
+            swagger, policyLevel, apiPolicies, scopes, paths,
+        } = this.state;
+
+        const { classes, api } = this.props;
+
+        if (this.state.notFound) {
+            return <ResourceNotFound message={this.props.resourceNotFountMessage} />;
+        }
+        if (swagger === null || paths === null) {
+            return <Progress />;
+        }
+        const apiResources = api.apis
+            .map((i) => {
+                const operations = i.operations.map(item => item.uritemplate);
+                const x = Object.keys(paths).filter(i2 => operations.includes(i2))
+                const out  = x.reduce((acc,cur) => {
+                    acc[cur] = paths[cur]
+                    return acc;
+                },{})
+                return {
+                    name:i.name,
+                    paths:out
+                }
+            })
         return (
             <div className={classes.root}>
                 <div className={classes.titleWrapper}>
                     <Typography variant='h4' align='left' className={classes.mainTitle}>
-                        <FormattedMessage id='Apis.Details.Resources.Resources.resources' defaultMessage='Resources'/>
+                        <FormattedMessage id='Apis.Details.Resources.Resources.resources' defaultMessage='Resources' />
                     </Typography>
                     <Button size='small' className={classes.button}>
-                        <EditIcon className={classes.buttonIcon}/>
-                        <FormattedMessage id='Apis.Details.Resources.Resources.edit.resources.button'
-                                          defaultMessage='Edit Resources'/>
+                        <EditIcon className={classes.buttonIcon} />
+                        <FormattedMessage
+                            id='Apis.Details.Resources.Resources.edit.resources.button'
+                            defaultMessage='Edit Resources'
+                        />
                     </Button>
                 </div>
                 <div className={classes.contentWrapper}>
                     <List>
                         {Object.keys(apiResources).map((key) => {
                             const resource = apiResources[key];
-                            return (
-                                <div className={classes.root}>
+                            return( <div className={classes.root}>
                                     <Typography className={classes.heading} variant='h5'>
-                                        {resource.name}
-                                    </Typography>
-
-                                    {Object.keys(resource.operations).map((innerKey) => {
-                                        const operation = (resource.operations)[innerKey];
+                                    {resource.name}
+                                </Typography>
+                                    <div className={classes.root}>
+                                    {Object.keys(resource.paths).map((key) => {
+                                        const path = resource.paths[key];
+                                        const that = this;
+                                        console.info(path);
                                         return (
                                             <div>
                                                 <ExpansionPanel defaultExpanded className={classes.expansionPanel}>
-                                                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                                                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
                                                         <Typography className={classes.heading} variant='h6'>
-                                                            {operation.uritemplate}
+                                                            {key}
                                                         </Typography>
                                                     </ExpansionPanelSummary>
                                                     <ExpansionPanelDetails className={classes.expansionPanelDetails}>
-                                                        <Resource path={operation.uritemplate}
-                                                                  method={operation.httpVerb}
-                                                                  scopes={api.scopes}
-                                                                  addRemoveToDeleteList={this.addRemoveToDeleteList}
-                                                        />
+                                                        {Object.keys(path).map((innerKey) => {
+                                                             console.info("Hi");
+                                                            return CONSTS.HTTP_METHODS.includes(innerKey) ? (
+                                                                <Resource
+                                                                    path={key}
+                                                                    method={innerKey}
+                                                                    methodData={path[innerKey]}
+                                                                    updatePath={that.updatePath}
+                                                                    scopes={scopes}
+                                                                    apiPolicies={apiPolicies}
+                                                                    isAPIProduct
+                                                                    onRef={ref => this.childResources.push(ref)}
+                                                                    policyLevel={policyLevel}
+                                                                />
+                                                            ) : null;
+                                                        })}
                                                     </ExpansionPanelDetails>
                                                 </ExpansionPanel>
-                                            </div>);
+                                            </div>
+                                        );
                                     })}
                                 </div>
-                            );
+                            </div>);
                         })}
                     </List>
                 </div>
@@ -192,7 +267,7 @@ ProductResourcesView.propTypes = {
         pathname: PropTypes.shape({}),
     }).isRequired,
     parentClasses: PropTypes.shape({}).isRequired,
-    api: PropTypes.shape({id: PropTypes.string}).isRequired,
+    api: PropTypes.shape({ id: PropTypes.string }).isRequired,
 };
 
 export default withStyles(styles)(ProductResourcesView);
