c1921c4e261a81bac9f4b9e6c14db95590d5794b
ruwiniwj <ruwi1025@gmail.com>
2019-08-25 12:06:27 +0530

    Fix subscription table pagination and add license header

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/components/Apis/Details/Subscriptions/SubscriptionsTable.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/components/Apis/Details/Subscriptions/SubscriptionsTable.jsx
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import React, { Component } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
@@ -495,7 +513,7 @@ class SubscriptionsTable extends Component {
                                     <TableRow>
                                         <TablePagination
                                             rowsPerPageOptions={[5, 10, 25, 50, 100]}
-                                            colSpan={3}
+                                            colSpan={5}
                                             count={selectedSubscriptions.length}
                                             rowsPerPage={rowsPerPage}
                                             page={page}
