4d84fce7eca33f42873351a0f1813b9a41ed7e51
uvindra <uvindra>
2019-08-29 11:21:38 +0530

    Merge with master

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/gen/java/org/wso2/carbon/apimgt/rest/api/store/v1/SubscriptionsApiService.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/gen/java/org/wso2/carbon/apimgt/rest/api/store/v1/SubscriptionsApiService.java
@@ -7,6 +7,8 @@ import org.apache.cxf.jaxrs.ext.MessageContext;
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 
+import org.wso2.carbon.apimgt.api.APIManagementException;
+
 import org.wso2.carbon.apimgt.rest.api.store.v1.dto.ErrorDTO;
 import java.util.List;
 import org.wso2.carbon.apimgt.rest.api.store.v1.dto.SubscriptionDTO;
@@ -22,9 +24,9 @@ import javax.ws.rs.core.SecurityContext;
 
 
 public interface SubscriptionsApiService {
-      public Response subscriptionsGet(String apiId, String applicationId, String apiType, String groupId, Integer offset, Integer limit, String ifNoneMatch, MessageContext messageContext);
-      public Response subscriptionsMultiplePost(List<SubscriptionDTO> body, MessageContext messageContext);
-      public Response subscriptionsPost(SubscriptionDTO body, MessageContext messageContext);
-      public Response subscriptionsSubscriptionIdDelete(String subscriptionId, String ifMatch, MessageContext messageContext);
-      public Response subscriptionsSubscriptionIdGet(String subscriptionId, String ifNoneMatch, MessageContext messageContext);
+      public Response subscriptionsGet(String apiId, String applicationId, String apiType, String groupId, Integer offset, Integer limit, String ifNoneMatch, MessageContext messageContext) throws APIManagementException;
+      public Response subscriptionsMultiplePost(List<SubscriptionDTO> body, MessageContext messageContext) throws APIManagementException;
+      public Response subscriptionsPost(SubscriptionDTO body, MessageContext messageContext) throws APIManagementException;
+      public Response subscriptionsSubscriptionIdDelete(String subscriptionId, String ifMatch, MessageContext messageContext) throws APIManagementException;
+      public Response subscriptionsSubscriptionIdGet(String subscriptionId, String ifNoneMatch, MessageContext messageContext) throws APIManagementException;
 }
