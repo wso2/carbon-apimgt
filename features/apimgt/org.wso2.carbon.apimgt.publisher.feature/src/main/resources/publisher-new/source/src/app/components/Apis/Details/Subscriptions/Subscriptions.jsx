9c81ce04735c6a39dd0e682e77627ec965e00996
ruwiniwj <ruwi1025@gmail.com>
2019-08-30 14:39:07 +0530

    Make Subscriptions a functional component

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/components/Apis/Details/Subscriptions/Subscriptions.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/source/src/app/components/Apis/Details/Subscriptions/Subscriptions.jsx
@@ -16,13 +16,10 @@
  * under the License.
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 
-import API from 'AppData/api';
-import { Progress } from 'AppComponents/Shared';
-import ResourceNotFound from 'AppComponents/Base/Errors/ResourceNotFound';
 import SubscriptionsTable from './SubscriptionsTable';
 import SubscriptionPoliciesManage from './SubscriptionPoliciesManage';
 
@@ -38,63 +35,19 @@ const styles = theme => ({
  * @class Subscriptions
  * @extends {Component}
  */
-class Subscriptions extends Component {
-    constructor(props) {
-        super(props);
-        this.api_uuid = props.api.id;
-        this.state = {
-            api: null,
-            notFound: false,
-        };
-    }
+function Subscriptions(props) {
+    const { api } = props;
 
-    componentDidMount() {
-        const api = new API();
-        const promisedAPI = api.get(this.api_uuid);
-        promisedAPI
-            .then((response) => {
-                this.setState({ api: response.obj });
-            })
-            .catch((error) => {
-                if (process.env.NODE_ENV !== 'production') {
-                    console.log(error);
-                }
-                const { status } = error;
-                if (status === 404) {
-                    this.setState({ notFound: true });
-                }
-            });
-    }
-
-    render() {
-        const { api, notFound } = this.state;
-        const { resourceNotFoundMessage } = this.props;
-        if (notFound) {
-            return (<ResourceNotFound message={resourceNotFoundMessage} />);
-        }
-
-        if (!api) {
-            return <Progress />;
-        }
-
-        return (
-            <div>
-                <SubscriptionsTable api={api} />
-                <SubscriptionPoliciesManage api={api} />
-            </div>
-        );
-    }
+    return (
+        <div>
+            <SubscriptionsTable api={api} />
+            <SubscriptionPoliciesManage api={api} />
+        </div>
+    );
 }
 
-Subscriptions.defaultProps = {
-    resourceNotFoundMessage: 'Resource not found!',
-};
-
 Subscriptions.propTypes = {
-    api: PropTypes.shape({
-        id: PropTypes.string,
-    }).isRequired,
-    resourceNotFoundMessage: PropTypes.string,
+    api: PropTypes.shape({}).isRequired,
 };
 
 export default withStyles(styles)(Subscriptions);
