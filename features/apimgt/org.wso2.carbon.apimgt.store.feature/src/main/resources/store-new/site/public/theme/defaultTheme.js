1bc40dbbb87c242c64b693218837525f4470ae09
TanyaM <tanyamadurapperuma@gmail.com>
2019-09-10 16:56:14 +0530

    Add grant type display names list

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store-new/site/public/theme/defaultTheme.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store-new/site/public/theme/defaultTheme.js
@@ -154,4 +154,15 @@ const Configurations = {
     app: {
         context: APP_CONTEXT,
     },
+    grantTypes: {
+        authorization_code: 'Code',
+        implicit: 'Implicit',
+        refresh_token: 'Refresh Token',
+        password: 'Password',
+        'iwa:ntlm': 'IWA-NTLM',
+        client_credentials: 'Client Credentials',
+        'urn:ietf:params:oauth:grant-type:saml2-bearer': 'SAML2',
+        'urn:ietf:params:oauth:grant-type:jwt-bearer': 'JWT',
+        kerberos: 'Kerberos',
+    },
 };
