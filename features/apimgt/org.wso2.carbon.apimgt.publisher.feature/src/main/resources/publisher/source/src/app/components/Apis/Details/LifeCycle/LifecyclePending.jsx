e6871710b407627d10bd65a4d39f661579bad4d3
dushansilva <dushansilva95@gmail.com>
2019-09-28 22:54:00 +0530

    add workflow task remove confirm dialog

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/LifeCycle/LifecyclePending.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/LifeCycle/LifecyclePending.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { FormattedMessage, useIntl } from 'react-intl';
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 import { useAPI } from 'AppComponents/Apis/Details/components/ApiContext';
 import Alert from 'AppComponents/Shared/Alert';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core/';
+import Slide from '@material-ui/core/Slide';
 
 const lifecyclePending = (props) => {
     const { currentState } = props;
     const intl = useIntl();
+    const [isOpen, setOpen] = useState(false);
     const [api, updateAPI] = useAPI();
     const deleteTask = () => {
         const { id } = api;
@@ -44,7 +46,7 @@ const lifecyclePending = (props) => {
                 <Box display='block' mt={0.5}>
                     <Typography variant='subtitle2'>
                         <FormattedMessage
-                            id='Apis.Details.LifeCycle.LifeCycleUpdate.LifecyclePending.adjective'
+                            id='Apis.Details.LifeCycle.LifeCycleUpdate.LifecyclePending.current.state'
                             defaultMessage='Current state is'
                         />
                         {' '}
@@ -56,7 +58,7 @@ const lifecyclePending = (props) => {
                         size='small'
                         variant='outlined'
                         color='primary'
-                        onClick={deleteTask}
+                        onClick={() => setOpen(true)}
                     >
                         <FormattedMessage
                             id='Apis.Details.LifeCycle.LifeCycleUpdate.LifecyclePending.delete.task'
@@ -66,6 +68,41 @@ const lifecyclePending = (props) => {
 
                 </Box>
             </Box>
+            <Dialog open={isOpen} transition={Slide}>
+                <DialogTitle>
+                    <FormattedMessage
+                        id='Apis.Details.LifeCycle.LifeCycleUpdate.LifecyclePending.dialog.title'
+                        defaultMessage='Delete Task'
+                    />
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        <FormattedMessage
+                            id='Apis.Details.LifeCycle.LifeCycleUpdate.LifecyclePending.dialog.text.description'
+                            defaultMessage='The life cycle task will be removed'
+                        />
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button dense onClick={() => setOpen(false)}>
+                        <FormattedMessage
+                            id='Apis.Details.LifeCycle.LifeCycleUpdate.LifecyclePending.dialog.cancel'
+                            defaultMessage='Cancel'
+                        />
+                    </Button>
+                    <Button
+                        size='small'
+                        variant='outlined'
+                        color='primary'
+                        onClick={deleteTask}
+                    >
+                        <FormattedMessage
+                            id='Apis.Details.LifeCycle.LifeCycleUpdate.LifecyclePending.dialog,delete'
+                            defaultMessage='Delete'
+                        />
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Paper>
     );
 };
