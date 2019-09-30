913413b33d233b3ea2feae363a17c0ba6bd86ca6
Menaka Jayawardena <menaka12350@gmail.com>
2019-09-30 20:51:04 +0530

    Fix configId validation.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/utils/PublisherAlertsAPIUtils.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/utils/PublisherAlertsAPIUtils.java
@@ -42,7 +42,7 @@ public class PublisherAlertsAPIUtils {
         String userName = RestApiUtil.getLoggedInUsername();
         String decodedConfigurationId = new String(Base64.getDecoder().decode(configId.getBytes()));
         String[] parameters = decodedConfigurationId.split("#");
-        if (parameters.length != 3) {
+        if (parameters.length != 2) {
             RestApiUtil.handleBadRequest(
                     "The configuration id validation failed. Should be {apiName}#{apiVersion}#{tenantDomain}",
                     log);
@@ -52,9 +52,6 @@ public class PublisherAlertsAPIUtils {
             APIProvider apiProvider = RestApiUtil.getLoggedInUserProvider();
             if (!apiProvider.isApiNameExist(parameters[0])) {
                 RestApiUtil.handleBadRequest("Invalid API Name", log);
-            }
-            if (!MultitenantUtils.getTenantDomain(userName).equals(parameters[2])) {
-                RestApiUtil.handleBadRequest("Invalid Tenant Domain", log);
             }
         } catch (APIManagementException e) {
             RestApiUtil.handleInternalServerError("Error while validating payload", e, log);
