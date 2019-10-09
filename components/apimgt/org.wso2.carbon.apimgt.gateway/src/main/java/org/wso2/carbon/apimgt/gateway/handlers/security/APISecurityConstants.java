f5b2642adb4b85b6f680e0933fee77b498759418
Hiranya Abeyrathne <hiranyaa@wso2.com>
2019-10-09 22:15:03 +0530

    Fixed graphql specific intermittent issue

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/APISecurityConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/APISecurityConstants.java
@@ -63,7 +63,12 @@ public class APISecurityConstants {
 
     public static final int API_AUTH_MISSING_OPEN_API_DEF = 900911;
     public static final String API_AUTH_MISSING_OPEN_API_DEF_ERROR_MESSAGE = "Internal Server Error";
+
+    public static final int GRAPHQL_INVALID_QUERY = 900422;
     public static final String GRAPHQL_API_FAILURE_HANDLER = "_graphql_failure_handler";
+    public static final String GRAPHQL_INVALID_QUERY_MESSAGE= "INVALID QUERY";
+
+
 
     // We have added this because we need to add an additional description to the original one and we need to
     // separate the 2 messages
