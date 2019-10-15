67896aac22ed29b7ccf5f5b89572949ad4e13968
dushaniw <dushaniw@wso2.com>
2019-10-15 17:38:59 +0530

    Fix issue when importing/updating applications with --owner and --preserveOwner.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/ImportApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/ImportApiServiceImpl.java
@@ -181,6 +181,8 @@ public class ImportApiServiceImpl extends ImportApiService {
                 Application application = consumer.getApplicationById(appId);
                 applicationDetails.setId(appId);
                 applicationDetails.setUUID(application.getUUID());
+                applicationDetails.setOwner(application.getOwner());
+                applicationDetails.updateSubscriber(application.getSubscriber());
                 consumer.updateApplication(applicationDetails);
             } else {
                 appId = consumer.addApplication(applicationDetails, ownerId);
@@ -189,7 +191,7 @@ public class ImportApiServiceImpl extends ImportApiService {
             List<APIIdentifier> skippedAPIs = new ArrayList<>();
             if (skipSubscriptions == null || !skipSubscriptions) {
                 skippedAPIs = importExportManager
-                        .importSubscriptions(applicationDetails, username, appId, update);
+                        .importSubscriptions(applicationDetails, ownerId, appId, update);
             }
             Application importedApplication = consumer.getApplicationById(appId);
             importedApplication.setOwner(ownerId);
@@ -203,7 +205,7 @@ public class ImportApiServiceImpl extends ImportApiService {
                 // Add application keys if present and keys does not exists in the current application
                 if (applicationDetails.getKeys().size() > 0 && importedApplication.getKeys().size() == 0) {
                     for (APIKey apiKey : applicationDetails.getKeys()) {
-                        importExportManager.addApplicationKey(username, importedApplication, apiKey);
+                        importExportManager.addApplicationKey(ownerId, importedApplication, apiKey);
                     }
                 }
             }
