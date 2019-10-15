95efec069fbe31b173d581c992170fb2c44c04a3
Eranga Liyanage <erangatl@gmail.com>
2019-10-15 12:02:37 +0530

    Fix publisher 404 page

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Base/Errors/ResourceNotFound.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Base/Errors/ResourceNotFound.jsx
@@ -20,24 +20,64 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
+import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Image404 from './Custom404Image';
 
 const ResourceNotFound = (props) => {
     const { response } = props;
     const message = props.message || {};
 
     return (
-        <div>
-            <div className='message message-danger'>
-                <Typography variant='h6' gutterBottom>
-                    { message.title }
-                </Typography>
-                <Typography variant='subtitle1' gutterBottom>
-                    { message.body }
-                    <span style={{ color: 'green' }}> {response ? response.statusText : ''} </span>
-                </Typography>
-                {message.more}
-            </div>
-        </div>
+        <React.Fragment>
+            <Container maxWidth='md'>
+                <Grid container alignItems='center' justify='center' style={{ height: '100%' }}>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant='h5' gutterBottom>
+                            {message.title}
+                        </Typography>
+                        <Typography variant='subtitle1' gutterBottom>
+                            {message.body}
+                            <span style={{ color: 'green' }}> {response ? response.statusText : ''} </span>
+                        </Typography>
+                        {message.more}
+                        <Box py={5}>
+                            <Box pb={2}>
+                                <Typography variant='subtitle1'>
+                                    <FormattedMessage
+                                        id='Base.Errors.ResourceNotFound.more.links'
+                                        defaultMessage='You may check below links'
+                                    />
+                                </Typography>
+                            </Box>
+                            <Link to='/apis/' style={{ marginRight: 8 }}>
+                                <Button variant='contained' color='primary'>
+                                    <FormattedMessage
+                                        id='Base.Errors.ResourceNotFound.api.list'
+                                        defaultMessage='API List'
+                                    />
+                                </Button>
+                            </Link>
+                            <Link to='/api-products/'>
+                                <Button variant='contained' color='primary'>
+                                    <FormattedMessage
+                                        id='Base.Errors.ResourceNotFound.api.product.list'
+                                        defaultMessage='API Product List'
+                                    />
+                                </Button>
+                            </Link>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        {/* Image */}
+                        <Image404 style={{ fontSize: 400, fill: '#ccc' }} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </React.Fragment>
     );
 };
 
@@ -45,12 +85,11 @@ ResourceNotFound.defaultProps = {
     message: {
         title: <FormattedMessage
             id='Base.Errors.ResourceNotfound.default_tittle'
-            defaultMessage='ResourceNotFound'
+            defaultMessage='Page Not Found'
         />,
         body: <FormattedMessage
             id='Base.Errors.ResourceNotfound.default_body'
-            defaultMessage={'No Error message give, Please give a proper error message when using `ResourceNotFound` ' +
-            'Component'}
+            defaultMessage='The page you are looking for is not available'
         />,
     },
 };
