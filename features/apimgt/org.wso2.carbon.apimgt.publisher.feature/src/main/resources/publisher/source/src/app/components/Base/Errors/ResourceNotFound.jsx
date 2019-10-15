e8d6256e4f85b66f65f3ea8fc2d6162d585a4070
Eranga Liyanage <erangatl@gmail.com>
2019-10-15 15:39:57 +0530

    Fix 404 style

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Base/Errors/ResourceNotFound.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Base/Errors/ResourceNotFound.jsx
@@ -18,6 +18,7 @@
 
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import Grid from '@material-ui/core/Grid';
@@ -34,48 +35,54 @@ const ResourceNotFound = (props) => {
     return (
         <React.Fragment>
             <Container maxWidth='md'>
-                <Grid container alignItems='center' justify='center' style={{ height: '100%' }}>
-                    <Grid item xs={12} md={6}>
-                        <Typography variant='h5' gutterBottom>
-                            {message.title}
-                        </Typography>
-                        <Typography variant='subtitle1' gutterBottom>
-                            {message.body}
-                            <span style={{ color: 'green' }}> {response ? response.statusText : ''} </span>
-                        </Typography>
-                        {message.more}
-                        <Box py={5}>
-                            <Box pb={2}>
-                                <Typography variant='subtitle1'>
-                                    <FormattedMessage
-                                        id='Base.Errors.ResourceNotFound.more.links'
-                                        defaultMessage='You may check below links'
-                                    />
-                                </Typography>
-                            </Box>
-                            <Link to='/apis/' style={{ marginRight: 8 }}>
-                                <Button variant='contained' color='primary'>
-                                    <FormattedMessage
-                                        id='Base.Errors.ResourceNotFound.api.list'
-                                        defaultMessage='API List'
-                                    />
-                                </Button>
-                            </Link>
-                            <Link to='/api-products/'>
-                                <Button variant='contained' color='primary'>
-                                    <FormattedMessage
-                                        id='Base.Errors.ResourceNotFound.api.product.list'
-                                        defaultMessage='API Product List'
-                                    />
-                                </Button>
-                            </Link>
+                <Box padding={4}>
+                    <Paper elevation={0} >
+                        <Box padding={4}>
+                            <Grid container alignItems='center' justify='center' style={{ height: '100%' }}>
+                                <Grid item xs={12} md={6}>
+                                    <Typography variant='h5' gutterBottom>
+                                        {message.title}
+                                    </Typography>
+                                    <Typography variant='subtitle1' gutterBottom>
+                                        {message.body}
+                                        <span style={{ color: 'green' }}> {response ? response.statusText : ''} </span>
+                                    </Typography>
+                                    {message.more}
+                                    <Box py={5}>
+                                        <Box pb={2}>
+                                            <Typography variant='subtitle1'>
+                                                <FormattedMessage
+                                                    id='Base.Errors.ResourceNotFound.more.links'
+                                                    defaultMessage='You may check below links'
+                                                />
+                                            </Typography>
+                                        </Box>
+                                        <Link to='/apis/' style={{ marginRight: 8 }}>
+                                            <Button variant='contained' color='primary'>
+                                                <FormattedMessage
+                                                    id='Base.Errors.ResourceNotFound.api.list'
+                                                    defaultMessage='API List'
+                                                />
+                                            </Button>
+                                        </Link>
+                                        <Link to='/api-products/'>
+                                            <Button variant='contained' color='primary'>
+                                                <FormattedMessage
+                                                    id='Base.Errors.ResourceNotFound.api.product.list'
+                                                    defaultMessage='API Product List'
+                                                />
+                                            </Button>
+                                        </Link>
+                                    </Box>
+                                </Grid>
+                                <Grid item xs={12} md={6}>
+                                    {/* Image */}
+                                    <Image404 style={{ fontSize: 400, fill: '#ccc' }} />
+                                </Grid>
+                            </Grid>
                         </Box>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        {/* Image */}
-                        <Image404 style={{ fontSize: 400, fill: '#ccc' }} />
-                    </Grid>
-                </Grid>
+                    </Paper>
+                </Box>
             </Container>
         </React.Fragment>
     );
