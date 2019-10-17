47984317268bbfdc5cde16147731d182479dedc6
kavishkafernando <kavishkafdo95@gmail.com>
2019-10-17 10:39:25 +0530

    Fixing comments

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/mappings/APIMappingUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/mappings/APIMappingUtil.java
@@ -856,9 +856,9 @@ public class APIMappingUtil {
     private static boolean isSubscriptionAvailable(String apiTenant, String subscriptionAvailability,
                                                   String subscriptionAllowedTenants) {
 
-        String tenantDomain = RestApiUtil.getLoggedInUserTenantDomain();
+        String userTenant = RestApiUtil.getLoggedInUserTenantDomain();
         boolean subscriptionAllowed = false;
-        if (!tenantDomain.equals(apiTenant)) {
+        if (!userTenant.equals(apiTenant)) {
             if (APIConstants.SUBSCRIPTION_TO_ALL_TENANTS.equals(subscriptionAvailability)) {
                 subscriptionAllowed = true;
             } else if (APIConstants.SUBSCRIPTION_TO_SPECIFIC_TENANTS.equals(subscriptionAvailability)) {
@@ -867,7 +867,7 @@ public class APIMappingUtil {
                     allowedTenants = subscriptionAllowedTenants.split(",");
                     if (allowedTenants != null) {
                         for (String tenant : allowedTenants) {
-                            if (tenant != null && tenantDomain.equals(tenant.trim())) {
+                            if (tenant != null && tenant.trim().equals(userTenant)) {
                                 subscriptionAllowed = true;
                                 break;
                             }
