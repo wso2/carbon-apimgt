7e3f28db10a04d3a9ce109180738b68f07e8b6dd
Ruthryi <ruthryi@wso2.com>
2019-10-22 13:55:10 +0530

    Reviewed UI text for Publisher

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/APIDefinition/ImportDefinition.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/APIDefinition/ImportDefinition.jsx
@@ -143,7 +143,7 @@ export default function ImportDefinition(props) {
                 console.log(err);
                 Alert.error(intl.formatMessage({
                     id: 'Apis.Details.APIDefinition.APIDefinition.error.updating.graphQL.schema',
-                    defaultMessage: 'Error while updating graphQL schema',
+                    defaultMessage: 'Error while updating GraphQL schema',
                 }));
             });
     }
