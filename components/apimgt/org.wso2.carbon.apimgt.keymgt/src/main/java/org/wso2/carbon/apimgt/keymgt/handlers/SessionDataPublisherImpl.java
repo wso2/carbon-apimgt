d5ed5571a9f22a0637e84b0da4500b1db62e01b5
Ishara Cooray <isharac@wso2.com>
2019-11-13 14:39:31 +0530

    fix migration for SP app change

--- components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/SessionDataPublisherImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/SessionDataPublisherImpl.java
@@ -57,6 +57,7 @@ public class SessionDataPublisherImpl extends AbstractAuthenticationDataPublishe
     public static final Log log = LogFactory.getLog(SessionDataPublisherImpl.class);
     private static final String handlerName = "APIMSessionDataPublisherImpl";
     private static final String DEVPORTAL_CLIENT_APP_NAME = "admin_devportal";
+    private static final String DEVPORTAL_CLIENT_APP_NAME_OLD = "admin_store";
     private static final String PUBLISHER_CLIENT_APP_NAME = "admin_publisher";
 
     @Override public void doPublishAuthenticationStepSuccess(AuthenticationData authenticationData) {
@@ -135,8 +136,9 @@ public class SessionDataPublisherImpl extends AbstractAuthenticationDataPublishe
         }
 
         for (OAuthConsumerAppDTO appDTO : appDTOs) {
-            if (StringUtils.equalsIgnoreCase(DEVPORTAL_CLIENT_APP_NAME, appDTO.getApplicationName()) || StringUtils
-                    .equalsIgnoreCase(PUBLISHER_CLIENT_APP_NAME, appDTO.getApplicationName())) {
+            if (StringUtils.equalsIgnoreCase(DEVPORTAL_CLIENT_APP_NAME_OLD, appDTO.getApplicationName()) ||
+                    StringUtils.equalsIgnoreCase(DEVPORTAL_CLIENT_APP_NAME, appDTO.getApplicationName()) ||
+                    StringUtils.equalsIgnoreCase(PUBLISHER_CLIENT_APP_NAME, appDTO.getApplicationName())) {
                 Set<AccessTokenDO> accessTokenDOs = null;
                 try {
                     // Retrieve all ACTIVE or EXPIRED access tokens for particular client authorized by this user
