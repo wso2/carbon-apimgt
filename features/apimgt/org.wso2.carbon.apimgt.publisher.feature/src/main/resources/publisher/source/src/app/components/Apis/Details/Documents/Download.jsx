6d79a0a8e8e6ceacdcd452a47cb4038b333aff65
kavishkafernando <kavishkafdo95@gmail.com>
2019-11-22 11:36:24 +0530

    Fix issue #6846

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/Documents/Download.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/Documents/Download.jsx
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { FormattedMessage, injectIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
@@ -29,6 +29,64 @@ function Download(props) {
     const { intl } = props;
 
     const { docId, apiId } = props;
+    const [isFileAvailable, setIsFileAvailable] = useState(false);
+    const [isSuccessful, setIsSuccessful] = useState(false);
+
+    useEffect(() => {
+        const api = new Api();
+        const promised_get_content = api.getFileForDocument(apiId, docId);
+        promised_get_content
+            .then((done) => {
+                setIsSuccessful(true);
+                setIsFileAvailable(true);
+            })
+            .catch((error) => {
+                setIsSuccessful(true);
+                setIsFileAvailable(false);
+            });
+    }, []);
+    /**
+     * Download the document related file
+     * @param {any} response Response of download file
+     */
+    const downloadFile = (response, doc) => {
+        let fileName = '';
+        const contentDisposition = response.headers['content-disposition'];
+
+        if (contentDisposition && contentDisposition.indexOf('attachment') !== -1) {
+            const fileNameReg = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+            const matches = fileNameReg.exec(contentDisposition);
+            if (matches != null && matches[1]) fileName = matches[1].replace(/['"]/g, '');
+        }
+        const contentType = response.headers['content-type'];
+        const blob = new Blob([response.data], {
+            type: contentType,
+        });
+        if (typeof window.navigator.msSaveBlob !== 'undefined') {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            const URL = window.URL || window.webkitURL;
+            const downloadUrl = URL.createObjectURL(blob);
+
+            if (fileName) {
+                const aTag = document.createElement('a');
+                if (typeof aTag.download === 'undefined') {
+                    window.location = downloadUrl;
+                } else {
+                    aTag.href = downloadUrl;
+                    aTag.download = fileName;
+                    document.body.appendChild(aTag);
+                    aTag.click();
+                }
+            } else {
+                window.location = downloadUrl;
+            }
+
+            setTimeout(() => {
+                URL.revokeObjectURL(downloadUrl);
+            }, 100);
+        }
+    };
     const handleDownload = () => {
         const api = new Api();
         const promised_get_content = api.getFileForDocument(apiId, docId);
@@ -48,7 +106,8 @@ function Download(props) {
     };
 
     return (
-        <Button onClick={handleDownload}>
+        isSuccessful &&
+        <Button onClick={handleDownload} disabled={!isFileAvailable}>
             <Icon>arrow_downward</Icon>
             <FormattedMessage
                 id='Apis.Details.Documents.Download.documents.listing.download'
