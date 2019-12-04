659aa2e4fbe9841eed208efedd77e304e3825991
Sanjula Madurapperuma <sanjula99@gmail.com>
2019-12-04 07:00:47 +0530

    Adding Unit tests for API Security Audit feature

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/test/java/org/wso2/carbon/apimgt/impl/APIProviderImplWrapper.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/test/java/org/wso2/carbon/apimgt/impl/APIProviderImplWrapper.java
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.axis2.AxisFault;
+import org.json.simple.JSONObject;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.model.API;
 import org.wso2.carbon.apimgt.api.model.APIIdentifier;
@@ -111,6 +112,12 @@ public class APIProviderImplWrapper extends APIProviderImpl {
         ThrottlePolicyTemplateBuilder policyBuilder = new ThrottlePolicyTemplateBuilder();
         policyBuilder.setPolicyTemplateLocation(POLICY_LOCATION);
         return policyBuilder;
+    }
+
+
+    @Override
+    public JSONObject getSecurityAuditAttributesFromConfig(String userId) throws APIManagementException {
+        return super.getSecurityAuditAttributesFromConfig(userId);
     }
     
     @Override
