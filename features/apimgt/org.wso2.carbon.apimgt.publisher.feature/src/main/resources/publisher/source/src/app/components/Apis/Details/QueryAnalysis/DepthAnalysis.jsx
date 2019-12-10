6025a7eb34c7109c4d64c0ca7a1a2e8e63570840
ashera96 <asherasilva100@gmail.com>
2019-12-10 12:20:01 +0530

    Started working on the DepthAnalysis related UI

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/QueryAnalysis/DepthAnalysis.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/QueryAnalysis/DepthAnalysis.jsx
@@ -24,11 +24,38 @@ import Switch from '@material-ui/core/Switch';
 import Typography from '@material-ui/core/Typography';
 import { FormattedMessage } from 'react-intl';
 import Box from '@material-ui/core/Box';
+import FormControl from '@material-ui/core/FormControl';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+// /**
+//  * Tab Panel configurations
+//  * @param {*} props props
+//  * @returns {*} content which are displayed under the tab
+//  */
+// function DefaultDepthDialog() {
+//     return (
+
+//     );
+// }
+
 
 const useStyles = makeStyles(theme => ({
-    checkLabel: {
-        paddingTop: 4,
-        backgroundColor: theme.palette.background.paper,
+    button: {
+        height: 30,
+        marginLeft: 30,
+    },
+    formControl: {
+        textAlign: 'center',
+        padding: `0 ${theme.spacing(7)}px`,
+    },
+    dialog: {
+        width: '400px',
     },
 }));
 
@@ -39,12 +66,42 @@ const useStyles = makeStyles(theme => ({
 function DepthAnalysis() {
     const classes = useStyles();
     const [state, setState] = React.useState({
-        depthCheck: true,
+        depthCheck: false,
+        // defaultDepth: 2,
+        showPageContent: false,
     });
+
+    const [defaultDepth, setDefaultDepth] = React.useState(2);
 
     const handleDepthToggle = name => (event) => {
         setState({ ...state, [name]: event.target.checked });
     };
+
+    const handleClose = () => {
+        this.setState({
+            depthCheck: false,
+        });
+    };
+
+    const onChangeDefaultDepth = (newDepth) => {
+        this.setState({
+            defaultDepth: newDepth,
+        });
+    };
+
+    const onDepthValueSave = () => {
+        onChangeDefaultDepth(this.state.defaultDepth);
+        this.setState({
+            depthCheck: true,
+            showPageContent: true,
+        });
+    };
+
+    // const handleDefaultDepthInput = (event) => {
+    //     this.setState({
+    //         defaultDepth: event.target.value,
+    //     });
+    // };
 
     return (
         <React.Fragment>
@@ -52,28 +109,85 @@ function DepthAnalysis() {
                 <Typography variant='h6'>
                     <FormattedMessage
                         id='Apis.Details.QueryAnalysis.DepthAnalysis.query.depth'
-                        defaultMessage='Query Depth'
+                        defaultMessage='Role-Depth Mappings'
                     />
                 </Typography>
             </Box>
-            <FormGroup row>
-                <Typography variant='subtitle2' className={classes.checkLabel}>
-                    <FormattedMessage
-                        id='Apis.Details.Depth.Analysis.DepthAnalysisCheck'
-                        defaultMessage='Enable / Disable Depth Limitation'
-                    />
-                </Typography>
-                <FormControlLabel
-                    control={
-                        <Switch
-                            checked={state.depthCheck}
-                            onChange={handleDepthToggle('depthCheck')}
-                            value='depthCheck'
-                            color='primary'
+            <Box display='flex' justifyContent='center' m={1} p={1} >
+                <FormControl component='fieldset'>
+                    <FormGroup aria-label='position' row>
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={state.depthCheck}
+                                    onChange={handleDepthToggle('depthCheck')}
+                                    value='depthCheck'
+                                    color='primary'
+                                />}
+                            label='Enable / Disable Depth Limitation'
+                            labelPlacement='start'
                         />
-                    }
-                />
-            </FormGroup>
+                    </FormGroup>
+                </FormControl>
+            </Box>
+            {(state.depthCheck === true && state.showPageContent === false) ? (
+                <Dialog
+                    open={state.depthCheck}
+                    onClose={handleClose}
+                    aria-labelledby='alert-dialog-title'
+                    aria-describedby='alert-dialog-description'
+                >
+                    <DialogTitle id='alert-dialog-title'>Default depth limitation value</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id='alert-dialog-description'>
+                            This default depth limitation value is used to block malicious queries if none of the
+                            role-depth mappings are applicable
+                        </DialogContentText>
+                    </DialogContent>
+                    <FormControl className={classes.formControl}>
+                        <TextField
+                            id='defaultDepthLimitation'
+                            label='Default Depth Limitation'
+                            placeholder='Default Depth Limitation'
+                            // error={this.state.valid.name.invalid}
+                            // helperText={
+                            //     this.state.valid.name.invalid ? (
+                            //         this.state.valid.name.error
+                            //     ) : (
+                            //         <FormattedMessage
+                            //             id='Apis.Details.QueryAnalysis.DepthAnalysis.dialog.depth.value'
+                            //             defaultMessage='Enter Depth Limitation Value ( Ex: 20 )'
+                            //         />
+                            //     )
+                            // }
+                            margin='normal'
+                            variant='outlined'
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            value={defaultDepth}
+                            onChange={event => setDefaultDepth(event.target.value)}
+                        />
+                    </FormControl>
+                    <DialogActions>
+                        <Button onClick={onDepthValueSave} color='primary' className={classes.button}>
+                            <FormattedMessage
+                                id='Apis.Details.QueryAnalysis.DepthAnalysis.default.depth.limitation.add.button'
+                                defaultMessage='Save'
+                            />
+                        </Button>
+                        <Button onClick={() => handleClose} color='primary' autoFocus>
+                            <FormattedMessage
+                                id='Apis.Details.QueryAnalysis.DepthAnalysis.defualt.depth.limitation.cancel.button'
+                                defaultMessage='Cancel'
+                            />
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            ) : null}
+            {state.showPageContent ? (
+                <h1>{state.defaultDepth}</h1>
+            ) : null}
         </React.Fragment>
     );
 }
