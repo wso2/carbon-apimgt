a0b32b49eb29703f4b1a7c56758491c584f72a9f
meruja <merujajoseph@gmail.com>
2020-01-13 13:58:15 +0530

    Fix exception during setup of grant types of application.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/mappings/SettingsMappingUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/mappings/SettingsMappingUtil.java
@@ -49,8 +49,22 @@ public class SettingsMappingUtil {
             settingsDTO.setApplicationSharingEnabled(APIUtil.isMultiGroupAppSharingEnabled());
             settingsDTO.setMapExistingAuthApps(APIUtil.isMapExistingAuthAppsEnabled());
             Map<String, Environment> environments = APIUtil.getEnvironments();
-            Environment environment = environments.get(RestApiConstants.DEFAULT_ENVIRONMENT);
-            settingsDTO.apiGatewayEndpoint(environment.getApiGatewayEndpoint());
+            if (environments.size()==1){
+                Map.Entry<String,Environment> entry = environments.entrySet().iterator().next();
+                Environment singleEnvironment = environments.get(entry.getKey());
+                settingsDTO.apiGatewayEndpoint(singleEnvironment.getApiGatewayEndpoint());
+            } else {
+                for (Map.Entry<String, Environment> entry : environments.entrySet()) {
+                    Environment multipleEnvironment = environments.get(entry.getKey());
+                    if (multipleEnvironment.isDefault()) {
+                        settingsDTO.apiGatewayEndpoint(multipleEnvironment.getApiGatewayEndpoint());
+                        break;
+                    }
+                }
+                Map.Entry<String,Environment> entry = environments.entrySet().iterator().next();
+                Environment singleEnvironment = environments.get(entry.getKey());
+                settingsDTO.apiGatewayEndpoint(singleEnvironment.getApiGatewayEndpoint());
+            }
         } else {
             settingsDTO.setScopes(GetScopeList());
             settingsDTO.setApplicationSharingEnabled(APIUtil.isMultiGroupAppSharingEnabled());
