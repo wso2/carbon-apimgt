a3302f3919f8ecf269b4aac7a17020768aff1cbe
ashera96 <asherasilva100@gmail.com>
2020-01-29 03:44:25 +0530

    Removed Publisher UI code

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/index.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/index.jsx
@@ -25,7 +25,6 @@ import EndpointIcon from '@material-ui/icons/GamesOutlined';
 import PersonPinCircleOutlinedIcon from '@material-ui/icons/PersonPinCircleOutlined';
 import ResourcesIcon from '@material-ui/icons/VerticalSplit';
 import ScopesIcon from '@material-ui/icons/VpnKey';
-import QueryAnalysisIcon from '@material-ui/icons/Security';
 import DocumentsIcon from '@material-ui/icons/LibraryBooks';
 import BusinessIcon from '@material-ui/icons/Business';
 import CodeIcon from '@material-ui/icons/Code';
@@ -66,7 +65,6 @@ import Environments from './Environments/Environments';
 import Subscriptions from './Subscriptions/Subscriptions';
 import Comments from './Comments/Comments';
 import Scope from './Scopes';
-import QueryAnalysis from './QueryAnalysis/QueryAnalysis';
 import Security from './Security';
 import APIDefinition from './APIDefinition/APIDefinition';
 import APIDetailsTopMenu from './components/APIDetailsTopMenu';
@@ -570,16 +568,6 @@ class Details extends Component {
                                 Icon={<ScopesIcon />}
                             />
                         )}
-                        {api.type === 'GRAPHQL' && (
-                            <LeftMenuItem
-                                text={intl.formatMessage({
-                                    id: 'Apis.Details.index.query.analysis',
-                                    defaultMessage: 'query analysis',
-                                })}
-                                to={pathPrefix + 'query analysis'}
-                                Icon={<QueryAnalysisIcon />}
-                            />
-                        )}
                         <LeftMenuItem
                             text={intl.formatMessage({
                                 id: 'Apis.Details.index.business.info',
@@ -692,10 +680,6 @@ class Details extends Component {
                                 />
 
                                 <Route path={Details.subPaths.SCOPES} component={() => <Scope api={api} />} />
-                                <Route
-                                    path={Details.subPaths.QUERY_ANALYSIS}
-                                    component={() => <QueryAnalysis api={api} />}
-                                />
                                 <Route path={Details.subPaths.DOCUMENTS} component={() => <Documents api={api} />} />
                                 <Route
                                     path={Details.subPaths.DOCUMENTS_PRODUCT}
@@ -768,7 +752,6 @@ Details.subPaths = {
     RESOURCES_PRODUCT: '/api-products/:apiprod_uuid/resources',
     RESOURCES_PRODUCT_EDIT: '/api-products/:apiprod_uuid/resources/edit',
     SCOPES: '/apis/:api_uuid/scopes',
-    QUERY_ANALYSIS: '/apis/:api_uuid/query analysis',
     DOCUMENTS: '/apis/:api_uuid/documents',
     DOCUMENTS_PRODUCT: '/api-products/:apiprod_uuid/documents',
     SUBSCRIPTIONS_PRODUCT: '/api-products/:apiprod_uuid/subscriptions',
