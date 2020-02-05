013839e42900234ee6a655db46cd2278a7bac651
1akshitha <lakshitha@wso2.com>
2020-02-05 21:50:54 +0530

    Added license and fixed minor issues

--- components/apimgt/org.wso2.carbon.apimgt.event.output.adapter.http.extended.oauth/src/main/java/org/wso2/carbon/apimgt/event/output/adapter/http/extended/oauth/ExtendedHTTPEventAdapterFactory.java
+++ components/apimgt/org.wso2.carbon.apimgt.event.output.adapter.http.extended.oauth/src/main/java/org/wso2/carbon/apimgt/event/output/adapter/http/extended/oauth/ExtendedHTTPEventAdapterFactory.java
@@ -1,20 +1,20 @@
 /*
- *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
+
 package org.wso2.carbon.apimgt.event.output.adapter.http.extended.oauth;
 
 import org.wso2.carbon.apimgt.event.output.adapter.http.extended.oauth.internal.util.ExtendedHTTPEventAdapterConstants;
@@ -23,11 +23,12 @@ import org.wso2.carbon.event.output.adapter.core.*;
 import java.util.*;
 
 /**
- * The recommendation event adapter factory class to create a recommendation output adapter
+ * The Extended HTTP event adapter factory class to create an Extended Http output adapter
  */
 public class ExtendedHTTPEventAdapterFactory extends OutputEventAdapterFactory {
     private ResourceBundle resourceBundle =
-            ResourceBundle.getBundle("org.wso2.carbon.event.output.adapter.recommendationevent.i18n.Resources", Locale.getDefault());
+            ResourceBundle.getBundle("org.wso2.carbon.apimgt.event.output.adapter.http.extended.oauth.i18n.Resources",
+                    Locale.getDefault());
 
     @Override
     public String getType() {
