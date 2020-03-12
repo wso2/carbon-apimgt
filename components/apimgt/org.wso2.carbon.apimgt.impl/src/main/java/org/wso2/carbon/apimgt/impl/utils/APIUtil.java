82616c96b1aaf6ab1c728dfa6ca79bfec1431aed
Tharindu Dharmarathna <tharindua@wso2.com>
2020-03-12 16:01:56 +0530

    fix tenant loading issues

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
@@ -7162,8 +7162,7 @@ public final class APIUtil {
      */
     public static JSONObject getTenantRESTAPIScopesConfig(String tenantDomain) throws APIManagementException {
         JSONObject restAPIConfigJSON = null;
-        int tenantId = getTenantIdFromTenantDomain(tenantDomain);
-        JSONObject tenantConfJson = getTenantConfig(tenantId);
+        JSONObject tenantConfJson = getTenantConfig(tenantDomain);
         if (tenantConfJson != null) {
             restAPIConfigJSON = getRESTAPIScopesFromTenantConfig(tenantConfJson);
             if (restAPIConfigJSON == null) {
@@ -7181,8 +7180,7 @@ public final class APIUtil {
      */
     public static JSONObject getTenantRESTAPIScopeRoleMappingsConfig(String tenantDomain) throws APIManagementException {
         JSONObject restAPIConfigJSON = null;
-        int tenantId = getTenantIdFromTenantDomain(tenantDomain);
-        JSONObject tenantConfJson = getTenantConfig(tenantId);
+        JSONObject tenantConfJson = getTenantConfig(tenantDomain);
         if (tenantConfJson != null) {
             restAPIConfigJSON = getRESTAPIScopeRoleMappingsFromTenantConfig(tenantConfJson);
             if (restAPIConfigJSON == null) {
@@ -7213,6 +7211,9 @@ public final class APIUtil {
             if (tenantConfigCache.containsKey(cacheName)) {
                 return (JSONObject) tenantConfigCache.get(cacheName);
             } else {
+                if (tenantId != MultitenantConstants.SUPER_TENANT_ID) {
+                    loadTenantRegistry(tenantId);
+                }
                 RegistryService registryService = ServiceReferenceHolder.getInstance().getRegistryService();
                 UserRegistry registry = registryService.getConfigSystemRegistry(tenantId);
                 Resource resource;
@@ -10247,9 +10248,7 @@ public final class APIUtil {
         public static boolean isPerTenantServiceProviderEnabled(String tenantDomain) throws APIManagementException,
                 RegistryException {
 
-        int tenantId = getTenantIdFromTenantDomain(tenantDomain);
-        loadTenantRegistry(tenantId);
-        JSONObject tenantConfig = getTenantConfig(tenantId);
+            JSONObject tenantConfig = getTenantConfig(tenantDomain);
         if (tenantConfig.containsKey(APIConstants.ENABLE_PER_TENANT_SERVICE_PROVIDER_CREATION)) {
             return (boolean) tenantConfig.get(APIConstants.ENABLE_PER_TENANT_SERVICE_PROVIDER_CREATION);
         }
