a50f7f7888ddb754b8337e687ecc2005b7359c51
Ishara Cooray <isharac@wso2.com>
2020-04-27 18:28:33 +0530

    Moved throttling service  REST API to swagger-codegen-maven-plugin fix product-apim#7957

--- components/apimgt/org.wso2.carbon.apimgt.throttling.service/src/gen/java/org/wso2/carbon/throttle/service/ThrottleApi.java
+++ components/apimgt/org.wso2.carbon.apimgt.throttling.service/src/gen/java/org/wso2/carbon/throttle/service/ThrottleApi.java
@@ -1,44 +1,50 @@
 package org.wso2.carbon.throttle.service;
 
-import org.wso2.carbon.throttle.service.dto.*;
-import org.wso2.carbon.throttle.service.ThrottleApiService;
-import org.wso2.carbon.throttle.service.factories.ThrottleApiServiceFactory;
-
-import io.swagger.annotations.ApiParam;
-
 import org.wso2.carbon.throttle.service.dto.ErrorDTO;
 import org.wso2.carbon.throttle.service.dto.ThrottledEventDTO;
+import org.wso2.carbon.throttle.service.ThrottleApiService;
+import org.wso2.carbon.throttle.service.impl.ThrottleApiServiceImpl;
+import org.wso2.carbon.apimgt.api.APIManagementException;
 
-import java.util.List;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.inject.Inject;
 
+import io.swagger.annotations.*;
 import java.io.InputStream;
+
+import org.apache.cxf.jaxrs.ext.MessageContext;
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 
-import javax.ws.rs.core.Response;
-import javax.ws.rs.*;
-
+import java.util.Map;
+import java.util.List;
+import javax.validation.constraints.*;
 @Path("/throttle")
 
+@Api(description = "the throttle API")
+
 @Produces({ "application/json" })
-@io.swagger.annotations.Api(value = "/throttle", description = "the throttle API")
+
+
 public class ThrottleApi  {
 
-   private final ThrottleApiService delegate = ThrottleApiServiceFactory.getThrottleApi();
+  @Context MessageContext securityContext;
+
+ThrottleApiService delegate = new ThrottleApiServiceImpl();
+
 
     @GET
     
     
     @Produces({ "application/json" })
-    @io.swagger.annotations.ApiOperation(value = "throttled events available", notes = "This will provide access to throttled events in database.", response = ThrottledEventDTO.class, responseContainer = "List")
-    @io.swagger.annotations.ApiResponses(value = { 
-        @io.swagger.annotations.ApiResponse(code = 200, message = "An array of shops around you"),
-        
-        @io.swagger.annotations.ApiResponse(code = 200, message = "Unexpected error") })
-
-    public Response throttleGet(@ApiParam(value = "**Search condition**.\n\nYou can search for an application by specifying the name as \"query\" attribute.\n\nEg.\n\"app1\" will match an application if the name is exactly \"app1\".\n\nCurrently this does not support wildcards. Given name must exactly match the application name.") @QueryParam("query") String query)
-    {
-    return delegate.throttleGet(query);
+    @ApiOperation(value = "throttled events available", notes = "This will provide access to throttled events in database. ", response = ThrottledEventDTO.class, responseContainer = "List", tags={ "Shops" })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "An array of shops around you", response = ThrottledEventDTO.class, responseContainer = "List"),
+        @ApiResponse(code = 200, message = "Unexpected error", response = ErrorDTO.class) })
+    public Response throttleGet( @ApiParam(value = "**Search condition**.  You can search for an application by specifying the name as \"query\" attribute.  Eg. \"app1\" will match an application if the name is exactly \"app1\".  Currently this does not support wildcards. Given name must exactly match the application name. ")  @QueryParam("query") String query) throws APIManagementException{
+        return delegate.throttleGet(query, securityContext);
     }
 }
-
--- components/apimgt/org.wso2.carbon.apimgt.throttling.service/src/gen/java/org/wso2/carbon/throttle/service/dto/ErrorDTO.java
+++ components/apimgt/org.wso2.carbon.apimgt.throttling.service/src/gen/java/org/wso2/carbon/throttle/service/dto/ErrorDTO.java
@@ -1,29 +1,32 @@
 package org.wso2.carbon.throttle.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import javax.validation.constraints.*;
+
 
 import io.swagger.annotations.*;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Objects;
 
-import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.*;
+import org.wso2.carbon.apimgt.rest.api.util.annotations.Scope;
 
 
 
-@ApiModel(description = "")
-public class ErrorDTO  {
+public class ErrorDTO   {
   
-  
-  
-  private Integer code = null;
-  
-  
-  private String message = null;
-  
-  
-  private String fields = null;
+    private Integer code = null;
+    private String message = null;
+    private String fields = null;
 
-  
   /**
    **/
+  public ErrorDTO code(Integer code) {
+    this.code = code;
+    return this;
+  }
+
+  
   @ApiModelProperty(value = "")
   @JsonProperty("code")
   public Integer getCode() {
@@ -33,9 +36,14 @@ public class ErrorDTO  {
     this.code = code;
   }
 
-  
   /**
    **/
+  public ErrorDTO message(String message) {
+    this.message = message;
+    return this;
+  }
+
+  
   @ApiModelProperty(value = "")
   @JsonProperty("message")
   public String getMessage() {
@@ -45,9 +53,14 @@ public class ErrorDTO  {
     this.message = message;
   }
 
-  
   /**
    **/
+  public ErrorDTO fields(String fields) {
+    this.fields = fields;
+    return this;
+  }
+
+  
   @ApiModelProperty(value = "")
   @JsonProperty("fields")
   public String getFields() {
@@ -57,17 +70,47 @@ public class ErrorDTO  {
     this.fields = fields;
   }
 
-  
 
   @Override
-  public String toString()  {
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ErrorDTO error = (ErrorDTO) o;
+    return Objects.equals(code, error.code) &&
+        Objects.equals(message, error.message) &&
+        Objects.equals(fields, error.fields);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(code, message, fields);
+  }
+
+  @Override
+  public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("class ErrorDTO {\n");
     
-    sb.append("  code: ").append(code).append("\n");
-    sb.append("  message: ").append(message).append("\n");
-    sb.append("  fields: ").append(fields).append("\n");
-    sb.append("}\n");
+    sb.append("    code: ").append(toIndentedString(code)).append("\n");
+    sb.append("    message: ").append(toIndentedString(message)).append("\n");
+    sb.append("    fields: ").append(toIndentedString(fields)).append("\n");
+    sb.append("}");
     return sb.toString();
   }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
 }
+
