0306120991d26501a01ab73218aaa537c1ff3525
Chanaka Jayasena <chanaka3d@gmail.com>
2020-04-30 08:39:26 +0530

    Fixing eslint issues

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin_new/source/src/app/components/AdminPages/Microgateways/List.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin_new/source/src/app/components/AdminPages/Microgateways/List.jsx
@@ -1,5 +1,24 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -15,8 +34,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import EditIcon from '@material-ui/icons/Edit';
-import Paper from '@material-ui/core/Paper';
-import MUIDataTable from "mui-datatables";
+import MUIDataTable from 'mui-datatables';
 import ContentBase from 'AppComponents/AdminPages/Addons/ContentBase';
 import HelpBase from 'AppComponents/AdminPages/Addons/HelpBase';
 import InlineProgress from 'AppComponents/AdminPages/Addons/InlineProgress';
@@ -24,207 +42,219 @@ import AddEdit from 'AppComponents/AdminPages/Microgateways/AddEdit';
 import Alert from 'AppComponents/Shared/Alert';
 import Delete from 'AppComponents/AdminPages/MicroGateways/Delete';
 
-// Mock API call to save or edit
-function apiCall() { 
-  return new Promise(function(resolve, reject) { 
-      setTimeout(() => {resolve([
-        { id: '1', label: "West Wing", description: "It's somewhat hot"},
-        { id: '2', label: "East Wing", description: "It's cool"},
-        { id: '3', label: "South Wing", description: "It's red zone"},
-        { id: '4', label: "Noth Wing", description: "It's blue zone"},
-      ])}, 1000); 
-  }); 
-} 
+/**
+ * Mock API call
+ * @returns {Promise}.
+ */
+function apiCall() {
+    return new Promise(((resolve) => {
+        setTimeout(() => {
+            resolve([
+                { id: '1', label: 'West Wing', description: "It's somewhat hot" },
+                { id: '2', label: 'East Wing', description: "It's cool" },
+                { id: '3', label: 'South Wing', description: "It's red zone" },
+                { id: '4', label: 'Noth Wing', description: "It's blue zone" },
+            ]);
+        }, 1000);
+    }));
+}
 
 const styles = (theme) => ({
-  searchBar: {
-    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
-  },
-  searchInput: {
-    fontSize: theme.typography.fontSize,
-  },
-  block: {
-    display: 'block',
-  },
-  addUser: {
-    marginRight: theme.spacing(1),
-  },
-  contentWrapper: {
-    margin: '40px 16px',
-  },
-  secondaryBar: {
-    zIndex: 0,
-  },
-  menuButton: {
-    marginLeft: -theme.spacing(1),
-  },
-  iconButtonAvatar: {
-    padding: 4,
-  },
-  link: {
-    textDecoration: 'none',
-    color: 'rgba(255, 255, 255, 0.7)',
-    '&:hover': {
-      color: theme.palette.common.white,
+    searchBar: {
+        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
     },
-  },
-  button: {
-    borderColor: 'rgba(255, 255, 255, 0.7)',
-  },
-  main: {
-    flex: 1,
-    padding: theme.spacing(6, 4),
-    background: '#eaeff1',
-  },
+    searchInput: {
+        fontSize: theme.typography.fontSize,
+    },
+    block: {
+        display: 'block',
+    },
+    contentWrapper: {
+        margin: '40px 16px',
+    },
+    button: {
+        borderColor: 'rgba(255, 255, 255, 0.7)',
+    },
 });
 
+/**
+ * Render a list
+ * @param {JSON} props .
+ * @returns {JSX} Header AppBar components.
+ */
 function ListLabels(props) {
-  const { classes } = props;
-  const [data, setData] = useState(null);
-  const [searchText, setSearchText] = useState('');
+    const { classes } = props;
+    const [data, setData] = useState(null);
+    const [searchText, setSearchText] = useState('');
 
-  const filterData = (event) => {
-    setSearchText(event.target.value);
-  };
-  const fetchData = () => {
+    const filterData = (event) => {
+        setSearchText(event.target.value);
+    };
+    const fetchData = () => {
     // Fetch data from backend
-    setData(null);
-    let promiseAPICall = apiCall();
-    promiseAPICall.then((data) => {
-        setData(data);
-    })
-    .catch((e) => {
-        Alert.error(e);
-    })
-    
-  }
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const columns = [
-    {
-      name: "label",
-      label: "Label",
-      options: {
-        filter: true,
-        sort: true,
-      }
-    },
-    {
-      name: "description",
-      label: "Description",
-      options: {
-        filter: true,
-        sort: false,
-      }
-    },
-    {
-      name: "id",
-      label: "Actions",
-      options: {
+        setData(null);
+        const promiseAPICall = apiCall();
+        promiseAPICall.then((LocalData) => {
+            setData(LocalData);
+        })
+            .catch((e) => {
+                Alert.error(e);
+            });
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+    const columns = [
+        {
+            name: 'label',
+            label: 'Label',
+            options: {
+                filter: true,
+                sort: true,
+            },
+        },
+        {
+            name: 'description',
+            label: 'Description',
+            options: {
+                filter: true,
+                sort: false,
+            },
+        },
+        {
+            name: 'id',
+            label: 'Actions',
+            options: {
+                filter: false,
+                sort: false,
+                customBodyRender: (value, tableMeta) => {
+                    const dataRow = data[tableMeta.rowIndex];
+                    return (
+                        <>
+                            <AddEdit
+                                dataRow={dataRow}
+                                updateList={() => fetchData()}
+                                icon={<EditIcon />}
+                                title='Edit Microgateway'
+                            />
+                            <Delete dataRow={dataRow} updateList={() => fetchData()} />
+                        </>
+                    );
+                },
+                setCellProps: () => {
+                    return {
+                        style: { width: 200 },
+                    };
+                },
+            },
+        },
+    ];
+
+
+    const options = {
+        filterType: 'checkbox',
+        selectableRows: 'none',
         filter: false,
-        sort: false,
-        customBodyRender: (value, tableMeta) => {
-          const dataRow = data[tableMeta.rowIndex];
-          return <>
-                    <AddEdit dataRow={dataRow} updateList={() => fetchData()} icon={<EditIcon />} title='Edit Microgateway' />
-                    <Delete dataRow={dataRow} updateList={() => fetchData()} />
-                </>  
-        },
-        setCellProps: () => {
-          return {
-            style: {width: 200}
-          };
-        },
-      }
-    },
-  ];
+        search: false,
+        print: false,
+        download: false,
+        viewColumns: false,
+        customToolbar: null,
+        responsive: 'stacked',
+        searchText,
+    };
+    const help = (
+        <HelpBase>
+            <List component='nav' aria-label='main mailbox folders'>
+                <ListItem button>
+                    <ListItemIcon>
+                        <RefreshIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='Inbox' />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <RefreshIcon />
+                    </ListItemIcon>
+                    <ListItemText primary='Drafts' />
+                </ListItem>
+            </List>
+        </HelpBase>
+    );
+    return (
 
+        <>
+            {/*
+                without pageStyle attribute = This is the default case where it
+                takes fixed size center part of the screen.
+                pageStyle='full-page' = Take the full content area.
+                pageStyle='no-paper' = Avoid from displaying background paper. ( For dashbord we need this )
+             */}
+            <ContentBase title='Microgateways' subtitle='Microgateways' help={help} pageStyle='full-page'>
+                <AppBar className={classes.searchBar} position='static' color='default' elevation={0}>
+                    <Toolbar>
+                        <Grid container spacing={2} alignItems='center'>
+                            <Grid item>
+                                <SearchIcon className={classes.block} color='inherit' />
+                            </Grid>
+                            <Grid item xs>
+                                <TextField
+                                    fullWidth
+                                    placeholder={(
+                                        <FormattedMessage
+                                            id='AdminPages.Microgateways.List.search.default'
+                                            defaultMessage='Search by Microgateway label'
+                                        />
+                                    )}
+                                    InputProps={{
+                                        disableUnderline: true,
+                                        className: classes.searchInput,
+                                    }}
+                                    onChange={filterData}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <AddEdit
+                                    updateList={() => fetchData()}
+                                    triggerButtonText='Add Microgateway'
+                                    title='Add Microgateway'
+                                />
+                                <Tooltip title='Reload'>
+                                    <IconButton onClick={fetchData}>
+                                        <RefreshIcon className={classes.block} color='inherit' />
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
 
-  const options = {
-    filterType: 'checkbox',
-    selectableRows: 'none',
-    filter: false,
-    search: false,
-    print: false,
-    download: false,
-    viewColumns: false,
-    customToolbar: null,
-    responsive: 'stacked',
-    searchText,
-  };
-  const help = <HelpBase>
-    <List component="nav" aria-label="main mailbox folders">
-      <ListItem button>
-        <ListItemIcon>
-          <RefreshIcon />
-        </ListItemIcon>
-        <ListItemText primary="Inbox" />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon>
-          <RefreshIcon />
-        </ListItemIcon>
-        <ListItemText primary="Drafts" />
-      </ListItem>
-    </List>
-  </HelpBase>
-  return (
-
-    <>
-      <ContentBase title='Microgateways' subtitle='Microgateways' help={help} pageStyle={'full-page'}>
-
-        <AppBar className={classes.searchBar} position="static" color="default" elevation={0}>
-          <Toolbar>
-            <Grid container spacing={2} alignItems="center">
-              <Grid item>
-                <SearchIcon className={classes.block} color="inherit" />
-              </Grid>
-              <Grid item xs>
-                <TextField
-                  fullWidth
-                  placeholder="Search by Microgateway label"
-                  InputProps={{
-                    disableUnderline: true,
-                    className: classes.searchInput,
-                  }}
-                  onChange={filterData}
-                />
-              </Grid>
-              <Grid item>
-                <AddEdit updateList={() => fetchData()} triggerButtonText='Add Microgateway' title='Add Microgateway' />
-                <Tooltip title="Reload">
-                  <IconButton onClick={fetchData}>
-                    <RefreshIcon className={classes.block} color="inherit" />
-                  </IconButton>
-                </Tooltip>
-              </Grid>
-            </Grid>
-          </Toolbar>
-        </AppBar>
-
-        {data && data.length > 0 && (<MUIDataTable
-          title={null}
-          data={data}
-          columns={columns}
-          options={options}
-        />)}
-        {data && data.length === 0 && (<div className={classes.contentWrapper}>
-          <Typography color="textSecondary" align="center">
-            <FormattedMessage
-              id='AdminPages.DemoTable.nodata.message'
-              defaultMessage='No Gateway Labels Yet'
-            />
-          </Typography>
-        </div>)}
-        {!data && (<InlineProgress />)}
-      </ContentBase>
-    </>
-  );
+                {data && data.length > 0 && (
+                    <MUIDataTable
+                        title={null}
+                        data={data}
+                        columns={columns}
+                        options={options}
+                    />
+                )}
+                {data && data.length === 0 && (
+                    <div className={classes.contentWrapper}>
+                        <Typography color='textSecondary' align='center'>
+                            <FormattedMessage
+                                id='AdminPages.Microgateways.List.nodata.message'
+                                defaultMessage='No Gateway Labels Yet'
+                            />
+                        </Typography>
+                    </div>
+                )}
+                {!data && (<InlineProgress />)}
+            </ContentBase>
+        </>
+    );
 }
 
 ListLabels.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.shape({}).isRequired,
 };
 
 export default withStyles(styles)(ListLabels);
