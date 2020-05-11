295aab158267314ee72f34914386310b02a58610
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2020-05-11 13:53:08 +0530

    Implement Breadcrumbs

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin_new/source/src/app/components/Base/Header/Breadcrumbs.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/admin_new/source/src/app/components/Base/Header/Breadcrumbs.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import { useIntl } from 'react-intl';
 import Typography from '@material-ui/core/Typography';
+import RouteMenuMapping from 'AppComponents/Base/RouteMenuMapping';
 import { Breadcrumbs as MUIBreadcrumbs } from '@material-ui/core';
-import Link from '@material-ui/core/Link';
 
 /**
- * Render header component
- * @param {JSON} event .
- * @returns {void}.
+ * Look for the relevant page IDs for given path
+ * @param {JSON} routeMenuMapping Route Menu Map object
+ * @param {string} currentPath page path to find IDs
+ * @param {list} pageIDs Internal parameter to yield pageIDs
+ * @returns {list} page IDs for given path
  */
-function handleClick(event) {
-    event.preventDefault();
+function getPageIDs(routeMenuMapping, currentPath, pageIDs = []) {
+    const routeEntries = routeMenuMapping.entries();
+    for (const routeEntry of routeEntries) {
+        for (const routeDetail of routeEntry) {
+            if (typeof routeDetail.children !== 'undefined') {
+                const result = getPageIDs(routeDetail.children, currentPath, pageIDs);
+                if (result !== '') {
+                    result.unshift(routeDetail.id);
+                    return result;
+                }
+            }
+            if (routeDetail.path === currentPath) {
+                return [routeDetail.id];
+            }
+        }
+    }
+    return '';
 }
 
 /**
@@ -17,15 +35,21 @@ function handleClick(event) {
  * @returns {JSX} breadcrumbs.
  */
 export default function Breadcrumbs() {
+    const intl = useIntl();
+    const routeMenuMapping = RouteMenuMapping(intl);
+    // eslint-disable-next-line no-restricted-globals
+    let pathnames = location.pathname.split('/').filter((x) => x);
+    pathnames = pathnames.slice(1);
+
+    const stringPath = '/' + pathnames.join('/');
+    const pageIDs = getPageIDs(routeMenuMapping, stringPath);
+
+
     return (
         <MUIBreadcrumbs aria-label='breadcrumb'>
-            <Link color='inherit' href='/' onClick={handleClick}>
-        Material-UI
-            </Link>
-            <Link color='inherit' href='/getting-started/installation/' onClick={handleClick}>
-        Core
-            </Link>
-            <Typography color='textPrimary'>Breadcrumb</Typography>
+            {pageIDs.map((page) => {
+                return <Typography color='textPrimary'>{page}</Typography>;
+            })}
         </MUIBreadcrumbs>
     );
 }
