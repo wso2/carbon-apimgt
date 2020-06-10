03ac898d6f5bc97ff117386a103073cb39fdab76
1akshitha <lakshitha@wso2.com>
2020-06-10 15:48:38 +0530

    Remove decoding the byte stream

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/gatewayartifactsynchronizer/DBSaver.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/gatewayartifactsynchronizer/DBSaver.java
@@ -32,7 +32,7 @@ public class DBSaver implements ArtifactSaver {
             throws ArtifactSynchronizerException {
 
         try {
-            byte[] gatewayAPIDTOAsBytes = Base64.encodeBase64(new Gson().toJson(gatewayAPIDTO).getBytes());
+            byte[] gatewayAPIDTOAsBytes = new Gson().toJson(gatewayAPIDTO).getBytes();
             ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(gatewayAPIDTOAsBytes);
             if (!apiMgtDAO.isAPIDetailsExists(gatewayAPIDTO.getApiId())) {
                 apiMgtDAO.addGatewayPublishedAPIDetails(gatewayAPIDTO);
@@ -53,7 +53,7 @@ public class DBSaver implements ArtifactSaver {
             throws ArtifactSynchronizerException {
 
         try {
-            byte[] gatewayAPIDTOAsBytes = Base64.encodeBase64(new Gson().toJson(gatewayAPIDTO).getBytes());
+            byte[] gatewayAPIDTOAsBytes = new Gson().toJson(gatewayAPIDTO).getBytes();
             ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(gatewayAPIDTOAsBytes);
             apiMgtDAO.updateGatewayPublishedAPIArtifacts(gatewayAPIDTO.getApiId(), gatewayAPIDTO.getGatewayLabel(),
                     byteArrayInputStream, gatewayAPIDTOAsBytes.length, gatewayInstruction);
