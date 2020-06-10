d6c765a9223d6299ba14fed4fbe4681b5c8a0c7e
Sarangan0219 <sarangan@wso2.com>
2020-06-11 03:03:05 +0530

    Add database entries for retrieving

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -15527,6 +15527,26 @@ public class ApiMgtDAO {
         return baip;
     }
 
+    public List<ByteArrayInputStream> getAllGatewayPublishedAPIArtifacts(String label) throws APIManagementException {
+        List<ByteArrayInputStream> baip = new ArrayList<>();
+        try (Connection connection = APIMgtDBUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(SQLConstants.GET_ALL_API_ARTIFACT)) {
+            statement.setString(1, label);
+            statement.setString(2, APIConstants.GatewayArtifactSynchronizer.GATEWAY_INSTRUCTION_PUBLISH);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                byte[] st = (byte[]) rs.getObject(1);
+                ByteArrayInputStream byteArrayInputStream= new ByteArrayInputStream(st);
+                baip.add(byteArrayInputStream);
+                log.info("Retrieved an api from DB");
+            }
+            return baip;
+        } catch (SQLException e) {
+            handleException("Failed to get artifacts " , e);
+        }
+        return baip;
+    }
+
     /**
      * Check whether the API is published in any of the Gateways
      *
