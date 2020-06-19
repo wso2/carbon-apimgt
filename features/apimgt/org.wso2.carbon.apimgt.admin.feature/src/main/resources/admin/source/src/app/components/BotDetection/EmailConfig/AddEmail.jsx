2f63aecd1584169aa0e7451f128b4e30e7932f82
chathuranga95 <chathuranga.15@cse.mrt.ac.lk>
2020-06-19 16:17:42 +0530

    Fix initial validation for email adding

--- features/apimgt/org.wso2.carbon.apimgt.admin.feature/src/main/resources/admin/source/src/app/components/BotDetection/EmailConfig/AddEmail.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.admin.feature/src/main/resources/admin/source/src/app/components/BotDetection/EmailConfig/AddEmail.jsx
@@ -43,16 +43,18 @@ function AddEmail(props) {
         updateList, icon, triggerButtonText, title, emailList,
     } = props;
     const classes = useStyles();
-    const [email, setEmail] = useState('');
+    const [email, setEmail] = useState();
 
     const onChange = (e) => {
         setEmail(e.target.value);
     };
 
-    const validateEmail = (value) => {
+    const validateEmail = () => {
+        if (email === undefined) {
+            return false;
+        }
         const schema = Joi.string().email().empty();
-        const validationError = schema.validate(value).error;
-
+        const validationError = schema.validate(email).error;
         if (validationError) {
             const errorType = validationError.details[0].type;
             if (errorType === 'any.empty') {
@@ -127,8 +129,8 @@ function AddEmail(props) {
                     </span>
                 )}
                 fullWidth
-                error={validateEmail(email)}
-                helperText={validateEmail(email) || 'Enter Email address'}
+                error={validateEmail()}
+                helperText={validateEmail() || 'Enter Email address'}
                 variant='outlined'
             />
         </FormDialogBase>
