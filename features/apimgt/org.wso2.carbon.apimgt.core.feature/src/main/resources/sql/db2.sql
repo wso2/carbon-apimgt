686f3a550b65e231198479099a58630a224583ff
Sarangan0219 <sarangan@wso2.com>
2020-07-06 14:06:34 +0530

    Add description in db scripts

--- features/apimgt/org.wso2.carbon.apimgt.core.feature/src/main/resources/sql/db2.sql
+++ features/apimgt/org.wso2.carbon.apimgt.core.feature/src/main/resources/sql/db2.sql
@@ -2540,6 +2540,9 @@ CREATE TABLE AM_API_CATEGORIES (
   PRIMARY KEY (UUID)
 ) /
 
+-- AM_GW_PUBLISHED_API_DETAILS & AM_GW_API_ARTIFACTS are independent tables for Artifact synchronizer feature which --
+-- should not have any referential integrity constraints with other tables in AM database--
+
 CREATE TABLE AM_GW_PUBLISHED_API_DETAILS (
   API_ID varchar(255) NOT NULL,
   TENANT_DOMAIN varchar(255),
