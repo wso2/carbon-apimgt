2c88451c269e40dd80c5fea9e202270cc5d6de12
meruja <merujajoseph@gmail.com>
2020-07-08 16:46:57 +0530

    Revert "Added role-alias mapping."
    This reverts commit 38fac0d09ddd57e13e830feaff4f1a5eaaaf5306.
    
    revert commit

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/gen/java/org/wso2/carbon/apimgt/rest/api/admin/v1/SystemScopesApiService.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/gen/java/org/wso2/carbon/apimgt/rest/api/admin/v1/SystemScopesApiService.java
@@ -10,7 +10,6 @@ import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 
 import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.ErrorDTO;
-import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.RoleAliasListDTO;
 import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.ScopeListDTO;
 import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.ScopeSettingsDTO;
 
@@ -24,7 +23,5 @@ import javax.ws.rs.core.SecurityContext;
 
 public interface SystemScopesApiService {
       public Response systemScopesGet(MessageContext messageContext) throws APIManagementException;
-      public Response systemScopesRoleAliasesGet(MessageContext messageContext) throws APIManagementException;
-      public Response systemScopesRoleAliasesPut(RoleAliasListDTO body, MessageContext messageContext) throws APIManagementException;
       public Response systemScopesScopeNameGet(String scopeName, String username, MessageContext messageContext) throws APIManagementException;
 }
