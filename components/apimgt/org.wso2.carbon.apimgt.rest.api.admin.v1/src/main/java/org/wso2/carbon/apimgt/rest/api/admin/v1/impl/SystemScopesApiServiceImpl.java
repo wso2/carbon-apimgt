203197a1ec6d7acf3897d46ec7435fcc15987923
meruja <merujajoseph@gmail.com>
2020-07-08 16:43:59 +0530

    Added role-alias mapping.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/v1/impl/SystemScopesApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/v1/impl/SystemScopesApiServiceImpl.java
@@ -1,23 +1,35 @@
 package org.wso2.carbon.apimgt.rest.api.admin.v1.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.json.simple.JSONObject;
 
 import org.wso2.carbon.apimgt.api.APIAdmin;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.ExceptionCodes;
 import org.wso2.carbon.apimgt.impl.APIAdminImpl;
+import org.wso2.carbon.apimgt.impl.APIConstants;
+import org.wso2.carbon.apimgt.impl.APIMRegistryService;
+import org.wso2.carbon.apimgt.impl.APIMRegistryServiceImpl;
 import org.wso2.carbon.apimgt.impl.utils.APIUtil;
 import org.wso2.carbon.apimgt.rest.api.admin.v1.*;
 import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.*;
 
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.MessageContext;
 
 import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.ErrorDTO;
 import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.ScopeSettingsDTO;
 import org.wso2.carbon.apimgt.rest.api.admin.v1.utils.mappings.SystemScopesMappingUtil;
 import org.wso2.carbon.apimgt.rest.api.util.utils.RestApiUtil;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
+import org.wso2.carbon.user.api.UserStoreException;
 import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 
 import java.util.Base64;
@@ -31,33 +43,34 @@ public class SystemScopesApiServiceImpl implements SystemScopesApiService {
 
     private static final Log log = LogFactory.getLog(SystemScopesApiServiceImpl.class);
 
-    public Response systemScopesScopeNameGet(String scope, String username, MessageContext messageContext) throws APIManagementException{
+    public Response systemScopesScopeNameGet(String scope, String username, MessageContext messageContext)
+            throws APIManagementException {
         ScopeSettingsDTO scopeSettingsDTO = new ScopeSettingsDTO();
         APIAdmin apiAdmin = new APIAdminImpl();
         String decodedScope = new String(Base64.getDecoder().decode(scope));
-        boolean existence ;
+        boolean existence;
 
-            if (username == null) {
-                existence = apiAdmin.isScopeExists(RestApiUtil.getLoggedInUsername(), decodedScope);
-                if (existence) {
-                    scopeSettingsDTO.setName(decodedScope);
-                } else {
-                    throw new APIManagementException("Scope Not Found.  Scope : " + decodedScope +
-                            ExceptionCodes.SCOPE_NOT_FOUND);
-                }
+        if (username == null) {
+            existence = apiAdmin.isScopeExists(RestApiUtil.getLoggedInUsername(), decodedScope);
+            if (existence) {
+                scopeSettingsDTO.setName(decodedScope);
             } else {
-                existence = apiAdmin.isScopeExistsForUser(username, decodedScope);
-                if (existence) {
-                    scopeSettingsDTO.setName(decodedScope);
-                } else {
-                    throw new APIManagementException("User does not have  scope. Username :" + username + " Scope : "
-                            + decodedScope + ExceptionCodes.SCOPE_NOT_FOUND_FOR_USER);
-                }
+                throw new APIManagementException("Scope Not Found. Scope : " + decodedScope +
+                        ExceptionCodes.SCOPE_NOT_FOUND);
             }
+        } else {
+            existence = apiAdmin.isScopeExistsForUser(username, decodedScope);
+            if (existence) {
+                scopeSettingsDTO.setName(decodedScope);
+            } else {
+                throw new APIManagementException("User does not have scope. Username : " + username + " Scope : "
+                        + decodedScope, ExceptionCodes.SCOPE_NOT_FOUND_FOR_USER);
+            }
+        }
         return Response.ok().entity(scopeSettingsDTO).build();
     }
 
-    public Response systemScopesGet(MessageContext messageContext) throws APIManagementException {
+    public Response systemScopesGet(MessageContext messageContext) {
         try {
             Map<String, String> scopeRoleMapping = APIUtil.getRESTAPIScopesForTenant(MultitenantUtils
                     .getTenantDomain(RestApiUtil.getLoggedInUsername()));
@@ -78,5 +91,60 @@ public class SystemScopesApiServiceImpl implements SystemScopesApiService {
                 .getTenantDomain(RestApiUtil.getLoggedInUsername()));
         ScopeListDTO scopeListDTO = SystemScopesMappingUtil.fromScopeListToScopeListDTO(scopeRoleMapping);
         return Response.ok().entity(scopeListDTO).build();
+    }
+
+    @Override
+    public Response systemScopesRoleAliasesGet(MessageContext messageContext) throws APIManagementException {
+        String tenantDomain = MultitenantUtils.getTenantDomain(RestApiUtil.getLoggedInUsername());
+        JSONObject tenantConfig = APIUtil.getTenantConfig(tenantDomain);
+        JSONObject roleMapping = (JSONObject) tenantConfig.get(APIConstants.REST_API_ROLE_MAPPINGS_CONFIG);
+        RoleAliasListDTO roleAliasListDTO = new RoleAliasListDTO();
+        if (roleMapping != null) {
+            roleAliasListDTO = SystemScopesMappingUtil.fromRoleAliasListToRoleAliasListDTO(
+                    SystemScopesMappingUtil.createMapOfRoleMapping((roleMapping)));
+        }
+        return Response.ok().entity(roleAliasListDTO).build();
+    }
+
+    @Override
+    public Response systemScopesRoleAliasesPut(RoleAliasListDTO body, MessageContext messageContext)
+            throws APIManagementException {
+        String tenantDomain = MultitenantUtils.getTenantDomain(RestApiUtil.getLoggedInUsername());
+        RoleAliasListDTO roleAliasListDTO = new RoleAliasListDTO();
+        //read from tenant-conf.json
+        JsonObject existingTenantConfObject = new JsonObject();
+
+        try {
+            APIMRegistryService apimRegistryService = new APIMRegistryServiceImpl();
+            String existingTenantConf = apimRegistryService.getConfigRegistryResourceContent(tenantDomain,
+                    APIConstants.API_TENANT_CONF_LOCATION);
+            existingTenantConfObject = new JsonParser().parse(existingTenantConf).getAsJsonObject();
+        } catch (RegistryException e) {
+            APIUtil.handleException("Couldn't read tenant configuration from tenant registry", e);
+        } catch (UserStoreException e) {
+            APIUtil.handleException("Couldn't read tenant configuration from User Store", e);
+        }
+        JSONObject responseJson = SystemScopesMappingUtil.createJsonObjectOfRoleMapping(body);
+        existingTenantConfObject.remove(APIConstants.REST_API_ROLE_MAPPINGS_CONFIG);
+        JsonElement jsonElement = new JsonParser().parse(String.valueOf(responseJson));
+        existingTenantConfObject.add(APIConstants.REST_API_ROLE_MAPPINGS_CONFIG, jsonElement);
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String formattedTenantConf = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(existingTenantConfObject.toString());
+            APIUtil.updateTenantConf(existingTenantConfObject.toString(), tenantDomain);
+            if (log.isDebugEnabled()) {
+                log.debug("Finalized tenant-conf.json: " + formattedTenantConf);
+            }
+        } catch (JsonProcessingException e) {
+            throw new APIManagementException("Error while formatting tenant-conf.json of tenant: " + tenantDomain, e);
+        }
+        JSONObject tenantConfig = APIUtil.getTenantConfig(tenantDomain);
+        JSONObject roleMapping = (JSONObject) tenantConfig.get(APIConstants.REST_API_ROLE_MAPPINGS_CONFIG);
+        if (roleMapping != null) {
+            roleAliasListDTO = SystemScopesMappingUtil.fromRoleAliasListToRoleAliasListDTO(
+                    SystemScopesMappingUtil.createMapOfRoleMapping((roleMapping)));
+        }
+        return Response.ok().entity(roleAliasListDTO).build();
     }
 }
--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/v1/utils/mappings/SystemScopesMappingUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/v1/utils/mappings/SystemScopesMappingUtil.java
@@ -22,12 +22,15 @@ import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
 import org.json.simple.JSONObject;
 import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.RoleAliasDTO;
+import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.RoleAliasListDTO;
 import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.ScopeDTO;
 import org.wso2.carbon.apimgt.rest.api.admin.v1.dto.ScopeListDTO;
 import org.wso2.carbon.apimgt.rest.api.util.utils.RestApiUtil;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -35,7 +38,7 @@ public class SystemScopesMappingUtil {
 
     private static final Log log = LogFactory.getLog(SystemScopesMappingUtil.class);
     private static final Object lock = new Object();
-    private static volatile Map<String, List<String>>  portalScopeList = new HashMap<>();
+    private static volatile Map<String, List<String>> portalScopeList = new HashMap<>();
 
     /**
      * Convert list of API Scope to ScopeListDTO
@@ -43,8 +46,8 @@ public class SystemScopesMappingUtil {
      * @param scopeRoleMapping Map of a Role Scope  Mapping
      * @return ScopeListDTO list containing role scope mapping data
      */
-    public static ScopeListDTO fromScopeListToScopeListDTO(Map<String, String>  scopeRoleMapping)
-            throws APIManagementException{
+    public static ScopeListDTO fromScopeListToScopeListDTO(Map<String, String> scopeRoleMapping)
+            throws APIManagementException {
         ScopeListDTO scopeListDTO = new ScopeListDTO();
         scopeListDTO.setCount(scopeRoleMapping.size());
         scopeListDTO.setList(fromRoleScopeMapToRoleScopeDTOList(scopeRoleMapping));
@@ -57,7 +60,7 @@ public class SystemScopesMappingUtil {
      * @param scopeRoleMapping Map of a Role Scope  Mapping
      * @return RoleScopeDTO list
      */
-    private static List<ScopeDTO> fromRoleScopeMapToRoleScopeDTOList(Map<String, String>  scopeRoleMapping)
+    private static List<ScopeDTO> fromRoleScopeMapToRoleScopeDTOList(Map<String, String> scopeRoleMapping)
             throws APIManagementException {
         List<ScopeDTO> scopeDTOs = new ArrayList<>(scopeRoleMapping.size());
 
@@ -69,7 +72,7 @@ public class SystemScopesMappingUtil {
             }
         }
 
-        for (Map.Entry<String, List<String>>  mapping : portalScopeList.entrySet()) {
+        for (Map.Entry<String, List<String>> mapping : portalScopeList.entrySet()) {
             if (scopeRoleMapping.containsKey(mapping.getKey())) {
                 ScopeDTO roleScopeDTO = new ScopeDTO();
                 roleScopeDTO.setName(mapping.getKey());
@@ -78,7 +81,7 @@ public class SystemScopesMappingUtil {
                 roleScopeDTO.setTag(mapping.getValue().get(1));
                 scopeDTOs.add(roleScopeDTO);
             } else {
-                log.warn("The scope "+ mapping.getKey() +" does not exist in tenant.conf");
+                log.warn("The scope " + mapping.getKey() + " does not exist in tenant.conf");
             }
         }
         return scopeDTOs;
@@ -101,5 +104,65 @@ public class SystemScopesMappingUtil {
         }
         responseJson.put("Scope", scopeJson);
         return responseJson;
+    }
+    /**
+     * Convert list of role alias mapping to RoleAliasListDTO
+     *
+     * @param roleMapping Map of a Role Alias Mapping
+     * @return RoleAliasListDTO list containing role scope mapping data
+     */
+    public static RoleAliasListDTO fromRoleAliasListToRoleAliasListDTO(Map<String, List<String>> roleMapping) {
+        RoleAliasListDTO roleAliasListDTO = new RoleAliasListDTO();
+        roleAliasListDTO.setCount(roleMapping.size());
+        roleAliasListDTO.setList(fromRoleAliasObjectToRoleAliasDTOList(roleMapping));
+        return roleAliasListDTO;
+    }
+
+    /**
+     * Converts api scope-role mapping to RoleScopeDTO List.
+     *
+     * @param roleMapping Map of a Role Scope  Mapping
+     * @return RoleScopeDTO list
+     */
+    private static List<RoleAliasDTO> fromRoleAliasObjectToRoleAliasDTOList(Map<String, List<String>> roleMapping) {
+        List<RoleAliasDTO> roleAliasDTOS = new ArrayList<>(roleMapping.size());
+        for (Map.Entry<String, List<String>> mapping : roleMapping.entrySet()) {
+            RoleAliasDTO roleAliasDTO = new RoleAliasDTO();
+            roleAliasDTO.setRole(mapping.getKey());
+            roleAliasDTO.setAliases(mapping.getValue());
+            roleAliasDTOS.add(roleAliasDTO);
+        }
+        return roleAliasDTOS;
+    }
+
+    /**
+     * Extract roles and aliases and create JSONObject
+     *
+     * @param body
+     * @return JSONObject   role-alias data
+     */
+    public static JSONObject createJsonObjectOfRoleMapping(RoleAliasListDTO body) {
+        JSONObject roleJson = new JSONObject();
+        for (RoleAliasDTO roleAlias : body.getList()) {
+            roleJson.put(roleAlias.getRole(), roleAlias.getAliases());
+        }
+        return roleJson;
+    }
+
+    /**
+     * Extract roles and aliases and create MAP from JSONObject
+     *
+     * @param roleMapping JSONObject
+     * @return Converted MAP of role alias list
+     */
+    public static Map<String, List<String>> createMapOfRoleMapping(JSONObject roleMapping) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (Object role : roleMapping.keySet()) {
+            List<String> result = new ArrayList<>();
+            String key = (String) role;
+            result.add((String) roleMapping.get(key));
+            map.put(key, result);
+        }
+        return map;
     }
 }
