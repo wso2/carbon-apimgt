29062325ac53da6d5ca4ea84175f9da8fcad6bb8
tharikaGitHub <tharika.m92@gmail.com>
2020-07-13 10:53:54 +0530

    Remove unused hardcoded password and username fields

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dto/EventHubConfigurationDto.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dto/EventHubConfigurationDto.java
@@ -120,8 +120,6 @@ public class EventHubConfigurationDto {
         private String type = "Binary";
         private String receiverUrlGroup = "tcp://localhost:9611";
         private String authUrlGroup = "ssl://localhost:9711";
-        private String username = "admin";
-        private String password = "admin";
 
         public String getType() {
 
@@ -151,26 +149,6 @@ public class EventHubConfigurationDto {
         public void setAuthUrlGroup(String authUrlGroup) {
 
             this.authUrlGroup = authUrlGroup;
-        }
-
-        public String getUsername() {
-
-            return username;
-        }
-
-        public void setUsername(String username) {
-
-            this.username = username;
-        }
-
-        public String getPassword() {
-
-            return password;
-        }
-
-        public void setPassword(String password) {
-
-            this.password = password;
         }
     }
 
