d52f681a7c651efd6b6a0c5bb325268210308509
Sarangan0219 <sarangan@wso2.com>
2020-07-14 16:35:30 +0530

    edit dao tests

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/test/java/org/wso2/carbon/apimgt/impl/dao/test/GatewayArtifactsMgtDAOTest.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/test/java/org/wso2/carbon/apimgt/impl/dao/test/GatewayArtifactsMgtDAOTest.java
@@ -7,8 +7,11 @@ import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.model.API;
 import org.wso2.carbon.apimgt.api.model.APIIdentifier;
@@ -25,6 +28,7 @@ import org.wso2.carbon.apimgt.impl.internal.ServiceReferenceHolder;
 import org.wso2.carbon.apimgt.impl.notifier.Notifier;
 import org.wso2.carbon.apimgt.impl.notifier.SubscriptionsNotifier;
 import org.wso2.carbon.apimgt.impl.utils.APIMgtDBUtil;
+import org.wso2.carbon.apimgt.impl.utils.GatewayArtifactsMgtDBUtil;
 import org.wso2.carbon.base.MultitenantConstants;
 import org.wso2.carbon.identity.core.util.IdentityConfigParser;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
@@ -35,12 +39,16 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamException;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( {KeyManagerHolder.class})
 public class GatewayArtifactsMgtDAOTest {
     public static GatewayArtifactsMgtDAO gatewayArtifactsMgtDAO;
     private API api;
@@ -48,6 +56,9 @@ public class GatewayArtifactsMgtDAOTest {
     private String version;
     private APIIdentifier apiId;
     private String label;
+    private String apiUUID;
+    private String gatewayruntimeArtifacts;
+
 
     @Before
     public void setUp() throws Exception {
@@ -55,14 +66,15 @@ public class GatewayArtifactsMgtDAOTest {
         APIManagerConfiguration config = new APIManagerConfiguration();
         initializeDatabase(dbConfigPath);
         config.load(dbConfigPath);
-        ServiceReferenceHolder.getInstance().setAPIManagerConfigurationService(new APIManagerConfigurationServiceImpl
-                (config));
+        ServiceReferenceHolder.getInstance().setAPIManagerConfigurationService(new APIManagerConfigurationServiceImpl(config));
         List<Notifier> notifierList = new ArrayList<>();
         SubscriptionsNotifier subscriptionsNotifier = new SubscriptionsNotifier();
         notifierList.add(subscriptionsNotifier);
         ServiceReferenceHolder.getInstance().getNotifiersMap().put(subscriptionsNotifier.getType(), notifierList);
         PowerMockito.mockStatic(KeyManagerHolder.class);
-        APIMgtDBUtil.initialize();
+        ServiceReferenceHolder.getInstance().getAPIManagerConfigurationService().getAPIManagerConfiguration().getGatewayArtifactSynchronizerProperties().
+                setArtifactSynchronizerDataSource("java:/comp/env/jdbc/WSO2AM_DB");
+        GatewayArtifactsMgtDBUtil.initialize();
         gatewayArtifactsMgtDAO = GatewayArtifactsMgtDAO.getInstance();
         IdentityTenantUtil.setRealmService(new TestRealmService());
         String identityConfigPath = System.getProperty("IdentityConfigurationPath");
@@ -75,7 +87,17 @@ public class GatewayArtifactsMgtDAOTest {
         apiId = new APIIdentifier("testGateway", apiName, version);
         api = new API(apiId);
         label = "Production and Sandbox";
+        UUID uuid = UUID.randomUUID();
+        apiUUID = uuid.toString();
+
+//        gatewayruntimeArtifacts = {"name":"PizzaShackAPI","version":"1.0.0","provider":"admin","tenantDomain":"carbon.super","apiId":"a7f58a46-2fa4-4d4e-b244-57dd6c03a5ae","override":true,"sequencesToBeRemove":["admin--PizzaShackAPI:v1.0.0--In","admin--PizzaShackAPI:v1.0.0--Fault","admin--PizzaShackAPI:v1.0.0--Out"],"localEntriesToBeRemove":["a7f58a46-2fa4-4d4e-b244-57dd6c03a5ae"],"endpointEntriesToBeRemove":["PizzaShackAPI--v1.0.0_APIsandboxEndpoint","PizzaShackAPI--v1.0.0_APIproductionEndpoint"]}
+//        byte[] gatewayRuntimeArtifactsAsBytes =  gatewayruntimeArtifacts.getBytes();
+//        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(gatewayRuntimeArtifactsAsBytes);
+//
+//        {"endpointEntriesToBeRemove":["PizzaShackAPI--v1.0.0_APIsandboxEndpoint","PizzaShackAPI--v1.0.0_APIproductionEndpoint"],"provider":"admin","sequencesToBeRemove":["admin--PizzaShackAPI:v1.0.0--In","admin--PizzaShackAPI:v1.0.0--Fault","admin--PizzaShackAPI:v1.0.0--Out"],"name":"PizzaShackAPI","override":true,"localEntriesToBeRemove":["a7f58a46-2fa4-4d4e-b244-57dd6c03a5ae"],"version":"1.0.0","tenantDomain":"carbon.super","apiId":"a7f58a46-2fa4-4d4e-b244-57dd6c03a5ae"}
+
     }
+
 
     private static void initializeDatabase(String configFilePath) {
 
@@ -124,23 +146,25 @@ public class GatewayArtifactsMgtDAOTest {
 
     @Test
     public void testAddGatewayPublishedAPIDetails() throws APIManagementException {
-        boolean result = gatewayArtifactsMgtDAO.addGatewayPublishedAPIDetails(api.getUUID(), apiName, version,
+        boolean result = gatewayArtifactsMgtDAO.addGatewayPublishedAPIDetails(apiUUID, apiName, version,
                 String.valueOf(MultitenantConstants.SUPER_TENANT_ID));
         Assert.assertTrue(result);
     }
 
-//    @Test
-//    public void testAddGatewayPublishedAPIArtifacts() throws APIManagementException {
-//        boolean result = gatewayArtifactsMgtDAO.addGatewayPublishedAPIArtifacts(api.getUUID(),
-//                "Production and Sandbox" ,
-//                APIConstants.GatewayArtifactSynchronizer.GATEWAY_INSTRUCTION_PUBLISH, SQLConstants.ADD_GW_API_ARTIFACT);
-//        Assert.assertTrue(result);
-//    }
+    @Test
+    public void testAddGatewayPublishedAPIArtifacts() throws APIManagementException {
+        ByteArrayInputStream anyInputStream = new ByteArrayInputStream("test data".getBytes());
+        boolean result = gatewayArtifactsMgtDAO.addGatewayPublishedAPIArtifacts(apiUUID,
+                label , anyInputStream, 1,
+                APIConstants.GatewayArtifactSynchronizer.GATEWAY_INSTRUCTION_PUBLISH, SQLConstants.ADD_GW_API_ARTIFACT);
+        Assert.assertTrue(result);
+    }
 
     @Test
     public void testGetGatewayPublishedAPIArtifacts() throws APIManagementException {
         String gatewayPublishedAPIArtifacts =
-                gatewayArtifactsMgtDAO.getGatewayPublishedAPIArtifacts(api.getUUID(), label, APIConstants.GatewayArtifactSynchronizer.GATEWAY_INSTRUCTION_PUBLISH);
+                gatewayArtifactsMgtDAO.getGatewayPublishedAPIArtifacts(apiUUID, label,
+                        APIConstants.GatewayArtifactSynchronizer.GATEWAY_INSTRUCTION_PUBLISH);
         Assert.assertNotNull(gatewayPublishedAPIArtifacts);
     }
 
@@ -152,19 +176,19 @@ public class GatewayArtifactsMgtDAOTest {
 
     @Test
     public void testIsAPIPublishedInAnyGateway() throws APIManagementException {
-        boolean isApiPublished = gatewayArtifactsMgtDAO.isAPIPublishedInAnyGateway(api.getUUID());
+        boolean isApiPublished = gatewayArtifactsMgtDAO.isAPIPublishedInAnyGateway(apiUUID);
         Assert.assertTrue(isApiPublished);
     }
 
     @Test
     public void testIsAPIDetailsExists() throws APIManagementException {
-        boolean isApiPublished = gatewayArtifactsMgtDAO.isAPIDetailsExists(api.getUUID());
+        boolean isApiPublished = gatewayArtifactsMgtDAO.isAPIDetailsExists(apiUUID);
         Assert.assertTrue(isApiPublished);
     }
 
     @Test
     public void testIsAPIArtifactExists() throws APIManagementException {
-        boolean isApiPublished = gatewayArtifactsMgtDAO.isAPIArtifactExists(api.getUUID(), label);
+        boolean isApiPublished = gatewayArtifactsMgtDAO.isAPIArtifactExists(apiUUID, label);
         Assert.assertTrue(isApiPublished);
     }
 
