8ab8085cae7cadc02e0b2f5e7419bfd1c813513e
Hasunie <hasunie@wso2.com>
2020-07-24 14:31:19 +0530

    #8810

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/TestKeyAuthenticator.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/TestKeyAuthenticator.java
@@ -16,6 +16,12 @@
 
 package org.wso2.carbon.apimgt.gateway.handlers.security;
 
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMNamespace;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.Constants;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.HttpStatus;
@@ -24,6 +30,8 @@ import org.apache.synapse.MessageContext;
 import org.apache.synapse.SynapseConstants;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
 import org.apache.synapse.rest.AbstractHandler;
+import org.apache.synapse.transport.passthru.PassThroughConstants;
+import org.apache.synapse.transport.passthru.util.RelayUtils;
 import org.wso2.carbon.apimgt.gateway.APIMgtGatewayConstants;
 import org.wso2.carbon.apimgt.gateway.handlers.Utils;
 
@@ -70,15 +78,18 @@ public class TestKeyAuthenticator extends AbstractHandler {
         Map headers = (Map) axis2MC.getProperty((APIMgtGatewayConstants.TRANSPORT_HEADERS));
         String originalToken = testKey;
         Object header = headers.get(APIMgtGatewayConstants.TEST_KEY);
+        int errorStatus;
         if (header == null) {
-            handleAuthenticationError(messageContext);
+            errorStatus = APISecurityConstants.API_AUTH_MISSING_CREDENTIALS;
+            handleAuthenticationError(messageContext, errorStatus);
         }
         String requestHeader = header.toString();
         if (originalToken.equals(requestHeader)) {
             logger.debug("Successfully authenticating the user for the API");
             return true;
         } else {
-            handleAuthenticationError(messageContext);
+            errorStatus = APISecurityConstants.API_AUTH_INVALID_CREDENTIALS;
+            handleAuthenticationError(messageContext, errorStatus);
             return false;
         }
     }
@@ -88,24 +99,78 @@ public class TestKeyAuthenticator extends AbstractHandler {
         return true;
     }
 
-    private void handleAuthenticationError(MessageContext mc) {
-        mc.setProperty(SynapseConstants.ERROR_CODE, "401");
-        mc.setProperty(SynapseConstants.ERROR_MESSAGE, "Authorize error");
-        mc.setProperty(SynapseConstants.ERROR_EXCEPTION, "Either test token is missing or found an invalid test token");
+    private void handleAuthenticationError(MessageContext mc, int status) {
+        mc.setProperty(SynapseConstants.ERROR_CODE, HttpStatus.SC_UNAUTHORIZED);
+        mc.setProperty(APIMgtGatewayConstants.HTTP_RESPONSE_STATUS_CODE, HttpStatus.SC_UNAUTHORIZED);
+        mc.setProperty(SynapseConstants.ERROR_DETAIL, "Either test token is missing or found an invalid test token");
 
         Mediator sequence = mc.getSequence(APISecurityConstants.API_AUTH_FAILURE_HANDLER);
-
-        mc.setProperty(APIMgtGatewayConstants.HTTP_RESPONSE_STATUS_CODE, HttpStatus.SC_UNAUTHORIZED);
+        // By default we send a 401 response back
+        org.apache.axis2.context.MessageContext axis2MC = ((Axis2MessageContext) mc).
+                getAxis2MessageContext();
+        // This property need to be set to avoid sending the content in pass-through pipe (request message)
+        // as the response.
+        axis2MC.setProperty(PassThroughConstants.MESSAGE_BUILDER_INVOKED, Boolean.TRUE);
+        try {
+            RelayUtils.consumeAndDiscardMessage(axis2MC);
+        } catch (AxisFault axisFault) {
+            //In case of an error it is logged and the process is continued because we're setting a fault message in the payload.
+            logger.error("Error occurred while consuming and discarding the message", axisFault);
+        }
+        axis2MC.setProperty(Constants.Configuration.MESSAGE_TYPE, "application/soap+xml");
+        mc.setProperty(APIMgtGatewayConstants.HTTP_RESPONSE_STATUS_CODE, status);
         // Invoke the custom error handler specified by the user
         if (sequence != null && !sequence.mediate(mc)) {
             // If needed user should be able to prevent the rest of the fault handling
             // logic from getting executed
             return;
         }
+        APISecurityException e = new APISecurityException(status, mc.getProperty(SynapseConstants.
+                ERROR_DETAIL).toString());
+        if (mc.isDoingPOX() || mc.isDoingGET()) {
+            setFaultPayload(mc, e);
+        } else {
+            setSOAPFault(mc, e);
+        }
         sendFault(mc, HttpStatus.SC_UNAUTHORIZED);
     }
 
     protected void sendFault(MessageContext messageContext, int status) {
         Utils.sendFault(messageContext, status);
+    }
+
+    protected void setFaultPayload(MessageContext messageContext, APISecurityException e) {
+        Utils.setFaultPayload(messageContext, getFaultPayload(e));
+    }
+
+    protected void setSOAPFault(MessageContext messageContext, APISecurityException e) {
+        Utils.setSOAPFault(messageContext, "Client", "Authentication Failure", e.getMessage());
+    }
+
+    protected OMElement getFaultPayload(APISecurityException e) {
+        OMFactory fac = OMAbstractFactory.getOMFactory();
+        OMNamespace ns = fac.createOMNamespace(APISecurityConstants.API_SECURITY_NS,
+                APISecurityConstants.API_SECURITY_NS_PREFIX);
+        OMElement payload = fac.createOMElement("fault", ns);
+
+        OMElement errorCode = fac.createOMElement("code", ns);
+        errorCode.setText(String.valueOf(e.getErrorCode()));
+        OMElement errorMessage = fac.createOMElement("message", ns);
+        errorMessage.setText(APISecurityConstants.getAuthenticationFailureMessage(e.getErrorCode()));
+        OMElement errorDetail = fac.createOMElement("description", ns);
+        errorDetail.setText(APISecurityConstants.getFailureMessageDetailDescription(e.getErrorCode(), e.getMessage()));
+
+        // if custom auth header is configured, the error message should specify its name instead of default value
+        if (e.getErrorCode() == APISecurityConstants.API_AUTH_MISSING_CREDENTIALS) {
+            String errorDescription =
+                    APISecurityConstants.getFailureMessageDetailDescription(e.getErrorCode(), e.getMessage()) +
+                            " test-key is missing";
+            errorDetail.setText(errorDescription);
+        }
+
+        payload.addChild(errorCode);
+        payload.addChild(errorMessage);
+        payload.addChild(errorDetail);
+        return payload;
     }
 }
