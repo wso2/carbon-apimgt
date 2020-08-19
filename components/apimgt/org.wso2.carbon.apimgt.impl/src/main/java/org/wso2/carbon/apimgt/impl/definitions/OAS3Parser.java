0d3ff9c41a0ca9835c6b2ed6b7740f38cdf1003a
Rakhitha Ranathunge <rakhitharr@live.com>
2020-08-19 10:05:31 +0530

    added comments

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OAS3Parser.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OAS3Parser.java
@@ -1460,6 +1460,7 @@ public class OAS3Parser extends APIDefinition {
             if (defaultScheme == null) {
                 SecurityScheme newDefault = new SecurityScheme();
                 newDefault.setType(SecurityScheme.Type.OAUTH2);
+                //Populating the default security scheme with default values
                 OAuthFlows newDefaultFlows = new OAuthFlows();
                 OAuthFlow newDefaultFlow = new OAuthFlow();
                 newDefaultFlow.setAuthorizationUrl("https://test.com");
