024ec843c594d31385f2506f2a6a3ebb8c8119bf
athiththan11 <code19111995@gmail.com>
2020-09-09 23:52:42 +0530

    remove setScopes

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/jwt/JWTValidatorImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/jwt/JWTValidatorImpl.java
@@ -167,10 +167,6 @@ public class JWTValidatorImpl implements JWTValidator {
         jwtValidationInfo.setIssuedTime(jwtClaimsSet.getIssueTime().getTime());
         jwtValidationInfo.setUser(jwtClaimsSet.getSubject());
         jwtValidationInfo.setJti(jwtClaimsSet.getJWTID());
-        if(jwtClaimsSet.getClaim(APIConstants.JwtTokenConstants.SCOPE) != null){
-            jwtValidationInfo.setScopes(Arrays.asList(jwtClaimsSet.getStringClaim(APIConstants.JwtTokenConstants.SCOPE)
-                    .split(APIConstants.JwtTokenConstants.SCOPE_DELIMITER)));
-        }
     }
     private JWKSet retrieveJWKSet() throws IOException, ParseException {
         String jwksInfo = JWTUtil
