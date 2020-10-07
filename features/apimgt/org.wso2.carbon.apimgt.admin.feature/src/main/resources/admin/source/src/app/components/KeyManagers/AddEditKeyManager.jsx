3a650b63b652864ba4ebe8862f8f91d343a0a884
chamilaadhi <cka.adhi@gmail.com>
2020-10-07 13:11:26 +0530

    Fix formatting issue

--- features/apimgt/org.wso2.carbon.apimgt.admin.feature/src/main/resources/admin/source/src/app/components/KeyManagers/AddEditKeyManager.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.admin.feature/src/main/resources/admin/source/src/app/components/KeyManagers/AddEditKeyManager.jsx
@@ -645,7 +645,12 @@ function AddEditKeyManager(props) {
                                             })}
                                         />
                                         <Box ml={1}>
-                                            <Button margin='dense' variant='outlined' disabled={!wellKnownEndpoint} onClick={importKMConfig}>
+                                            <Button
+                                                margin='dense'
+                                                variant='outlined'
+                                                disabled={!wellKnownEndpoint}
+                                                onClick={importKMConfig}
+                                            >
                                                 <FormattedMessage
                                                     id='KeyManagers.AddEditKeyManager.form.import.button'
                                                     defaultMessage='Import'
