cea6f97d42d31d0062b87ddc6586b23ddb572765
dushaniw <dushaniwellappili@gmail.com>
2020-12-06 11:43:34 +0530

    Add Interceptor skelatons.

---
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/interceptors/request/LogRequestInInterceptor.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.apimgt.rest.api.util.interceptors.request;
+
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+
+public class LogRequestInInterceptor extends AbstractPhaseInterceptor {
+
+    public LogRequestInInterceptor(String phase) {
+
+        super(Phase.PRE_INVOKE);
+    }
+
+    @Override
+    public void handleMessage(Message message) throws Fault {
+
+        //Read the resource path and verbs for history events on API Manager configuration
+        //Check whether invoking request matches with that list, if yes add event and payload to the Exchange object
+    }
+}
+
---
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/interceptors/response/LogResponseOutInterceptor.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.apimgt.rest.api.util.interceptors.response;
+
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+
+public class LogResponseOutInterceptor extends AbstractPhaseInterceptor {
+
+    public LogResponseOutInterceptor(String phase) {
+
+        super(Phase.PRE_LOGICAL);
+    }
+
+    @Override
+    public void handleMessage(Message message) throws Fault {
+
+        //Check if event is in Exchange map, if the response code is 200 or 201, record the event in DB
+
+    }
+}
+
