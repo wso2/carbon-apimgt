a9111e8025898b31955528df2aa70563beec2545
Ishara Cooray <isharac@wso2.com>
2020-12-08 07:59:29 +0530

    fix for TIETOOEMSUB-124

--- components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/DefaultKeyValidationHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/DefaultKeyValidationHandler.java
@@ -160,8 +160,14 @@ public class DefaultKeyValidationHandler extends AbstractKeyValidationHandler {
         }
         SubscriptionDataStore tenantSubscriptionStore =
                 SubscriptionDataHolder.getInstance().getTenantSubscriptionStore(tenantDomain);
-        API api = tenantSubscriptionStore.getApiByContextAndVersion(validationContext.getContext(),
-                validationContext.getVersion());
+        String version = validationContext.getVersion();
+        if (StringUtils.isNotEmpty(version) && version.startsWith(APIConstants.DEFAULT_VERSION_PREFIX)) {
+            String[] tempArr = version.split(APIConstants.DEFAULT_VERSION_PREFIX);
+            if (tempArr.length > 1) {
+                version = tempArr[1];
+            }
+        }
+        API api = tenantSubscriptionStore.getApiByContextAndVersion(validationContext.getContext(), version);
         boolean scopesValidated = false;
         if (api != null) {
 
