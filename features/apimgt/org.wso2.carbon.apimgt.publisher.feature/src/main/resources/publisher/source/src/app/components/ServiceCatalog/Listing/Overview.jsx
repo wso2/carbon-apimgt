37362a3003c7f91ca85f211dae8ad1abd44900c7
tharikaGitHub <tharika.m92@gmail.com>
2021-01-08 18:38:35 +0530

    Add Service definition download and view options

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/ServiceCatalog/Listing/Overview.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/ServiceCatalog/Listing/Overview.jsx
@@ -16,7 +16,12 @@
  * under the License.
  */
 
-import React, { useState, useEffect } from 'react';
+import React, {
+    useState,
+    useEffect,
+    lazy,
+    Suspense,
+} from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
@@ -36,9 +41,14 @@ import Container from '@material-ui/core/Container';
 import Utils from 'AppData/Utils';
 import CloudDownloadRounded from '@material-ui/icons/CloudDownloadRounded';
 import ResourceNotFound from 'AppComponents/Base/Errors/ResourceNotFound';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import CreateApi from 'AppComponents/ServiceCatalog/CreateApi';
 import Usages from 'AppComponents/ServiceCatalog/Listing/Usages';
 import Listing from 'AppComponents/ServiceCatalog/Listing/Listing';
+import VerticalDivider from 'AppComponents/Shared/VerticalDivider';
+import SwapHorizontalCircle from '@material-ui/icons/SwapHorizontalCircle';
+import YAML from 'js-yaml';
 import Box from '@material-ui/core/Box';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import { Link } from 'react-router-dom';
@@ -46,6 +56,8 @@ import Paper from '@material-ui/core/Paper';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import LocalOfferOutlinedIcon from '@material-ui/icons/LocalOfferOutlined';
+
+const MonacoEditor = lazy(() => import('react-monaco-editor' /* webpackChunkName: "APIDefMonacoEditor" */));
 
 const useStyles = makeStyles((theme) => ({
     preview: {
@@ -108,10 +120,25 @@ const useStyles = makeStyles((theme) => ({
     downloadServiceFlex: {
         display: 'flex',
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginLeft: -8,
+    },
+    downloadServiceGroup: {
+        display: 'flex',
+        flexDirection: 'row',
         marginLeft: -8,
     },
     iconSpacing: {
         marginRight: theme.spacing(1),
+    },
+    button: {
+        marginLeft: theme.spacing(2),
+    },
+    buttonIcon: {
+        marginRight: theme.spacing(1),
+    },
+    downloadButtonSpacing: {
+        marginLeft: theme.spacing(1),
     },
 }));
 
@@ -128,6 +155,10 @@ function Overview(props) {
     const serviceId = match.params.service_uuid;
     const [service, setService] = useState(null);
     const [notFound, setNotFound] = useState(true);
+    const [serviceDefinition, setServiceDefinition] = useState({});
+    const [openReadOnlyDefinition, setOpenReadOnlyDefinition] = useState(false);
+    const [format, setFormat] = useState('yaml');
+    const [convertTo, setConvertTo] = useState(null);
 
     // Get Service Details
     const getService = () => {
@@ -159,11 +190,65 @@ function Overview(props) {
             } else {
                 Alert.error(intl.formatMessage({
                     id: 'ServiceCatalog.Listing.Overview.download.service.zip.error',
-                    defaultMessage: 'Something went wrong while downloading the Service Definition.',
+                    defaultMessage: 'Something went wrong while downloading the Service.',
                 }));
             }
         });
     }
+
+    /**
+     * Toggle the format of the service definition.
+     * JSON -> YAML, YAML -> JSON
+     */
+    const onChangeFormatClick = () => {
+        let formattedString = '';
+        if (convertTo === 'json') {
+            formattedString = JSON.stringify(YAML.load(serviceDefinition), null, 1);
+        } else {
+            formattedString = YAML.safeDump(YAML.safeLoad(serviceDefinition));
+        }
+        setServiceDefinition(formattedString);
+        const tmpConvertTo = convertTo;
+        setConvertTo(format);
+        setFormat(tmpConvertTo);
+    };
+
+    const getConvertToFormat = (value) => {
+        return value === 'json' ? 'yaml' : 'json';
+    };
+
+    const showServiceDefinition = () => {
+        if (openReadOnlyDefinition) {
+            setOpenReadOnlyDefinition(false);
+            setConvertTo(null);
+            setServiceDefinition({});
+            setFormat('yaml');
+        } else {
+            const promisedServiceDefinition = ServiceCatalog.getServiceDefinition(serviceId);
+            promisedServiceDefinition.then((data) => {
+                if (service.definitionType !== 'GRAPHQL_SDL') {
+                    setServiceDefinition(YAML.safeDump(YAML.safeLoad(data)));
+                    setFormat('yaml');
+                    setConvertTo(getConvertToFormat(format));
+                } else {
+                    setServiceDefinition(data.obj.schemaDefinition);
+                    setFormat('txt');
+                    setConvertTo(null);
+                }
+                setOpenReadOnlyDefinition(true);
+            }).catch((error) => {
+                if (error.response) {
+                    Alert.error(error.response.body.description);
+                } else {
+                    Alert.error(intl.formatMessage({
+                        id: 'ServiceCatalog.Listing.Overview.download.service.def.error',
+                        defaultMessage: 'Something went wrong while retrieving the Service Definition.',
+                    }));
+                }
+            });
+        }
+        return null;
+    };
 
     useEffect(() => {
         getService();
@@ -314,6 +399,14 @@ function Overview(props) {
         );
     }
 
+    const editorOptions = {
+        selectOnLineNumbers: true,
+        readOnly: true,
+        smoothScrolling: true,
+        wordWrap: 'on',
+        scrollBeyondLastLine: false,
+    };
+
     return (
         <>
             <Container maxWidth='md'>
@@ -441,21 +534,70 @@ function Overview(props) {
                                                 </div>
                                             </TableCell>
                                             <TableCell>
-                                                <div>
-                                                    <Button
-                                                        onClick={
-                                                            () => exportService(service.displayName, service.version)
-                                                        }
-                                                        color='primary'
-                                                        className={classes.downloadServiceFlex}
-                                                    >
-                                                        <CloudDownloadRounded className={classes.iconSpacing} />
-                                                        <FormattedMessage
-                                                            id='ServiceCatalog.Listing.Overview.download.service'
-                                                            defaultMessage='Download'
-                                                        />
-                                                    </Button>
+                                                <div className={classes.downloadServiceFlex}>
+                                                    <div className={classes.downloadServiceGroup}>
+                                                        <Button
+                                                            onClick={
+                                                                () => exportService(
+                                                                    service.displayName,
+                                                                    service.version,
+                                                                )
+                                                            }
+                                                            color='primary'
+                                                            className={classes.downloadButtonSpacing}
+                                                        >
+                                                            <CloudDownloadRounded className={classes.iconSpacing} />
+                                                            <FormattedMessage
+                                                                id='ServiceCatalog.Listing.Overview.download.service'
+                                                                defaultMessage='Download'
+                                                            />
+                                                        </Button>
+                                                        <VerticalDivider height={30} />
+                                                        <Button
+                                                            onClick={showServiceDefinition}
+                                                            color='primary'
+                                                            endIcon={
+                                                                openReadOnlyDefinition
+                                                                    ? (<ExpandLessIcon />) : (<ExpandMoreIcon />)
+                                                            }
+                                                        >
+                                                            <FormattedMessage
+                                                                id='ServiceCatalog.Listing.Overview.view.definition'
+                                                                defaultMessage='View Definition'
+                                                            />
+                                                        </Button>
+                                                    </div>
+                                                    { service.definitionType !== 'GRAPHQL_SDL' && convertTo && (
+                                                        <div>
+                                                            <Button
+                                                                size='small'
+                                                                className={classes.button}
+                                                                onClick={onChangeFormatClick}
+                                                            >
+                                                                <SwapHorizontalCircle className={classes.buttonIcon} />
+                                                                <FormattedMessage
+                                                                    id='ServiceCatalog.Listing.Overview.convert.to'
+                                                                    defaultMessage='Convert to'
+                                                                />
+                                                                {' '}
+                                                                {convertTo}
+                                                            </Button>
+                                                        </div>
+                                                    )}
                                                 </div>
+                                                {openReadOnlyDefinition && (
+                                                    <Suspense fallback={<Progress />}>
+                                                        <MonacoEditor
+                                                            language={format}
+                                                            width={(service.definitionType !== 'GRAPHQL_SDL')
+                                                                ? 'calc(100% + 55px)' : 'calc(100% + 120px)'}
+                                                            height='calc(75vh - 200px)'
+                                                            theme='vs-dark'
+                                                            value={serviceDefinition}
+                                                            options={editorOptions}
+                                                        />
+                                                    </Suspense>
+                                                )}
                                             </TableCell>
                                         </TableRow>
                                         <TableRow>
