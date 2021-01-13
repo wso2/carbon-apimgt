8c4b51c3ae8abc82112e8decf491609930023d6a
yasasrangika <rangikaymendis@gmail.com>
2021-01-13 13:25:01 +0530

    Util to support create hashmap to retreiving resources by service key

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.service.catalog/src/main/java/org/wso2/carbon/apimgt/rest/api/service/catalog/utils/DataMappingUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.service.catalog/src/main/java/org/wso2/carbon/apimgt/rest/api/service/catalog/utils/DataMappingUtil.java
@@ -2,8 +2,12 @@ package org.wso2.carbon.apimgt.rest.api.service.catalog.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.apimgt.api.model.EndPointInfo;
+import org.wso2.carbon.apimgt.api.model.ServiceCatalogEntry;
 import org.wso2.carbon.apimgt.api.model.ServiceCatalogInfo;
+import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.rest.api.service.catalog.dto.ServiceDTO;
 
 import java.io.File;
@@ -11,8 +15,12 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class DataMappingUtil {
+
+    private static final Log log = LogFactory.getLog(Md5HashGenerator.class);
 
     /**
      * Converts a single metadata file content into a model object
@@ -32,16 +40,14 @@ public class DataMappingUtil {
      * Converts a single metadata file content into a model object
      *
      * @param file Metadata file
-     * @param md5 md5 hash value
      * @return Converted ServiceCatalogInfo model object
      * @throws IOException
      */
-    public static ServiceCatalogInfo fromServiceDTOToServiceCatalogInfo(File file, String md5) throws IOException {
+    public static ServiceCatalogInfo fromServiceDTOToServiceCatalogInfo(File file) throws IOException {
 
         ServiceDTO serviceDTO = fromMetadataFileToServiceDTO(file);
 
         ServiceCatalogInfo serviceCatalogInfo = new ServiceCatalogInfo();
-        serviceCatalogInfo.setMd5(md5);
         serviceCatalogInfo.setName(serviceDTO.getName());
         serviceCatalogInfo.setVersion(serviceDTO.getVersion());
         serviceCatalogInfo.setDisplayName(serviceDTO.getDisplayName());
@@ -69,5 +75,45 @@ public class DataMappingUtil {
         endPointInfo.setUuid(uuid);
         endPointInfo.setEndPointDef(inputFile);
         return endPointInfo;
+    }
+
+    public static HashMap<String, ServiceCatalogEntry> fromDirToServiceCatalogEntryMap(String path) {
+
+        // We can use list: then we can go through it and if we need name or something we can just use getters
+
+        HashMap<String, ServiceCatalogEntry> endpointDetails = new HashMap<String, ServiceCatalogEntry>();
+        File[] files = new File(path).listFiles();
+        assert files != null;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                ServiceCatalogEntry serviceCatalogEntry = new ServiceCatalogEntry();
+                EndPointInfo endPointInfo = new EndPointInfo();
+                ServiceCatalogInfo serviceInfo;
+                File[] fList = Objects.requireNonNull(file.listFiles());
+                String key = null;
+                try {
+                    for (File aFile : fList) {
+                        if (aFile.getName().startsWith(APIConstants.METADATA_FILE_NAME)) {
+                            serviceInfo = fromServiceDTOToServiceCatalogInfo(aFile);
+                            serviceCatalogEntry.setServiceCatalogInfo(serviceInfo);
+                            key = keyGenerator(serviceInfo);
+                            endPointInfo.setMetadata(new FileInputStream(aFile));
+                        } else {
+                            endPointInfo.setEndPointDef(new FileInputStream(aFile));
+                        }
+                    }
+                } catch (IOException e) {
+                    log.error("Failed to fetch metadata information from zip due to " + e.getMessage(), e);
+                }
+                serviceCatalogEntry.setEndPointInfo(endPointInfo);
+                endpointDetails.put(key, serviceCatalogEntry);
+            }
+        }
+        return endpointDetails;
+    }
+
+    public static String keyGenerator(ServiceCatalogInfo serviceCatalogInfo){
+        String key =  serviceCatalogInfo.getName() + APIConstants.KEY_SEPARATOR + serviceCatalogInfo.getVersion();
+        return key.toLowerCase();
     }
 }
