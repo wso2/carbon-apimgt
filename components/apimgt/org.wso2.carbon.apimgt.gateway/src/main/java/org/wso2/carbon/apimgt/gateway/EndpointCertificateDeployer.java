dd46e91f698bcf909d5fb203f37cf064f8b3a2d2
Tharindu Dharmarathna <tharindua@wso2.com>
2021-01-18 23:11:22 +0530

    add endpoint certificate retriaval and update

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/EndpointCertificateDeployer.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/EndpointCertificateDeployer.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * you may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.apimgt.gateway;
 
 import com.google.gson.Gson;
@@ -11,25 +28,27 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.util.EntityUtils;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.dto.CertificateMetadataDTO;
+import org.wso2.carbon.apimgt.gateway.internal.ServiceReferenceHolder;
 import org.wso2.carbon.apimgt.impl.APIConstants;
-import org.wso2.carbon.apimgt.impl.certificatemgt.ResponseCode;
+import org.wso2.carbon.apimgt.impl.certificatemgt.CertificateManagerImpl;
 import org.wso2.carbon.apimgt.impl.dto.EventHubConfigurationDto;
 import org.wso2.carbon.apimgt.impl.gatewayartifactsynchronizer.exception.ArtifactSynchronizerException;
-import org.wso2.carbon.apimgt.impl.internal.ServiceReferenceHolder;
 import org.wso2.carbon.apimgt.impl.utils.APIUtil;
-import org.wso2.carbon.apimgt.impl.utils.CertificateMgtUtils;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class used to deploy Certificates into Gateway.
+ */
 public class EndpointCertificateDeployer {
     private static final Log log = LogFactory.getLog(EndpointCertificateDeployer.class);
     private String tenantDomain;
-    private final EventHubConfigurationDto eventHubConfigurationDto = ServiceReferenceHolder.getInstance()
-            .getAPIManagerConfigurationService().getAPIManagerConfiguration().getEventHubConfigurationDto();
+    private final EventHubConfigurationDto eventHubConfigurationDto =
+            ServiceReferenceHolder.getInstance().getAPIManagerConfiguration().getEventHubConfigurationDto();
     private String baseURL = eventHubConfigurationDto.getServiceUrl() + APIConstants.INTERNAL_WEB_APP_EP;
 
     public EndpointCertificateDeployer(String tenantDomain) {
@@ -63,22 +82,29 @@ public class EndpointCertificateDeployer {
 
     private void retrieveCertificatesAndDeploy(CloseableHttpResponse closeableHttpResponse) throws IOException {
 
+        boolean tenantFlowStarted = false;
         if (closeableHttpResponse.getStatusLine().getStatusCode() == 200) {
             String content = EntityUtils.toString(closeableHttpResponse.getEntity());
             List<CertificateMetadataDTO> certificateMetadataDTOList;
             Type listType = new TypeToken<List<CertificateMetadataDTO>>() {
             }.getType();
             certificateMetadataDTOList = new Gson().fromJson(content, listType);
-            for (CertificateMetadataDTO certificateMetadataDTO : certificateMetadataDTOList) {
-                ResponseCode responseCode = CertificateMgtUtils.getInstance()
-                        .addCertificateToSenderTrustStore(certificateMetadataDTO.getCertificate(),
-                                certificateMetadataDTO.getAlias());
-                if (responseCode.getResponseCode() == ResponseCode.SUCCESS.getResponseCode()) {
-                    log.debug("Endpoint Certificate " + certificateMetadataDTO.getAlias() + " Deployed Successfully");
-                }else if (responseCode.getResponseCode() == ResponseCode.ALIAS_EXISTS_IN_TRUST_STORE.getResponseCode()){
-                    log.error("Endpoint Certificate alias " + certificateMetadataDTO.getAlias() + " already exists");
+
+            try {
+                PrivilegedCarbonContext.startTenantFlow();
+                PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(tenantDomain, true);
+                tenantFlowStarted = true;
+                for (CertificateMetadataDTO certificateMetadataDTO : certificateMetadataDTOList) {
+                    CertificateManagerImpl.getInstance()
+                            .addCertificateToGateway(certificateMetadataDTO.getCertificate(),
+                                    certificateMetadataDTO.getAlias());
+                }
+            } finally {
+                if (tenantFlowStarted) {
+                    PrivilegedCarbonContext.endTenantFlow();
                 }
             }
+
         }
     }
 
