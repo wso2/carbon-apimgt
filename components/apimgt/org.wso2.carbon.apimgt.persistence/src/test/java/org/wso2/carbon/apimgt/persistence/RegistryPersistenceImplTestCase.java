45029f87f9e8e7a21b9157cb9ed197ce756969a8
chamilaadhi <cka.adhi@gmail.com>
2021-01-22 13:18:26 +0530

    Fix test

--- components/apimgt/org.wso2.carbon.apimgt.persistence/src/test/java/org/wso2/carbon/apimgt/persistence/RegistryPersistenceImplTestCase.java
+++ components/apimgt/org.wso2.carbon.apimgt.persistence/src/test/java/org/wso2/carbon/apimgt/persistence/RegistryPersistenceImplTestCase.java
@@ -17,6 +17,9 @@ package org.wso2.carbon.apimgt.persistence;
 
 import static org.mockito.Mockito.times;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -26,10 +29,14 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.wso2.carbon.apimgt.persistence.dto.Organization;
+import org.wso2.carbon.apimgt.persistence.dto.UserContext;
 import org.wso2.carbon.apimgt.persistence.internal.ServiceReferenceHolder;
 import org.wso2.carbon.apimgt.persistence.utils.RegistryPersistenceUtil;
 import org.wso2.carbon.context.CarbonContext;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.governance.api.common.dataobjects.GovernanceArtifact;
 import org.wso2.carbon.governance.api.generic.GenericArtifactManager;
+import org.wso2.carbon.governance.api.util.GovernanceUtils;
 import org.wso2.carbon.registry.core.Registry;
 import org.wso2.carbon.registry.core.service.RegistryService;
 import org.wso2.carbon.user.core.UserRealm;
@@ -37,7 +44,8 @@ import org.wso2.carbon.user.core.service.RealmService;
 import org.wso2.carbon.user.core.tenant.TenantManager;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ CarbonContext.class, RegistryPersistenceUtil.class, ServiceReferenceHolder.class })
+@PrepareForTest({ CarbonContext.class, RegistryPersistenceUtil.class, ServiceReferenceHolder.class,
+        PrivilegedCarbonContext.class, GovernanceUtils.class })
 public class RegistryPersistenceImplTestCase {
     private final int SUPER_TENANT_ID = -1234;
     private final String SUPER_TENANT_DOMAIN = "carbon.super";
@@ -50,7 +58,6 @@ public class RegistryPersistenceImplTestCase {
     }
 
     @Test
-    @Ignore
     public void testRegistrySelectionForSuperTenantUser() throws Exception {
 
         RegistryService registryService = Mockito.mock(RegistryService.class);
@@ -60,6 +67,18 @@ public class RegistryPersistenceImplTestCase {
         PowerMockito.mockStatic(CarbonContext.class);
         CarbonContext context = Mockito.mock(CarbonContext.class);
         PowerMockito.when(CarbonContext.getThreadLocalCarbonContext()).thenReturn(context);
+        
+        PowerMockito.mockStatic(PrivilegedCarbonContext.class);
+        PrivilegedCarbonContext privilegedContext = Mockito.mock(PrivilegedCarbonContext.class);
+        PowerMockito.when(PrivilegedCarbonContext.getThreadLocalCarbonContext()).thenReturn(privilegedContext);
+
+        PowerMockito.mockStatic(GovernanceUtils.class);
+        GovernanceArtifact artifact = Mockito.mock(GovernanceArtifact.class);
+        List<GovernanceArtifact> artifacts = new ArrayList<GovernanceArtifact>();
+        artifacts.add(artifact);
+        PowerMockito.when(GovernanceUtils.findGovernanceArtifacts(Mockito.any(String.class),
+                Mockito.any(Registry.class), Mockito.any(String.class), Mockito.any(Boolean.class)))
+                .thenReturn(artifacts);
 
         PowerMockito.mockStatic(RegistryPersistenceUtil.class);
 
@@ -89,18 +108,19 @@ public class RegistryPersistenceImplTestCase {
         Mockito.when(artifactManager.getGenericArtifact(Mockito.any(String.class))).thenReturn(null);
         
         // trigger registry object creation
-        apiPersistenceInstance.getDevPortalAPI(new Organization(SUPER_TENANT_DOMAIN), "xxxxx");
-        Mockito.verify(registryService, times(1)).getGovernanceUserRegistry("admin", SUPER_TENANT_ID);
+        UserContext ctx = new UserContext("user", new Organization(SUPER_TENANT_DOMAIN), null, null);
+        apiPersistenceInstance.searchAPIsForDevPortal(new Organization(SUPER_TENANT_DOMAIN), "", 0, 10, ctx );
+        Mockito.verify(registryService, times(1)).getGovernanceUserRegistry("user", SUPER_TENANT_ID);
 
         apiPersistenceInstance = new RegistryPersistenceImplWrapper(tenantManager,
                 registryService);
         // trigger registry object creation
-        apiPersistenceInstance.getDevPortalAPI(new Organization(SUPER_TENANT_DOMAIN), "xxxxx");
+        ctx = new UserContext("wso2.anonymous.user", new Organization(SUPER_TENANT_DOMAIN), null, null);
+        apiPersistenceInstance.searchAPIsForDevPortal(new Organization(SUPER_TENANT_DOMAIN), "", 0, 10, ctx );
         Mockito.verify(registryService, times(1)).getGovernanceUserRegistry("wso2.anonymous.user", SUPER_TENANT_ID);
 
     }
     @Test
-    @Ignore
     public void testRegistrySelectionForTenantUser() throws Exception {
 
         RegistryService registryService = Mockito.mock(RegistryService.class);
@@ -110,6 +130,18 @@ public class RegistryPersistenceImplTestCase {
         PowerMockito.mockStatic(CarbonContext.class);
         CarbonContext context = Mockito.mock(CarbonContext.class);
         PowerMockito.when(CarbonContext.getThreadLocalCarbonContext()).thenReturn(context);
+        
+        PowerMockito.mockStatic(PrivilegedCarbonContext.class);
+        PrivilegedCarbonContext privilegedContext = Mockito.mock(PrivilegedCarbonContext.class);
+        PowerMockito.when(PrivilegedCarbonContext.getThreadLocalCarbonContext()).thenReturn(privilegedContext);
+
+        PowerMockito.mockStatic(GovernanceUtils.class);
+        GovernanceArtifact artifact = Mockito.mock(GovernanceArtifact.class);
+        List<GovernanceArtifact> artifacts = new ArrayList<GovernanceArtifact>();
+        artifacts.add(artifact);
+        PowerMockito.when(GovernanceUtils.findGovernanceArtifacts(Mockito.any(String.class),
+                Mockito.any(Registry.class), Mockito.any(String.class), Mockito.any(Boolean.class)))
+                .thenReturn(artifacts);
 
         PowerMockito.mockStatic(RegistryPersistenceUtil.class);
 
@@ -138,21 +170,21 @@ public class RegistryPersistenceImplTestCase {
         
         APIPersistence apiPersistenceInstance = new RegistryPersistenceImplWrapper(tenantManager, registryService);
         
+
         // trigger registry object creation
-        apiPersistenceInstance.getDevPortalAPI(new Organization(TENANT_DOMAIN), "xxxxx");
+        UserContext ctx = new UserContext("user", new Organization(TENANT_DOMAIN), null, null);
+        apiPersistenceInstance.searchAPIsForDevPortal(new Organization(TENANT_DOMAIN), "", 0, 10, ctx );
         
-        Mockito.verify(registryService, times(1)).getGovernanceUserRegistry("admin", TENANT_ID);
+        Mockito.verify(registryService, times(1)).getGovernanceUserRegistry("user", TENANT_ID);
         
-        apiPersistenceInstance = new RegistryPersistenceImplWrapper(tenantManager, registryService);
-        // trigger registry object creation
-        apiPersistenceInstance.getDevPortalAPI(new Organization(TENANT_DOMAIN), "xxxxx");
+        ctx = new UserContext("wso2.anonymous.user", new Organization(TENANT_DOMAIN), null, null);
+        apiPersistenceInstance.searchAPIsForDevPortal(new Organization(TENANT_DOMAIN), "", 0, 10, ctx );
         Mockito.verify(registryService, times(1)).getGovernanceUserRegistry("wso2.anonymous.user", TENANT_ID);
         
         
     }
     
     @Test
-    @Ignore
     public void testRegistrySelectionForTenantUserCrossTenatAccess() throws Exception {
 
         RegistryService registryService = Mockito.mock(RegistryService.class);
@@ -163,6 +195,18 @@ public class RegistryPersistenceImplTestCase {
         PowerMockito.mockStatic(CarbonContext.class);
         CarbonContext context = Mockito.mock(CarbonContext.class);
         PowerMockito.when(CarbonContext.getThreadLocalCarbonContext()).thenReturn(context);
+        
+        PowerMockito.mockStatic(PrivilegedCarbonContext.class);
+        PrivilegedCarbonContext privilegedContext = Mockito.mock(PrivilegedCarbonContext.class);
+        PowerMockito.when(PrivilegedCarbonContext.getThreadLocalCarbonContext()).thenReturn(privilegedContext);
+
+        PowerMockito.mockStatic(GovernanceUtils.class);
+        GovernanceArtifact artifact = Mockito.mock(GovernanceArtifact.class);
+        List<GovernanceArtifact> artifacts = new ArrayList<GovernanceArtifact>();
+        artifacts.add(artifact);
+        PowerMockito.when(GovernanceUtils.findGovernanceArtifacts(Mockito.any(String.class),
+                Mockito.any(Registry.class), Mockito.any(String.class), Mockito.any(Boolean.class)))
+                .thenReturn(artifacts);
 
         PowerMockito.mockStatic(RegistryPersistenceUtil.class);
 
@@ -192,7 +236,9 @@ public class RegistryPersistenceImplTestCase {
         Mockito.when(artifactManager.getGenericArtifact(Mockito.any(String.class))).thenReturn(null);
         
         // trigger registry object creation. access super tenant api
-        apiPersistenceInstance.getDevPortalAPI(new Organization(SUPER_TENANT_DOMAIN), "xxxxx");
+        UserContext ctx = new UserContext("user", new Organization(TENANT_DOMAIN), null, null);
+        apiPersistenceInstance.searchAPIsForDevPortal(new Organization(SUPER_TENANT_DOMAIN), "", 0, 10, ctx );
+
         // check whether super tenant's system registy is accessed
         Mockito.verify(registryService, times(1)).getGovernanceSystemRegistry((SUPER_TENANT_ID));
 
