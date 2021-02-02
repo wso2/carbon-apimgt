ab0d811fb468c8cef21eb2450d3ba612acbcacb7
Chamindu36 <cudakara@gmail.com>
2021-02-02 17:48:36 +0530

    Add apiKey and Basicauth check values for velocity context

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/template/APIConfigContext.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/template/APIConfigContext.java
@@ -85,10 +85,23 @@ public class APIConfigContext extends ConfigContext {
             context.put("apiIsBlocked", Boolean.FALSE);
         }
         String apiSecurity = api.getApiSecurity();
+        //if API is secured with ouath2
         if (apiSecurity == null || apiSecurity.contains(APIConstants.DEFAULT_API_SECURITY_OAUTH2)) {
             context.put("apiIsOauthProtected", Boolean.TRUE);
         } else {
             context.put("apiIsOauthProtected", Boolean.FALSE);
+        }
+        //if API is secured with api_Key
+        if (apiSecurity.contains(APIConstants.API_SECURITY_API_KEY)) {
+            context.put("apiIsApiKeyProtected", Boolean.TRUE);
+        } else {
+            context.put("apiIsApiKeyProtected", Boolean.FALSE);
+        }
+        //if API is secured with basic_auth
+        if (apiSecurity.contains(APIConstants.API_SECURITY_BASIC_AUTH)) {
+            context.put("apiIsBasicAuthProtected", Boolean.TRUE);
+        } else {
+            context.put("apiIsBasicAuthProtected", Boolean.FALSE);
         }
         if (api.isEnabledSchemaValidation()) {
             context.put("enableSchemaValidation", Boolean.TRUE);
