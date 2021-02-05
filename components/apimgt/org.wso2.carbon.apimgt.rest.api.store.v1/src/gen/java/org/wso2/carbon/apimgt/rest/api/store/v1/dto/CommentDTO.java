7823194949061a7bcf2889a55db772675bdbef0f
Thisal Tennakoon <thisaltennakoon@gmail.com>
2021-02-05 14:00:04 +0530

    Update CommentDTO.java
    mvn build after changing comment schema

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/gen/java/org/wso2/carbon/apimgt/rest/api/store/v1/dto/CommentDTO.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/gen/java/org/wso2/carbon/apimgt/rest/api/store/v1/dto/CommentDTO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.wso2.carbon.apimgt.rest.api.store.v1.dto.CommentListDTO;
 import org.wso2.carbon.apimgt.rest.api.store.v1.dto.CommenterInfoDTO;
 import javax.validation.constraints.*;
 
@@ -25,7 +26,13 @@ public class CommentDTO   {
     private String content = null;
     private String createdTime = null;
     private String createdBy = null;
+    private String updatedTime = null;
+    private String updatedBy = null;
+    private String category = "general";
+    private String parentCommentId = null;
+    private String entryPoint = null;
     private CommenterInfoDTO commenterInfo = null;
+    private CommentListDTO replies = null;
 
   /**
    **/
@@ -98,6 +105,91 @@ public class CommentDTO   {
 
   /**
    **/
+  public CommentDTO updatedTime(String updatedTime) {
+    this.updatedTime = updatedTime;
+    return this;
+  }
+
+  
+  @ApiModelProperty(value = "")
+  @JsonProperty("updatedTime")
+  public String getUpdatedTime() {
+    return updatedTime;
+  }
+  public void setUpdatedTime(String updatedTime) {
+    this.updatedTime = updatedTime;
+  }
+
+  /**
+   **/
+  public CommentDTO updatedBy(String updatedBy) {
+    this.updatedBy = updatedBy;
+    return this;
+  }
+
+  
+  @ApiModelProperty(example = "admin", value = "")
+  @JsonProperty("updatedBy")
+  public String getUpdatedBy() {
+    return updatedBy;
+  }
+  public void setUpdatedBy(String updatedBy) {
+    this.updatedBy = updatedBy;
+  }
+
+  /**
+   **/
+  public CommentDTO category(String category) {
+    this.category = category;
+    return this;
+  }
+
+  
+  @ApiModelProperty(value = "")
+  @JsonProperty("category")
+  public String getCategory() {
+    return category;
+  }
+  public void setCategory(String category) {
+    this.category = category;
+  }
+
+  /**
+   **/
+  public CommentDTO parentCommentId(String parentCommentId) {
+    this.parentCommentId = parentCommentId;
+    return this;
+  }
+
+  
+  @ApiModelProperty(value = "")
+  @JsonProperty("parentCommentId")
+  public String getParentCommentId() {
+    return parentCommentId;
+  }
+  public void setParentCommentId(String parentCommentId) {
+    this.parentCommentId = parentCommentId;
+  }
+
+  /**
+   **/
+  public CommentDTO entryPoint(String entryPoint) {
+    this.entryPoint = entryPoint;
+    return this;
+  }
+
+  
+  @ApiModelProperty(value = "")
+  @JsonProperty("entryPoint")
+  public String getEntryPoint() {
+    return entryPoint;
+  }
+  public void setEntryPoint(String entryPoint) {
+    this.entryPoint = entryPoint;
+  }
+
+  /**
+   **/
   public CommentDTO commenterInfo(CommenterInfoDTO commenterInfo) {
     this.commenterInfo = commenterInfo;
     return this;
@@ -114,6 +206,24 @@ public class CommentDTO   {
     this.commenterInfo = commenterInfo;
   }
 
+  /**
+   **/
+  public CommentDTO replies(CommentListDTO replies) {
+    this.replies = replies;
+    return this;
+  }
+
+  
+  @ApiModelProperty(value = "")
+      @Valid
+  @JsonProperty("replies")
+  public CommentListDTO getReplies() {
+    return replies;
+  }
+  public void setReplies(CommentListDTO replies) {
+    this.replies = replies;
+  }
+
 
   @Override
   public boolean equals(java.lang.Object o) {
@@ -128,12 +238,18 @@ public class CommentDTO   {
         Objects.equals(content, comment.content) &&
         Objects.equals(createdTime, comment.createdTime) &&
         Objects.equals(createdBy, comment.createdBy) &&
-        Objects.equals(commenterInfo, comment.commenterInfo);
+        Objects.equals(updatedTime, comment.updatedTime) &&
+        Objects.equals(updatedBy, comment.updatedBy) &&
+        Objects.equals(category, comment.category) &&
+        Objects.equals(parentCommentId, comment.parentCommentId) &&
+        Objects.equals(entryPoint, comment.entryPoint) &&
+        Objects.equals(commenterInfo, comment.commenterInfo) &&
+        Objects.equals(replies, comment.replies);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, content, createdTime, createdBy, commenterInfo);
+    return Objects.hash(id, content, createdTime, createdBy, updatedTime, updatedBy, category, parentCommentId, entryPoint, commenterInfo, replies);
   }
 
   @Override
@@ -145,7 +261,13 @@ public class CommentDTO   {
     sb.append("    content: ").append(toIndentedString(content)).append("\n");
     sb.append("    createdTime: ").append(toIndentedString(createdTime)).append("\n");
     sb.append("    createdBy: ").append(toIndentedString(createdBy)).append("\n");
+    sb.append("    updatedTime: ").append(toIndentedString(updatedTime)).append("\n");
+    sb.append("    updatedBy: ").append(toIndentedString(updatedBy)).append("\n");
+    sb.append("    category: ").append(toIndentedString(category)).append("\n");
+    sb.append("    parentCommentId: ").append(toIndentedString(parentCommentId)).append("\n");
+    sb.append("    entryPoint: ").append(toIndentedString(entryPoint)).append("\n");
     sb.append("    commenterInfo: ").append(toIndentedString(commenterInfo)).append("\n");
+    sb.append("    replies: ").append(toIndentedString(replies)).append("\n");
     sb.append("}");
     return sb.toString();
   }
