7b12e5b68d7076943d3fc1691c4deb4fbe3c9077
Chanaka Jayasena <chanaka3d@gmail.com>
2021-02-05 20:30:04 +0530

    Changing the url of the key gen wizard

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Apis/Details/Credentials/Wizard/GenerateKeysStep.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Apis/Details/Credentials/Wizard/GenerateKeysStep.jsx
@@ -280,7 +280,7 @@ const generateKeysStep = (props) => {
                                                 defaultMessage='Token Endpoint'
                                             />
                                         </TableCell>
-                                        <TableCell>{keyManager.additionalProperties['sandbox_token_endpoint'] || keyManager.tokenEndpoint}</TableCell>
+                                        <TableCell>{keyManager.tokenEndpoint || keyManager.tokenEndpoint}</TableCell>
                                     </TableRow>
                                     <TableRow>
                                         <TableCell component='th' scope='row' className={classes.leftCol}>
@@ -289,7 +289,7 @@ const generateKeysStep = (props) => {
                                                 defaultMessage='Revoke Endpoint'
                                             />
                                         </TableCell>
-                                        <TableCell>{keyManager.additionalProperties['sandbox_revoke_endpoint'] || keyManager.revokeEndpoint}</TableCell>
+                                        <TableCell>{keyManager.revokeEndpoint || keyManager.revokeEndpoint}</TableCell>
                                     </TableRow>
                                     <TableRow>
                                         <TableCell component='th' scope='row' className={classes.leftCol}>
