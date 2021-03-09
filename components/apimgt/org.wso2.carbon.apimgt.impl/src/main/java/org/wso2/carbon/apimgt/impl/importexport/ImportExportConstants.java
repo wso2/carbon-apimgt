e3620fcfe630caf9fbb433fea2603d0b38113396
wasuradananjith <wasuradananjith@gmail.com>
2021-03-09 19:22:24 +0530

    Change the APIM version to v4.0.0 for APICTL

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/importexport/ImportExportConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/importexport/ImportExportConstants.java
@@ -178,7 +178,7 @@ public final class ImportExportConstants {
 
     public static final String TYPE_DEPLOYMENT_ENVIRONMENTS = "deployment_environments";
 
-    public static final String APIM_VERSION = "v4";
+    public static final String APIM_VERSION = "v4.0.0";
 
     public static final String ENDPOINT_CONFIG = "endpointConfig";
 
