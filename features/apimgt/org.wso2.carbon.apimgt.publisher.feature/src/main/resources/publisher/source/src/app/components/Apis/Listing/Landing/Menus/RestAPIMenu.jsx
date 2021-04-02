f3d73e4ba2522395afd2e0bc0068f69e8b13e7a5
ruwiniwj <ruwi1025@gmail.com>
2021-04-02 11:54:46 +0530

    Revert "fix hiding sample deploy if already deployed"
    This reverts commit a36a5fe0e09913ff48e7cfd27ced5e2e2445d93a.

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Listing/Landing/Menus/RestAPIMenu.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Listing/Landing/Menus/RestAPIMenu.jsx
@@ -1,5 +1,5 @@
 
-import React, { useMemo } from 'react';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import LandingMenuItem from 'AppComponents/Apis/Listing/Landing/components/LandingMenuItem';
 import LandingMenu from 'AppComponents/Apis/Listing/Landing/components/LandingMenu';
@@ -8,30 +8,12 @@ import SampleAPI from 'AppComponents/Apis/Listing/SampleAPI/SampleAPI';
 import Divider from '@material-ui/core/Divider';
 import Box from '@material-ui/core/Box';
 import Configurations from 'Config';
-import { useApiListContext } from 'AppComponents/Shared/ApiListContext';
-
-/**
- * Verify whether the sample API has already been deployed.
- * @param {(Array | null)} apisAndApiProducts list of APIs
- * @returns {boolean} true if API already deployed/exists
- */
-const verifyIfSampleDeployed = (apisAndApiProducts) => {
-    if (apisAndApiProducts && Array.isArray(apisAndApiProducts)) {
-        return apisAndApiProducts.some((api) => api.name === 'PizzaShackAPI' && api.version === '1.0.0'
-            && api.context === '/pizzashack');
-    } else {
-        return false;
-    }
-};
 
 const RestAPIMenu = (props) => {
     const { icon, isCreateMenu } = props;
     const Component = isCreateMenu ? APICreateMenuSection : LandingMenu;
     const dense = isCreateMenu;
-    const { alwaysShowDeploySampleButton = true } = Configurations.apis || {};
-    const { apisAndApiProducts } = useApiListContext();
-    const isSampleDeployed = useMemo(() => verifyIfSampleDeployed(apisAndApiProducts), [apisAndApiProducts]);
-
+    const { alwaysShowDeploySampleButton } = Configurations.apis;
     return (
         <Component
             title={(
@@ -75,7 +57,7 @@ const RestAPIMenu = (props) => {
                     defaultMessage='Import Open API'
                 />
             </LandingMenuItem>
-            {alwaysShowDeploySampleButton && !isSampleDeployed && (
+            {alwaysShowDeploySampleButton && (
                 <>
                     <Box width={1}>
                         <Divider light variant='middle' />
--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Listing/TableView/TableView.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Listing/TableView/TableView.jsx
@@ -35,7 +35,6 @@ import TopMenu from 'AppComponents/Apis/Listing/components/TopMenu';
 import CustomIcon from 'AppComponents/Shared/CustomIcon';
 import SampleAPIProduct from 'AppComponents/Apis/Listing/SampleAPI/SampleAPIProduct';
 import Alert from 'AppComponents/Shared/Alert';
-import { ApiListContextProvider } from 'AppComponents/Shared/ApiListContext';
 
 const styles = (theme) => ({
     contentInside: {
@@ -444,11 +443,7 @@ class TableView extends React.Component {
         }
         if (apisAndApiProducts.length === 0 && !query) {
             return (
-                <ApiListContextProvider
-                    value={{
-                        apisAndApiProducts,
-                    }}
-                >
+                <>
                     <TopMenu
                         data={apisAndApiProducts}
                         count={totalCount}
@@ -462,16 +457,12 @@ class TableView extends React.Component {
                     ) : (
                         <APILanding />
                     )}
-                </ApiListContextProvider>
+                </>
             );
         }
 
         return (
-            <ApiListContextProvider
-                value={{
-                    apisAndApiProducts,
-                }}
-            >
+            <>
                 <TopMenu
                     data={apisAndApiProducts}
                     count={totalCount}
@@ -486,7 +477,7 @@ class TableView extends React.Component {
                         <MUIDataTable title='' data={apisAndApiProducts} columns={columns} options={options} />
                     </MuiThemeProvider>
                 </div>
-            </ApiListContextProvider>
+            </>
         );
     }
 }
