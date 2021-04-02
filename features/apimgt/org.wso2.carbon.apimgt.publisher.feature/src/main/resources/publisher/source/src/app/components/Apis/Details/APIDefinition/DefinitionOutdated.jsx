6586303ad6140357c8d9779aaa6c175b0e155015
tharikaGitHub <tharika.m92@gmail.com>
2021-04-02 22:08:50 +0530

    Fix Show Diff Loading issue

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/APIDefinition/DefinitionOutdated.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/APIDefinition/DefinitionOutdated.jsx
@@ -28,6 +28,7 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import ReactDiffViewer from 'react-diff-viewer';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import ServiceCatalog from 'AppData/ServiceCatalog';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
 import API from 'AppData/api.js';
 import { FormattedMessage } from 'react-intl';
@@ -52,46 +53,87 @@ export default function DefinitionOutdated(props) {
     const [showDiff, setShowDiff] = useState(false);
     const [newDefinition, setNewDefinition] = useState('');
     const [oldDefinition, setOldDefinition] = useState('');
+    const [isRendering, setIsRendering] = useState(false);
     const { updateAPI } = useContext(APIContext);
+
+    /**
+     * Re import service definition
+     */
     function reimportService() {
         const promisedReimportService = API.reimportService(api.id);
         promisedReimportService.then(() => {
-            Alert.info('Re-import service successfully!');
+            Alert.info(
+                <FormattedMessage
+                    id='Apis.Details.APIDefinition.DefinitionOutdated.import.success'
+                    defaultMessage='Re-imported API definition successfully!'
+                />,
+            );
             setOpenImport(false);
         }).catch((error) => {
             if (error.response) {
                 Alert.error(error.response.body.description);
             } else {
-                const message = 'Error while Re-import service';
-                Alert.error(message);
+                Alert.error(
+                    <FormattedMessage
+                        id='Apis.Details.APIDefinition.DefinitionOutdated.import.error'
+                        defaultMessage='Error while re-importing API definition'
+                    />,
+                );
             }
-            console.error(error);
         }).finally(() => {
             updateAPI();
         });
     }
 
+    /**
+     * Show diff between old service definition and new service definition
+     */
     function showdiff() {
-        setShowDiff(true);
+        setIsRendering(true);
         const promisedServices = ServiceCatalog.searchServiceByKey(api.serviceInfo.key);
         promisedServices.then((data) => {
             return ServiceCatalog.getServiceDefinition(data.body.list[0].id).then((file) => {
-                setNewDefinition(JSON.stringify(file, null, 2));
+                api.getSwagger(api.id).then((resp) => {
+                    setOldDefinition(JSON.stringify(resp.obj, null, 2));
+                    setNewDefinition(JSON.stringify(file, null, 2));
+                }).catch((error) => {
+                    if (error.response) {
+                        Alert.error(error.response.body.description);
+                    } else {
+                        Alert.error(
+                            <FormattedMessage
+                                id='Apis.Details.APIDefinition.DefinitionOutdated.outdated.definition.error'
+                                defaultMessage='Something went wrong while getting the outdated API definition.'
+                            />,
+                        );
+                    }
+                }).finally(() => {
+                    setShowDiff(true);
+                    setIsRendering(false);
+                });
             }).catch((error) => {
                 if (error.response) {
                     Alert.error(error.response.body.description);
                 } else {
-                    Alert.error('Something went wrong while getting the Service Definition.');
+                    Alert.error(
+                        <FormattedMessage
+                            id='Apis.Details.APIDefinition.DefinitionOutdated.new.api.definition.error'
+                            defaultMessage='Something went wrong while getting the new API definition.'
+                        />,
+                    );
                 }
             });
         }).catch((error) => {
-            console.error(error);
-            Alert.error('ServiceCatalog.Listing.Listing.error.loading');
-        });
-        api.getSwagger(api.id).then((resp) => {
-            setOldDefinition(JSON.stringify(resp.obj, null, 2));
-        }).catch((err) => {
-            console.err(err);
+            if (error.response) {
+                Alert.error(error.response.body.description);
+            } else {
+                Alert.error(
+                    <FormattedMessage
+                        id='Apis.Details.APIDefinition.DefinitionOutdated.service.retrieve.error'
+                        defaultMessage='Something went wrong while retrieving service information'
+                    />,
+                );
+            }
         });
     }
 
@@ -117,7 +159,7 @@ export default function DefinitionOutdated(props) {
                 >
                     <WarningIcon className={classes.buttonWarningColor} />
                     <FormattedMessage
-                        id='Apis.Details.APIDefinition.APIDefinition.outdated.definition'
+                        id='Apis.Details.APIDefinition.DefinitionOutdated.outdated.definition.heading'
                         defaultMessage='Outdated Definition'
                     />
                 </Button>
@@ -132,7 +174,7 @@ export default function DefinitionOutdated(props) {
                     <DialogTitle id='alert-dialog-title'>
                         <Typography align='left'>
                             <FormattedMessage
-                                id='Apis.Details.APIDefinition.APIDefinition.outdated.api.definition'
+                                id='Apis.Details.APIDefinition.DefinitionOutdated.outdated.definition'
                                 defaultMessage='Outdated Definition'
                             />
                         </Typography>
@@ -140,13 +182,12 @@ export default function DefinitionOutdated(props) {
                     <DialogContent>
                         <DialogContentText id='alert-dialog-description'>
                             <FormattedMessage
-                                id='Apis.Details.APIDefinition.APIDefinition.api.outdated.definition'
-                                defaultMessage='Current API definition outdated,
-                                You can either re-import existing
-                                resource or create new version'
+                                id='Apis.Details.APIDefinition.DefinitionOutdated.api.outdated.definition'
+                                defaultMessage='Current API definition is outdated.
+                                You can either re-import the new definition or create a new version of this API.'
                             />
                         </DialogContentText>
-                        {showDiff && (
+                        {showDiff && !isRendering && (
                             <ReactDiffViewer
                                 oldValue={oldDefinition}
                                 newValue={newDefinition}
@@ -159,23 +200,27 @@ export default function DefinitionOutdated(props) {
                     <DialogActions>
                         <Button onClick={handleClose} color='primary'>
                             <FormattedMessage
-                                id='Apis.Details.APIDefinition.APIDefinition.btn.cancel'
+                                id='Apis.Details.APIDefinition.DefinitionOutdated.btn.cancel'
                                 defaultMessage='Cancel'
                             />
                         </Button>
                         {!showDiff ? (
                             <Button onClick={showdiff} color='primary'>
-                                <FormattedMessage
-                                    id='Apis.Details.APIDefinition.APIDefinition.btn.show.diff'
-                                    defaultMessage='Show Diff'
-                                />
+                                {isRendering ? (<CircularProgress size={24} />) : (
+                                    <FormattedMessage
+                                        id='Apis.Details.APIDefinition.DefinitionOutdated.btn.show.diff'
+                                        defaultMessage='Show Diff'
+                                    />
+                                )}
                             </Button>
                         ) : (
                             <Button onClick={hideDiff} color='primary'>
-                                <FormattedMessage
-                                    id='Apis.Details.APIDefinition.APIDefinition.btn.hide.diff'
-                                    defaultMessage='Hide Diff'
-                                />
+                                {isRendering ? (<CircularProgress size={24} />) : (
+                                    <FormattedMessage
+                                        id='Apis.Details.APIDefinition.DefinitionOutdated.btn.hide.diff'
+                                        defaultMessage='Hide Diff'
+                                    />
+                                )}
                             </Button>
                         )}
                         <Button
@@ -185,7 +230,7 @@ export default function DefinitionOutdated(props) {
                             variant='contained'
                         >
                             <FormattedMessage
-                                id='Apis.Details.APIDefinition.APIDefinition.btn.reimport'
+                                id='Apis.Details.APIDefinition.DefinitionOutdated.btn.reimport'
                                 defaultMessage='Re-import'
                             />
                         </Button>
