08d00014ee3902b749155b3783d5eec398283cf6
dushansilva <dushansilva95@gmail.com>
2021-04-05 17:10:50 +0530

    fix suggestion
    Co-authored-by: Malintha Amarasinghe <malintha.prasan@gmail.com>

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Apis/Listing/APICardView.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Apis/Listing/APICardView.jsx
@@ -211,7 +211,7 @@ class APICardView extends React.Component {
                 name: 'isSubscriptionAvailable',
                 label: intl.formatMessage({
                     id: 'Apis.Listing.APIList.isSubscriptionAvailable',
-                    defaultMessage: 'IS Subscription Available',
+                    defaultMessage: 'Is Subscription Available',
                 }),
                 options: {
                     display: 'excluded',
