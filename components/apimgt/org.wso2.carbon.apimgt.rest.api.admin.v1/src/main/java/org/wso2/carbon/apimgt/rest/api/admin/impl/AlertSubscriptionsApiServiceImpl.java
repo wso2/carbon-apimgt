7b97bed3d70fe837213bcafbf24b97f8693dd89f
Malintha Amarasinghe <malintha.prasan@gmail.com>
2021-04-17 12:35:17 +0530

    removed v1

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/AlertSubscriptionsApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/AlertSubscriptionsApiServiceImpl.java
@@ -15,7 +15,7 @@
  *
  */
 
-package org.wso2.carbon.apimgt.rest.api.admin.v1.impl;
+package org.wso2.carbon.apimgt.rest.api.admin.impl;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
