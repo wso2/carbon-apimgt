f7e2b852f9c2411cd1e894dd16a985c3d22c1b33
Malintha Amarasinghe <malintha.prasan@gmail.com>
2021-04-17 12:39:31 +0530

    moved and remove v1

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/ApiCategoriesApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/impl/ApiCategoriesApiServiceImpl.java
@@ -14,7 +14,7 @@
  *  limitations under the License.
  *
  */
-package org.wso2.carbon.apimgt.rest.api.admin.v1.impl;
+package org.wso2.carbon.apimgt.rest.api.admin.impl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
