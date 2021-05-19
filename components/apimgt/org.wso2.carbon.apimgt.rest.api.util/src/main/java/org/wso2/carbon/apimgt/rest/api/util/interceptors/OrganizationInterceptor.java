6a502b6a2930fcbf62a7070678d920d60ac0c546
Nirothipan <nirothipan@gmail.com>
2021-05-19 21:27:58 +0530

    From https://github.com/wso2/carbon-apimgt/pull/10505

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/interceptors/OrganizationInterceptor.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/interceptors/OrganizationInterceptor.java
@@ -16,9 +16,6 @@
 
 package org.wso2.carbon.apimgt.rest.api.util.interceptors;
 
-import java.util.ArrayList;
-import java.util.TreeMap;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -26,16 +23,18 @@ import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
+import org.wso2.carbon.apimgt.impl.APIConstants;
+import org.wso2.carbon.apimgt.impl.APIManagerConfiguration;
+import org.wso2.carbon.apimgt.impl.internal.ServiceReferenceHolder;
 import org.wso2.carbon.apimgt.impl.utils.APIUtil;
-import org.wso2.carbon.apimgt.rest.api.common.RestApiCommonUtil;
 import org.wso2.carbon.apimgt.rest.api.common.RestApiConstants;
-import org.wso2.carbon.apimgt.rest.api.util.exception.BadRequestException;
+import org.wso2.carbon.apimgt.rest.api.util.resolver.OrganizationResolver;
 import org.wso2.carbon.apimgt.rest.api.util.utils.RestApiUtil;
-import org.wso2.carbon.user.api.UserStoreException;
 
 public class OrganizationInterceptor extends AbstractPhaseInterceptor {
 
     private static final Log logger = LogFactory.getLog(OrganizationInterceptor.class);
+    private final String DEFAULT_RESOLVER = "org.wso2.carbon.apimgt.rest.api.util.resolver.OnPremResolver";
 
     public OrganizationInterceptor() {
         // We will use PRE_INVOKE phase as we need to process message before hit actual
@@ -45,44 +44,21 @@ public class OrganizationInterceptor extends AbstractPhaseInterceptor {
 
     @Override
     public void handleMessage(Message message) throws Fault {
-        String queryString = (String) message.get(Message.QUERY_STRING);
-        String organizationId = null;
-        if (queryString != null) {
-            String[] queries = queryString.split("&");
-            if (queries != null) {
-                for (int i = 0; i < queries.length; i++) {
-                    if (queries[i].startsWith("organizationId")) {
-                        organizationId = queries[i].split("=")[1];
-                        message.put(RestApiConstants.ORGANIZATION, organizationId);
-                        break;
-                    }
-                }
-            }
-        }
-        if (organizationId == null) {
-            ArrayList requestedTenantDomain = (ArrayList) ((TreeMap) (message.get(Message.PROTOCOL_HEADERS)))
-                    .get(RestApiConstants.HEADER_X_WSO2_TENANT);
-            String tenantDomain = null;
-            if (requestedTenantDomain != null) {
-                tenantDomain = RestApiUtil.getRequestedTenantDomain(requestedTenantDomain.get(0).toString());
-                try {
-                    if (!APIUtil.isTenantAvailable(tenantDomain)) {
-                        RestApiUtil.handleBadRequest("Provided tenant domain '" + tenantDomain + "' is invalid",
-                                901300L, logger);
 
-                    }
-                } catch (UserStoreException | BadRequestException e) {
-                    String errorMessage = "Error while checking availability of tenant " + tenantDomain;
-                    RestApiUtil.handleInternalServerError(errorMessage, e, logger);
-                }
-            }
-            if (StringUtils.isEmpty(tenantDomain)) {
-                tenantDomain = RestApiCommonUtil.getLoggedInUserTenantDomain();
-            }
-
-            message.put(RestApiConstants.ORGANIZATION, tenantDomain);
+        APIManagerConfiguration config = ServiceReferenceHolder.getInstance().getAPIManagerConfigurationService()
+                .getAPIManagerConfiguration();
+        String className = config.getFirstProperty(APIConstants.ORG_RESOLVER);
+        if (StringUtils.isEmpty(className)) {
+            className = DEFAULT_RESOLVER;
         }
 
+        try {
+            OrganizationResolver resolver = (OrganizationResolver) Class.forName(className).newInstance();
+            String organization = resolver.resolve(message);
+            message.put(RestApiConstants.ORGANIZATION, organization);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            RestApiUtil.handleInternalServerError("Error while resolving the organization resolver", e, logger);
+        }
         logger.debug("Organization :" + message.get(RestApiConstants.ORGANIZATION));
     }
 }
---
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.util/src/main/java/org/wso2/carbon/apimgt/rest/api/util/resolver/OrganizationResolver.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.apimgt.rest.api.util.resolver;
+
+import org.apache.cxf.message.Message;
+
+public interface OrganizationResolver {
+
+    /**
+     * Resolve the organization from the cxf message
+     * @param message
+     * @return String organization
+     */
+    public String resolve(Message message);
+
+}
