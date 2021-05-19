1def9ab4658a65bf7073927928ff5960d29d28a0
Prasanna Dangalla <prasannadangalla@gmail.com>
2021-05-20 01:34:26 +0530

    Corrected speeling mistake.

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/CertificateMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/CertificateMgtDAO.java
@@ -58,7 +58,7 @@ public class CertificateMgtDAO {
     private static CertificateMgtDAO certificateMgtDAO = new CertificateMgtDAO();
 
     /**
-     * Private constructorq
+     * Private constructor
      */
     private CertificateMgtDAO() {
 
