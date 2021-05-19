534d4be8cc5defc36448ff73d0a986d9022e92e1
tharikaGitHub <tharika.m92@gmail.com>
2021-05-19 07:07:00 +0530

    Fix revision access in devportal

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/AbstractAPIManager.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/AbstractAPIManager.java
@@ -3863,24 +3863,21 @@ public abstract class AbstractAPIManager implements APIManager {
         // workflow status
         APIIdentifier apiId = api.getId();
         WorkflowDTO workflow;
-        if (api.isRevision()) {
-            workflow = APIUtil.getAPIWorkflowStatus(api.getRevisionedApiId(), WF_TYPE_AM_API_STATE);
+        String currentApiUuid;
+        APIRevision apiRevision = ApiMgtDAO.getInstance().checkAPIUUIDIsARevisionUUID(uuid);
+        if (apiRevision != null && apiRevision.getApiUUID() != null) {
+            currentApiUuid = apiRevision.getApiUUID();
         } else {
-            workflow = APIUtil.getAPIWorkflowStatus(api.getUuid(), WF_TYPE_AM_API_STATE);
+            currentApiUuid = uuid;
         }
+        workflow = APIUtil.getAPIWorkflowStatus(currentApiUuid, WF_TYPE_AM_API_STATE);
         if (workflow != null) {
             WorkflowStatus status = workflow.getStatus();
             api.setWorkflowStatus(status.toString());
         }
         // TODO try to use a single query to get info from db
         // Ratings
-        int internalId;
-        if (api.isRevision()) {
-            //Get the original API ID if revisioned
-            internalId = apiMgtDAO.getAPIID(api.getRevisionedApiId());
-        } else {
-            internalId = apiMgtDAO.getAPIID(api.getUuid());
-        }
+        int internalId = apiMgtDAO.getAPIID(currentApiUuid);
         api.setRating(APIUtil.getAverageRating(internalId));
         apiId.setId(internalId);
         apiMgtDAO.setServiceStatusInfoToAPI(api, internalId);
@@ -3903,12 +3900,7 @@ public abstract class AbstractAPIManager implements APIManager {
         api.setAvailableTiers(availableTier);
 
         //Scopes
-        Map<String, Scope> scopeToKeyMapping;
-        if (api.isRevision()) {
-            scopeToKeyMapping = APIUtil.getAPIScopes(api.getRevisionedApiId(), requestedTenantDomain);
-        } else {
-            scopeToKeyMapping = APIUtil.getAPIScopes(api.getUuid(), requestedTenantDomain);
-        }
+        Map<String, Scope> scopeToKeyMapping = APIUtil.getAPIScopes(currentApiUuid, requestedTenantDomain);
         api.setScopes(new LinkedHashSet<>(scopeToKeyMapping.values()));
 
         //templates
