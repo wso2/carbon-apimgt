7aaffa46c5cea590024b24963d88bc16e5820df2
Vithursa <vithu30@hotmail.com>
2021-07-21 13:20:47 +0530

    Send httpMethod and path params to Lambda fucntion

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/AWSLambdaMediator.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/AWSLambdaMediator.java
@@ -64,6 +64,8 @@ public class AWSLambdaMediator extends AbstractMediator {
     private static final String QUERY_STRING_PARAMETERS = "queryStringParameters";
     private static final String HEADER_PARAMETER = "headers";
     private static final String BODY_PARAMETER = "body";
+    private static final String PATH = "path";
+    private static final String HTTP_METHOD = "httpMethod";
 
     public AWSLambdaMediator() {
 
@@ -119,6 +121,8 @@ public class AWSLambdaMediator extends AbstractMediator {
             payload.add(PATH_PARAMETERS, pathParameters);
             payload.add(QUERY_STRING_PARAMETERS, queryStringParameters);
             payload.add(BODY_PARAMETER, new JsonParser().parse(body).getAsJsonObject());
+            payload.addProperty(HTTP_METHOD, (String) messageContext.getProperty(APIConstants.REST_METHOD));
+            payload.addProperty(PATH, (String) messageContext.getProperty(APIConstants.API_ELECTED_RESOURCE));
             if (log.isDebugEnabled()) {
                 log.debug("Passing the payload " + payload.toString() + " to AWS Lambda function with resource name "
                         + resourceName);
