15303ad78c5bd7dc6305878bdb6bc36461827194
amali <maamalilakshika@gmail.com>
2021-08-13 13:29:38 +0530

    Fix DB timezone issue

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -15983,6 +15983,7 @@ public class ApiMgtDAO {
                 statement.setString(3, apiRevision.getRevisionUUID());
                 statement.setString(4, apiRevision.getDescription());
                 statement.setString(5, apiRevision.getCreatedBy());
+                statement.setTimestamp(6, new Timestamp(System.currentTimeMillis()));
                 statement.executeUpdate();
 
                 // Retrieve API ID
@@ -16363,6 +16364,7 @@ public class ApiMgtDAO {
                     statement.setString(2, VHostUtils.resolveIfDefaultVhostToNull(envName, vhost));
                     statement.setString(3, apiRevisionId);
                     statement.setBoolean(4, apiRevisionDeployment.isDisplayOnDevportal());
+                    statement.setTimestamp(5, new Timestamp(System.currentTimeMillis()));
                     statement.addBatch();
                 }
                 statement.executeBatch();
@@ -16399,6 +16401,7 @@ public class ApiMgtDAO {
                         statement.setString(1, deployedAPIRevision.getDeployment());
                         statement.setString(2, VHostUtils.resolveIfDefaultVhostToNull(envName, vhost));
                         statement.setString(3, apiRevisionId);
+                        statement.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
                         statement.addBatch();
                     }
                     statement.executeBatch();
@@ -17032,6 +17035,7 @@ public class ApiMgtDAO {
                 statement.setString(3, apiRevision.getRevisionUUID());
                 statement.setString(4, apiRevision.getDescription());
                 statement.setString(5, apiRevision.getCreatedBy());
+                statement.setTimestamp(6, new Timestamp(System.currentTimeMillis()));
                 statement.executeUpdate();
 
                 // Retrieve API Product ID
