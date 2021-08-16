9b05c633caceeef40a346fa45f22ea87e461930d
rashm1n <rashmin.ravindu@gmail.com>
2021-08-16 10:51:01 +0530

    Refactoring ApplicationPurge Class

--- components/apimgt/org.wso2.carbon.apimgt.cleanup.service/src/main/java/org/wso2/carbon/apimgt/cleanup/service/ApplicationPurge.java
+++ components/apimgt/org.wso2.carbon.apimgt.cleanup.service/src/main/java/org/wso2/carbon/apimgt/cleanup/service/ApplicationPurge.java
@@ -23,7 +23,6 @@ import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.model.Subscriber;
 import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.impl.dao.ApiMgtDAO;
-import org.wso2.carbon.apimgt.api.model.Application;
 
 import javax.cache.Cache;
 import javax.cache.Caching;
@@ -32,100 +31,40 @@ import java.util.*;
 public class ApplicationPurge implements OrganizationPurge{
     protected ApiMgtDAO apiMgtDAO;
     private static final Log log = LogFactory.getLog(ApplicationPurge.class);
-    private Map<String, String> taskList;
-
-    private final int MAX_TRIES = 5;
 
     public ApplicationPurge() {
         apiMgtDAO = ApiMgtDAO.getInstance();
-        init();
     }
 
     public ApplicationPurge(ApiMgtDAO apiMgtDAO) {
         this.apiMgtDAO = apiMgtDAO;
-        init();
     }
-
-    private void init() {
-        taskList = new LinkedHashMap<>();
-
-        taskList.put(APIConstants.ApplicationPurgeConstants.PENDING_SUBSCRIPTION_REMOVAL,
-                APIConstants.ApplicationPurgeConstants.STATUS_PENDING);
-        taskList.put(APIConstants.ApplicationPurgeConstants.APPLICATION_CREATION_WF_REMOVAL,
-                APIConstants.ApplicationPurgeConstants.STATUS_PENDING);
-        taskList.put(APIConstants.ApplicationPurgeConstants.APPLICATION_REGISTRATION_REMOVAL,
-                APIConstants.ApplicationPurgeConstants.STATUS_PENDING);
-        taskList.put(APIConstants.ApplicationPurgeConstants.APPLICATION_REMOVAL,
-                APIConstants.ApplicationPurgeConstants.STATUS_PENDING);
-        taskList.put(APIConstants.ApplicationPurgeConstants.SUBSCRIPTION_REMOVAL,
-                APIConstants.ApplicationPurgeConstants.STATUS_PENDING);
-    }
-
     /**
      * Delete organization related application data
      *
      * @param organization organization
      * @throws APIManagementException if failed to clean up organization
      */
-    @Override
-    public void deleteOrganization(String organization) throws APIManagementException {
-        int[] applicationIdList = null;
-        for (String task : taskList.keySet()) {
+    @Override public void deleteOrganization(String organization) throws APIManagementException {
+        try {
 
-            int count = 0;
-            int maxTries = MAX_TRIES;
+            removePendingSubscriptions(organization);
 
-            while (true) {
-                try {
-                    switch (task) {
-                    case APIConstants.ApplicationPurgeConstants.PENDING_SUBSCRIPTION_REMOVAL:
-                        if (taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_PENDING)
-                                || taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_FAILED)) {
-                            removePendingSubscriptions(organization);
-                            taskList.put(task, APIConstants.ApplicationPurgeConstants.STATUS_SUCCESSFUL);
-                        }
-                        break;
-                    case APIConstants.ApplicationPurgeConstants.APPLICATION_CREATION_WF_REMOVAL:
-                        if (taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_PENDING)
-                                || taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_FAILED)) {
-                            removeApplicationCreationWorkflows(organization);
-                            taskList.put(task, APIConstants.ApplicationPurgeConstants.STATUS_SUCCESSFUL);
-                        }
-                        break;
-                    case APIConstants.ApplicationPurgeConstants.APPLICATION_REGISTRATION_REMOVAL:
-                        if (taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_PENDING)
-                                || taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_FAILED)) {
-                            deletePendingApplicationRegistrations(organization);
-                            taskList.put(task, APIConstants.ApplicationPurgeConstants.STATUS_SUCCESSFUL);
-                        }
-                        break;
-                    case APIConstants.ApplicationPurgeConstants.APPLICATION_REMOVAL:
-                        if (taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_PENDING)
-                                || taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_FAILED)) {
-                            deleteApplicationList(organization);
-                            taskList.put(task, APIConstants.ApplicationPurgeConstants.STATUS_SUCCESSFUL);
-                        }
-                        break;
-                    case APIConstants.ApplicationPurgeConstants.SUBSCRIPTION_REMOVAL:
-                        if (taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_PENDING)
-                                || taskList.get(task).equals(APIConstants.ApplicationPurgeConstants.STATUS_FAILED)) {
-                            deleteSubscribers(organization);
-                            taskList.put(task, APIConstants.ApplicationPurgeConstants.STATUS_SUCCESSFUL);
-                        }
-                        break;
-                    }
-                    break;
-                } catch (APIManagementException e) {
-                    log.error("Error in " + task + " in application deletion sub component", e);
-                    String msg = "Failed to execute application deletion of organization: " + organization;
-                    taskList.put(task, APIConstants.ApplicationPurgeConstants.STATUS_FAILED);
-                    if (++count == maxTries) {
-                        throw new APIManagementException(msg, e);
-                    }
-                }
-            }
+            removeApplicationCreationWorkflows(organization);
+
+            deletePendingApplicationRegistrations(organization);
+
+            deleteApplicationList(organization);
+
+            deleteSubscribers(organization);
+
+        } catch (APIManagementException e) {
+            String message = "Error while deleting application data related to organization: "+organization;
+            log.error(message, e);
+            throw new APIManagementException(message, e);
         }
     }
+
 
     private void removeApplicationCreationWorkflows(String organization) throws APIManagementException {
         apiMgtDAO.removeApplicationCreationWorkflows(organization);
