e25f921a98e22a363847f3edbd6ddab2fdd94a4d
krishan <krishan.wijesena@gmail.com>
2021-09-29 15:43:10 +0530

    Fix the authorization header per tenant issue

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/ApisApiServiceImpl.java
@@ -992,6 +992,16 @@ public class ApisApiServiceImpl implements ApisApiService {
 
             // Extracting clicked API name by the user, for the recommendation system
             String userName = RestApiCommonUtil.getLoggedInUsername();
+            if (api.isAPIProduct() && StringUtils.isBlank(api.getApiProduct().getAuthorizationHeader())) {
+                String authHeader = APIUtil.getOAuthConfiguration(organization,
+                        APIConstants.AUTHORIZATION_HEADER);
+                api.getApiProduct().setAuthorizationHeader(authHeader);
+            }
+            if (!api.isAPIProduct() && StringUtils.isBlank(api.getApi().getAuthorizationHeader())) {
+                String authHeader = APIUtil.getOAuthConfiguration(organization,
+                        APIConstants.AUTHORIZATION_HEADER);
+                api.getApi().setAuthorizationHeader(authHeader);
+            }
             apiConsumer.publishClickedAPI(api, userName);
 
             if (APIConstants.PUBLISHED.equals(status) || APIConstants.PROTOTYPED.equals(status)
