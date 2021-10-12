30c5b8238428a05e9319f8c85c25eab4e0c4e21d
fazlan nazeem <fazlann@wso2.com>
2021-10-12 18:55:36 +0530

    Remove unwanted file change

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
@@ -1027,8 +1027,6 @@ public final class APIConstants {
 
     public static final String ACCOUNT_LOCKED_CLAIM = "http://wso2.org/claims/identity/accountLocked";
 
-
-
     //TODO: move this to a common place (& Enum) to be accessible by all components
     public static class KeyValidationStatus {
 
