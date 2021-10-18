5f54aa93af510d4d2fb06d00005ef6be6e214f5a
HeshanSudarshana <khsudarshana@gmail.com>
2021-10-18 09:26:15 +0530

    Add type ASYNC to be identified as an Async API

--- components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/model/API.java
+++ components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/model/API.java
@@ -1264,6 +1264,6 @@ public class API implements Serializable {
     }
 
     public boolean isAsync() {
-        return "WS".equals(type) || "WEBSUB".equals(type) || "SSE".equals(type);
+        return "WS".equals(type) || "WEBSUB".equals(type) || "SSE".equals(type) || "ASYNC".equals(type);
     }
 }
