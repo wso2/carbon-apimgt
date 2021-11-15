d3d05b3188641b6bf1ba10bf127287106607864d
dushani <dushaniw@wso2.com>
2021-11-15 15:03:20 +0530

    fix and add new test cases for ws and graphql api subscribe event path.

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/test/java/org/wso2/carbon/apimgt/gateway/handlers/WebsocketHandlerTestCase.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/test/java/org/wso2/carbon/apimgt/gateway/handlers/WebsocketHandlerTestCase.java
@@ -19,8 +19,12 @@
 package org.wso2.carbon.apimgt.gateway.handlers;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelId;
 import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import org.junit.Assert;
 import org.junit.Before;
@@ -30,15 +34,35 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.gateway.dto.GraphQLOperationDTO;
+import org.wso2.carbon.apimgt.gateway.handlers.graphQL.GraphQLConstants;
+import org.wso2.carbon.apimgt.gateway.inbound.InboundMessageContext;
+import org.wso2.carbon.apimgt.gateway.inbound.InboundMessageContextDataHolder;
+import org.wso2.carbon.apimgt.gateway.inbound.websocket.InboundProcessorResponseDTO;
+import org.wso2.carbon.apimgt.gateway.inbound.websocket.utils.InboundWebsocketProcessorUtil;
+import org.wso2.carbon.apimgt.impl.APIConstants;
+import org.wso2.carbon.apimgt.impl.dto.VerbInfoDTO;
+import org.wso2.carbon.apimgt.keymgt.model.entity.API;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
+
+import java.util.UUID;
 
 /**
  * Test class for WebsocketHandler
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({PrivilegedCarbonContext.class, DataPublisherUtil.class})
+@PrepareForTest({PrivilegedCarbonContext.class, DataPublisherUtil.class, InboundWebsocketProcessorUtil.class})
 public class WebsocketHandlerTestCase {
+
+    private static final String channelIdString = "11111";
+    private ChannelHandlerContext channelHandlerContext;
+    private Channel channel;
+    private ChannelId channelId;
+    private ChannelPromise channelPromise;
+    private WebsocketHandler websocketHandler;
+    private WebSocketFrame msg;
+    private org.wso2.carbon.apimgt.keymgt.model.entity.API websocketAPI;
+    private org.wso2.carbon.apimgt.keymgt.model.entity.API graphQLAPI;
 
     @Before
     public void setup() {
@@ -46,50 +70,132 @@ public class WebsocketHandlerTestCase {
         PowerMockito.mockStatic(PrivilegedCarbonContext.class);
         PrivilegedCarbonContext privilegedCarbonContext = Mockito.mock(PrivilegedCarbonContext.class);
         PowerMockito.when(PrivilegedCarbonContext.getThreadLocalCarbonContext()).thenReturn(privilegedCarbonContext);
-
-    }
-
-    /*
-    * This method tests write() when msg is not a WebSocketFrame
-    * */
-    @Test
-    public void testWrite() throws Exception {
-        ChannelHandlerContext channelHandlerContext = Mockito.mock(ChannelHandlerContext.class);
-        ChannelPromise channelPromise = Mockito.mock(ChannelPromise.class);
-        Object msg = "msg";
-        PowerMockito.mockStatic(DataPublisherUtil.class);
-        WebsocketHandler websocketHandler = new WebsocketHandler();
-        websocketHandler.write(channelHandlerContext, msg, channelPromise);
-        Assert.assertTrue(true);  // No error has occurred. hence test passes.
-
-    }
-
-    /*
-   * This method tests write() when msg is a WebSocketFrame
-   * */
-    @Test
-    public void testWrite1() throws Exception {
+        PowerMockito.mockStatic(InboundWebsocketProcessorUtil.class);
         PowerMockito.mockStatic(DataPublisherUtil.class);
         PowerMockito.mockStatic(PrivilegedCarbonContext.class);
-        ChannelHandlerContext channelHandlerContext = Mockito.mock(ChannelHandlerContext.class);
-        ChannelPromise channelPromise = Mockito.mock(ChannelPromise.class);
-        WebSocketFrame msg = Mockito.mock(WebSocketFrame.class);
+        channelHandlerContext = Mockito.mock(ChannelHandlerContext.class);
+        channel = Mockito.mock(Channel.class);
+        channelId = Mockito.mock(ChannelId.class);
+        channelPromise = Mockito.mock(ChannelPromise.class);
+        msg = Mockito.mock(WebSocketFrame.class);
         ByteBuf content = Mockito.mock(ByteBuf.class);
         Mockito.when(msg.content()).thenReturn(content);
-        WebsocketHandler websocketHandler = new WebsocketHandler() {
-            @Override
-            protected boolean isAllowed(ChannelHandlerContext ctx, WebSocketFrame msg) throws APIManagementException {
-                return true;
-            }
+        websocketHandler = new WebsocketHandler();
+        Mockito.when(channelHandlerContext.channel()).thenReturn(channel);
+        Mockito.when(channel.id()).thenReturn(channelId);
+        Mockito.when(channelId.asLongText()).thenReturn(channelIdString);
+        websocketAPI = new API(UUID.randomUUID().toString(), 1, "admin", "WSAPI", "1.0.0", "/wscontext", "Unlimited",
+                APIConstants.API_TYPE_WS, APIConstants.PUBLISHED_STATUS, false);
+        graphQLAPI = new API(UUID.randomUUID().toString(), 2, "admin", "GraphQLAPI", "1.0.0", "graphql", "Unlimited",
+                APIConstants.GRAPHQL_API, APIConstants.PUBLISHED_STATUS, false);
+    }
 
-            @Override
-            protected String getClientIp(ChannelHandlerContext ctx) {
-                return "192.168.0.100";
-            }
-        };
-        WebsocketInboundHandler websocketInboundHandler = Mockito.mock(WebsocketInboundHandler.class);
-        Mockito.when(websocketInboundHandler.doThrottle(channelHandlerContext, msg)).thenReturn(true);
+    /*
+     * This method tests write() when msg is not a WebSocketFrame
+     * */
+    @Test
+    public void testWSWrite() throws Exception {
+
+        Object msg = "msg";
         websocketHandler.write(channelHandlerContext, msg, channelPromise);
-        Assert.assertTrue(true);  // No error has occurred. hence test passes.
+        Assert.assertTrue((InboundMessageContextDataHolder.getInstance().getInboundMessageContextMap()
+                .containsKey(channelIdString))); // No error has occurred context exists in data-holder map.
+
+        msg = Mockito.mock(CloseWebSocketFrame.class);
+        websocketHandler.write(channelHandlerContext, msg, channelPromise);
+        Assert.assertFalse((InboundMessageContextDataHolder.getInstance().getInboundMessageContextMap()
+                .containsKey(channelIdString))); // Closing connection. Remove context from data-holder map.
+    }
+
+    /*
+     * This method tests write() when msg is a WebSocketFrame for WebSocket API.
+     * */
+    @Test
+    public void testWSWriteSuccessResponse() throws Exception {
+
+        InboundMessageContext inboundMessageContext = new InboundMessageContext();
+        inboundMessageContext.setElectedAPI(websocketAPI);
+        InboundMessageContextDataHolder.getInstance().addInboundMessageContextForConnection(channelIdString,
+                inboundMessageContext);
+        InboundProcessorResponseDTO responseDTO = new InboundProcessorResponseDTO();
+        PowerMockito.when(InboundWebsocketProcessorUtil.doThrottle(Mockito.anyInt(), Mockito.anyObject(),
+                Mockito.anyObject(), Mockito.anyObject())).thenReturn(responseDTO);
+        websocketHandler.write(channelHandlerContext, msg, channelPromise);
+        Assert.assertTrue((InboundMessageContextDataHolder.getInstance().getInboundMessageContextMap()
+                .containsKey(channelIdString)));// No error has occurred context exists in data-holder map.
+    }
+
+    /*
+     * This method tests write() when msg is a WebSocketFrame for WebSocket API and returns error responses.
+     * */
+    @Test
+    public void testWSWriteErrorResponse() throws Exception {
+
+        InboundMessageContext inboundMessageContext = new InboundMessageContext();
+        inboundMessageContext.setElectedAPI(websocketAPI);
+        InboundMessageContextDataHolder.getInstance().addInboundMessageContextForConnection(channelIdString,
+                inboundMessageContext);
+        InboundProcessorResponseDTO responseDTO = new InboundProcessorResponseDTO();
+        responseDTO.setError(true);
+        responseDTO.setCloseConnection(true);
+        PowerMockito.when(InboundWebsocketProcessorUtil.doThrottle(Mockito.anyInt(), Mockito.anyObject(),
+                Mockito.anyObject(), Mockito.anyObject())).thenReturn(responseDTO);
+        websocketHandler.write(channelHandlerContext, msg, channelPromise);
+        Assert.assertFalse(InboundMessageContextDataHolder.getInstance().getInboundMessageContextMap()
+                .containsKey(channelIdString));  // Closing connection error has occurred
+
+        //Websocket frame error has occurred
+        InboundMessageContextDataHolder.getInstance().addInboundMessageContextForConnection(channelIdString,
+                inboundMessageContext);
+        responseDTO.setError(true);
+        responseDTO.setCloseConnection(false);
+        websocketHandler.write(channelHandlerContext, msg, channelPromise);
+        Assert.assertTrue((InboundMessageContextDataHolder.getInstance().getInboundMessageContextMap()
+                .containsKey(channelIdString)));
+    }
+
+    @Test
+    public void testGraphQLWriteResponse() throws Exception {
+
+        InboundMessageContext inboundMessageContext = new InboundMessageContext();
+        inboundMessageContext.setElectedAPI(graphQLAPI);
+        InboundMessageContextDataHolder.getInstance().addInboundMessageContextForConnection(channelIdString,
+                inboundMessageContext);
+        msg = new TextWebSocketFrame("{\"id\":\"1\",\"type\":\"start\",\"payload\":{\"variables\":{},"
+                + "\"extensions\":{},\"operationName\":null,"
+                + "\"query\":\"subscription {\\n  liftStatusChange {\\n    id\\n    name\\n    }\\n}\\n\"}}");
+
+        VerbInfoDTO verbInfoDTO = new VerbInfoDTO();
+        verbInfoDTO.setHttpVerb(GraphQLConstants.SubscriptionConstants.HTTP_METHOD_NAME);
+        verbInfoDTO.setAuthType("OAUTH");
+        GraphQLOperationDTO graphQLOperationDTO = new GraphQLOperationDTO(verbInfoDTO, "liftStatusChange");
+        inboundMessageContext.addVerbInfoForGraphQLMsgId("1", graphQLOperationDTO);
+        InboundProcessorResponseDTO responseDTO = new InboundProcessorResponseDTO();
+        PowerMockito.when(InboundWebsocketProcessorUtil
+                .validateScopes(Mockito.anyObject(), Mockito.anyObject(), Mockito.anyObject())).thenReturn(responseDTO);
+        PowerMockito.when(InboundWebsocketProcessorUtil.doThrottleForGraphQL(Mockito.anyInt(), Mockito.anyObject(),
+                Mockito.anyObject(), Mockito.anyObject())).thenReturn(responseDTO);
+        PowerMockito.when(InboundWebsocketProcessorUtil.authenticateToken(inboundMessageContext))
+                .thenReturn(responseDTO);
+        //happy path
+        websocketHandler.write(channelHandlerContext, msg, channelPromise);
+        Assert.assertTrue((InboundMessageContextDataHolder.getInstance().getInboundMessageContextMap()
+                .containsKey(channelIdString)));// No error has occurred context exists in data-holder map.
+
+        //close connection error
+        responseDTO.setError(true);
+        responseDTO.setCloseConnection(true);
+        websocketHandler.write(channelHandlerContext, msg, channelPromise);
+        Assert.assertFalse(InboundMessageContextDataHolder.getInstance().getInboundMessageContextMap()
+                .containsKey(channelIdString));  // Closing connection error has occurred
+
+        //Websocket frame error has occurred
+        InboundMessageContextDataHolder.getInstance().addInboundMessageContextForConnection(channelIdString,
+                inboundMessageContext);
+        responseDTO.setError(true);
+        responseDTO.setCloseConnection(false);
+        websocketHandler.write(channelHandlerContext, msg, channelPromise);
+        Assert.assertTrue((InboundMessageContextDataHolder.getInstance().getInboundMessageContextMap()
+                .containsKey(channelIdString)));
     }
 }
