dcebfdd4a45b2dfed1a56f01befd34d9e5039787
binodmx <binod@wso2.com>
2022-01-10 16:43:07 +0530

    Fix creating high #cursors in a session

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -1288,7 +1288,6 @@ public class ApiMgtDAO {
         PreparedStatement getIncludedApisInProduct = null;
         PreparedStatement getSubscribedApisAndProducts = null;
         ResultSet resultSet = null;
-        ResultSet resultSet1 = null;
         Set<String> scopeKeysSet = new HashSet<>();
         Set<Integer> apiIdSet = new HashSet<>();
         int tenantId = APIUtil.getTenantId(subscriber.getName());
@@ -1299,15 +1298,16 @@ public class ApiMgtDAO {
             getSubscribedApisAndProducts.setInt(1, tenantId);
             getSubscribedApisAndProducts.setInt(2, applicationId);
             resultSet = getSubscribedApisAndProducts.executeQuery();
+            String getIncludedApisInProductQuery = SQLConstants.GET_INCLUDED_APIS_IN_PRODUCT_SQL;
+            getIncludedApisInProduct = conn.prepareStatement(getIncludedApisInProductQuery);
             while (resultSet.next()) {
                 int apiId = resultSet.getInt("API_ID");
-                String getIncludedApisInProductQuery = SQLConstants.GET_INCLUDED_APIS_IN_PRODUCT_SQL;
-                getIncludedApisInProduct = conn.prepareStatement(getIncludedApisInProductQuery);
                 getIncludedApisInProduct.setInt(1, apiId);
-                resultSet1 = getIncludedApisInProduct.executeQuery();
-                while (resultSet1.next()) {
-                    int includedApiId = resultSet1.getInt("API_ID");
-                    apiIdSet.add(includedApiId);
+                try (ResultSet resultSet1 = getIncludedApisInProduct.executeQuery()) {
+                    while (resultSet1.next()) {
+                        int includedApiId = resultSet1.getInt("API_ID");
+                        apiIdSet.add(includedApiId);
+                    }
                 }
                 apiIdSet.add(apiId);
             }
@@ -1332,7 +1332,7 @@ public class ApiMgtDAO {
             handleException("Failed to retrieve scopes for application subscription ", e);
         } finally {
             APIMgtDBUtil.closeAllConnections(getSubscribedApisAndProducts, null, resultSet);
-            APIMgtDBUtil.closeAllConnections(getIncludedApisInProduct, null, resultSet1);
+            APIMgtDBUtil.closeAllConnections(getIncludedApisInProduct, null, null);
         }
         return scopeKeysSet;
     }
