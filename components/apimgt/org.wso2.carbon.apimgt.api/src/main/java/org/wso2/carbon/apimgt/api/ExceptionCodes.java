5e74a80be124669aebaf2ec924052c55ff1353d0
shanakama <shanakama@wso2.com>
2022-06-01 21:40:11 +0530

    Exception code for revision existance

--- components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/ExceptionCodes.java
+++ components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/ExceptionCodes.java
@@ -510,7 +510,9 @@ public enum ExceptionCodes implements ErrorHandler {
     SUBSCRIPTION_TIER_NOT_ALLOWED(902002, "Subscription Tier is not allowed for user", 403, "Subscription Tier %s is" +
             " not allowed for user %s ", false),
     INVALID_KEY_MANAGER_REQUEST(902003, "Invalid Request sent to Key Manager.", 400, "Invalid Request sent to Key Manager.Error from Backend : %s", false),
-    INTERNAL_SERVER_ERROR_FROM_KEY_MANAGER(902004, "Internal Server Error from Key Manager", 500, "Internal Server Error from Key Manager.Error from Backend : %s", true);
+    INTERNAL_SERVER_ERROR_FROM_KEY_MANAGER(902004, "Internal Server Error from Key Manager", 500, "Internal Server Error from Key Manager.Error from Backend : %s", true),
+    REVISION_ALREADY_DEPLOYED(902005, "Revision deployment state conflicted", 409,
+            "Revision deployment request conflicted with the current deployment state of the revision %s. Please try again later", false);
 
     private final long errorCode;
     private final String errorMessage;
--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -16551,9 +16551,12 @@ public class ApiMgtDAO {
                     connection.rollback();
                     // handle concurrent db entry update. Fix duplicate primary key issue.
                     if (e.getMessage().toLowerCase().contains("primary key violation") ||
-                            e.getMessage().toLowerCase().contains("duplicate entry")) {
-                        log.debug("Duplicate entries detected for Revision UUID " + apiRevisionId +
+                            e.getMessage().toLowerCase().contains("duplicate entry") ||
+                            e.getMessage().contains("Violation of PRIMARY KEY constraint")) {
+                        log.warn("Duplicate entries detected for Revision UUID " + apiRevisionId +
                                 " while adding deployed API revisions", e);
+                        throw new APIManagementException("Failed to add deployed API Revision for Revision UUID "
+                                + apiRevisionId,  e, ExceptionCodes.REVISION_ALREADY_DEPLOYED);
                     } else {
                         handleException("Failed to add deployed API Revision for Revision UUID "
                                 + apiRevisionId, e);
