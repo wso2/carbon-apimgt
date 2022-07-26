0288b3e4696aece946ab83360144265cdb0d6fa2
shnrndk <shnrndk@gmail.com>
2022-07-26 12:10:04 +0530

    Updated APIConstants

--- components/apimgt/org.wso2.carbon.apimgt.persistence/src/main/java/org/wso2/carbon/apimgt/persistence/APIConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.persistence/src/main/java/org/wso2/carbon/apimgt/persistence/APIConstants.java
@@ -233,6 +233,7 @@ public final class APIConstants {
     public static final String API_OVERVIEW_STATUS_SEARCH_TYPE_KEY = "status=";
     public static final String ENABLE_STORE_SEARCH_TYPE_KEY = "enableStore=(true OR null)";
     public static final String LCSTATE_SEARCH_KEY = "lcState";
+    public static final String API_OVERVIEW_STATUS_SEARCH_KEY = "overview_status";
     public static final String DOCUMENTATION_SEARCH_TYPE_PREFIX = "doc";
     public static final String DOCUMENTATION_SEARCH_TYPE_PREFIX_WITH_EQUALS = "doc=";
     public static final String TAGS_SEARCH_TYPE_PREFIX = "tags";
