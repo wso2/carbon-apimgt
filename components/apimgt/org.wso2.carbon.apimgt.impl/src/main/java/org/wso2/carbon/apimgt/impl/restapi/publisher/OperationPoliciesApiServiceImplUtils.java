191ea9e6b765de65a078bff458a289362f352b99
Rakhitha Ranathunge <rakhitharr@live.com>
2022-09-01 10:36:21 +0530

    Refactor operation policy related operations

---
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/restapi/publisher/OperationPoliciesApiServiceImplUtils.java
@@ -0,0 +1,66 @@
+package org.wso2.carbon.apimgt.impl.restapi.publisher;
+
+import org.wso2.carbon.apimgt.api.model.OperationPolicyData;
+import org.wso2.carbon.apimgt.api.model.OperationPolicyDefinition;
+import org.wso2.carbon.apimgt.api.model.OperationPolicySpecification;
+import org.wso2.carbon.apimgt.impl.utils.APIUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OperationPoliciesApiServiceImplUtils {
+
+    /**
+     * @param policySpecification Operation policy spec
+     * @param organization        Validated organization
+     * @return OperationPolicyData object
+     */
+    public static OperationPolicyData prepareOperationPolicyData(OperationPolicySpecification policySpecification,
+                                                                 String organization) {
+        OperationPolicyData operationPolicyData = new OperationPolicyData();
+        operationPolicyData.setOrganization(organization);
+        operationPolicyData.setSpecification(policySpecification);
+
+        return operationPolicyData;
+    }
+
+    /**
+     * @param policyData       Operation policy data
+     * @param policyDefinition Operation policy definition object
+     * @param definition       Policy definition
+     * @param gatewayType      Policy gateway type
+     */
+    public static void preparePolicyDefinition(
+            OperationPolicyData policyData, OperationPolicyDefinition policyDefinition,
+            String definition, OperationPolicyDefinition.GatewayType gatewayType) {
+        policyDefinition.setContent(definition);
+        policyDefinition.setGatewayType(gatewayType);
+        policyDefinition.setMd5Hash(APIUtil.getMd5OfOperationPolicyDefinition(policyDefinition));
+
+        if (OperationPolicyDefinition.GatewayType.Synapse.equals(gatewayType)) {
+            policyData.setSynapsePolicyDefinition(policyDefinition);
+        } else if (OperationPolicyDefinition.GatewayType.ChoreoConnect.equals(gatewayType)) {
+            policyData.setCcPolicyDefinition(policyDefinition);
+        }
+
+        policyData.setMd5Hash(APIUtil.getMd5OfOperationPolicy(policyData));
+    }
+
+    /**
+     * @param query Request query
+     * @return Map of query params
+     */
+    public static Map<String, String> getQueryParams(String query) {
+        Map<String, String> queryParamMap = new HashMap<String, String>();
+        String[] queryParams = query.split(" ");
+        for (String param : queryParams) {
+            String[] keyVal = param.split(":");
+            if (keyVal.length == 2) {
+                queryParamMap.put(keyVal[0], keyVal[1]);
+            }
+        }
+
+        return queryParamMap;
+    }
+
+}
