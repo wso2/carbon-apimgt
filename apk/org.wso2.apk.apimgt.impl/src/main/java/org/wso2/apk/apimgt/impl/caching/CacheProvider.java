7ccabe4f8d699e128ebdfcc1273a993913ba0bed
Chameera Rupasinghe <chameerarupasinghe@gmail.com>
2022-10-19 10:36:00 +0530

    tenant config cache initail implementation

--- apk/org.wso2.apk.apimgt.impl/src/main/java/org/wso2/apk/apimgt/impl/caching/CacheProvider.java
+++ apk/org.wso2.apk.apimgt.impl/src/main/java/org/wso2/apk/apimgt/impl/caching/CacheProvider.java
@@ -17,16 +17,47 @@
  */
 package org.wso2.apk.apimgt.impl.caching;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.lang3.StringUtils;
 import org.wso2.apk.apimgt.impl.APIConstants;
 import org.wso2.apk.apimgt.impl.APIManagerConfiguration;
+import org.wso2.apk.apimgt.impl.config.APIMConfigService;
+import org.wso2.apk.apimgt.impl.config.APIMConfigServiceImpl;
 import org.wso2.apk.apimgt.impl.internal.ServiceReferenceHolder;
 import org.wso2.carbon.base.ServerConfiguration;
 
 import java.util.concurrent.TimeUnit;
 
-public class CacheProvider<T> {
+public class CacheProvider {
 
-    public static CacheStore createTenantConfigCache() {
+    static APIMConfigService apimConfigService = new APIMConfigServiceImpl();
+
+    public static void createTenantConfigCache() {
+
+        Cache<?, ?> cache = CacheManager.getCache(APIConstants.TENANT_CONFIG_CACHE_NAME);
+        if (cache != null) {
+            return;
+        }
+        initiateTenantConfigCache();
+    }
+
+    public static Cache<String, JsonObject> getTenantConfigCache() {
+
+        Cache<?, ?> cache = CacheManager.getCache(APIConstants.TENANT_CONFIG_CACHE_NAME);
+        if (cache != null) {
+            return (Cache<String, JsonObject>) cache;
+        }
+
+        initiateTenantConfigCache();
+        return (Cache<String, JsonObject>) CacheManager.getCache(APIConstants.TENANT_CONFIG_CACHE_NAME);
+    }
+
+    private static void initiateTenantConfigCache() {
 
         String apimGWCacheExpiry = getApiManagerConfiguration().getFirstProperty(APIConstants.TOKEN_CACHE_EXPIRY);
         long expiryDuration = Long.parseLong(apimGWCacheExpiry);
@@ -34,9 +65,31 @@ public class CacheProvider<T> {
         if (apimGWCacheExpiry.isEmpty()) {
             expiryDuration = getDefaultCacheTimeout();
         }
-        return new CacheStore<String>(expiryDuration, TimeUnit.SECONDS);
-    }
 
+        LoadingCache<String, JsonObject> tenantConfigCache =
+                CacheBuilder.newBuilder()
+                        .expireAfterAccess(expiryDuration, TimeUnit.SECONDS)
+                        .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+                        .build(
+                                new CacheLoader<String, JsonObject>() {
+                                    @Override
+                                    public JsonObject load(String key) throws Exception {
+
+                                        String[] parts = key.split("_");
+                                        if (parts.length > 0) {
+                                            String organization = parts[0];
+                                            String tenantConfig = apimConfigService.getTenantConfig(organization);
+                                            if (StringUtils.isNotEmpty(tenantConfig)) {
+                                                return new JsonParser().parse(tenantConfig).getAsJsonObject();
+                                            }
+                                        }
+                                        return new JsonObject();
+                                    }
+                                }
+                        );
+
+        CacheManager.addCache(APIConstants.TENANT_CONFIG_CACHE_NAME, tenantConfigCache);
+    }
 
     public static long getDefaultCacheTimeout() {
         if (ServerConfiguration.getInstance().getFirstProperty(APIConstants.DEFAULT_CACHE_TIMEOUT) != null) {
