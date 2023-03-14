1c737d7021f05c4312bda50b5b082db889969374
Suneth Samarasinghe <imsuneth@gmail.com>
2023-03-15 01:33:03 +0530

    Fix api-product GW access issue

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/APIKeyValidator.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/APIKeyValidator.java
@@ -706,21 +706,7 @@ public class APIKeyValidator {
             throws APISecurityException {
         if (uriTemplates == null) {
             synchronized (this) {
-                if (uriTemplates == null) {
-                    String swagger = (String) messageContext.getProperty(APIMgtGatewayConstants.OPEN_API_STRING);
-                    if (swagger != null) {
-                        APIDefinition oasParser;
-                        try {
-                            oasParser = OASParserUtil.getOASParser(swagger);
-                            uriTemplates = new ArrayList<>();
-                            uriTemplates.addAll(oasParser.getURITemplates(swagger));
-                            return uriTemplates;
-                        } catch (APIManagementException e) {
-                            log.error("Error while parsing swagger content to get URI Templates", e);
-                        }
-                    }
-                    uriTemplates = dataStore.getAPIProductURITemplates(context, apiVersion);
-                }
+                uriTemplates = dataStore.getAPIProductURITemplates(context, apiVersion);
             }
         }
         return uriTemplates;
