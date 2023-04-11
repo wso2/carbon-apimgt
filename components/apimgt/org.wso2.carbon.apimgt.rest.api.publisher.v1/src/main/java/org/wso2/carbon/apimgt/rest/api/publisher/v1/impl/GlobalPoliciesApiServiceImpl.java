3ccab90fed17e9d214260bbc041d7a4113a19e6f
yasasrangika <rangikaymendis@gmail.com>
2023-04-11 18:08:03 +0530

    re-use opertaion policy models

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/GlobalPoliciesApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/GlobalPoliciesApiServiceImpl.java
@@ -3,23 +3,17 @@ package org.wso2.carbon.apimgt.rest.api.publisher.v1.impl;
 import org.wso2.carbon.apimgt.rest.api.publisher.v1.*;
 import org.wso2.carbon.apimgt.rest.api.publisher.v1.dto.*;
 
-import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.MessageContext;
 
 import org.wso2.carbon.apimgt.rest.api.publisher.v1.dto.ErrorDTO;
-import org.wso2.carbon.apimgt.rest.api.publisher.v1.dto.GlobalPoliciesDTO;
 
-import java.util.List;
-
-import java.io.InputStream;
+import java.util.Map;
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
-
 
 public class GlobalPoliciesApiServiceImpl implements GlobalPoliciesApiService {
 
-    public Response addGlobalPolicy(List<GlobalPoliciesDTO> globalPoliciesDTO, MessageContext messageContext) {
+    public Response addGlobalPolicy(Map<String, APIOperationPoliciesDTO> requestBody, MessageContext messageContext) {
         // remove errorObject and add implementation code!
         ErrorDTO errorObject = new ErrorDTO();
         Response.Status status = Response.Status.NOT_IMPLEMENTED;
