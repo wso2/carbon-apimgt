2992f39c50d5dbc4c7c1bc04e647e3ee99732a2a
Isuru Wijesiri <isurumaduranga.16@cse.mrt.ac.lk>
2023-08-25 15:17:02 +0530

    Make params configurable

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/APIMgtGatewayConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/APIMgtGatewayConstants.java
@@ -195,7 +195,6 @@ public class APIMgtGatewayConstants {
     public static final String TRANSPORT_WS = "ws";
     public static final String TRANSPORT_WSS = "wss";
     public static final String TRANSACTION_COUNTER_CONFIG_ROOT = "APIGateway.TransactionCounter";
-    public static final String TRANSACTION_COUNTER_ENABLED = TRANSACTION_COUNTER_CONFIG_ROOT + ".Enabled";
     public static final String TRANSACTION_COUNTER_PRODUCER_THREAD_POOL_SIZE = TRANSACTION_COUNTER_CONFIG_ROOT +
             ".ProducerThreadPoolSize";
     public static final String TRANSACTION_COUNTER_QUEUE_SIZE = TRANSACTION_COUNTER_CONFIG_ROOT + ".QueueSize";
@@ -205,7 +204,16 @@ public class APIMgtGatewayConstants {
     public static final String TRANSACTION_COUNTER_RECORD_INTERVAL = TRANSACTION_COUNTER_CONFIG_ROOT
             + ".ProducerScheduledInterval";
     public static final String TRANSACTION_COUNTER_MAX_RETRY_COUNT = TRANSACTION_COUNTER_CONFIG_ROOT + ".MaxRetryCount";
+    public static final String TRANSACTION_COUNTER_MIN_RETRY_COUNT = TRANSACTION_COUNTER_CONFIG_ROOT + ".MinRetryCount";
     public static final String TRANSACTION_COUNTER_MAX_TRANSACTION_RECORDS_PER_COMMIT = TRANSACTION_COUNTER_CONFIG_ROOT
             + ".MaxBatchSize";
+    public static final String TRANSACTION_COUNTER_CONSUMER_COMMIT_INTERVAL = TRANSACTION_COUNTER_CONFIG_ROOT
+            + ".PublisherScheduledInterval";
+    public static final String TRANSACTION_COUNTER_SERVER_ID = TRANSACTION_COUNTER_CONFIG_ROOT + ".ServerID";
+    public static final String TRANSACTION_COUNTER_SERVICE = TRANSACTION_COUNTER_CONFIG_ROOT + ".ServiceURL";
+    public static final String TRANSACTION_COUNTER_SERVICE_USERNAME = TRANSACTION_COUNTER_CONFIG_ROOT
+            + ".ServiceUsername";
+    public static final String TRANSACTION_COUNTER_SERVICE_PASSWORD = TRANSACTION_COUNTER_CONFIG_ROOT
+            + ".ServicePassword";
 }
 
--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/transaction/TransactionCountConfig.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/transaction/TransactionCountConfig.java
@@ -7,37 +7,47 @@ import org.wso2.carbon.apimgt.impl.internal.ServiceReferenceHolder;
 public class TransactionCountConfig {
 
     private static int PRODUCER_THREAD_POOL_SIZE;
-    private static int CONSUMER_COMMIT_INTERVAL = 5;
+    private static int CONSUMER_COMMIT_INTERVAL;
     private static int TRANSACTION_RECORD_QUEUE_SIZE;
     private static String TRANSACTION_COUNT_STORE_CLASS;
     private static double MAX_TRANSACTION_COUNT;
     private static int TRANSACTION_COUNT_RECORD_INTERVAL;
     private static int MAX_RETRY_COUNT;
     private static int MAX_TRANSACTION_RECORDS_PER_COMMIT;
-    private static String SERVER_ID = "Gateway1";
-    private static String TRANSACTION_COUNT_SERVVICE = "https://localhost:8080/transactioncount";
-    private static String TRANSACTION_COUNT_SERVVICE_USERNAME = "admin";
-    private static String TRANSACTION_COUNT_SERVVICE_PASSWORD = "admin";
+    private static String SERVER_ID;
+    private static String TRANSACTION_COUNT_SERVICE;
+    private static String TRANSACTION_COUNT_SERVICE_USERNAME;
+    private static String TRANSACTION_COUNT_SERVICE_PASSWORD;
 
     static {
         APIManagerConfiguration apiManagerConfiguration = ServiceReferenceHolder.getInstance()
                 .getAPIManagerConfigurationService().getAPIManagerConfiguration();
 
         if (apiManagerConfiguration != null) {
-            PRODUCER_THREAD_POOL_SIZE = Integer.parseInt(apiManagerConfiguration.getFirstProperty(
-                    APIMgtGatewayConstants.TRANSACTION_COUNTER_PRODUCER_THREAD_POOL_SIZE));
-            TRANSACTION_RECORD_QUEUE_SIZE = Integer.parseInt(apiManagerConfiguration.getFirstProperty(
-                    APIMgtGatewayConstants.TRANSACTION_COUNTER_QUEUE_SIZE));
+            SERVER_ID = apiManagerConfiguration.getFirstProperty(
+                    APIMgtGatewayConstants.TRANSACTION_COUNTER_SERVER_ID);
             TRANSACTION_COUNT_STORE_CLASS = apiManagerConfiguration.getFirstProperty(
                     APIMgtGatewayConstants.TRANSACTION_COUNTER_STORE_CLASS);
-            MAX_TRANSACTION_COUNT = Double.parseDouble(apiManagerConfiguration.getFirstProperty(
-                    APIMgtGatewayConstants.TRANSACTION_COUNTER_MAX_TRANSACTION_COUNT));
+            TRANSACTION_RECORD_QUEUE_SIZE = Integer.parseInt(apiManagerConfiguration.getFirstProperty(
+                    APIMgtGatewayConstants.TRANSACTION_COUNTER_QUEUE_SIZE));
+            PRODUCER_THREAD_POOL_SIZE = Integer.parseInt(apiManagerConfiguration.getFirstProperty(
+                    APIMgtGatewayConstants.TRANSACTION_COUNTER_PRODUCER_THREAD_POOL_SIZE));
             TRANSACTION_COUNT_RECORD_INTERVAL = Integer.parseInt(apiManagerConfiguration.getFirstProperty(
                     APIMgtGatewayConstants.TRANSACTION_COUNTER_RECORD_INTERVAL));
-            MAX_RETRY_COUNT = Integer.parseInt(apiManagerConfiguration.getFirstProperty(
-                    APIMgtGatewayConstants.TRANSACTION_COUNTER_MAX_RETRY_COUNT));
+            MAX_TRANSACTION_COUNT = Double.parseDouble(apiManagerConfiguration.getFirstProperty(
+                    APIMgtGatewayConstants.TRANSACTION_COUNTER_MAX_TRANSACTION_COUNT));
+            CONSUMER_COMMIT_INTERVAL = Integer.parseInt(apiManagerConfiguration.getFirstProperty(
+                    APIMgtGatewayConstants.TRANSACTION_COUNTER_CONSUMER_COMMIT_INTERVAL));
             MAX_TRANSACTION_RECORDS_PER_COMMIT = Integer.parseInt(apiManagerConfiguration.getFirstProperty(
                     APIMgtGatewayConstants.TRANSACTION_COUNTER_MAX_TRANSACTION_RECORDS_PER_COMMIT));
+            MAX_RETRY_COUNT = Integer.parseInt(apiManagerConfiguration.getFirstProperty(
+                    APIMgtGatewayConstants.TRANSACTION_COUNTER_MAX_RETRY_COUNT));
+            TRANSACTION_COUNT_SERVICE = apiManagerConfiguration.getFirstProperty(
+                    APIMgtGatewayConstants.TRANSACTION_COUNTER_SERVICE);
+            TRANSACTION_COUNT_SERVICE_USERNAME = apiManagerConfiguration.getFirstProperty(
+                    APIMgtGatewayConstants.TRANSACTION_COUNTER_SERVICE_USERNAME);
+            TRANSACTION_COUNT_SERVICE_PASSWORD = apiManagerConfiguration.getFirstProperty(
+                    APIMgtGatewayConstants.TRANSACTION_COUNTER_SERVICE_PASSWORD);
         }
     }
 
@@ -74,15 +84,15 @@ public class TransactionCountConfig {
     }
 
     public static String getTransactionCountService() {
-        return TRANSACTION_COUNT_SERVVICE;
+        return TRANSACTION_COUNT_SERVICE;
     }
 
     public static String getTransactionCountServiceUsername() {
-        return TRANSACTION_COUNT_SERVVICE_USERNAME;
+        return TRANSACTION_COUNT_SERVICE_USERNAME;
     }
 
     public static String getTransactionCountServicePassword() {
-        return TRANSACTION_COUNT_SERVVICE_PASSWORD;
+        return TRANSACTION_COUNT_SERVICE_PASSWORD;
     }
 
     public static int getConsumerCommitInterval() {
