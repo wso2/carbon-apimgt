b1c73f8e378428b43c914826afa235a8ec45a0fd
yasasrangika <rangikaymendis@gmail.com>
2023-12-13 09:13:36 +0530

    commit gen class changes

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/GatewayPoliciesApiService.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/gen/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/GatewayPoliciesApiService.java
@@ -1,49 +1,34 @@
-/*
- * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.wso2.carbon.apimgt.rest.api.publisher.v1;
 
+import org.wso2.carbon.apimgt.rest.api.publisher.v1.*;
+import org.wso2.carbon.apimgt.rest.api.publisher.v1.dto.*;
+
 import org.apache.cxf.jaxrs.ext.MessageContext;
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 
 import org.wso2.carbon.apimgt.api.APIManagementException;
 
+import org.wso2.carbon.apimgt.rest.api.publisher.v1.dto.ErrorDTO;
 import org.wso2.carbon.apimgt.rest.api.publisher.v1.dto.GatewayPolicyDeploymentDTO;
+import org.wso2.carbon.apimgt.rest.api.publisher.v1.dto.GatewayPolicyMappingDataListDTO;
+import org.wso2.carbon.apimgt.rest.api.publisher.v1.dto.GatewayPolicyMappingInfoDTO;
 import org.wso2.carbon.apimgt.rest.api.publisher.v1.dto.GatewayPolicyMappingsDTO;
 import java.util.List;
 
+import java.util.List;
+
+import java.io.InputStream;
+
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
 
 public interface GatewayPoliciesApiService {
-      public Response addGatewayPoliciesToFlows(GatewayPolicyMappingsDTO gatewayPolicyMappingsDTO,
-              MessageContext messageContext) throws APIManagementException;
-
-      public Response deleteGatewayPolicyByPolicyId(String gatewayPolicyMappingId, MessageContext messageContext)
-              throws APIManagementException;
-
-      public Response engageGlobalPolicy(String gatewayPolicyMappingId,
-              List<GatewayPolicyDeploymentDTO> gatewayPolicyDeploymentDTO, MessageContext messageContext)
-              throws APIManagementException;
-
-      public Response getAllGatewayPolicies(Integer limit, Integer offset, String query, MessageContext messageContext)
-              throws APIManagementException;
-
-      public Response getGatewayPolicyMappingContentByPolicyMappingId(String gatewayPolicyMappingId,
-              MessageContext messageContext) throws APIManagementException;
-
-      public Response updateGatewayPoliciesToFlows(String gatewayPolicyMappingId,
-              GatewayPolicyMappingsDTO gatewayPolicyMappingsDTO, MessageContext messageContext)
-              throws APIManagementException;
+      public Response addGatewayPoliciesToFlows(GatewayPolicyMappingsDTO gatewayPolicyMappingsDTO, MessageContext messageContext) throws APIManagementException;
+      public Response deleteGatewayPolicyByPolicyId(String gatewayPolicyMappingId, MessageContext messageContext) throws APIManagementException;
+      public Response engageGlobalPolicy(String gatewayPolicyMappingId, List<GatewayPolicyDeploymentDTO> gatewayPolicyDeploymentDTO, MessageContext messageContext) throws APIManagementException;
+      public Response getAllGatewayPolicies(Integer limit, Integer offset, String query, MessageContext messageContext) throws APIManagementException;
+      public Response getGatewayPolicyMappingContentByPolicyMappingId(String gatewayPolicyMappingId, MessageContext messageContext) throws APIManagementException;
+      public Response updateGatewayPoliciesToFlows(String gatewayPolicyMappingId, GatewayPolicyMappingsDTO gatewayPolicyMappingsDTO, MessageContext messageContext) throws APIManagementException;
 }
