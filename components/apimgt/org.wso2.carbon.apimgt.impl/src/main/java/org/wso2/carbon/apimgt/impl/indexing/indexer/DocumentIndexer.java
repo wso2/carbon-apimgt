dc39fc02bd2903c6086a806e62b7ae62a8cb05c2
Dinith Herath <dinithherath18332@gmail.com>
2024-02-08 11:49:12 +0530

    Add related fixes for PDFBox bump

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/indexing/indexer/DocumentIndexer.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/indexing/indexer/DocumentIndexer.java
@@ -23,7 +23,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
+import org.apache.pdfbox.io.RandomAccessReadBuffer;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.cos.COSDocument;
@@ -182,9 +182,9 @@ public class DocumentIndexer extends RXTIndexer {
                 inputStream = contentResource.getContentStream();
                 switch (extension) {
                 case APIConstants.PDF_EXTENSION:
-                    PDFParser pdfParser = new PDFParser(new RandomAccessBufferedFileInputStream(inputStream));
+                    PDFParser pdfParser = new PDFParser(new RandomAccessReadBuffer(inputStream));
                     pdfParser.parse();
-                    COSDocument cosDocument = pdfParser.getDocument();
+                    COSDocument cosDocument = pdfParser.parse().getDocument();
                     PDFTextStripper stripper = new PDFTextStripper();
                     contentString = stripper.getText(new PDDocument(cosDocument));
                     break;
