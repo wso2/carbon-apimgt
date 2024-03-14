5ab29b0268fc2d4a539e8f906dde8cf41589c8b9
Ashera Silva <asherasilva100@gmail.com>
2024-03-14 13:00:30 +0530

    Add health check

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/AiApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/AiApiServiceImpl.java
@@ -1,5 +1,13 @@
 package org.wso2.carbon.apimgt.rest.api.store.v1.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.rest.api.store.v1.*;
 import org.wso2.carbon.apimgt.rest.api.store.v1.dto.*;
 
@@ -11,7 +19,10 @@ import org.wso2.carbon.apimgt.rest.api.store.v1.dto.ApiChatExecuteResponseDTO;
 import org.wso2.carbon.apimgt.rest.api.store.v1.dto.ApiChatPreparationRequestDTO;
 import org.wso2.carbon.apimgt.rest.api.store.v1.dto.ApiChatPreparationResponseDTO;
 import org.wso2.carbon.apimgt.rest.api.store.v1.dto.ErrorDTO;
+import org.wso2.carbon.apimgt.impl.utils.APIUtil;
+import org.wso2.carbon.apimgt.rest.api.util.utils.RestApiUtil;
 
+import java.net.MalformedURLException;
 import java.util.List;
 
 import java.io.InputStream;
@@ -21,6 +32,7 @@ import javax.ws.rs.core.SecurityContext;
 
 
 public class AiApiServiceImpl implements AiApiService {
+    private static final Log log = LogFactory.getLog(AiApiServiceImpl.class);
 
     public Response apiChatExecute(String apiChatRequestId, ApiChatExecuteRequestDTO apiChatExecuteRequestDTO, MessageContext messageContext) {
         // remove errorObject and add implementation code!
@@ -43,12 +55,22 @@ public class AiApiServiceImpl implements AiApiService {
     }
 
     public Response getApiChatHealth(MessageContext messageContext) {
-        // remove errorObject and add implementation code!
-        ErrorDTO errorObject = new ErrorDTO();
-        Response.Status status = Response.Status.NOT_IMPLEMENTED;
-        errorObject.setCode((long) status.getStatusCode());
-        errorObject.setMessage(status.toString());
-        errorObject.setDescription("The requested resource has not been implemented");
-        return Response.status(status).entity(errorObject).build();
+        try {
+            if (APIUtil.isApiChatEnabled()) {
+                CloseableHttpResponse response = APIUtil.getAIServiceHealth(APIConstants.API_CHAT_ENDPOINT, APIConstants.API_CHAT_AUTH_TOKEN);
+                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                    return Response.ok().build();
+                } else {
+                    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+                }
+            }
+        } catch (MalformedURLException e) {
+            String errorMessage = "Malformed URL detected when attempting to perform health check against API Chat service";
+            RestApiUtil.handleInternalServerError(errorMessage, e, log);
+        } catch (APIManagementException e) {
+            String errorMessage = "Error encountered while connecting to the API Chat service";
+            RestApiUtil.handleInternalServerError(errorMessage, e, log);
+        }
+        return null;
     }
 }
