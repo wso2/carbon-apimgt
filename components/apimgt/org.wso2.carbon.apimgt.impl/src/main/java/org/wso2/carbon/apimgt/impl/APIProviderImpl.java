37b6cc6f483fd8817f6d92d5270e313aa944cd8c
dulith <dulithcsenanayake@gmail.com>
2024-04-08 13:46:54 +0530

    Populate default version only if the migration enabled

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -5295,7 +5295,9 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
                 populateAPIProductInformation(uuid, organization, product);
                 populateAPIStatus(product);
                 populateAPITier(product);
-                populateDefaultVersion(product);
+                if (migrationEnabled == null) {
+                    populateDefaultVersion(product);
+                }
                 return product;
             } else {
                 String msg = "Failed to get API Product. API Product artifact corresponding to artifactId " + uuid
