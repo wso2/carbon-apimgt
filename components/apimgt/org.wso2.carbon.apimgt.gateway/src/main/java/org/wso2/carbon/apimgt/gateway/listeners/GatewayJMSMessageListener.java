b68cbaae1e025d4a6f546601b5e48d24f7069d08
pasant9 <pasan96tennakoon@gmail.com>
2024-10-08 16:36:49 +0530

    Improve error handling in LLM Provider Events

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/listeners/GatewayJMSMessageListener.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/listeners/GatewayJMSMessageListener.java
@@ -423,10 +423,25 @@ public class GatewayJMSMessageListener implements MessageListener, JMSConnection
                 }
             }
         } else if (EventType.LLM_PROVIDER_CREATE.toString().equals(eventType)) {
-            createLLMProvider(eventJson);
+            try {
+                createLLMProvider(eventJson);
+            } catch (Exception e) {
+                log.error("Error while handling LLM_PROVIDER_CREATE event", e);
+            }
+
         } else if (EventType.LLM_PROVIDER_DELETE.toString().equals(eventType)) {
+            try {
+                deleteLLMProvider(eventJson);
+            } catch (Exception e) {
+                log.error("Error while handling LLM_PROVIDER_DELETE event", e);
+            }
             deleteLLMProvider(eventJson);
         } else if (EventType.LLM_PROVIDER_UPDATE.toString().equals(eventType)) {
+            try {
+                updateLLMProvider(eventJson);
+            } catch (Exception e) {
+                log.error("Error while handling LLM_PROVIDER_UPDATE event", e);
+            }
             updateLLMProvider(eventJson);
         }
     }
