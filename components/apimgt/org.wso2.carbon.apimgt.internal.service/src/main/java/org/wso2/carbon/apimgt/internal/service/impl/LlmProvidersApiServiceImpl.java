cdf6b7067f73ee40af082046cb2cbc205501618f
pasant9 <pasan96tennakoon@gmail.com>
2024-10-14 10:25:45 +0530

    Improve internal REST APIs for AI APIs

--- components/apimgt/org.wso2.carbon.apimgt.internal.service/src/main/java/org/wso2/carbon/apimgt/internal/service/impl/LlmProvidersApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.internal.service/src/main/java/org/wso2/carbon/apimgt/internal/service/impl/LlmProvidersApiServiceImpl.java
@@ -22,37 +22,27 @@ import org.wso2.carbon.apimgt.api.APIAdmin;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.model.LLMProvider;
 import org.wso2.carbon.apimgt.impl.APIAdminImpl;
-import org.wso2.carbon.apimgt.internal.service.*;
-import org.wso2.carbon.apimgt.internal.service.dto.*;
+import org.wso2.carbon.apimgt.internal.service.LlmProvidersApiService;
 
-import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.MessageContext;
 
-import org.wso2.carbon.apimgt.internal.service.dto.ErrorDTO;
+import org.wso2.carbon.apimgt.internal.service.dto.LLMProviderDTO;
 import org.wso2.carbon.apimgt.internal.service.dto.LLMProviderListDTO;
 
 import java.util.List;
-
-import java.io.InputStream;
 import java.util.stream.Collectors;
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.SecurityContext;
 
 public class LlmProvidersApiServiceImpl implements LlmProvidersApiService {
 
     @Override
-    public Response getLLMProviderById(String llmProviderId, MessageContext messageContext) throws APIManagementException {
+    public Response getLLMProviderById(String llmProviderId, MessageContext messageContext)
+            throws APIManagementException {
 
         APIAdmin admin = new APIAdminImpl();
         LLMProvider llmProvider = admin.getLLMProvider(null, llmProviderId);
-        LLMProviderDTO llmProviderDto = new LLMProviderDTO();
-        llmProviderDto.setId(llmProvider.getId());
-        llmProviderDto.setName(llmProvider.getName());
-        llmProviderDto.setApiVersion(llmProvider.getApiVersion());
-        llmProviderDto.setOrganization(llmProvider.getOrganization());
-        llmProviderDto.setConfigurations(llmProvider.getConfigurations());
-        return Response.ok().entity(llmProviderDto).build();
+        return Response.ok().entity(mapToLLMProviderDTO(llmProvider)).build();
     }
 
     /**
@@ -67,23 +57,32 @@ public class LlmProvidersApiServiceImpl implements LlmProvidersApiService {
                                     MessageContext messageContext) throws APIManagementException {
 
         APIAdmin admin = new APIAdminImpl();
-        List<LLMProvider> LLMProviderList = admin.getLLMProviders(organization, name, apiVersion, null);
+        List<LLMProvider> llmProviderList = admin.getLLMProviders(organization, name, apiVersion, null);
 
-        List<LLMProviderDTO> llmProviderDtoList = LLMProviderList.stream()
-                .map(llmProvider -> {
-                    LLMProviderDTO llmProviderDto = new LLMProviderDTO();
-                    llmProviderDto.setId(llmProvider.getId());
-                    llmProviderDto.setName(llmProvider.getName());
-                    llmProviderDto.setApiVersion(llmProvider.getApiVersion());
-                    llmProviderDto.setOrganization(llmProvider.getOrganization());
-                    llmProviderDto.setConfigurations(llmProvider.getConfigurations());
-                    return llmProviderDto;
-                })
+        List<LLMProviderDTO> llmProviderDtoList = llmProviderList.stream()
+                .map(this::mapToLLMProviderDTO)
                 .collect(Collectors.toList());
 
         LLMProviderListDTO llmProviderListDTO = new LLMProviderListDTO();
         llmProviderListDTO.setApis(llmProviderDtoList);
 
         return Response.ok().entity(llmProviderListDTO).build();
+    }
+
+    /**
+     * Map LLMProvider to LLMProviderDTO.
+     *
+     * @param llmProvider The LLMProvider object.
+     * @return The corresponding LLMProviderDTO object.
+     */
+    private LLMProviderDTO mapToLLMProviderDTO(LLMProvider llmProvider) {
+
+        LLMProviderDTO llmProviderDto = new LLMProviderDTO();
+        llmProviderDto.setId(llmProvider.getId());
+        llmProviderDto.setName(llmProvider.getName());
+        llmProviderDto.setApiVersion(llmProvider.getApiVersion());
+        llmProviderDto.setOrganization(llmProvider.getOrganization());
+        llmProviderDto.setConfigurations(llmProvider.getConfigurations());
+        return llmProviderDto;
     }
 }
