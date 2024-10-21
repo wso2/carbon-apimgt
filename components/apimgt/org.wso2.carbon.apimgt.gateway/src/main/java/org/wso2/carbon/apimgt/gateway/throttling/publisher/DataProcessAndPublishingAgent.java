2536487fdec843c4f9e8b752d49f7f78f81c79af
AnuGayan <anushaj@wso2.com>
2024-10-21 23:18:33 +0530

    Fix NullPointer issue when invoking AI API resources

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/throttling/publisher/DataProcessAndPublishingAgent.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/throttling/publisher/DataProcessAndPublishingAgent.java
@@ -70,14 +70,14 @@ public class DataProcessAndPublishingAgent implements Runnable {
     String apiName;
     String appId;
     String ipAddress;
-    Long totalTokens;
-    Long promptTokens;
-    Long completionTokens;
+    Long totalTokens = 0L;
+    Long promptTokens = 0L;
+    Long completionTokens = 0L;
     Map<String, String> headersMap;
     Map<String, Object> customPropertyMap;
     private AuthenticationContext authenticationContext;
 
-    private long messageSizeInBytes;
+    private long messageSizeInBytes = 0L;
 
     public DataProcessAndPublishingAgent() {
 
