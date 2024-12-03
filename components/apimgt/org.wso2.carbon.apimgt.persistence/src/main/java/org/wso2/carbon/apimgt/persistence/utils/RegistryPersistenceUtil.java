946c8932da53af7d9833ac3a3a95a04a3634535c
rusirijayodaillesinghe <rusiri@wso2.com>
2024-12-03 23:15:34 +0530

    Fix tag based search

--- components/apimgt/org.wso2.carbon.apimgt.persistence/src/main/java/org/wso2/carbon/apimgt/persistence/utils/RegistryPersistenceUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.persistence/src/main/java/org/wso2/carbon/apimgt/persistence/utils/RegistryPersistenceUtil.java
@@ -1938,6 +1938,9 @@ public class RegistryPersistenceUtil {
                 case "group.sort":
                     outputMap.put("group.sort", "overview_" + value);
                     break;
+                case "tags":
+                    outputMap.put("tags", value);
+                    break;
                 default:
                     // Add any other cases if needed
                     outputMap.put("overview_" + key, value);
