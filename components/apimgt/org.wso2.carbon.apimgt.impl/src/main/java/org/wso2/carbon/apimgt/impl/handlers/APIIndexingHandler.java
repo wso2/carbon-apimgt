b7e592aea8b23ff99f25dd4dbed450547a606551
Oshanath Rajawasam <oshanath@wso2.com>
2024-12-16 10:12:52 +0530

    Added custom API Indexing handler to bypass indexing of old API revisions.

---
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/handlers/APIIndexingHandler.java
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2024, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.apimgt.impl.handlers;
+
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
+import org.wso2.carbon.registry.core.jdbc.handlers.RequestContext;
+import org.wso2.carbon.registry.indexing.IndexingHandler;
+
+/**
+ * This is the Indexing handler to skip indexing of older API revisions
+ */
+public class APIIndexingHandler extends IndexingHandler {
+    public void put(RequestContext requestContext) throws RegistryException {
+        if (requestContext.getResourcePath().getPath().contains("/apimgt/applicationdata/apis/")) {
+            return;
+        }
+        super.put(requestContext);
+    }
+}
