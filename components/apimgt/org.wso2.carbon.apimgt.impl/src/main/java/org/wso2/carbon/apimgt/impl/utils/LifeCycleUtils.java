3e56dfae7b9ed7a67cb62a5ceead61fed44e0f1c
piyumaldk <piyumaldk@gmail.com>
2025-01-10 17:04:39 +0530

    Next gen devportal configurations 3 - Publishing Part

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/LifeCycleUtils.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/LifeCycleUtils.java
@@ -3,13 +3,19 @@ package org.wso2.carbon.apimgt.impl.utils;
 import org.apache.axis2.util.JavaUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.APIProvider;
@@ -28,6 +34,7 @@ import org.wso2.carbon.apimgt.impl.notifier.events.SubscriptionEvent;
 import org.wso2.carbon.apimgt.impl.recommendationmgt.RecommendationEnvironment;
 import org.wso2.carbon.apimgt.impl.recommendationmgt.RecommenderDetailsExtractor;
 import org.wso2.carbon.apimgt.impl.recommendationmgt.RecommenderEventPublisher;
+import org.wso2.carbon.apimgt.impl.restapi.publisher.ApisApiServiceImplUtils;
 import org.wso2.carbon.apimgt.impl.workflow.WorkflowConstants;
 import org.wso2.carbon.apimgt.impl.workflow.WorkflowException;
 import org.wso2.carbon.apimgt.impl.workflow.WorkflowExecutor;
@@ -93,7 +100,7 @@ public class LifeCycleUtils {
 
         // Next Gen Dev Portal Publication
         if (APIConstants.PUBLISHED.equals(targetStatus) && APIUtil.isNewPortalEnabled()) {
-            publishInNewPortal(orgId);
+            publishInNewPortal(orgId, apiTypeWrapper);
         }
 
         // Change the lifecycle state in the database
@@ -126,10 +133,9 @@ public class LifeCycleUtils {
         }
     }
 
-    public static void publishInNewPortal(String tenantName) throws APIManagementException {
+    public static void publishInNewPortal(String tenantName, ApiTypeWrapper apiTypeWrapper) throws APIManagementException {
         String baseUrl = APIUtil.getNewPortalURL();
         int tenantId = APIUtil.getTenantIdFromTenantDomain(tenantName);
-        String apiUrl = baseUrl + "/devportal/b2b/organizations/" + tenantName;
 
         Certificate cert = SigningUtil.getPublicCertificate(tenantId);
         PrivateKey privateKey = SigningUtil.getSigningKey(tenantId);
@@ -137,19 +143,71 @@ public class LifeCycleUtils {
         SSLContext sslContext = configureSSLContext(cert, privateKey);
         SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
 
-        String orgId = getNewPortalOrgId(apiUrl, sslsf);
+        String orgId = getNewPortalOrgId(baseUrl + "/devportal/b2b/organizations/" + tenantName, sslsf);
 
         if (orgId.isEmpty()){
             // Create a new org from tenant Name
             // Get that org ID and update orgID
         }
+        API api = apiTypeWrapper.getApi();
+        String apiDefinition = ApisApiServiceImplUtils.getApiDefinition(api);
+        String apiMetaData = getApiMetaData(tenantName, apiTypeWrapper);
 
-        // Get API Specification
-        // Construct API Info
-        // Publish API
+        int statusCode = publishApiMetadata(baseUrl + "/devportal/b2b/organizations/" + orgId + "/apis",
+                apiMetaData, apiDefinition, sslsf);
 
-        log.info("orgID: " + orgId);
+        if (statusCode == 201) {
+            log.info("The API " + apiTypeWrapper.getName() +
+                    " has been successfully published to the NextGen Developer Portal at " + baseUrl + ".");
+        } else {
+            log.error("Failed to publish the API " + apiTypeWrapper.getName() +
+                    " to the NextGen Developer Portal at " + baseUrl + ". Status code: " + statusCode);
+        }
+    }
 
+    private static String getApiMetaData(String orgName, ApiTypeWrapper apiTypeWrapper) {
+        API api = apiTypeWrapper.getApi();
+
+        JSONObject apiInfo = new JSONObject();
+        apiInfo.put("referenceID", apiTypeWrapper.getUuid() != null ? apiTypeWrapper.getUuid() : "");
+        apiInfo.put("apiName", apiTypeWrapper.getName() != null ? apiTypeWrapper.getName() : "");
+        apiInfo.put("orgName", orgName != null ? orgName : "");
+        apiInfo.put("provider", api.getId().getProviderName() != null ? api.getId().getProviderName() : "");
+        // TODO: Find What is the single category
+        apiInfo.put("apiCategory", "");
+        apiInfo.put("apiDescription", api.getDescription() != null ? api.getDescription() : "");
+        apiInfo.put("visibility", api.getVisibility() != null ? api.getVisibility() : "");
+
+        JSONArray visibleGroupsArray = new JSONArray();
+        if (api.getVisibleRoles() != null) {
+            String[] visibleRoles = api.getVisibleRoles().split(",");
+            for (String role : visibleRoles) {
+                visibleGroupsArray.add(role);
+            }
+        }
+        apiInfo.put("visibleGroups", visibleGroupsArray);
+
+        JSONObject owners = new JSONObject();
+        owners.put("technicalOwner", api.getTechnicalOwner() != null ? api.getTechnicalOwner() : "");
+        owners.put("technicalOwnerEmail", api.getTechnicalOwnerEmail() != null ? api.getTechnicalOwnerEmail() : "");
+        owners.put("businessOwner", api.getBusinessOwner() != null ? api.getBusinessOwner() : "");
+        owners.put("businessOwnerEmail", api.getBusinessOwnerEmail() != null ? api.getBusinessOwnerEmail() : "");
+
+        apiInfo.put("owners", owners);
+        apiInfo.put("apiVersion", api.getId().getVersion() != null ? api.getId().getVersion() : "");
+        apiInfo.put("apiType", apiTypeWrapper.getType() != null ? apiTypeWrapper.getType() : "");
+
+        JSONObject endPoints = new JSONObject();
+        endPoints.put("sandboxURL", api.getApiExternalSandboxEndpoint() != null ? api.getApiExternalSandboxEndpoint() : "");
+        endPoints.put("productionURL", api.getApiExternalProductionEndpoint() != null ? api.getApiExternalProductionEndpoint() : "");
+
+        JSONObject response = new JSONObject();
+        response.put("apiInfo", apiInfo);
+        // TODO: Find where we can get policies for Apps
+        response.put("subscriptionPolicies", new JSONArray());
+        response.put("endPoints", endPoints);
+
+        return response.toJSONString();
     }
 
     private static String getNewPortalOrgId(String apiUrl, SSLConnectionSocketFactory sslsf) throws APIManagementException {
@@ -169,6 +227,25 @@ public class LifeCycleUtils {
             }
         } catch (IOException e) {
             throw new APIManagementException(e);
+        }
+    }
+
+    public static int publishApiMetadata(String apiUrl, String apiMetadata, String apiDefinition, SSLConnectionSocketFactory sslsf) throws APIManagementException {
+        try (CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build()) {
+            HttpPost httpPost = new HttpPost(apiUrl);
+
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.addTextBody("apiMetadata", apiMetadata, ContentType.APPLICATION_JSON);
+            builder.addBinaryBody("apiDefinition", apiDefinition.getBytes(), ContentType.APPLICATION_JSON, "apiDefinition.json");
+
+            HttpEntity multipart = builder.build();
+            httpPost.setEntity(multipart);
+
+            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+                return response.getStatusLine().getStatusCode();
+            }
+        } catch (IOException e) {
+            throw new APIManagementException("Error while sending API metadata and definition: " + e.getMessage(), e);
         }
     }
 
