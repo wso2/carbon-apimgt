41c52440ab3c4e2a5efd9f2a97a16697970319a2
Chilliwiddit <nivedhith@wso2.com>
2025-01-24 12:24:23 +0530

    Add monetizedSubscription registration for SubscriptionCreationSimpleWorkflowExecutor

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/SubscriptionCreationSimpleWorkflowExecutor.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/SubscriptionCreationSimpleWorkflowExecutor.java
@@ -24,14 +24,18 @@ import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.WorkflowResponse;
 import org.wso2.carbon.apimgt.api.model.API;
 import org.wso2.carbon.apimgt.api.model.APIProduct;
+import org.wso2.carbon.apimgt.api.model.ApiTypeWrapper;
+import org.wso2.carbon.apimgt.api.model.Tier;
 import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.impl.dao.ApiMgtDAO;
 import org.wso2.carbon.apimgt.impl.dto.SubscriptionWorkflowDTO;
 import org.wso2.carbon.apimgt.impl.dto.WorkflowDTO;
+import org.wso2.carbon.apimgt.impl.monetization.AbstractMonetization;
+import org.wso2.carbon.apimgt.impl.monetization.MonetizationSubscription;
 
 import java.util.List;
 
-public class SubscriptionCreationSimpleWorkflowExecutor extends WorkflowExecutor {
+public class SubscriptionCreationSimpleWorkflowExecutor extends SubscriptionWorkflowExecutor {
 
     private static final Log log = LogFactory.getLog(SubscriptionCreationSimpleWorkflowExecutor.class);
 
@@ -43,6 +47,19 @@ public class SubscriptionCreationSimpleWorkflowExecutor extends WorkflowExecutor
     @Override
     public List<WorkflowDTO> getWorkflowDetails(String workflowStatus) throws WorkflowException {
         return null;
+    }
+
+    /**
+     * Returns an instance of MonetizationSubscription to be called within complete for adding or removing monetized subscriptions
+     *
+     * @return an instance of MonetizationSubscription
+     * @throws APIManagementException due to it calling the getMonetizationImplClass method
+     */
+    @Override
+    public MonetizationSubscription getMonetizationSubscriptionClass() throws APIManagementException {
+        AbstractMonetization monetizationImpl = (AbstractMonetization) super.getMonetizationImplClass();
+        MonetizationSubscription subscriptionImpl = monetizationImpl.getMonetizationSubscriptionClass();
+        return subscriptionImpl;
     }
 
     /**
@@ -81,9 +98,33 @@ public class SubscriptionCreationSimpleWorkflowExecutor extends WorkflowExecutor
         workflowDTO.setUpdatedTime(System.currentTimeMillis());
         super.complete(workflowDTO);
         ApiMgtDAO apiMgtDAO = ApiMgtDAO.getInstance();
+
+        API api = null;
+        APIProduct product = null;
+
         try {
             apiMgtDAO.updateSubscriptionStatus(Integer.parseInt(workflowDTO.getWorkflowReference()),
                     APIConstants.SubscriptionStatus.UNBLOCKED);
+
+            ApiTypeWrapper apiTypeWrapper = getAPIorAPIProductwithWorkflowDTO(workflowDTO);
+            Tier tier = getAPIorAPIProductTier(apiTypeWrapper, workflowDTO);
+            boolean isApiProduct = apiTypeWrapper.isAPIProduct();
+            boolean isMonetizationEnabled = false;
+            MonetizationSubscription subscriptionImpl = getMonetizationSubscriptionClass();
+            if (isApiProduct) {
+                product = apiTypeWrapper.getApiProduct();
+                isMonetizationEnabled = product.getMonetizationStatus();
+                if (isMonetizationEnabled && APIConstants.COMMERCIAL_TIER_PLAN.equals(tier.getTierPlan())) {
+                    subscriptionImpl.monetizeSubscription(workflowDTO, product);
+                }
+            } else {
+                api = apiTypeWrapper.getApi();
+                isMonetizationEnabled = api.getMonetizationStatus();
+                if (isMonetizationEnabled && APIConstants.COMMERCIAL_TIER_PLAN.equals(tier.getTierPlan())) {
+                    subscriptionImpl.monetizeSubscription(workflowDTO, api);
+                }
+            }
+
         } catch (APIManagementException e) {
             log.error("Could not complete subscription creation workflow", e);
             throw new WorkflowException("Could not complete subscription creation workflow", e);
