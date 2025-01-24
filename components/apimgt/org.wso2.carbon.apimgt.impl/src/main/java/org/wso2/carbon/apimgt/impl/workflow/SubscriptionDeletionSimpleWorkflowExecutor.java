547b0f9a7d270b26e2938071cdf1a7a16cb86e47
Chilliwiddit <nivedhith@wso2.com>
2025-01-24 12:25:43 +0530

    Add monetizedSubscription registration for SubscriptionDeletionSimpleWorkflowExecutor

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/SubscriptionDeletionSimpleWorkflowExecutor.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/SubscriptionDeletionSimpleWorkflowExecutor.java
@@ -20,19 +20,20 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.WorkflowResponse;
-import org.wso2.carbon.apimgt.api.model.API;
-import org.wso2.carbon.apimgt.api.model.APIIdentifier;
-import org.wso2.carbon.apimgt.api.model.APIProduct;
+import org.wso2.carbon.apimgt.api.model.*;
+import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.impl.dao.ApiMgtDAO;
 import org.wso2.carbon.apimgt.impl.dto.SubscriptionWorkflowDTO;
 import org.wso2.carbon.apimgt.impl.dto.WorkflowDTO;
+import org.wso2.carbon.apimgt.impl.monetization.AbstractMonetization;
+import org.wso2.carbon.apimgt.impl.monetization.MonetizationSubscription;
 
 import java.util.List;
 
 /**
  * Simple workflow executor for subscription delete action
  */
-public class SubscriptionDeletionSimpleWorkflowExecutor extends WorkflowExecutor {
+public class SubscriptionDeletionSimpleWorkflowExecutor extends SubscriptionWorkflowExecutor {
     private static final Log log = LogFactory.getLog(SubscriptionDeletionSimpleWorkflowExecutor.class);
 
     @Override
@@ -47,6 +48,19 @@ public class SubscriptionDeletionSimpleWorkflowExecutor extends WorkflowExecutor
         return null;
     }
 
+    /**
+     * Returns an instance of MonetizationSubscription to be called within complete for adding or removing monetized subscriptions
+     *
+     * @return an instance of MonetizationSubscription
+     * @throws APIManagementException due to it calling the getMonetizationImplClass method
+     */
+    @Override
+    public MonetizationSubscription getMonetizationSubscriptionClass() throws APIManagementException {
+        AbstractMonetization monetizationImpl = (AbstractMonetization) super.getMonetizationImplClass();
+        MonetizationSubscription subscriptionImpl = monetizationImpl.getMonetizationSubscriptionClass();
+        return subscriptionImpl;
+    }
+
     @Override
     public WorkflowResponse execute(WorkflowDTO workflowDTO) throws WorkflowException {
         workflowDTO.setStatus(WorkflowStatus.APPROVED);
@@ -59,12 +73,34 @@ public class SubscriptionDeletionSimpleWorkflowExecutor extends WorkflowExecutor
         ApiMgtDAO apiMgtDAO = ApiMgtDAO.getInstance();
         SubscriptionWorkflowDTO subWorkflowDTO = (SubscriptionWorkflowDTO) workflowDTO;
         String errorMsg = null;
+        API api = null;
+        APIProduct product = null;
 
         try {
             APIIdentifier identifier = new APIIdentifier(subWorkflowDTO.getApiProvider(),
                     subWorkflowDTO.getApiName(), subWorkflowDTO.getApiVersion());
             identifier.setId(Integer.parseInt(subWorkflowDTO.getMetadata(WorkflowConstants.PayloadConstants.API_ID)));
             apiMgtDAO.removeSubscription(identifier, ((SubscriptionWorkflowDTO) workflowDTO).getApplicationId());
+
+            ApiTypeWrapper apiTypeWrapper = getAPIorAPIProductwithWorkflowDTO(workflowDTO);
+            Tier tier = getAPIorAPIProductTier(apiTypeWrapper, workflowDTO);
+            boolean isApiProduct = apiTypeWrapper.isAPIProduct();
+            boolean isMonetizationEnabled = false;
+            MonetizationSubscription subscriptionImpl = getMonetizationSubscriptionClass();
+            if (isApiProduct) {
+                product = apiTypeWrapper.getApiProduct();
+                isMonetizationEnabled = product.getMonetizationStatus();
+                if (isMonetizationEnabled && APIConstants.COMMERCIAL_TIER_PLAN.equals(tier.getTierPlan())) {
+                    subscriptionImpl.deleteMonetizedSubscription(workflowDTO, product);
+                }
+            } else {
+                api = apiTypeWrapper.getApi();
+                isMonetizationEnabled = api.getMonetizationStatus();
+                if (isMonetizationEnabled && APIConstants.COMMERCIAL_TIER_PLAN.equals(tier.getTierPlan())) {
+                    subscriptionImpl.deleteMonetizedSubscription(workflowDTO, api);
+                }
+            }
+
         } catch (APIManagementException e) {
             errorMsg = "Could not complete subscription deletion workflow for api: " + subWorkflowDTO.getApiName();
             throw new WorkflowException(errorMsg, e);
