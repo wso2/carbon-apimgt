0aa464363f5f79e96283c685c022cbf438b5cbd1
Chilliwiddit <nivedhith@wso2.com>
2025-01-24 12:25:51 +0530

    Add monetizedSubscription registration for SubscriptionUpdateApprovalWorkflowExecutor

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/SubscriptionUpdateApprovalWorkflowExecutor.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/SubscriptionUpdateApprovalWorkflowExecutor.java
@@ -21,17 +21,23 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.WorkflowResponse;
+import org.wso2.carbon.apimgt.api.model.API;
+import org.wso2.carbon.apimgt.api.model.APIProduct;
+import org.wso2.carbon.apimgt.api.model.ApiTypeWrapper;
+import org.wso2.carbon.apimgt.api.model.Tier;
 import org.wso2.carbon.apimgt.impl.APIConstants;
 import org.wso2.carbon.apimgt.impl.dao.ApiMgtDAO;
 import org.wso2.carbon.apimgt.impl.dto.SubscriptionWorkflowDTO;
 import org.wso2.carbon.apimgt.impl.dto.WorkflowDTO;
+import org.wso2.carbon.apimgt.impl.monetization.AbstractMonetization;
+import org.wso2.carbon.apimgt.impl.monetization.MonetizationSubscription;
 
 import java.util.List;
 
 /**
  * Approval workflow for API Subscription Tier Update.
  */
-public class SubscriptionUpdateApprovalWorkflowExecutor extends WorkflowExecutor {
+public class SubscriptionUpdateApprovalWorkflowExecutor extends SubscriptionWorkflowExecutor {
 
     private static final Log log = LogFactory.getLog(SubscriptionUpdateApprovalWorkflowExecutor.class);
 
@@ -43,6 +49,19 @@ public class SubscriptionUpdateApprovalWorkflowExecutor extends WorkflowExecutor
     @Override
     public List<WorkflowDTO> getWorkflowDetails(String workflowStatus) throws WorkflowException {
         return null;
+    }
+
+    /**
+     * Returns an instance of MonetizationSubscription to be called within complete for adding or removing monetized subscriptions
+     *
+     * @return an instance of MonetizationSubscription
+     * @throws APIManagementException due to it calling the getMonetizationImplClass method
+     */
+    @Override
+    public MonetizationSubscription getMonetizationSubscriptionClass() throws APIManagementException {
+        AbstractMonetization monetizationImpl = (AbstractMonetization) super.getMonetizationImplClass();
+        MonetizationSubscription subscriptionImpl = monetizationImpl.getMonetizationSubscriptionClass();
+        return subscriptionImpl;
     }
 
     /**
@@ -89,9 +108,31 @@ public class SubscriptionUpdateApprovalWorkflowExecutor extends WorkflowExecutor
         SubscriptionWorkflowDTO subscriptionWorkflowDTO = (SubscriptionWorkflowDTO) workflowDTO;
         if (WorkflowStatus.APPROVED.equals(workflowDTO.getStatus())) {
             ApiMgtDAO apiMgtDAO = ApiMgtDAO.getInstance();
+            API api = null;
+            APIProduct product = null;
             try {
                 apiMgtDAO.updateSubscriptionStatusAndTier(Integer.parseInt(subscriptionWorkflowDTO.getWorkflowReference()),
                         APIConstants.SubscriptionStatus.UNBLOCKED);
+
+                ApiTypeWrapper apiTypeWrapper = getAPIorAPIProductwithWorkflowDTO(workflowDTO);
+                Tier tier = getAPIorAPIProductTier(apiTypeWrapper, workflowDTO);
+                boolean isApiProduct = apiTypeWrapper.isAPIProduct();
+                boolean isMonetizationEnabled = false;
+                MonetizationSubscription subscriptionImpl = getMonetizationSubscriptionClass();
+                if (isApiProduct) {
+                    product = apiTypeWrapper.getApiProduct();
+                    isMonetizationEnabled = product.getMonetizationStatus();
+                    if (isMonetizationEnabled && APIConstants.COMMERCIAL_TIER_PLAN.equals(tier.getTierPlan())) {
+                        subscriptionImpl.monetizeSubscription(workflowDTO, product);
+                    }
+                } else {
+                    api = apiTypeWrapper.getApi();
+                    isMonetizationEnabled = api.getMonetizationStatus();
+                    if (isMonetizationEnabled && APIConstants.COMMERCIAL_TIER_PLAN.equals(tier.getTierPlan())) {
+                        subscriptionImpl.monetizeSubscription(workflowDTO, api);
+                    }
+                }
+
             } catch (APIManagementException e) {
                 log.error("Could not complete subscription update workflow", e);
                 throw new WorkflowException("Could not complete subscription update workflow", e);
