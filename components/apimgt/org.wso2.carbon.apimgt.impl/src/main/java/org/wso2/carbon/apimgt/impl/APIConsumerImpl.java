50be8ea5d9ed89b7f8019adc86d3bc8a95fd3613
Nayanatharapmc <chethmi.21@cse.mrt.ac.lk>
2025-05-12 17:16:30 +0530

    Remove commented check for GraphQL API type in APIConsumerImpl

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConsumerImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConsumerImpl.java
@@ -3378,7 +3378,6 @@ APIConstants.AuditLogConstants.DELETED, this.username);
             ObjectMapper objectMapper = new ObjectMapper();
             ObjectNode payload = objectMapper.createObjectNode();
 
-            // check if the apitype is graphql
             if (APIConstants.APITransportType.GRAPHQL.name().equalsIgnoreCase(apiType)) {
                 String graphQLSchema = getGraphqlSchemaDefinition(apiId, organization);
                 payload.put(APIConstants.GRAPHQL_SCHEMA, graphQLSchema);
