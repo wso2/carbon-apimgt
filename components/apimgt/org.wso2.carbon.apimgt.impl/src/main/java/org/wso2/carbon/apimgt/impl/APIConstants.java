b5d31e1a0f496aee0c8d7730b8905762b14bb835
Menuka-Senevirathne <gayathmenuka@gmail.com>
2025-06-09 11:15:36 +0530

    Introduce a new status as UPDATE_PENDING and add an addtional variable as existingApplication ApplicationWorkflowDTO

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
@@ -1346,6 +1346,7 @@ public final class APIConstants {
         public static final String APPLICATION_REJECTED = "REJECTED";
         public static final String APPLICATION_ONHOLD = "ON_HOLD";
         public static final String DELETE_PENDING = "DELETE_PENDING";
+        public static final String UPDATE_PENDING = "UPDATE_PENDING";
     }
 
     public static class APIRevisionStatus {
