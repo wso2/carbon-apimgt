43bc47ca4515ba16d1bd8e9f36685f8de7bd3318
Menuka-Senevirathne <gayathmenuka@gmail.com>
2025-06-09 15:14:34 +0530

    Improve execute method by setting property values to workflowDTO and implement complete method
    Update the logic in createWorkflowDTO with WF_TYPE_AM_APPLICATION_UPDATE

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/ApplicationUpdateApprovalWorkflowExecutor.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/ApplicationUpdateApprovalWorkflowExecutor.java
@@ -2,14 +2,16 @@ package org.wso2.carbon.apimgt.impl.workflow;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.apimgt.api.WorkflowResponse;
 import org.wso2.carbon.apimgt.api.model.Application;
+import org.wso2.carbon.apimgt.impl.dao.ApiMgtDAO;
 import org.wso2.carbon.apimgt.impl.dto.ApplicationWorkflowDTO;
 import org.wso2.carbon.apimgt.impl.dto.WorkflowDTO;
 
 import java.util.List;
 
-public class ApplicationUpdateApprovalWorkflowExecutor extends WorkflowExecutor{
+public class ApplicationUpdateApprovalWorkflowExecutor extends WorkflowExecutor {
 
     private static final Log log = LogFactory.getLog(ApplicationUpdateApprovalWorkflowExecutor.class);
 
@@ -24,7 +26,7 @@ public class ApplicationUpdateApprovalWorkflowExecutor extends WorkflowExecutor{
     }
 
     @Override
-    public WorkflowResponse execute (WorkflowDTO workflowDTO) throws WorkflowException {
+    public WorkflowResponse execute(WorkflowDTO workflowDTO) throws WorkflowException {
 
         if (log.isDebugEnabled()) {
             log.debug("Executing Application Update Approval Workflow.. ");
@@ -33,16 +35,35 @@ public class ApplicationUpdateApprovalWorkflowExecutor extends WorkflowExecutor{
         Application application = applicationWorkflowDTO.getApplication();
         Application existingApplication = applicationWorkflowDTO.getExistingApplication();
         String message = "Approve application " + application.getName() + " creation request from application creator -"
-                + applicationWorkflowDTO.getUserName() ;
+                + applicationWorkflowDTO.getUserName();
         workflowDTO.setWorkflowDescription(message);
-        workflowDTO.setProperties("existingApplicationName",existingApplication.getName());
-        workflowDTO.setProperties("requestedApplicationName",application.getName());
+        workflowDTO.setProperties("existingApplicationName", existingApplication.getName());
+        workflowDTO.setProperties("requestedApplicationName", application.getName());
 
         super.execute(workflowDTO);
 
         return new GeneralWorkflowResponse();
     }
 
+    @Override
+    public WorkflowResponse complete(WorkflowDTO workFlowDTO) throws WorkflowException {
+        workFlowDTO.setUpdatedTime(System.currentTimeMillis());
+        ApiMgtDAO dao = ApiMgtDAO.getInstance();
+
+        try {
+            if (dao.getApplicationById(Integer.parseInt(workFlowDTO.getWorkflowReference())) != null) {
+                super.complete(workFlowDTO);
+            }
+
+        }catch (APIManagementException e) {
+            String msg = "Error occurred when retrieving the Application creation with workflow ID :" + workFlowDTO
+                    .getWorkflowReference();
+            log.error(msg, e);
+            throw new WorkflowException(msg, e);
+        }
+
+        return new GeneralWorkflowResponse();
+    }
 
 }
 
