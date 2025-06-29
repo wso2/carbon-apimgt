13a4edc4a54ff3944c9f5f82bf35658937198bed
Menuka-Senevirathne <gayathmenuka@gmail.com>
2025-06-29 23:16:10 +0530

    Add comments on class/methods in ApplicationUpdateSimpleWorkflowExecutor

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/ApplicationUpdateSimpleWorkflowExecutor.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/workflow/ApplicationUpdateSimpleWorkflowExecutor.java
@@ -11,6 +11,9 @@ import org.wso2.carbon.apimgt.impl.dto.WorkflowDTO;
 
 import java.util.List;
 
+/**
+ * Simple workflow for Application update process. This is the default workflow.
+ */
 public class ApplicationUpdateSimpleWorkflowExecutor extends WorkflowExecutor {
 
     private static final Log log = LogFactory.getLog(ApplicationUpdateSimpleWorkflowExecutor.class);
@@ -21,6 +24,13 @@ public class ApplicationUpdateSimpleWorkflowExecutor extends WorkflowExecutor {
         return null;
     }
 
+    /**
+     * This method executes application update simple workflow
+     *
+     * @param workFlowDTO - The WorkflowDTO which contains workflow contextual information related to the workflow.
+     * @return
+     * @throws WorkflowException
+     */
     public WorkflowResponse execute(WorkflowDTO workFlowDTO) throws WorkflowException {
         if (log.isDebugEnabled()) {
             log.info("Executing Application Update Workflow..");
@@ -30,12 +40,19 @@ public class ApplicationUpdateSimpleWorkflowExecutor extends WorkflowExecutor {
         return new GeneralWorkflowResponse();
     }
 
+    /**
+     * Completes the application update approval workflow
+     *
+     * @param workflowDTO - The WorkflowDTO which contains workflow contextual information related to the workflow.
+     * @return
+     * @throws WorkflowException
+     */
     public WorkflowResponse complete(WorkflowDTO workflowDTO) throws WorkflowException {
         if (log.isDebugEnabled()) {
             log.info("Complete  Application Update Workflow..");
         }
         String status = null;
-        //To DO
+
         if (WorkflowStatus.APPROVED.equals(workflowDTO.getStatus())) {
             status = APIConstants.ApplicationStatus.APPLICATION_APPROVED;
         }
