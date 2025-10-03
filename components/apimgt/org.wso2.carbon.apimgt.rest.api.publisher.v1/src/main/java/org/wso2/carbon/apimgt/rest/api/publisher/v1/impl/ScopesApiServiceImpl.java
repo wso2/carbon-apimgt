884de13ecbc9d84f9112de9b9a5ad8c273f1b8a2
hiranyakavishani <hiranyakavi@gmail.com>
2025-10-03 16:16:13 +0530

    Fix scope validation issue & policy validation issue

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ScopesApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ScopesApiServiceImpl.java
@@ -64,15 +64,21 @@ public class ScopesApiServiceImpl implements ScopesApiService {
 
         boolean isScopeExist = false;
         String scopeName = new String(Base64.getUrlDecoder().decode(name));
-        if (!APIUtil.isAllowedScope(scopeName)) {
-            try {
-                APIProvider apiProvider = RestApiCommonUtil.getLoggedInUserProvider();
-                String tenantDomain = RestApiCommonUtil.getLoggedInUserTenantDomain();
-                isScopeExist =
-                        apiProvider.isScopeKeyExist(scopeName, APIUtil.getTenantIdFromTenantDomain(tenantDomain));
-            } catch (APIManagementException e) {
-                RestApiUtil.handleInternalServerError("Error occurred while checking scope name", e, log);
+        try {
+            APIProvider apiProvider = RestApiCommonUtil.getLoggedInUserProvider();
+            String tenantDomain = RestApiCommonUtil.getLoggedInUserTenantDomain();
+            if (!APIUtil.isAllowedScope(scopeName)) {
+                try {
+                    isScopeExist =
+                            apiProvider.isScopeKeyExist(scopeName, APIUtil.getTenantIdFromTenantDomain(tenantDomain));
+                } catch (APIManagementException e) {
+                    RestApiUtil.handleInternalServerError("Error occurred while checking scope name", e, log);
+                }
             }
+            // Check whether the scope is already registered in KeyManager even if it is allowed/not allowed scope.
+            isScopeExist = isScopeExist || apiProvider.isScopeKeyExistInKeyManager(scopeName, tenantDomain);
+        } catch (APIManagementException e) {
+            RestApiUtil.handleInternalServerError("Error occurred while checking scope name", e, log);
         }
 
         if (isScopeExist) {
@@ -105,7 +111,8 @@ public class ScopesApiServiceImpl implements ScopesApiService {
                 throw new APIManagementException("Shared scope Display Name cannot be null or empty",
                         ExceptionCodes.SHARED_SCOPE_DISPLAY_NAME_NOT_SPECIFIED);
             }
-            if (apiProvider.isScopeKeyExist(scopeName, APIUtil.getTenantIdFromTenantDomain(tenantDomain))) {
+            if (apiProvider.isScopeKeyExist(scopeName, APIUtil.getTenantIdFromTenantDomain(tenantDomain)) ||
+                    apiProvider.isScopeKeyExistInKeyManager(scopeName, tenantDomain)) {
                 throw new APIManagementException(ExceptionCodes.from(ExceptionCodes.SCOPE_ALREADY_REGISTERED,
                         scopeName));
             }
