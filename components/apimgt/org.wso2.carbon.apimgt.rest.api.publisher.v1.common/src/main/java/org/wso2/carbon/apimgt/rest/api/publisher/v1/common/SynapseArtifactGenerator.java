3b4a12e44b1b2f16cbc4b63f925e35d0b9416908
dushaniw <dushaniw@wso2.com>
2025-10-10 12:20:33 +0530

    remove lock after synchronized.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/SynapseArtifactGenerator.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/SynapseArtifactGenerator.java
@@ -371,9 +371,9 @@ public class SynapseArtifactGenerator implements GatewayArtifactGenerator {
                 result.success = false;
                 result.errorMessage = e.getMessage();
             }
-            return result;
         }
         lockMap.remove(cacheKey, lock);
+        return result;
     }
     
     /**
