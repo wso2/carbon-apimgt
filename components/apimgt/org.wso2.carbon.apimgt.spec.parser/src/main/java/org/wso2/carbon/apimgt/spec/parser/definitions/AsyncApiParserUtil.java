0e21bd635d9dbd1758a40fae97d2eadcdded309c
janidu-wso2 <janidu@wso2.com>
2026-01-30 09:28:38 +0530

    url extraction logic

--- components/apimgt/org.wso2.carbon.apimgt.spec.parser/src/main/java/org/wso2/carbon/apimgt/spec/parser/definitions/AsyncApiParserUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.spec.parser/src/main/java/org/wso2/carbon/apimgt/spec/parser/definitions/AsyncApiParserUtil.java
@@ -986,40 +986,16 @@ public class AsyncApiParserUtil {
             JsonNode rootNode = new ObjectMapper().readTree(apiDefinition);
             if (rootNode.has("servers")) {
                 JsonNode serversNode = rootNode.get("servers");
-                if (serversNode.isArray() && serversNode.size() > 0) {
-                    JsonNode server = serversNode.get(0);
-                    if (server != null && server.has("url")) {
-                        String resolvedUrl = server.get("url").asText();
-                        if (server.has("variables") && server.get("variables").has("basePath")) {
-                            JsonNode basePath = server.get("variables").get("basePath");
-                            if (basePath.has("default")) {
-                                String stageName = basePath.get("default").asText();
-                                resolvedUrl = resolvedUrl
-                                        .replace("/{basePath}", "/" + stageName)
-                                        .replace("{basePath}", stageName);
-                            }
-                        }
-                        return resolvedUrl;
+                Iterator<JsonNode> elements = serversNode.elements();
+                if (elements.hasNext()) {
+                // Get the first server defined (e.g., 'production')
+                    JsonNode server = elements.next();
+                
+                // 4. Directly extract the URL
+                    if (server.has("url")) {
+                        return server.get("url").asText();
                     }
-                } else if (serversNode.isObject()) {
-                    Iterator<JsonNode> elements = serversNode.elements();
-                    if (elements.hasNext()) {
-                        JsonNode server = elements.next();
-                        if (server.has("url")) {
-                            String resolvedUrl = server.get("url").asText();
-                            if (server.has("variables") && server.get("variables").has("basePath")) {
-                                JsonNode basePath = server.get("variables").get("basePath");
-                                if (basePath.has("default")) {
-                                    String stageName = basePath.get("default").asText();
-                                    resolvedUrl = resolvedUrl
-                                            .replace("/{basePath}", "/" + stageName)
-                                            .replace("{basePath}", stageName);
-                                }
-                            }
-                            return resolvedUrl;
-                        }
-                    }
-                }
+                }    
             }
         } catch (IOException e) {
             log.error("Error while parsing definition to extract URL", e);
