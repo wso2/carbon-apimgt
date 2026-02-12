2bd6bd7a97d21f0e1219b8cdd20375ca02827cf7
janidu-wso2 <janidu@wso2.com>
2026-02-12 10:48:16 +0530

    async API Parse util revert

--- components/apimgt/org.wso2.carbon.apimgt.spec.parser/src/main/java/org/wso2/carbon/apimgt/spec/parser/definitions/AsyncApiParserUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.spec.parser/src/main/java/org/wso2/carbon/apimgt/spec/parser/definitions/AsyncApiParserUtil.java
@@ -971,35 +971,4 @@ public class AsyncApiParserUtil {
         });
         return builder.toString();
     }
-
-    /**
-     * Extracts the endpoint URL from the AsyncAPI definition.
-     *
-     * @param apiDefinition AsyncAPI definition as a string
-     * @return Resolved endpoint URL or null if not found
-     */
-    public static String getEndpointUrlFromAsyncApiDefinition(String apiDefinition) {
-        try {
-            if (apiDefinition == null || apiDefinition.trim().isEmpty()) {
-                return null;
-            }
-            JsonNode rootNode = new ObjectMapper().readTree(apiDefinition);
-            if (rootNode.has("servers")) {
-                JsonNode serversNode = rootNode.get("servers");
-                Iterator<JsonNode> elements = serversNode.elements();
-                if (elements.hasNext()) {
-                // Get the first server defined (e.g., 'production')
-                    JsonNode server = elements.next();
-                
-                // 4. Directly extract the URL
-                    if (server.has("url")) {
-                        return server.get("url").asText();
-                    }
-                }    
-            }
-        } catch (IOException e) {
-            log.error("Error while parsing definition to extract URL", e);
-        }
-        return null;
-    }
 }
