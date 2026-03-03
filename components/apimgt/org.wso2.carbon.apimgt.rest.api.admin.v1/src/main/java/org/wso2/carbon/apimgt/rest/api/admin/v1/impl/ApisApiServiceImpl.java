41549dfa032f715402dc3afc5040e1bb26ce4697
Chamal1120 <kenway213beep@gmail.com>
2026-03-03 15:28:14 +0530

    ...

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/v1/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.admin.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/admin/v1/impl/ApisApiServiceImpl.java
@@ -20,7 +20,6 @@ package org.wso2.carbon.apimgt.rest.api.admin.v1.impl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.logging.Log;
