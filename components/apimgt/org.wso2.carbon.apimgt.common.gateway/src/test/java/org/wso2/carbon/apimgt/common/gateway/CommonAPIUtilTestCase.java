95dfef7329c4fc4d7e22baff5d39659e21b05f15
BLasan <benuraab@gmail.com>
2026-03-18 17:20:32 +0530

    Add: Test Case for Proxy Hosts

--- components/apimgt/org.wso2.carbon.apimgt.common.gateway/src/test/java/org/wso2/carbon/apimgt/common/gateway/CommonAPIUtilTestCase.java
+++ components/apimgt/org.wso2.carbon.apimgt.common.gateway/src/test/java/org/wso2/carbon/apimgt/common/gateway/CommonAPIUtilTestCase.java
@@ -143,6 +143,70 @@ public class CommonAPIUtilTestCase {
     }
 
     @Test
+    public void testGetHttpClientWithTargetProxyHosts() {
+        String proxyHost = "127.0.0.1";
+        String proxyUsername = "user";
+        String proxyPassword = "pass";
+        String[] nonProxyHosts = new String[] {};
+        String proxyProtocol = "http";
+
+        HttpGet httpGet = new HttpGet("https://localhost:" + mockServer.getPort() + "/hello");
+        HttpClientConfigurationDTO.Builder builder = new HttpClientConfigurationDTO.Builder();
+
+        // Isolate this assertion from previous test traffic on the shared proxy server.
+        proxyServer.clear(request());
+
+        // Check if the proxyConfiguration is provided but the host is under nonProxyHosts, the outbound call is sent
+        // directly to the backend.
+        HttpGet httpGetWithTLS = new HttpGet("http://localhost:" + mockServer.getPort() + "/hello");
+        HttpClientConfigurationDTO nonProxyHostBasedProxyConfig = builder.withConnectionParams(connectionLimit,
+                        maximumConnectionsPerRoute, connectionTimeout)
+                // proxyProtocol here is https (due to existing limitation)
+                .withProxy(proxyHost, proxyServer.getPort(), proxyUsername, "random", proxyProtocol,
+                        new String[] { "*" }, new String[] { "localhost" }).build();
+        HttpClient clientForNonProxyHost = null;
+        clientForNonProxyHost = CommonAPIUtil.getHttpClient("https", nonProxyHostBasedProxyConfig, sslContext);
+
+        Assert.assertNotNull(clientForNonProxyHost);
+        HttpResponse nonProxyHostResponse = getHttpResponseFromClient(clientForNonProxyHost, httpGetWithTLS);
+        Assert.assertNotNull(nonProxyHostResponse);
+        Assert.assertEquals(200, nonProxyHostResponse.getStatusLine().getStatusCode());
+        // Specifically tests if the proxyServer did not respond at all.
+        proxyServer.verifyZeroInteractions();
+
+        // Given the proxy configuration, checks if the call is successfully routed via the proxy server.
+        HttpClientConfigurationDTO configuration = builder.withConnectionParams(connectionLimit,
+                        maximumConnectionsPerRoute, connectionTimeout)
+                .withProxy(proxyHost, proxyServer.getPort(), proxyUsername, proxyPassword, proxyProtocol, nonProxyHosts,
+                        new String[] { "localhost" }).build();
+
+        HttpClient client = null;
+        client = CommonAPIUtil.getHttpClient("https", configuration, sslContext);
+        Assert.assertNotNull(client);
+        HttpResponse httpResponse = getHttpResponseFromClient(client, httpGet);
+        Assert.assertNotNull(httpResponse);
+        Assert.assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+        try {
+            Assert.assertEquals("{\"hello\":\"world\"}", readInputStream(httpResponse.getEntity().getContent()));
+        } catch (IOException e) {
+            Assert.fail("Exception occurred while reading the content from response.");
+        }
+
+        // Given the proxy configuration with wrong credentials, checks if the call fails at the proxy server.
+        HttpClientConfigurationDTO configWithWrongProxyCredentials = builder.withConnectionParams(connectionLimit,
+                        maximumConnectionsPerRoute, connectionTimeout)
+                .withProxy(proxyHost, proxyServer.getPort(), proxyUsername, "random", proxyProtocol, nonProxyHosts,
+                        new String[] { "localhost" }).build();
+        HttpClient clientWithWrongProxyCreds = null;
+        clientWithWrongProxyCreds = CommonAPIUtil.getHttpClient("https", configWithWrongProxyCredentials, sslContext);
+        Assert.assertNotNull(clientWithWrongProxyCreds);
+
+        HttpResponse failedResponse = getHttpResponseFromClient(clientWithWrongProxyCreds, httpGet);
+        Assert.assertNotNull(failedResponse);
+        Assert.assertEquals(407, failedResponse.getStatusLine().getStatusCode());
+    }
+
+    @Test
     public void testGetHttpClientWithoutProxy() {
         // Checks https request without a proxy server
         HttpGet httpsGet = new HttpGet("https://localhost:" + mockServer.getPort() + "/hello");
