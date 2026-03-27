3997bb2e243711f90d2b884f6828c2011d600854
AnuGayan <anushaj@wso2.com>
2026-03-27 23:43:59 +0530

    Improve source

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/gateway/DeploymentModeResolver.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/gateway/DeploymentModeResolver.java
@@ -97,6 +97,11 @@ public final class DeploymentModeResolver {
             }
             synapseLabels.add(trimmed);
         }
+
+        if (log.isDebugEnabled()) {
+            log.debug("Resolved " + synapseLabels.size() + " Synapse labels and " + platformGatewayIds.size()
+                    + " platform gateway IDs for organization: " + organization);
+        }
         return new DeploymentTargets(synapseLabels, platformGatewayIds);
     }
 
