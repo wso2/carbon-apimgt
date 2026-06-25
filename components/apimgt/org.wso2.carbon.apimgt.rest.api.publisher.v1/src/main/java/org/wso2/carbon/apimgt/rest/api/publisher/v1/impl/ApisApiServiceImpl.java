a1cf0af52f2a9b4ad110dcb17f61d9c3864c5e96
Sachini De Silva <madusha.sachini@yahoo.com>
2026-06-25 07:21:58 +0530

    Allow only j2 synapse configs for API and operation policies in master

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
@@ -2446,8 +2446,7 @@ public class ApisApiServiceImpl implements ApisApiService {
                     if (org.apache.commons.lang3.StringUtils.isBlank(defFileContentType)) {
                         defFileContentType = synapsePolicyDefinitionFileDetail.getContentType().toString();
                     }
-                    if (!(APIConstants.J2_CONTENT_TYPE.equals(defFileContentType) ||
-                            APIConstants.XML_CONTENT_TYPE.equals(defFileContentType))) {
+                    if (!APIConstants.J2_CONTENT_TYPE.equals(defFileContentType)) {
                         throw new APIManagementException("Unsupported file type for Operation Policy");
                     }
                     String synapsePolicyDefinition =
