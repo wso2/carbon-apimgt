5e86700adec2d990cb622aa3dc563522de7bc4a8
Ravindu Amarasekara <rav.business.lak@gmail.com>
2026-07-08 03:29:32 +0530

    test: cover jwt claim validator array claims

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/test/java/org/wso2/carbon/apimgt/gateway/mediators/ClaimBasedResourceAccessValidationMediatorTest.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/test/java/org/wso2/carbon/apimgt/gateway/mediators/ClaimBasedResourceAccessValidationMediatorTest.java
@@ -17,16 +17,29 @@
  */
 package org.wso2.carbon.apimgt.gateway.mediators;
 
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.synapse.MessageContext;
+import org.apache.synapse.SynapseConstants;
 import org.apache.synapse.core.axis2.Axis2MessageContext;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.testng.Assert;
 import org.wso2.carbon.apimgt.gateway.APIMgtGatewayConstants;
+import org.wso2.carbon.apimgt.gateway.handlers.Utils;
+import org.wso2.carbon.apimgt.gateway.handlers.security.APISecurityConstants;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Utils.class)
 public class ClaimBasedResourceAccessValidationMediatorTest {
 
     MessageContext messageContext = Mockito.mock(Axis2MessageContext.class);
@@ -35,6 +48,17 @@ public class ClaimBasedResourceAccessValidationMediatorTest {
     public static final String APPLICATION = "APPLICATION";
     public static final String APPLICATION_USER = "APPLICATION_USER";
     public static final String AUT = "aut";
+    public static final String ROLES = "roles";
+    public static final String VALUE_1 = "value1";
+    public static final String VALUE_2 = "value2";
+    public static final String MISSING_VALUE = "missing";
+
+    @Before
+    public void setup() throws Exception {
+
+        PowerMockito.mockStatic(Utils.class);
+        PowerMockito.doNothing().when(Utils.class, "sendFault", Mockito.any(MessageContext.class), Mockito.anyInt());
+    }
 
     @Test
     public void testFlowWhenClaimsMatchingWithRegex() throws Exception {
@@ -123,6 +147,82 @@ public class ClaimBasedResourceAccessValidationMediatorTest {
         mediator.setAccessVerificationClaimValue(APPLICATION_USER);
         mediator.setAccessVerificationClaimValueRegex(claimValueRegex);
         mediator.setShouldAllowValidation(true);
+        Assert.assertTrue(mediator.mediate(messageContext));
+    }
+
+    @Test
+    public void testFlowWhenArrayClaimContainsConfiguredClaimWithoutRegex() throws Exception {
+
+        Map<String, Object> jwtTokenClaimsMap = new HashMap<>();
+        jwtTokenClaimsMap.put(ROLES, Arrays.asList(VALUE_1, VALUE_2));
+        Mockito.when(messageContext.getProperty(APIMgtGatewayConstants.JWT_CLAIMS))
+                .thenReturn(jwtTokenClaimsMap);
+        mediator.setAccessVerificationClaim(ROLES);
+        mediator.setAccessVerificationClaimValue(VALUE_1);
+        mediator.setAccessVerificationClaimValueRegex("");
+        mediator.setShouldAllowValidation(false);
+
+        Assert.assertTrue(mediator.mediate(messageContext));
+    }
+
+    @Test
+    public void testFlowWhenArrayClaimDoesNotContainConfiguredClaimWithoutRegex() throws Exception {
+
+        Map<String, Object> jwtTokenClaimsMap = new HashMap<>();
+        jwtTokenClaimsMap.put(ROLES, Arrays.asList(VALUE_1, VALUE_2));
+        Mockito.when(messageContext.getProperty(APIMgtGatewayConstants.JWT_CLAIMS))
+                .thenReturn(jwtTokenClaimsMap);
+        mediator.setAccessVerificationClaim(ROLES);
+        mediator.setAccessVerificationClaimValue(MISSING_VALUE);
+        mediator.setAccessVerificationClaimValueRegex("");
+        mediator.setShouldAllowValidation(false);
+
+        Assert.assertFalse(mediator.mediate(messageContext));
+        Mockito.verify(messageContext).setProperty(SynapseConstants.ERROR_CODE,
+                APISecurityConstants.API_AUTH_ACCESS_TOKEN_CLAIMS_MISMATCH);
+        Mockito.verify(messageContext).setProperty(SynapseConstants.ERROR_MESSAGE,
+                APISecurityConstants.API_AUTH_ACCESS_TOKEN_CLAIMS_MISMATCH_MESSAGE);
+        Mockito.verify(messageContext).setProperty(SynapseConstants.ERROR_DETAIL,
+                APISecurityConstants.API_AUTH_ACCESS_TOKEN_CLAIMS_MISMATCH_DESCRIPTION);
+        PowerMockito.verifyStatic(Utils.class);
+        Utils.sendFault(Mockito.eq(messageContext), Mockito.eq(HttpStatus.SC_FORBIDDEN));
+    }
+
+    @Test
+    public void testFlowWhenArrayClaimIsEmpty() throws Exception {
+
+        Map<String, Object> jwtTokenClaimsMap = new HashMap<>();
+        jwtTokenClaimsMap.put(ROLES, Collections.emptyList());
+        Mockito.when(messageContext.getProperty(APIMgtGatewayConstants.JWT_CLAIMS))
+                .thenReturn(jwtTokenClaimsMap);
+        mediator.setAccessVerificationClaim(ROLES);
+        mediator.setAccessVerificationClaimValue(VALUE_1);
+        mediator.setAccessVerificationClaimValueRegex("");
+        mediator.setShouldAllowValidation(false);
+
+        Assert.assertFalse(mediator.mediate(messageContext));
+        Mockito.verify(messageContext).setProperty(SynapseConstants.ERROR_CODE,
+                APISecurityConstants.API_AUTH_ACCESS_TOKEN_CLAIMS_INVALID);
+        Mockito.verify(messageContext).setProperty(SynapseConstants.ERROR_MESSAGE,
+                APISecurityConstants.API_AUTH_ACCESS_TOKEN_CLAIMS_INVALID_MESSAGE);
+        Mockito.verify(messageContext).setProperty(SynapseConstants.ERROR_DETAIL,
+                String.format("Token doesn't contain the claim \"%s\"", ROLES));
+        PowerMockito.verifyStatic(Utils.class);
+        Utils.sendFault(Mockito.eq(messageContext), Mockito.eq(HttpStatus.SC_FORBIDDEN));
+    }
+
+    @Test
+    public void testFlowWhenArrayClaimMatchesConfiguredRegex() throws Exception {
+
+        Map<String, Object> jwtTokenClaimsMap = new HashMap<>();
+        jwtTokenClaimsMap.put(ROLES, Arrays.asList("ADMIN", "VIEWER"));
+        Mockito.when(messageContext.getProperty(APIMgtGatewayConstants.JWT_CLAIMS))
+                .thenReturn(jwtTokenClaimsMap);
+        mediator.setAccessVerificationClaim(ROLES);
+        mediator.setAccessVerificationClaimValue("ADMIN");
+        mediator.setAccessVerificationClaimValueRegex("^[A-Z]+$");
+        mediator.setShouldAllowValidation(false);
+
         Assert.assertTrue(mediator.mediate(messageContext));
     }
 }
