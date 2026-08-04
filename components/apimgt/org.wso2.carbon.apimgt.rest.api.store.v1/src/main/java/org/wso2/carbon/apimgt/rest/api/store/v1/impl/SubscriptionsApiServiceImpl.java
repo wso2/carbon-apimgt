628cfd3ac8fd1afa5e425810d4ee6e312056693e
Nisan Abeywickrama <29643986+nisan-abeywickrama@users.noreply.github.com>
2026-08-04 11:42:23 +0530

    Improve devportal subscription retrieval for API and application Id parameters

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/SubscriptionsApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/SubscriptionsApiServiceImpl.java
@@ -106,7 +106,36 @@ public class SubscriptionsApiServiceImpl implements SubscriptionsApiService {
             String organization = RestApiUtil.getValidatedOrganization(messageContext);
             APIConsumer apiConsumer = RestApiCommonUtil.getConsumer(username);
             SubscriptionListDTO subscriptionListDTO;
-            if (!StringUtils.isEmpty(apiId)) {
+            if (!StringUtils.isEmpty(apiId) && !StringUtils.isEmpty(applicationId)) {
+                Application application = apiConsumer.getApplicationByUUID(applicationId);
+
+                if (application == null) {
+                    RestApiUtil.handleResourceNotFoundError(RestApiConstants.RESOURCE_APPLICATION, applicationId, log);
+                    return null;
+                }
+
+                if (!RestAPIStoreUtils.isUserAccessAllowedForApplication(application)) {
+                    RestApiUtil.handleAuthorizationFailure(RestApiConstants.RESOURCE_APPLICATION, applicationId, log);
+                }
+
+                int total = apiConsumer.getSubscriptionCount(subscriber, application.getName(), groupId);
+                subscriptions = apiConsumer.getPaginatedSubscribedAPIsByApplication(application, 0, total,
+                        organization);
+                for (SubscribedAPI subscribedAPI : subscriptions) {
+                    if (subscribedAPI.getIdentifier() != null
+                            && apiId.equals(subscribedAPI.getIdentifier().getUUID())) {
+                        subscribedAPIList.add(subscribedAPI);
+                        break;
+                    }
+                }
+
+                subscriptionListDTO = SubscriptionMappingUtil.fromSubscriptionListToDTO(subscribedAPIList, limit,
+                        offset, organization);
+                SubscriptionMappingUtil.setPaginationParams(subscriptionListDTO, apiId, applicationId, groupId, limit,
+                        offset, subscribedAPIList.size());
+                return Response.ok().entity(subscriptionListDTO).build();
+
+            } else if (!StringUtils.isEmpty(apiId)) {
                 // todo : FIX properly, need to done properly with backend side pagination.
                 // todo : getSubscribedIdentifiers() method should NOT be used. Appears to be too slow.
 
